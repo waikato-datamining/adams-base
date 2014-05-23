97ae2b47e9c5aa9aa4c323480a159815db1578a5
fracpete <fracpete@gmail.com>
2014-05-23 03:32:42 +0000

    add method for generating histogram per channel (R,G,B,A)

--- adams-core/src/main/java/adams/data/image/BufferedImageHelper.java
+++ adams-core/src/main/java/adams/data/image/BufferedImageHelper.java
@@ -398,4 +398,36 @@ public class BufferedImageHelper {
     
     return result;
   }
+  
+  /**
+   * Generates a histogram for each of the R, G and B channels.
+   * 
+   * @param img		the image to analyze
+   * @return		the histogram, 0 = R, 1 = G, 2 = B, 3 = A
+   */
+  public static int[][] histogram(BufferedImage img) {
+    int[][]	result;
+    int		width;
+    int		height;
+    int		x;
+    int		y;
+    int[]	split;
+    int		i;
+    
+    result = new int[4][256];
+    img    = convert(img, BufferedImage.TYPE_4BYTE_ABGR);
+    height = img.getHeight();
+    width  = img.getWidth();
+    
+    for (y = 0; y < height; y++) {
+      for (x = 0; x < width; x++) {
+	split = split(img.getRGB(x, y));
+	for (i = 0; i < split.length; i++)
+	  result[i][split[i]]++;
+      }
+    }
+    
+    
+    return result;
+  }
 }
