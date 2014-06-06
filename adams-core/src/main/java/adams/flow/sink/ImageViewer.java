e5ac3fe6fdfc4d451cb5df5748e90650caea6968
fracpete <fracpete@gmail.com>
2014-06-06 00:53:55 +0000

    fixed copyright

--- adams-core/src/main/java/adams/flow/sink/ImageViewer.java
+++ adams-core/src/main/java/adams/flow/sink/ImageViewer.java
@@ -15,7 +15,7 @@
 
 /*
  * ImageViewer.java
- * Copyright (C) 2010-2013 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2010-2014 University of Waikato, Hamilton, New Zealand
  */
 
 package adams.flow.sink;
