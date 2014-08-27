8fe1e19ff985e02cb5935a8366555a8ab74045a8
fracpete <fracpete@gmail.com>
2014-08-27 22:31:41 +0000

    added SpreadSheetRowBuffer transformer

---
+++ adams-spreadsheet/src/test/java/adams/flow/transformer/SpreadSheetRowBufferTest.java
@@ -0,0 +1,172 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * SpreadSheetRowBufferTest.java
+ * Copyright (C) 2014 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.flow.transformer;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import adams.core.option.AbstractArgumentOption;
+import adams.env.Environment;
+import adams.flow.AbstractFlowTest;
+import adams.flow.control.Flow;
+import adams.flow.core.AbstractActor;
+import adams.test.TmpFile;
+
+/**
+ * Test for SpreadSheetRowBuffer actor.
+ *
+ * @author fracpete
+ * @author adams.core.option.FlowJUnitTestProducer (code generator)
+ * @version $Revision$
+ */
+public class SpreadSheetRowBufferTest
+  extends AbstractFlowTest {
+
+  /**
+   * Initializes the test.
+   *
+   * @param name	the name of the test
+   */
+  public SpreadSheetRowBufferTest(String name) {
+    super(name);
+  }
+
+  /**
+   * Called by JUnit before each test method.
+   *
+   * @throws Exception 	if an error occurs.
+   */
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    
+    m_TestHelper.copyResourceToTmp("bolts.csv");
+    m_TestHelper.deleteFileFromTmp("dumpfile.txt");
+  }
+
+  /**
+   * Called by JUnit after each test method.
+   *
+   * @throws Exception	if tear-down fails
+   */
+  @Override
+  protected void tearDown() throws Exception {
+    m_TestHelper.deleteFileFromTmp("bolts.csv");
+    m_TestHelper.deleteFileFromTmp("dumpfile.txt");
+    
+    super.tearDown();
+  }
+
+  /**
+   * Performs a regression test, comparing against previously generated output.
+   */
+  public void testRegression() {
+    performRegressionTest(
+        new TmpFile[]{
+          new TmpFile("dumpfile.txt")
+        });
+  }
+
+  /**
+   * 
+   * Returns a test suite.
+   *
+   * @return		the test suite
+   */
+  public static Test suite() {
+    return new TestSuite(SpreadSheetRowBufferTest.class);
+  }
+
+  /**
+   * Used to create an instance of a specific actor.
+   *
+   * @return a suitably configured <code>AbstractActor</code> value
+   */
+  @Override
+  public AbstractActor getActor() {
+    AbstractArgumentOption    argOption;
+    
+    Flow flow = new Flow();
+    
+    try {
+      argOption = (AbstractArgumentOption) flow.getOptionManager().findByProperty("actors");
+      adams.flow.core.AbstractActor[] actors1 = new adams.flow.core.AbstractActor[4];
+
+      // Flow.FileSupplier
+      adams.flow.source.FileSupplier filesupplier2 = new adams.flow.source.FileSupplier();
+      argOption = (AbstractArgumentOption) filesupplier2.getOptionManager().findByProperty("files");
+      adams.core.io.PlaceholderFile[] files3 = new adams.core.io.PlaceholderFile[1];
+      files3[0] = (adams.core.io.PlaceholderFile) argOption.valueOf("${TMP}/bolts.csv");
+      filesupplier2.setFiles(files3);
+      actors1[0] = filesupplier2;
+
+      // Flow.SpreadSheetFileReader
+      adams.flow.transformer.SpreadSheetFileReader spreadsheetfilereader4 = new adams.flow.transformer.SpreadSheetFileReader();
+      argOption = (AbstractArgumentOption) spreadsheetfilereader4.getOptionManager().findByProperty("reader");
+      adams.data.io.input.CsvSpreadSheetReader csvspreadsheetreader6 = new adams.data.io.input.CsvSpreadSheetReader();
+      argOption = (AbstractArgumentOption) csvspreadsheetreader6.getOptionManager().findByProperty("dataRowType");
+      adams.data.spreadsheet.DenseDataRow densedatarow8 = new adams.data.spreadsheet.DenseDataRow();
+      csvspreadsheetreader6.setDataRowType(densedatarow8);
+
+      argOption = (AbstractArgumentOption) csvspreadsheetreader6.getOptionManager().findByProperty("spreadSheetType");
+      adams.data.spreadsheet.SpreadSheet spreadsheet10 = new adams.data.spreadsheet.SpreadSheet();
+      csvspreadsheetreader6.setSpreadSheetType(spreadsheet10);
+
+      spreadsheetfilereader4.setReader(csvspreadsheetreader6);
+
+      actors1[1] = spreadsheetfilereader4;
+
+      // Flow.SpreadSheetRowBuffer
+      adams.flow.transformer.SpreadSheetRowBuffer spreadsheetrowbuffer11 = new adams.flow.transformer.SpreadSheetRowBuffer();
+      argOption = (AbstractArgumentOption) spreadsheetrowbuffer11.getOptionManager().findByProperty("operation");
+      spreadsheetrowbuffer11.setOperation((adams.flow.transformer.SpreadSheetRowBuffer.Operation) argOption.valueOf("SPREADSHEET_TO_ROW"));
+      actors1[2] = spreadsheetrowbuffer11;
+
+      // Flow.DumpFile
+      adams.flow.sink.DumpFile dumpfile13 = new adams.flow.sink.DumpFile();
+      argOption = (AbstractArgumentOption) dumpfile13.getOptionManager().findByProperty("outputFile");
+      dumpfile13.setOutputFile((adams.core.io.PlaceholderFile) argOption.valueOf("${TMP}/dumpfile.txt"));
+      dumpfile13.setAppend(true);
+      actors1[3] = dumpfile13;
+      flow.setActors(actors1);
+
+      argOption = (AbstractArgumentOption) flow.getOptionManager().findByProperty("flowExecutionListener");
+      adams.flow.execution.NullListener nulllistener16 = new adams.flow.execution.NullListener();
+      flow.setFlowExecutionListener(nulllistener16);
+
+    }
+    catch (Exception e) {
+      fail("Failed to set up actor: " + e);
+    }
+    
+    return flow;
+  }
+
+  /**
+   * Runs the test from commandline.
+   *
+   * @param args	ignored
+   */
+  public static void main(String[] args) {
+    Environment.setEnvironmentClass(adams.env.Environment.class);
+    runTest(suite());
+  }
+}
+
