3e0ba6c6f21ce7dff4d2639408734a3fdda2e3e1
fracpete <fracpete@gmail.com>
2014-09-04 20:22:54 +0000

    actors for freeing up memory due to releasing images from ImageJ's batch mode operation

---
+++ adams-imaging/src/main/java/adams/flow/transformer/ImageJReleaseAllImages.java
@@ -0,0 +1,135 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * ImageJReleaseAllImages.java
+ * Copyright (C) 2014 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.flow.transformer;
+
+import ij.ImagePlus;
+import ij.macro.Interpreter;
+import adams.flow.core.Unknown;
+
+/**
+ <!-- globalinfo-start -->
+ * Removes all images from ImageJ's internal list of batch mode images, freeing up memory, whenever a token passes through.
+ * <p/>
+ <!-- globalinfo-end -->
+ *
+ <!-- flow-summary-start -->
+ * Input&#47;output:<br/>
+ * - accepts:<br/>
+ * &nbsp;&nbsp;&nbsp;adams.flow.core.Unknown<br/>
+ * - generates:<br/>
+ * &nbsp;&nbsp;&nbsp;adams.flow.core.Unknown<br/>
+ * <p/>
+ <!-- flow-summary-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: ImageJReleaseAllImages
+ * </pre>
+ * 
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded 
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow gets stopped in case this actor encounters an error;
+ * &nbsp;&nbsp;&nbsp; useful for critical actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision: 7723 $
+ */
+public class ImageJReleaseAllImages
+  extends AbstractTransformer {
+
+  /** for serialization. */
+  private static final long serialVersionUID = -992178802927111511L;
+  
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return 
+	"Removes all images from ImageJ's internal list of batch mode images, freeing "
+	+ "up memory, whenever a token passes through.";
+  }
+
+  /**
+   * Returns the class that the consumer accepts.
+   *
+   * @return		the Class of objects that can be processed
+   */
+  public Class[] accepts() {
+    return new Class[]{Unknown.class};
+  }
+
+  /**
+   * Returns the class of objects that it generates.
+   *
+   * @return		the class of objects that get generated
+   */
+  public Class[] generates() {
+    return new Class[]{Unknown.class};
+  }
+
+  /**
+   * Executes the flow item.
+   *
+   * @return		null if everything is fine, otherwise error message
+   */
+  @Override
+  protected String doExecute() {
+    String	result;
+    ImagePlus	img;
+
+    result = null;
+
+    if (isLoggingEnabled())
+      getLogger().info("# images: " + Interpreter.getBatchModeImageCount());
+    
+    while (Interpreter.getBatchModeImageCount() > 0) {
+      img = Interpreter.getBatchModeImage(0);
+      Interpreter.removeBatchModeImage(img);
+    }
+    
+    m_OutputToken = m_InputToken;
+    
+    return result;
+  }
+}
