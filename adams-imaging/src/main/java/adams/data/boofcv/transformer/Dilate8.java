dff7f664daf45d690cedde78a516d1c19f02260c
fracpete <fracpete@gmail.com>
2014-09-14 22:29:05 +0000

    added -num-times option to allow application of operation multiple times

--- adams-imaging/src/main/java/adams/data/boofcv/transformer/Dilate8.java
+++ adams-imaging/src/main/java/adams/data/boofcv/transformer/Dilate8.java
@@ -37,6 +37,12 @@ import boofcv.struct.image.ImageUInt8;
  * &nbsp;&nbsp;&nbsp;default: WARNING
  * </pre>
  * 
+ * <pre>-num-times &lt;int&gt; (property: numTimes)
+ * &nbsp;&nbsp;&nbsp;The number of times to apply this operation.
+ * &nbsp;&nbsp;&nbsp;default: 1
+ * &nbsp;&nbsp;&nbsp;minimum: 1
+ * </pre>
+ * 
  <!-- options-end -->
  *
  * @author  fracpete (fracpete at waikato dot ac dot nz)
@@ -48,6 +54,9 @@ public class Dilate8
   /** for serialization. */
   private static final long serialVersionUID = -95759083210389706L;
 
+  /** the number of times to apply this operation. */
+  protected int m_NumTimes;
+
   /**
    * Returns a string describing the object.
    *
@@ -57,7 +66,48 @@ public class Dilate8
   public String globalInfo() {
     return "Applies the BoofCV dilate8 algorithm to a binary image.";
   }
-  
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+	    "num-times", "numTimes",
+	    1, 1, null);
+  }
+
+  /**
+   * Sets the number of times to apply this operation.
+   *
+   * @param value	the number of times
+   */
+  public void setNumTimes(int value) {
+    m_NumTimes = value;
+    reset();
+  }
+
+  /**
+   * Returns the number of times to apply this operation.
+   *
+   * @return		the number of times
+   */
+  public int getNumTimes() {
+    return m_NumTimes;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the gui
+   */
+  public String numTimesTipText() {
+    return "The number of times to apply this operation.";
+  }
+
   /**
    * Performs no transformation at all, just returns the input.
    *
@@ -71,7 +121,7 @@ public class Dilate8
     ImageUInt8 			filtered;
     
     input     = (ImageUInt8) BoofCVHelper.toBoofCVImage(img.getImage(), BoofCVImageType.UNSIGNED_INT_8);
-    filtered  = BinaryImageOps.dilate8(input, 1, null);
+    filtered  = BinaryImageOps.dilate8(input, m_NumTimes, null);
     result    = new BoofCVImageContainer[1];
     result[0] = (BoofCVImageContainer) img.getHeader();
     result[0].setImage(filtered);
