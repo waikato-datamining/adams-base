5bd5b188270794f2be25520b3a6079c4c1bd80f5
fracpete <fracpete@gmail.com>
2014-10-06 01:10:43 +0000

    added methods for encoding URLs in filenames (and extracting them again)

--- adams-core/src/main/java/adams/core/net/InternetHelper.java
+++ adams-core/src/main/java/adams/core/net/InternetHelper.java
@@ -15,10 +15,11 @@
 
 /**
  * InetAddressHelper.java
- * Copyright (C) 2011-2013 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2011-2014 University of Waikato, Hamilton, New Zealand
  */
 package adams.core.net;
 
+import java.io.File;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.ArrayList;
@@ -251,5 +252,111 @@ public class InternetHelper {
    */
   public static String encodeBase64(byte[] raw) {
     return Base64.encodeBase64String(raw);
+  }
+  
+  /**
+   * Turns a URL into a valid filename.
+   * Converts the following characters:
+   * <pre>
+   * : -> (c)
+   * / -> (s)
+   * & -> (a)
+   * ? -> (q)
+   * </pre>
+   * 
+   * @param url		the URL to convert
+   * @return		the valid filename
+   */
+  public static String encodeUrlAsFilename(String url) {
+    String	result;
+    
+    result = url;
+    result = result.replace(":", "(c)");
+    result = result.replace("/", "(s)");
+    result = result.replace("&", "(a)");
+    result = result.replace("?", "(q)");
+    
+    return result;
+  }
+  
+  /**
+   * Converts a URL encoded in a filename back into a URL.
+   * <pre>
+   * (c) -> :
+   * (s) -> /
+   * (a) -> &
+   * (q) -> ?
+   * </pre>
+   * 
+   * @param filename	the filename to convert
+   * @return		the URL
+   */
+  public static String decodeUrlFromFilename(String filename) {
+    String	result;
+    
+    result = filename;
+    result = result.replace("(c)", ":");
+    result = result.replace("(s)", "/");
+    result = result.replace("(a)", "&");
+    result = result.replace("(q)", "?");
+    
+    return result;
+  }
+  
+  /**
+   * Extracts the URL stored in the full filename provided. If no occurrences
+   * of "((" and "))" in the string, the complete string is interpreted as 
+   * URL. Otherwise, the substring surrounded by "((" and "))" is extracted
+   * first. The URL is then decoded into a proper URL.
+   * 
+   * @param filename	the filename to extract the URL from
+   * @return		the extracted/decoded URL
+   * @see		#decodeUrlFromFilename(String)
+   */
+  public static String extractUrlFromFilename(String filename) {
+    String	result;
+    int		start;
+    int		end;
+    
+    start = filename.indexOf("((");
+    end   = filename.lastIndexOf("))");
+    if ((start > -1) && (end > start))
+      result = decodeUrlFromFilename(filename.substring(start + 2, end));
+    else
+      result = decodeUrlFromFilename(filename);
+    
+    return result;
+  }
+  
+  /**
+   * Constructs a filename, ensuring that the URL is encoded properly.
+   * 
+   * @param path	the path to use, ignored if null (separator gets added automatically)
+   * @param prefix	the prefix for the name, ignored if null
+   * @param url		the URL to encode
+   * @param suffix	the suffix for the name, ignored if null
+   * @param ext		the file extension, ignored if null (dot gets automatically)
+   */
+  public static String createUrlFilename(String path, String prefix, String url, String suffix, String ext) {
+    StringBuilder	result;
+    
+    result = new StringBuilder();
+    if (path != null) {
+      result.append(path);
+      if (!path.endsWith(File.separator))
+	result.append(File.separator);
+    }
+    if (prefix != null)
+      result.append(prefix);
+    result.append("((" + encodeUrlAsFilename(url) + "))");
+    if (suffix != null)
+      result.append(suffix);
+    if (ext != null) {
+      if (!ext.startsWith("."))
+	result.append(".");
+      result.append(ext);
+    }
+    
+    return result.toString();
   }
 }
