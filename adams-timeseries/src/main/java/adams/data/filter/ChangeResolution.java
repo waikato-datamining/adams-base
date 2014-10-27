a9efd71ca049c2a2c076a13518744a00e5e2b5c4
fracpete <fracpete@gmail.com>
2014-10-27 22:54:54 +0000

    fixed copyright year

--- adams-timeseries/src/main/java/adams/data/filter/ChangeResolution.java
+++ adams-timeseries/src/main/java/adams/data/filter/ChangeResolution.java
@@ -15,7 +15,7 @@
 
 /**
  * ChangeResolution.java
- * Copyright (C) 2013 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2014 University of Waikato, Hamilton, New Zealand
  */
 package adams.data.filter;
 
