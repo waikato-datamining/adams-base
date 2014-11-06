47456f91944427b0ad07101e687d609c26a2ec59
fracpete <fracpete@gmail.com>
2014-11-06 02:23:50 +0000

    rowscore for calculating percentage of non-missing cells in row

---
+++ adams-spreadsheet/src/main/java/adams/data/spreadsheet/rowscore/NonMissing.java
@@ -0,0 +1,97 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * NonMissing.java
+ * Copyright (C) 2014 University of Waikato, Hamilton, New Zealand
+ */
+package adams.data.spreadsheet.rowscore;
+
+import adams.data.spreadsheet.Row;
+import adams.data.spreadsheet.SpreadSheet;
+
+/**
+ <!-- globalinfo-start -->
+ * Computes the percentage of non-missing values per row.
+ * <p/>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class NonMissing
+  extends AbstractRowScore {
+
+  /** for serialization. */
+  private static final long serialVersionUID = -9037884201569670797L;
+  
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Computes the percentage of non-missing values per row.";
+  }
+
+  /**
+   * Returns how many score values will get generated.
+   * 
+   * @return		the number of scores
+   */
+  @Override
+  public int getNumScores() {
+    return 1;
+  }
+
+  /**
+   * Performs the actual calculation of the row score.
+   *
+   * @param sheet	the spreadsheet to generate the score for
+   * @param rowIndex	the row index
+   * @return		the generated score, null in case of an error
+   */
+  @Override
+  protected Double[] doCalculateScore(SpreadSheet sheet, int rowIndex) {
+    Double[]	result;
+    int		count;
+    int		i;
+    Row		row;
+    
+    result = null;
+    
+    count = 0;
+    row   = sheet.getRow(rowIndex);
+    for (i = 0; i < sheet.getColumnCount(); i++) {
+      if (row.hasCell(i) && !row.getCell(i).isMissing())
+	count++;
+    }
+    
+    result = new Double[]{
+	(double) count / (double) sheet.getColumnCount()
+    };
+    
+    return result;
+  }
+}
