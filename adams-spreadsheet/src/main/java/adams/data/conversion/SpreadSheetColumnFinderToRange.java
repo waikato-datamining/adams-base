8a35dd61f03f159503c56aa7e2cd50784b23c72c
fracpete <fracpete@gmail.com>
2014-11-06 20:13:58 +0000

    upgraded row/col range generators to output the individual indices with a custom separator (ie easy to store in variable and attach to array option)

--- adams-spreadsheet/src/main/java/adams/data/conversion/SpreadSheetColumnFinderToRange.java
+++ adams-spreadsheet/src/main/java/adams/data/conversion/SpreadSheetColumnFinderToRange.java
@@ -27,7 +27,7 @@ import adams.data.spreadsheet.columnfinder.NullFinder;
 
 /**
  <!-- globalinfo-start -->
- * Turns the columns that the specified column finder locates into a 1-based range string.
+ * Turns the columns that the specified column finder locates into a 1-based range string. Instead of a compressed range string (eg 1-4) it is also possible to output the individual indices (eg 1,2,3,4).
  * <p/>
  <!-- globalinfo-end -->
  *
@@ -40,6 +40,17 @@ import adams.data.spreadsheet.columnfinder.NullFinder;
  * <pre>-column-finder &lt;adams.data.spreadsheet.columnfinder.ColumnFinder&gt; (property: columnFinder)
  * &nbsp;&nbsp;&nbsp;The column finder to use.
  * &nbsp;&nbsp;&nbsp;default: adams.data.spreadsheet.columnfinder.NullFinder
+ * </pre>
+ * 
+ * <pre>-individual-indices &lt;boolean&gt; (property: individualIndices)
+ * &nbsp;&nbsp;&nbsp;If enabled then individual indices (1,2,3,4) are output instead of compressed 
+ * &nbsp;&nbsp;&nbsp;ranges (1-4).
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-index-separator &lt;java.lang.String&gt; (property: indexSeparator)
+ * &nbsp;&nbsp;&nbsp;The separator to use when outputting individual indices, eg comma or blank.
+ * &nbsp;&nbsp;&nbsp;default: ,
  * </pre>
  * 
  <!-- options-end -->
@@ -56,6 +67,12 @@ public class SpreadSheetColumnFinderToRange
   /** the ColumnFinder to apply. */
   protected ColumnFinder m_ColumnFinder;
 
+  /** whether to output individual indices (1,2,3,4) rather than compressed ranges (1-4). */
+  protected boolean m_IndividualIndices;
+  
+  /** the separator for the individual indices. */
+  protected String m_IndexSeparator;
+  
   /**
    * Returns a string describing the object.
    *
@@ -63,7 +80,10 @@ public class SpreadSheetColumnFinderToRange
    */
   @Override
   public String globalInfo() {
-    return "Turns the columns that the specified column finder locates into a 1-based range string.";
+    return 
+	"Turns the columns that the specified column finder locates into a "
+	+ "1-based range string. Instead of a compressed range string (eg 1-4) "
+	+ "it is also possible to output the individual indices (eg 1,2,3,4).";
   }
 
   /**
@@ -76,6 +96,14 @@ public class SpreadSheetColumnFinderToRange
     m_OptionManager.add(
 	    "column-finder", "columnFinder",
 	    new NullFinder());
+
+    m_OptionManager.add(
+	    "individual-indices", "individualIndices",
+	    false);
+
+    m_OptionManager.add(
+	    "index-separator", "indexSeparator",
+	    ",");
   }
 
   /**
@@ -108,13 +136,84 @@ public class SpreadSheetColumnFinderToRange
   }
 
   /**
+   * Sets whether to output individual indices (1,2,3,4) instead of
+   * compressed ranges (1-4).
+   *
+   * @param value	true if to output individual indices
+   */
+  public void setIndividualIndices(boolean value) {
+    m_IndividualIndices = value;
+    reset();
+  }
+
+  /**
+   * Returns whether to output individual indices (1,2,3,4) instead of
+   * compressed ranges (1-4).
+   *
+   * @return		true if to output individual indices
+   */
+  public boolean getIndividualIndices() {
+    return m_IndividualIndices;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the gui
+   */
+  public String individualIndicesTipText() {
+    return "If enabled then individual indices (1,2,3,4) are output instead of compressed ranges (1-4).";
+  }
+
+  /**
+   * Sets the separator for the individual indices.
+   *
+   * @param value	the index separator
+   */
+  public void setIndexSeparator(String value) {
+    if (value.length() > 0) {
+      m_IndexSeparator = value;
+      reset();
+    }
+    else {
+      getLogger().warning("Index separator cannot be empty!");
+    }
+  }
+
+  /**
+   * Returns the separator for the individual indices.
+   *
+   * @return		the index separator
+   */
+  public String getIndexSeparator() {
+    return m_IndexSeparator;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the gui
+   */
+  public String indexSeparatorTipText() {
+    return "The separator to use when outputting individual indices, eg comma or blank.";
+  }
+
+  /**
    * Returns a quick info about the object, which can be displayed in the GUI.
    *
    * @return		null if no info available, otherwise short string
    */
   @Override
   public String getQuickInfo() {
-    return QuickInfoHelper.toString(this, "columnFinder", m_ColumnFinder, "column finder: ");
+    String	result;
+    
+    result  = QuickInfoHelper.toString(this, "columnFinder", m_ColumnFinder, "column finder: ");
+    result += QuickInfoHelper.toString(this, "individualIndices", (m_IndividualIndices ? "indices" : "range"), ", type: ");
+    result += QuickInfoHelper.toString(this, "indexSeparator", "'" + m_IndexSeparator + "'", ", separator: ");
+    
+    return result;
   }
 
   /**
@@ -149,12 +248,25 @@ public class SpreadSheetColumnFinderToRange
     SpreadSheet		sheet;
     int[]		columns;
     Range		range;
+    StringBuilder	indices;
     
-    sheet  = (SpreadSheet) m_Input;
-    columns   = m_ColumnFinder.findColumns(sheet);
-    range  = new Range();
-    range.setIndices(columns);
-    result = range.getRange();
+    sheet   = (SpreadSheet) m_Input;
+    columns = m_ColumnFinder.findColumns(sheet);
+    
+    if (m_IndividualIndices) {
+      indices = new StringBuilder();
+      for (int index: columns) {
+	if (indices.length() > 0)
+	  indices.append(m_IndexSeparator);
+	indices.append("" + (index + 1));
+      }
+      result = indices.toString();
+    }
+    else {
+      range  = new Range();
+      range.setIndices(columns);
+      result = range.getRange();
+    }
     
     return result;
   }
