15cab4e25240e4296d80b4fce67321b14994ec63
fracpete <fracpete@gmail.com>
2014-11-10 05:07:51 +0000

    added image readers/writes for BoofCV and UFRaw

---
+++ adams-imaging/src/main/java/adams/data/io/input/UfrawImageReader.java
@@ -0,0 +1,119 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * UfrawImageReader.java
+ * Copyright (C) 2014 University of Waikato, Hamilton, New Zealand
+ */
+package adams.data.io.input;
+
+import java.awt.image.BufferedImage;
+
+import adams.core.Utils;
+import adams.core.io.PlaceholderFile;
+import adams.data.image.BufferedImageContainer;
+import adams.data.imagemagick.UFRawHelper;
+import adams.data.io.output.AbstractImageWriter;
+
+/**
+ <!-- globalinfo-start -->
+ * UFRaw image reader for: crw, cr2, nef, pef, sr2, arw, mrw, raf, dng
+ * <p/>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class UfrawImageReader
+  extends AbstractImageReader<BufferedImageContainer> {
+  
+  /** for serialization. */
+  private static final long serialVersionUID = 5347100846354068540L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "UFRaw image reader for: " + Utils.flatten(getFormatExtensions(), ", ");
+  }
+
+  /**
+   * Returns a string describing the format (used in the file chooser).
+   *
+   * @return 			a description suitable for displaying in the
+   * 				file chooser
+   */
+  @Override
+  public String getFormatDescription() {
+    return "UFraw image reader";
+  }
+
+  /**
+   * Returns the extension(s) of the format.
+   *
+   * @return 			the extension(s) (without the dot!)
+   */
+  @Override
+  public String[] getFormatExtensions() {
+    return new String[]{
+	"3fr", "arw", "bay", "bmq", "cine", "cr2", "crw", "cs1", "dc2", "dcr", 
+	"dng", "erf", "fff", "hdr", "ia", "jpg", "k25", "kc2", "kdc", "mdc", 
+	"mef", "mos", "mrw", "nef", "nrw", "orf", "pef", "pxn", "qtk", "raf", 
+	"raw", "rdc", "rw2", "sr2", "srf", "sti", "tif", "x3f"};
+  }
+
+  /**
+   * Returns, if available, the corresponding writer.
+   * 
+   * @return		the writer, null if none available
+   */
+  @Override
+  public AbstractImageWriter getCorrespondingWriter() {
+    return null;
+  }
+
+  /**
+   * Performs the actual reading of the image file.
+   * 
+   * @param file	the file to read
+   * @return		the image container, null if failed to read
+   */
+  @Override
+  protected BufferedImageContainer doRead(PlaceholderFile file) {
+    BufferedImageContainer	result;
+    BufferedImage		image;
+    
+    result = null;
+    image  = UFRawHelper.read(file);
+    
+    if (image != null) {
+      result = new BufferedImageContainer();
+      result.setImage(image);
+    }
+    
+    return result;
+  }
+}
---
+++ adams-imaging/src/main/java/adams/data/io/output/BoofCVImageWriter.java
@@ -0,0 +1,134 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * BoofCVImageWriter.java
+ * Copyright (C) 2014 University of Waikato, Hamilton, New Zealand
+ */
+package adams.data.io.output;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import adams.core.Utils;
+import adams.core.io.PlaceholderFile;
+import adams.data.image.BufferedImageContainer;
+import adams.data.io.input.AbstractImageReader;
+import adams.data.io.input.BoofCVImageReader;
+import boofcv.io.image.UtilImageIO;
+
+/**
+ <!-- globalinfo-start -->
+ * BoofCV image writer for: bmp, gif, jpeg, jpg, pgm, png, ppm, wbmp
+ * <p/>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class BoofCVImageWriter
+  extends AbstractImageWriter<BufferedImageContainer> {
+
+  /** for serialization. */
+  private static final long serialVersionUID = 6385191315392140321L;
+
+  /** the format extensions. */
+  protected String[] m_FormatExtensions;
+  
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "BoofCV image writer for: " + Utils.flatten(getFormatExtensions(), ", ");
+  }
+
+  /**
+   * Initializes the members.
+   */
+  @Override
+  protected void initialize() {
+    List<String>	formats;
+    
+    super.initialize();
+    
+    formats = new ArrayList<String>();
+    formats.addAll(Arrays.asList(ImageIO.getWriterFileSuffixes()));
+    if (!formats.contains("ppm"))
+      formats.add("ppm");
+    if (!formats.contains("pgm"))
+      formats.add("pgm");
+    Collections.sort(formats);
+    m_FormatExtensions = formats.toArray(new String[formats.size()]);
+  }
+
+  /**
+   * Returns a string describing the format (used in the file chooser).
+   *
+   * @return 			a description suitable for displaying in the
+   * 				file chooser
+   */
+  @Override
+  public String getFormatDescription() {
+    return "BoofCV image writer";
+  }
+
+  /**
+   * Returns the extension(s) of the format.
+   *
+   * @return 			the extension(s) (without the dot!)
+   */
+  @Override
+  public String[] getFormatExtensions() {
+    return m_FormatExtensions;
+  }
+
+  /**
+   * Returns, if available, the corresponding reader.
+   * 
+   * @return		the reader, null if none available
+   */
+  @Override
+  public AbstractImageReader getCorrespondingReader() {
+    return new BoofCVImageReader();
+  }
+
+  /**
+   * Performs the actual writing of the image file.
+   * 
+   * @param file	the file to write to
+   * @param cont	the image container to write
+   * @return		always null
+   */
+  @Override
+  protected String doWrite(PlaceholderFile file, BufferedImageContainer cont) {
+    UtilImageIO.saveImage(cont.getImage(), file.getAbsolutePath());
+    return null;
+  }
+}
