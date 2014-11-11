d74414f645b85bce8c45cf8b05116be936af6da9
fracpete <fracpete@gmail.com>
2014-11-11 08:33:33 +0000

    added methods for (cumulative) normal probability and inverse

--- adams-core/src/main/java/adams/data/statistics/StatUtils.java
+++ adams-core/src/main/java/adams/data/statistics/StatUtils.java
@@ -22,7 +22,10 @@ package adams.data.statistics;
 
 import java.util.Arrays;
 
+import org.apache.commons.math3.distribution.NormalDistribution;
+
 import adams.core.Utils;
+import adams.env.Environment;
 
 /**
  * A statistical helper class.
@@ -32,6 +35,9 @@ import adams.core.Utils;
  */
 public class StatUtils {
 
+  /** for computation. */
+  protected static NormalDistribution m_NormalDist = new NormalDistribution();
+  
   /**
    * Turns the byte array into a Byte array.
    *
@@ -1306,11 +1312,60 @@ public class StatUtils {
   }
 
   /**
+   * Returns the area under the Normal (Gaussian) probability density function,
+   * integrated from minus infinity to <tt>x</tt> (assumes mean is zero,
+   * variance is one).
+   * 
+   * <pre>
+   *                            x
+   *                             -
+   *                   1        | |          2
+   *  normal(x)  = ---------    |    exp( - t /2 ) dt
+   *               sqrt(2pi)  | |
+   *                           -
+   *                          -inf.
+   * 
+   *             =  ( 1 + erf(z) ) / 2
+   *             =  erfc(z) / 2
+   * </pre>
+   * 
+   * where <tt>z = x/sqrt(2)</tt>. Computation is via the functions
+   * <tt>errorFunction</tt> and <tt>errorFunctionComplement</tt>.
+   * 
+   * @param a the z-value
+   * @return the probability of the z value according to the normal pdf
+   */
+  public static double normalProbability(double a) {
+    return m_NormalDist.cumulativeProbability(a);
+  }
+
+  /**
+   * Returns the value, <tt>x</tt>, for which the area under the Normal
+   * (Gaussian) probability density function (integrated from minus infinity to
+   * <tt>x</tt>) is equal to the argument <tt>y</tt> (assumes mean is zero,
+   * variance is one).
+   * <p>
+   * For small arguments <tt>0 < y < exp(-2)</tt>, the program computes
+   * <tt>z = sqrt( -2.0 * log(y) )</tt>; then the approximation is
+   * <tt>x = z - log(z)/z  - (1/z) P(1/z) / Q(1/z)</tt>. There are two rational
+   * functions P/Q, one for <tt>0 < y < exp(-32)</tt> and the other for
+   * <tt>y</tt> up to <tt>exp(-2)</tt>. For larger arguments,
+   * <tt>w = y - 0.5</tt>, and <tt>x/sqrt(2pi) = w + w**3 R(w**2)/S(w**2))</tt>.
+   * 
+   * @param y0 the area under the normal pdf
+   * @return the z-value
+   */
+  public static double normalInverse(double y0) {
+    return m_NormalDist.inverseCumulativeProbability(y0);
+  }
+
+  /**
    * Just for testing.
    *
    * @param args	ignored
    */
   public static void main(String[] args) {
+    Environment.setEnvironmentClass(Environment.class);
     Double[] array = new Double[]{12.0, 3.2, 2.0, 6.0, -1.0, 99.0};
     System.out.println("array: " + Utils.arrayToString(array));
     System.out.println("sorted(asc): " + Utils.arrayToString(sort(array, true)));
