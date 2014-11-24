7d4bb591201ea9e48b18257db94b3575074be840
fracpete <fracpete@gmail.com>
2014-11-24 01:10:58 +0000

    introduced AbstractXYChartGenerator to consolidate common code from Bar/Line/ScatterPlot classes X column is now optional

--- adams-spreadsheet/src/main/java/adams/gui/tools/spreadsheetviewer/chart/LinePlot.java
+++ adams-spreadsheet/src/main/java/adams/gui/tools/spreadsheetviewer/chart/LinePlot.java
@@ -19,19 +19,11 @@
  */
 package adams.gui.tools.spreadsheetviewer.chart;
 
-import adams.data.DecimalFormatString;
 import adams.data.spreadsheet.SpreadSheet;
-import adams.data.spreadsheet.SpreadSheetColumnIndex;
-import adams.data.spreadsheet.SpreadSheetColumnRange;
 import adams.flow.control.Flow;
 import adams.flow.sink.SequencePlotter;
-import adams.flow.sink.sequenceplotter.SimplePlotUpdater;
 import adams.flow.sink.sequenceplotter.ViewDataClickAction;
 import adams.flow.transformer.SpreadSheetPlotGenerator;
-import adams.flow.transformer.plotgenerator.XYPlotGenerator;
-import adams.gui.visualization.core.AbstractColorProvider;
-import adams.gui.visualization.core.AxisPanelOptions;
-import adams.gui.visualization.core.DefaultColorProvider;
 import adams.gui.visualization.sequence.AbstractXYSequencePaintlet;
 import adams.gui.visualization.sequence.LinePaintlet;
 
@@ -92,19 +84,10 @@ import adams.gui.visualization.sequence.LinePaintlet;
  * @version $Revision$
  */
 public class LinePlot
-  extends AbstractChartGenerator {
+  extends AbstractXYChartGenerator {
 
   /** for serialization. */
   private static final long serialVersionUID = -2088311829009151566L;
-
-  /** the column to use as X value. */
-  protected SpreadSheetColumnIndex m_XColumn;
-
-  /** the columns to use as Y values. */
-  protected SpreadSheetColumnRange m_YColumns;
-  
-  /** the color provider to use. */
-  protected AbstractColorProvider m_ColorProvider;
 
   /**
    * Returns a string describing the object.
@@ -117,158 +100,6 @@ public class LinePlot
   }
 
   /**
-   * Adds options to the internal list of options.
-   */
-  @Override
-  public void defineOptions() {
-    super.defineOptions();
-
-    m_OptionManager.add(
-	    "x-column", "XColumn",
-	    "");
-
-    m_OptionManager.add(
-	    "y-columns", "YColumns",
-	    "");
-
-    m_OptionManager.add(
-	    "color-provider", "colorProvider",
-	    new DefaultColorProvider());
-  }
-
-  /**
-   * Initializes the members.
-   */
-  @Override
-  protected void initialize() {
-    super.initialize();
-
-    m_YColumns = new SpreadSheetColumnRange();
-    m_XColumn     = new SpreadSheetColumnIndex();
-  }
-
-  /**
-   * Returns a quick info about the actor, which will be displayed in the GUI.
-   *
-   * @return		null if no info available, otherwise short string
-   */
-  @Override
-  public String getQuickInfo() {
-    String	result;
-    String	variable;
-
-    result = "x: ";
-    variable = getOptionManager().getVariableForProperty("XColumn");
-    if (variable != null)
-      result += variable;
-    else
-      result += m_XColumn.getIndex();
-    
-    result   += ", y: ";
-    variable = getOptionManager().getVariableForProperty("YColumns");
-    if (variable != null)
-      result += variable;
-    else
-      result += m_YColumns.getRange();
-    
-    result   += ", color: ";
-    variable = getOptionManager().getVariableForProperty("colorProvider");
-    if (variable != null)
-      result += variable;
-    else
-      result += m_ColorProvider.getClass().getSimpleName();
-    
-    return result;
-  }
-
-  /**
-   * Sets the index of the column which values to use as X values.
-   *
-   * @param value	the column index
-   */
-  public void setXColumn(String value) {
-    m_XColumn.setIndex(value);
-    reset();
-  }
-
-  /**
-   * Returns the index of the column which values to use as X values.
-   *
-   * @return		the column index
-   */
-  public String getXColumn() {
-    return m_XColumn.getIndex();
-  }
-
-  /**
-   * Returns the tip text for this property.
-   *
-   * @return 		tip text for this property suitable for
-   * 			displaying in the GUI or for listing the options.
-   */
-  public String XColumnTipText() {
-    return "The index of the column which values to use on the X axis; " + m_XColumn.getExample();
-  }
-
-  /**
-   * Sets the Y column range to use in the chart.
-   *
-   * @param value	the column range
-   */
-  public void setYColumns(String value) {
-    m_YColumns.setRange(value);
-    reset();
-  }
-
-  /**
-   * Returns the current Y column range to use in the chart.
-   *
-   * @return		the column range
-   */
-  public String getYColumns() {
-    return m_YColumns.getRange();
-  }
-
-  /**
-   * Returns the tip text for this property.
-   *
-   * @return 		tip text for this property suitable for
-   * 			displaying in the GUI or for listing the options.
-   */
-  public String YColumnsTipText() {
-    return "The range of columns to use on the Y axis; " + m_YColumns.getExample();
-  }
-
-  /**
-   * Sets the color provider to use.
-   *
-   * @param value 	the color provider
-   */
-  public void setColorProvider(AbstractColorProvider value) {
-    m_ColorProvider = value;
-    reset();
-  }
-
-  /**
-   * Returns the color provider in use.
-   *
-   * @return 		the color provider
-   */
-  public AbstractColorProvider getColorProvider() {
-    return m_ColorProvider;
-  }
-
-  /**
-   * Returns the tip text for this property.
-   *
-   * @return 		tip text for this property suitable for
-   * 			displaying in the GUI or for listing the options.
-   */
-  public String colorProviderTipText() {
-    return "The color provider to use.";
-  }
-
-  /**
    * Adds the chart generation to the flow. The flow already contains 
    * forwarding of spreadsheet and selecting subset of rows.
    * 
@@ -278,62 +109,29 @@ public class LinePlot
    */
   @Override
   protected void addChartGeneration(Flow flow, String name, SpreadSheet sheet) {
-    XYPlotGenerator		generator;
     SpreadSheetPlotGenerator	pg;
     SequencePlotter		plotter;
-    SimplePlotUpdater		updater;
-    AxisPanelOptions		axis;
-    int[]			indices;
     ViewDataClickAction		action;
     LinePaintlet		paintlet;
 
-    m_XColumn.setData(sheet);
-    m_YColumns.setData(sheet);
-    
-    generator = new XYPlotGenerator();
-    generator.setXColumn(m_XColumn.getIndex());
-    generator.setPlotColumns(m_YColumns.getRange());
-    pg = new SpreadSheetPlotGenerator();
-    pg.setGenerator(generator);
+    pg = configureGenerator(sheet);
     flow.add(pg);
     
-    updater = new SimplePlotUpdater();
-    updater.setUpdateInterval(0);
     plotter = new SequencePlotter();
     if (name != null)
       plotter.setName(name);
     else
       plotter.setName("Line plot");
-    plotter.setTitle("Line plot");
-    plotter.setShortTitle(true);
+    plotter.setTitle(plotter.getName());
+
+    configureSequencePlotter(sheet, plotter);
+
     paintlet = new LinePaintlet();
     plotter.setPaintlet(paintlet);
-    plotter.setColorProvider(m_ColorProvider.shallowCopy());
-    plotter.setPlotUpdater(updater);
-    plotter.setWidth(m_Width);
-    plotter.setHeight(m_Height);
 
     action = new ViewDataClickAction();
     action.setHitDetector(((AbstractXYSequencePaintlet) paintlet).getHitDetector());
     plotter.setMouseClickAction(action);
-
-    axis = plotter.getAxisX();
-    axis.setLabel(sheet.getColumnName(m_XColumn.getIntIndex()));
-    axis.setNthValueToShow(1);
-    axis.setTickGenerator(columnTypeToTickGenerator(sheet, m_XColumn.getIntIndex()));
-    axis.setType(columnTypeToAxisType(sheet, m_XColumn.getIntIndex()));
-    axis.setCustomFormat(new DecimalFormatString("0.0"));
-    plotter.setAxisX(axis);
-    
-    indices = m_YColumns.getIntIndices();
-    axis = plotter.getAxisY();
-    axis.setLabel((indices.length == 1) ? sheet.getColumnName(indices[0]) : "Y");
-    axis.setNthValueToShow(1);
-    axis.setTickGenerator(columnTypesToTickGenerator(sheet, indices));
-    axis.setType(columnTypesToAxisType(sheet, indices));
-    axis.setCustomFormat(new DecimalFormatString("0.0"));
-    axis.setWidth(80);
-    plotter.setAxisY(axis);
 
     flow.add(plotter);
   }
