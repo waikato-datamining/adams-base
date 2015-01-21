e42b2dee3006d8c770b86367e433e8b9e86ec33c
fracpete <fracpete@gmail.com>
2015-01-21 21:29:09 +0000

    added Barcode feature generator that decodes the barcode in the image

---
+++ adams-imaging/src/main/java/adams/data/image/features/Barcode.java
@@ -0,0 +1,172 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Barcode.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.data.image.features;
+
+import adams.data.barcode.decode.AbstractBarcodeDecoder;
+import adams.data.featureconverter.HeaderDefinition;
+import adams.data.image.BufferedImageContainer;
+import adams.data.report.DataType;
+import adams.data.text.TextContainer;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ <!-- globalinfo-start -->
+ * Decodes any barcode in the image.
+ * <p/>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ * <pre>-converter &lt;adams.data.featureconverter.AbstractFeatureConverter&gt; (property: converter)
+ * &nbsp;&nbsp;&nbsp;The feature converter to use to produce the output data.
+ * &nbsp;&nbsp;&nbsp;default: adams.data.featureconverter.SpreadSheet -data-row-type adams.data.spreadsheet.DenseDataRow -spreadsheet-type adams.data.spreadsheet.SpreadSheet
+ * </pre>
+ * 
+ * <pre>-field &lt;adams.data.report.Field&gt; [-field ...] (property: fields)
+ * &nbsp;&nbsp;&nbsp;The fields to add to the output.
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-notes &lt;adams.core.base.BaseString&gt; [-notes ...] (property: notes)
+ * &nbsp;&nbsp;&nbsp;The notes to add as attributes to the generated data, eg 'PROCESS INFORMATION'
+ * &nbsp;&nbsp;&nbsp;.
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-decoder &lt;adams.data.barcode.decode.AbstractBarcodeDecoder&gt; (property: decoder)
+ * &nbsp;&nbsp;&nbsp;The decoder algorithm to use.
+ * &nbsp;&nbsp;&nbsp;default: adams.data.barcode.decode.PassThrough
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @author  Dale (dale at cs dot waikato dot ac dot nz)
+ * @version $Revision: 9196 $
+ */
+public class Barcode
+  extends AbstractBufferedImageFeatureGenerator {
+
+  /** for serialization. */
+  private static final long serialVersionUID = -8349656592325229512L;
+
+  /** the decoder to use. */
+  protected AbstractBarcodeDecoder m_Decoder;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Decodes any barcode in the image.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+        "decoder", "decoder",
+        new adams.data.barcode.decode.PassThrough());
+  }
+
+  /**
+   * Sets the decoder to use.
+   *
+   * @param value the decoder to use
+   */
+  public void setDecoder(AbstractBarcodeDecoder value) {
+    m_Decoder = value;
+    reset();
+  }
+
+  /**
+   * Returns the decoder in use.
+   *
+   * @return the decoder in use
+   */
+  public AbstractBarcodeDecoder getDecoder() {
+    return m_Decoder;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return tip text for this property suitable for displaying in the GUI or for listing the options.
+   */
+  public String decoderTipText() {
+    return "The decoder algorithm to use.";
+  }
+
+  /**
+   * Creates the header from a template image.
+   *
+   * @param img		the image to act as a template
+   * @return		the generated header
+   */
+  @Override
+  public HeaderDefinition createHeader(BufferedImageContainer img) {
+    HeaderDefinition	result;
+
+    result = new HeaderDefinition();
+    result.add("Barcode", DataType.STRING);
+
+    return result;
+  }
+
+  /**
+   * Performs the actual feature generation.
+   *
+   * @param img		the image to process
+   * @return		the generated features
+   */
+  @Override
+  public List<Object>[] generateRows(BufferedImageContainer img) {
+    List<Object>[]		result;
+    BufferedImage		image;
+    int[]			pixels;
+    int				i;
+    TextContainer               cont;
+    String                      barcode;
+
+    cont      = m_Decoder.decode(img);
+    barcode   = null;
+    if ((cont != null) && (cont.getContent() != null) && !cont.getContent().isEmpty())
+      barcode = cont.getContent();
+    result    = new List[1];
+    result[0] = new ArrayList<Object>();
+    result[0].add(barcode);
+
+    return result;
+  }
+}
