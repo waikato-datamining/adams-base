de98e8eaa03b95f1da6cd10535937c8d40bbd7e3
fracpete <fracpete@gmail.com>
2015-02-08 23:39:14 +0000

    sink for displaying histogram of image

---
+++ adams-imaging/src/test/java/adams/flow/sink/ImageHistogramTest.java
@@ -0,0 +1,143 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * ImageHistogramTest.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.flow.sink;
+
+import adams.core.option.AbstractArgumentOption;
+import adams.env.Environment;
+import adams.flow.AbstractFlowTest;
+import adams.flow.control.Flow;
+import adams.flow.core.AbstractActor;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Test for ImageHistogram actor.
+ *
+ * @author fracpete
+ * @author adams.core.option.FlowJUnitTestProducer (code generator)
+ * @version $Revision$
+ */
+public class ImageHistogramTest
+  extends AbstractFlowTest {
+
+  /**
+   * Initializes the test.
+   *
+   * @param name	the name of the test
+   */
+  public ImageHistogramTest(String name) {
+    super(name);
+  }
+
+  /**
+   * Called by JUnit before each test method.
+   *
+   * @throws Exception 	if an error occurs.
+   */
+  protected void setUp() throws Exception {
+    super.setUp();
+    
+    m_TestHelper.copyResourceToTmp("8726547192_1d08b00bdf_o-small.jpg");
+  }
+
+  /**
+   * Called by JUnit after each test method.
+   *
+   * @throws Exception	if tear-down fails
+   */
+  protected void tearDown() throws Exception {
+    m_TestHelper.deleteFileFromTmp("8726547192_1d08b00bdf_o-small.jpg");
+    
+    super.tearDown();
+  }
+
+  /**
+   * 
+   * Returns a test suite.
+   *
+   * @return		the test suite
+   */
+  public static Test suite() {
+    return new TestSuite(ImageHistogramTest.class);
+  }
+
+  /**
+   * Used to create an instance of a specific actor.
+   *
+   * @return a suitably configured <code>AbstractActor</code> value
+   */
+  public AbstractActor getActor() {
+    AbstractArgumentOption    argOption;
+    
+    Flow flow = new Flow();
+    
+    try {
+      argOption = (AbstractArgumentOption) flow.getOptionManager().findByProperty("actors");
+      adams.flow.core.AbstractActor[] actors1 = new adams.flow.core.AbstractActor[3];
+
+      // Flow.FileSupplier
+      adams.flow.source.FileSupplier filesupplier2 = new adams.flow.source.FileSupplier();
+      argOption = (AbstractArgumentOption) filesupplier2.getOptionManager().findByProperty("files");
+      adams.core.io.PlaceholderFile[] files3 = new adams.core.io.PlaceholderFile[1];
+      files3[0] = (adams.core.io.PlaceholderFile) argOption.valueOf("${TMP}/8726547192_1d08b00bdf_o-small.jpg");
+      filesupplier2.setFiles(files3);
+      actors1[0] = filesupplier2;
+
+      // Flow.ImageReader
+      adams.flow.transformer.ImageReader imagereader4 = new adams.flow.transformer.ImageReader();
+      argOption = (AbstractArgumentOption) imagereader4.getOptionManager().findByProperty("reader");
+      adams.data.io.input.JAIImageReader jaiimagereader6 = new adams.data.io.input.JAIImageReader();
+      imagereader4.setReader(jaiimagereader6);
+
+      actors1[1] = imagereader4;
+
+      // Flow.ImageHistogram
+      adams.flow.sink.ImageHistogram imagehistogram7 = new adams.flow.sink.ImageHistogram();
+      argOption = (AbstractArgumentOption) imagehistogram7.getOptionManager().findByProperty("writer");
+      adams.gui.print.NullWriter nullwriter9 = new adams.gui.print.NullWriter();
+      imagehistogram7.setWriter(nullwriter9);
+
+      actors1[2] = imagehistogram7;
+      flow.setActors(actors1);
+
+      argOption = (AbstractArgumentOption) flow.getOptionManager().findByProperty("flowExecutionListener");
+      adams.flow.execution.NullListener nulllistener11 = new adams.flow.execution.NullListener();
+      flow.setFlowExecutionListener(nulllistener11);
+
+    }
+    catch (Exception e) {
+      fail("Failed to set up actor: " + e);
+    }
+    
+    return flow;
+  }
+
+  /**
+   * Runs the test from commandline.
+   *
+   * @param args	ignored
+   */
+  public static void main(String[] args) {
+    Environment.setEnvironmentClass(adams.env.Environment.class);
+    runTest(suite());
+  }
+}
+
