5f1811d81322b2dd6ffa129ab9cdd2450e48f40d
fracpete <fracpete@gmail.com>
2015-04-01 00:10:10 +0000

    added conversion for turning spreadsheet into JSON array: SpreadSheetToJson

---
+++ adams-spreadsheet/src/main/java/adams/data/conversion/SpreadSheetToJson.java
@@ -0,0 +1,109 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * SpreadSheetToJson.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ */
+package adams.data.conversion;
+
+import adams.data.spreadsheet.Row;
+import adams.data.spreadsheet.SpreadSheet;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONAware;
+import net.minidev.json.JSONObject;
+
+/**
+ <!-- globalinfo-start -->
+ * Turns a spreadsheet into a JSON array. Missing values get omitted.
+ * <p/>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class SpreadSheetToJson
+  extends AbstractConversion {
+
+  /** for serialization. */
+  private static final long serialVersionUID = 4890225060389916155L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Turns a spreadsheet into a JSON array. Missing values get omitted.";
+  }
+
+  /**
+   * Returns the class that is accepted as input.
+   *
+   * @return		the class
+   */
+  @Override
+  public Class accepts() {
+    return SpreadSheet.class;
+  }
+
+  /**
+   * Returns the class that is generated as output.
+   *
+   * @return		the class
+   */
+  @Override
+  public Class generates() {
+    return JSONAware.class;
+  }
+
+  /**
+   * Performs the actual conversion.
+   *
+   * @return		the converted data
+   * @throws Exception	if something goes wrong with the conversion
+   */
+  @Override
+  protected Object doConvert() throws Exception {
+    JSONArray   result;
+    SpreadSheet sheet;
+    Row         header;
+    JSONObject  data;
+    int         i;
+
+    result = new JSONArray();
+    sheet  = (SpreadSheet) m_Input;
+    header = sheet.getHeaderRow();
+    for (Row row: sheet.rows()) {
+      data = new JSONObject();
+      for (i = 0; i < sheet.getColumnCount(); i++) {
+        if (row.hasCell(i) && !row.getCell(i).isMissing())
+          data.put(header.getCell(i).getContent(), row.getCell(i).getContent());
+      }
+      result.add(data);
+    }
+
+    return result;
+  }
+}
