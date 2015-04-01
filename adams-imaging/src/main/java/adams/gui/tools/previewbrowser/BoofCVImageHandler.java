d6b91943ad77aaf189cc28617b0a68988fd935c4
fracpete <fracpete@gmail.com>
2015-04-01 02:12:10 +0000

    additional image preview handlers

---
+++ adams-imaging/src/main/java/adams/gui/tools/previewbrowser/BoofCVImageHandler.java
@@ -0,0 +1,87 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * BoofCVImageHandler.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ */
+package adams.gui.tools.previewbrowser;
+
+import adams.core.Utils;
+import adams.data.io.input.BoofCVImageReader;
+import adams.gui.visualization.image.ImagePanel;
+
+import java.io.File;
+
+/**
+ <!-- globalinfo-start -->
+ * Displays the following image types: bmp,gif,jpeg,jpg,pgm,png,ppm,wbmp
+ * <p/>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision: 10619 $
+ */
+public class BoofCVImageHandler
+  extends AbstractContentHandler {
+
+  /** for serialization. */
+  private static final long serialVersionUID = -3962259305718630395L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Displays the following image types: " + Utils.arrayToString(getExtensions());
+  }
+
+  /**
+   * Returns the list of extensions (without dot) that this handler can
+   * take care of.
+   *
+   * @return		the list of extensions (no dot)
+   */
+  @Override
+  public String[] getExtensions() {
+    return new BoofCVImageReader().getFormatExtensions();
+  }
+
+  /**
+   * Creates the actual view.
+   *
+   * @param file	the file to create the view for
+   * @return		the view
+   */
+  @Override
+  protected PreviewPanel createPreview(File file) {
+    ImagePanel		panel;
+
+    panel = new ImagePanel();
+    panel.load(file, new BoofCVImageReader());
+
+    return new PreviewPanel(panel, panel.getPaintPanel());
+  }
+}
---
+++ adams-imaging/src/main/java/adams/gui/tools/previewbrowser/UfrawImageHandler.java
@@ -0,0 +1,87 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * UfrawImageHandler.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ */
+package adams.gui.tools.previewbrowser;
+
+import adams.core.Utils;
+import adams.data.io.input.UfrawImageReader;
+import adams.gui.visualization.image.ImagePanel;
+
+import java.io.File;
+
+/**
+ <!-- globalinfo-start -->
+ * Displays the following image types: 3fr,arw,bay,bmq,cine,cr2,crw,cs1,dc2,dcr,dng,erf,fff,hdr,ia,jpg,k25,kc2,kdc,mdc,mef,mos,mrw,nef,nrw,orf,pef,pxn,qtk,raf,raw,rdc,rw2,sr2,srf,sti,tif,x3f
+ * <p/>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class UfrawImageHandler
+  extends AbstractContentHandler {
+
+  /** for serialization. */
+  private static final long serialVersionUID = -3962259305718630395L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Displays the following image types: " + Utils.arrayToString(getExtensions());
+  }
+
+  /**
+   * Returns the list of extensions (without dot) that this handler can
+   * take care of.
+   *
+   * @return		the list of extensions (no dot)
+   */
+  @Override
+  public String[] getExtensions() {
+    return new UfrawImageReader().getFormatExtensions();
+  }
+
+  /**
+   * Creates the actual view.
+   *
+   * @param file	the file to create the view for
+   * @return		the view
+   */
+  @Override
+  protected PreviewPanel createPreview(File file) {
+    ImagePanel		panel;
+
+    panel = new ImagePanel();
+    panel.load(file, new UfrawImageReader());
+
+    return new PreviewPanel(panel, panel.getPaintPanel());
+  }
+}
