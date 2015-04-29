fc25eb48c7a45f16627b6b438a4c9cc4a34cdccc
fracpete <fracpete@gmail.com>
2015-04-29 22:10:33 +0000

    simple feature generators: Min, Dimensions

---
+++ adams-imaging/src/main/java/adams/data/image/features/Dimensions.java
@@ -0,0 +1,121 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Dimensions.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.data.image.features;
+
+import adams.data.featureconverter.HeaderDefinition;
+import adams.data.image.BufferedImageContainer;
+import adams.data.report.DataType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ <!-- globalinfo-start -->
+ * Outputs the dimensions of the image: width, height, area, ratio.
+ * <p/>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ * <pre>-converter &lt;adams.data.featureconverter.AbstractFeatureConverter&gt; (property: converter)
+ * &nbsp;&nbsp;&nbsp;The feature converter to use to produce the output data.
+ * &nbsp;&nbsp;&nbsp;default: adams.data.featureconverter.SpreadSheetFeatureConverter -data-row-type adams.data.spreadsheet.DenseDataRow -spreadsheet-type adams.data.spreadsheet.SpreadSheet
+ * </pre>
+ * 
+ * <pre>-field &lt;adams.data.report.Field&gt; [-field ...] (property: fields)
+ * &nbsp;&nbsp;&nbsp;The fields to add to the output.
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-notes &lt;adams.core.base.BaseString&gt; [-notes ...] (property: notes)
+ * &nbsp;&nbsp;&nbsp;The notes to add as attributes to the generated data, eg 'PROCESS INFORMATION'
+ * &nbsp;&nbsp;&nbsp;.
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @author  Dale (dale at cs dot waikato dot ac dot nz)
+ * @version $Revision: 9196 $
+ */
+public class Dimensions
+  extends AbstractBufferedImageFeatureGenerator {
+
+  /** for serialization. */
+  private static final long serialVersionUID = -8349656592325229512L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Outputs the dimensions of the image: width, height, area, ratio.";
+  }
+
+  /**
+   * Creates the header from a template image.
+   *
+   * @param img		the image to act as a template
+   * @return		the generated header
+   */
+  @Override
+  public HeaderDefinition createHeader(BufferedImageContainer img) {
+    HeaderDefinition	result;
+
+    result = new HeaderDefinition();
+    result.add("Width", DataType.NUMERIC);
+    result.add("Height", DataType.NUMERIC);
+    result.add("Area (w*h)", DataType.NUMERIC);
+    result.add("Ratio (w/h)", DataType.NUMERIC);
+
+    return result;
+  }
+
+  /**
+   * Performs the actual feature generation.
+   *
+   * @param img		the image to process
+   * @return		the generated features
+   */
+  @Override
+  public List<Object>[] generateRows(BufferedImageContainer img) {
+    List<Object>[]		result;
+
+    result    = new List[1];
+    result[0] = new ArrayList<Object>();
+    result[0].add(img.getWidth());
+    result[0].add(img.getHeight());
+    result[0].add(img.getWidth() * img.getHeight());
+    if (img.getHeight() > 0)
+      result[0].add(img.getWidth() / img.getHeight());
+    else
+      result[0].add(null);
+
+    return result;
+  }
+}
