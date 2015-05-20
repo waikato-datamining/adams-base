a1cceb4adbe70953b3a76ba4fa444aaded15dd41
fracpete <fracpete@gmail.com>
2015-05-20 04:11:13 +0000

    added array statistic: linear regression (slope/intercept)

---
+++ adams-core/src/main/java/adams/data/statistics/ArrayLinearRegression.java
@@ -0,0 +1,148 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * ArrayLinearRegression.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ */
+package adams.data.statistics;
+
+/**
+ <!-- globalinfo-start -->
+ * Calculates the slope and intercept of linear regression between two arrays (x and y).<br>
+ * If more than two arrays supplied, then the linear regression is computed between the first (x) and all the other ones (y).
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ * @param <T> the data to process
+ */
+public class ArrayLinearRegression<T extends Number>
+  extends AbstractArrayStatistic<T>
+  implements EqualLengthArrayStatistic {
+
+  /** for serialization. */
+  private static final long serialVersionUID = -5911270089583842477L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  public String globalInfo() {
+    return
+      "Calculates the slope and intercept of linear regression between two arrays (x and y).\n"
+      + "If more than two arrays supplied, then the linear regression is computed "
+      + "between the first (x) and all the other ones (y).";
+  }
+
+  /**
+   * Returns the length of the stored arrays.
+   *
+   * @return		the length of the arrays, -1 if none stored
+   */
+  public int getLength() {
+    if (size() > 0)
+      return get(0).length;
+    else
+      return -1;
+  }
+
+  /**
+   * Returns the minimum number of arrays that need to be present.
+   * -1 for unbounded.
+   *
+   * @return		the minimum number, -1 for unbounded
+   */
+  public int getMin() {
+    return 2;
+  }
+
+  /**
+   * Returns the maximum number of arrays that need to be present.
+   * -1 for unbounded.
+   *
+   * @return		the maximum number, -1 for unbounded
+   */
+  public int getMax() {
+    return -1;
+  }
+
+  /**
+   * Calculates the slope and intercept between the two arrays.
+   *
+   * @param x		the first array, representing the X values
+   * @param y		the second array, representing the Y values
+   * @return		intercept/slope
+   */
+  protected double[] linearRegression(Number[] x, Number[] y) {
+    double[]	xTimesY;
+    int		i;
+    int		n;
+    double	a;
+    double	b;
+
+    n      = x.length;
+    xTimesY = new double[n];
+    for (i = 0; i < n; i++)
+      xTimesY[i] = x[i].doubleValue() * y[i].doubleValue();
+
+    a = (StatUtils.sum(y) * StatUtils.sumOfSquares(x) - StatUtils.sum(x) * StatUtils.sum(xTimesY))
+        / (n * (StatUtils.sumOfSquares(x)) - Math.pow(StatUtils.sum(x), 2));
+    b = (n * StatUtils.sum(xTimesY) - StatUtils.sum(x) * StatUtils.sum(y))
+        / (n * StatUtils.sumOfSquares(x) - Math.pow(StatUtils.sum(x), 2));
+
+    return new double[]{a, b};
+  }
+
+  /**
+   * Generates the actual result.
+   *
+   * @return		the generated result
+   */
+  protected StatisticContainer doCalculate() {
+    StatisticContainer<Number>	result;
+    int				i;
+    double[]			lr;
+
+    result = new StatisticContainer<Number>(1, (size() - 1)*2);
+
+    for (i = 1; i < size(); i++) {
+      if (size() > 2) {
+	result.setHeader((i - 1)*2 + 0, "intercept_1-" + (i + 1));
+	result.setHeader((i - 1)*2 + 1, "slope_1-" + (i + 1));
+      }
+      else {
+	result.setHeader(0, "intercept");
+	result.setHeader(1, "slope");
+      }
+
+      lr = linearRegression(get(0), get(i));
+      result.setCell(0, (i - 1)*2 + 0, lr[0]);
+      result.setCell(0, (i - 1)*2 + 1, lr[1]);
+    }
+
+    return result;
+  }
+}
---
+++ adams-core/src/test/java/adams/data/statistics/ArrayLinearRegressionTest.java
@@ -0,0 +1,97 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * ArrayLinearRegressionTest.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.data.statistics;
+
+import adams.env.Environment;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Tests the adams.data.statistics.ArrayLinearRegression class. Run from commandline with: <p/>
+ * java adams.data.statistics.ArrayLinearRegressionTest
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class ArrayLinearRegressionTest
+  extends AbstractArrayStatisticTestCase<ArrayLinearRegression, Number> {
+
+  /**
+   * Initializes the test.
+   *
+   * @param name	the name of the test
+   */
+  public ArrayLinearRegressionTest(String name) {
+    super(name);
+  }
+
+  /**
+   * Returns the data used in the regression test.
+   *
+   * @return		the data
+   */
+  protected Number[][][] getRegressionInputData() {
+    Number[][][]	result;
+
+    result = new Number[][][]{
+	{
+	  {17.8,18.4,3,29.6,29,25.3,16,18.5,12.5,14.8,18.7,20.1,11.5,20.9,27.3,26.7,31.4,3.7,33.6,8.5},
+	  {17.690966295,18.8127779145,3.3837374692,29.313412074,28.1588867403,24.7165833722,15.9520974295,14.0074367493,12.4837409942,15.1978102491,19.2109292622,19.9498787108,11.9924352325,20.9209357764,27.9838522852,26.2497247507,30.4140318409,3.8298797721,32.7872438394,8.8162095405}
+	},
+    };
+
+    return result;
+  }
+
+  /**
+   * Returns the setups to use in the regression test.
+   *
+   * @return		the setups
+   */
+  protected ArrayLinearRegression[] getRegressionSetups() {
+    ArrayLinearRegression[]	result;
+
+    result = new ArrayLinearRegression[1];
+
+    result[0] = new ArrayLinearRegression();
+
+    return result;
+  }
+
+  /**
+   * Returns a test suite.
+   *
+   * @return		the test suite
+   */
+  public static Test suite() {
+    return new TestSuite(ArrayLinearRegressionTest.class);
+  }
+
+  /**
+   * Runs the test from commandline.
+   *
+   * @param args	ignored
+   */
+  public static void main(String[] args) {
+    Environment.setEnvironmentClass(Environment.class);
+    runTest(suite());
+  }
+}
