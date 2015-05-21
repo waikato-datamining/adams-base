1c7cf3b9c41143cd622f228dd0b88b4eb6967811
fracpete <fracpete@gmail.com>
2015-05-21 22:08:44 +0000

    added dummy paintlet that does not painting: NullPaintlet

---
+++ adams-core/src/main/java/adams/gui/visualization/sequence/NullPaintlet.java
@@ -0,0 +1,85 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * NullPaintlet.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.gui.visualization.sequence;
+
+import adams.gui.event.PaintEvent.PaintMoment;
+
+import java.awt.Graphics;
+
+/**
+ <!-- globalinfo-start -->
+ * Dummy paintlet that performs nor painting at all.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ * <pre>-stroke-thickness &lt;float&gt; (property: strokeThickness)
+ * &nbsp;&nbsp;&nbsp;The thickness of the stroke.
+ * &nbsp;&nbsp;&nbsp;default: 1.0
+ * &nbsp;&nbsp;&nbsp;minimum: 0.01
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class NullPaintlet
+  extends AbstractXYSequencePaintlet {
+
+  /** for serialization. */
+  private static final long serialVersionUID = 8242948176244747138L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Dummy paintlet that performs nor painting at all.";
+  }
+
+  /**
+   * Returns a new instance of the hit detector to use.
+   *
+   * @return		always null
+   */
+  @Override
+  public AbstractXYSequencePointHitDetector newHitDetector() {
+    return null;
+  }
+
+  /**
+   * The paint routine of the paintlet. Does nothing.
+   *
+   * @param g		the graphics context to use for painting
+   * @param moment	what {@link PaintMoment} is currently being painted
+   */
+  @Override
+  public void performPaint(Graphics g, PaintMoment moment) {
+  }
+}
