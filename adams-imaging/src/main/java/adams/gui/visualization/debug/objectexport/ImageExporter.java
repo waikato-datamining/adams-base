56e19c2935d7f17409e99bc573f3fabdbde7c603
fracpete <fracpete@gmail.com>
2015-06-05 01:54:01 +0000

    added image exporter

---
+++ adams-imaging/src/main/java/adams/gui/visualization/debug/objectexport/ImageExporter.java
@@ -0,0 +1,91 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * ImageExporter.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, NZ
+ */
+
+package adams.gui.visualization.debug.objectexport;
+
+import adams.core.ClassLocator;
+import adams.data.image.AbstractImageContainer;
+import adams.data.image.BufferedImageHelper;
+import adams.data.io.output.JAIImageWriter;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+/**
+ * Exports image objects (BufferedImage or derived from AbstractImageContainer).
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class ImageExporter
+  extends AbstractObjectExporter {
+
+  private static final long serialVersionUID = 4899389310274830738L;
+
+  /**
+   * Returns a string describing the format (used in the file chooser).
+   *
+   * @return 			a description suitable for displaying in the
+   * 				file chooser
+   */
+  public String getFormatDescription() {
+    return "Image files";
+  }
+
+  /**
+   * Returns the extension(s) of the format.
+   *
+   * @return 			the extension (without the dot!)
+   */
+  public String[] getFormatExtensions() {
+    return new JAIImageWriter().getFormatExtensions();
+  }
+
+  /**
+   * Checks whether the exporter can handle the specified class.
+   *
+   * @param cls		the class to check
+   * @return		true if the exporter can handle this type of object
+   */
+  @Override
+  public boolean handles(Class cls) {
+    return (cls == BufferedImage.class)
+	|| (ClassLocator.isSubclass(AbstractImageContainer.class, cls));
+  }
+
+  /**
+   * Performs the actual export.
+   *
+   * @param obj		the object to export
+   * @param file	the file to export to
+   * @return		null if successful, otherwise error message
+   */
+  @Override
+  protected String doExport(Object obj, File file) {
+    BufferedImage	img;
+
+    if (obj instanceof AbstractImageContainer)
+      img = ((AbstractImageContainer) obj).toBufferedImage();
+    else
+      img = (BufferedImage) obj;
+
+    return BufferedImageHelper.write(img, file);
+  }
+}
