e5605f8aad66d4c00936195e8f58bfc7d37e9f31
fracpete <fracpete@gmail.com>
2015-07-08 05:21:45 +0000

    added simple MJPEG video sequence actor

---
+++ adams-imaging/src/main/java/adams/flow/transformer/BoofCVMjpegVideoSequence.java
@@ -0,0 +1,216 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * BoofCVMjpegVideoSequence.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.flow.transformer;
+
+import adams.data.image.BufferedImageContainer;
+import adams.data.report.DataType;
+import adams.data.report.Field;
+import adams.flow.core.Token;
+import adams.flow.provenance.ActorType;
+import adams.flow.provenance.Provenance;
+import adams.flow.provenance.ProvenanceContainer;
+import adams.flow.provenance.ProvenanceInformation;
+import adams.flow.provenance.ProvenanceSupporter;
+import boofcv.io.image.SimpleImageSequence;
+import boofcv.io.video.VideoMjpegCodec;
+import boofcv.io.wrapper.images.JpegByteImageSequence;
+import boofcv.struct.image.ImageFloat32;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.List;
+
+/**
+ <!-- globalinfo-start -->
+ * Streams the individual frames from the MJPEG video file obtained as input.<br>
+ * Images are output as boofcv.struct.image.ImageFloat32.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- flow-summary-start -->
+ * Input&#47;output:<br>
+ * - accepts:<br>
+ * &nbsp;&nbsp;&nbsp;java.lang.String<br>
+ * &nbsp;&nbsp;&nbsp;java.io.File<br>
+ * - generates:<br>
+ * &nbsp;&nbsp;&nbsp;adams.data.image.BufferedImageContainer<br>
+ * <br><br>
+ <!-- flow-summary-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: BoofCVMjpegVideoSequence
+ * </pre>
+ * 
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded 
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow gets stopped in case this actor encounters an error;
+ * &nbsp;&nbsp;&nbsp; useful for critical actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class BoofCVMjpegVideoSequence
+  extends AbstractTransformer
+  implements ProvenanceSupporter {
+
+  /** for serialization. */
+  private static final long serialVersionUID = 3690378527551302472L;
+
+  /** the video. */
+  protected transient SimpleImageSequence m_Video;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return
+      "Streams the individual frames from the MJPEG video file obtained as input.\n"
+      + "Images are output as " + ImageFloat32.class.getName() + ".";
+  }
+
+  /**
+   * Returns the class that the consumer accepts.
+   *
+   * @return		the Class of objects that can be processed
+   */
+  public Class[] accepts() {
+    return new Class[]{String.class, File.class};
+  }
+
+  /**
+   * Returns the class of objects that it generates.
+   *
+   * @return		the Class of the generated tokens
+   */
+  @Override
+  public Class[] generates() {
+    return new Class[]{BufferedImageContainer.class};
+  }
+
+  /**
+   * Executes the flow item.
+   *
+   * @return		null if everything is fine, otherwise error message
+   */
+  @Override
+  protected String doExecute() {
+    String	result;
+    String	filename;
+
+    result = null;
+
+    if (m_InputToken.getPayload() instanceof String)
+      filename = (String) m_InputToken.getPayload();
+    else
+      filename = ((File) m_InputToken.getPayload()).getAbsolutePath();
+
+    if (filename.toLowerCase().endsWith("mjpeg")) {
+      try {
+	VideoMjpegCodec codec = new VideoMjpegCodec();
+	List<byte[]> data = codec.read(new FileInputStream(filename));
+	m_Video = new JpegByteImageSequence<ImageFloat32>(ImageFloat32.class, data, false);
+      }
+      catch (FileNotFoundException e) {
+	result = handleException("Failed to open vide", e);
+      }
+    }
+    else {
+      result = "Unknown movie type, must be an mjpeg: " + filename;
+    }
+
+    return result;
+  }
+
+  /**
+   * Checks whether there is pending output to be collected after
+   * executing the flow item.
+   *
+   * @return		true if there is pending output
+   */
+  @Override
+  public boolean hasPendingOutput() {
+    return (m_Video != null) && (m_Video.hasNext());
+  }
+
+  /**
+   * Returns the generated token.
+   *
+   * @return		the generated token
+   */
+  @Override
+  public Token output() {
+    Token			result;
+    BufferedImageContainer 	cont;
+    Field 			field;
+
+    m_Video.next();
+    cont = new BufferedImageContainer();
+    cont.setImage((BufferedImage) m_Video.getGuiImage());
+    field = new Field("Frame", DataType.NUMERIC);
+    cont.getReport().addField(field);
+    cont.getReport().setValue(field, m_Video.getFrameNumber());
+    result = new Token(cont);
+    updateProvenance(result);
+
+    return result;
+  }
+
+  /**
+   * Updates the provenance information in the provided container.
+   *
+   * @param cont	the provenance container to update
+   */
+  public void updateProvenance(ProvenanceContainer cont) {
+    if (Provenance.getSingleton().isEnabled())
+      cont.addProvenance(new ProvenanceInformation(ActorType.DATAGENERATOR, this, ((Token) cont).getPayload().getClass()));
+  }
+}
---
+++ adams-imaging/src/test/java/adams/flow/transformer/BoofCVMjpegVideoSequenceTest.java
@@ -0,0 +1,81 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * BoofCVMjpegVideoSequenceTest.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.flow.transformer;
+
+import adams.env.Environment;
+import adams.flow.AbstractFlowTest;
+import adams.flow.control.Flow;
+import adams.flow.core.AbstractActor;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Test for BoofCVMjpegVideoSequence actor.
+ * <br>
+ * NB: dummy test.
+ *
+ * @author fracpete
+ * @author adams.core.option.FlowJUnitTestProducer (code generator)
+ * @version $Revision$
+ */
+public class BoofCVMjpegVideoSequenceTest
+  extends AbstractFlowTest {
+
+  /**
+   * Initializes the test.
+   *
+   * @param name	the name of the test
+   */
+  public BoofCVMjpegVideoSequenceTest(String name) {
+    super(name);
+  }
+
+  /**
+   *
+   * Returns a test suite.
+   *
+   * @return		the test suite
+   */
+  public static Test suite() {
+    return new TestSuite(BoofCVMjpegVideoSequenceTest.class);
+  }
+
+  /**
+   * Used to create an instance of a specific actor.
+   *
+   * @return a suitably configured <code>AbstractActor</code> value
+   */
+  @Override
+  public AbstractActor getActor() {
+    return new Flow();
+  }
+
+  /**
+   * Runs the test from commandline.
+   *
+   * @param args	ignored
+   */
+  public static void main(String[] args) {
+    Environment.setEnvironmentClass(Environment.class);
+    runTest(suite());
+  }
+}
+
