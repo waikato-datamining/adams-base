f0648b7a9d23ccfd6c2bcaf0791b2bbf3d718037
fracpete <fracpete@gmail.com>
2015-07-16 21:01:03 +0000

    SelectCharset lets user select a character set, eg used for file encoding of text files

---
+++ adams-core/src/main/java/adams/flow/source/SelectCharset.java
@@ -0,0 +1,306 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * SelectCharset.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.flow.source;
+
+import adams.core.QuickInfoHelper;
+import adams.core.base.BaseCharset;
+import adams.core.management.CharsetHelper;
+import adams.flow.core.AutomatableInteractiveActor;
+import adams.flow.core.Token;
+import adams.gui.core.GUIHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ <!-- globalinfo-start -->
+ * Prompts the user to select a character set.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- flow-summary-start -->
+ * Input&#47;output:<br>
+ * - generates:<br>
+ * &nbsp;&nbsp;&nbsp;java.lang.String<br>
+ * <br><br>
+ <!-- flow-summary-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: SelectCharset
+ * </pre>
+ * 
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded 
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow gets stopped in case this actor encounters an error;
+ * &nbsp;&nbsp;&nbsp; useful for critical actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-stop-if-canceled &lt;boolean&gt; (property: stopFlowIfCanceled)
+ * &nbsp;&nbsp;&nbsp;If enabled, the flow gets stopped in case the user cancels the dialog.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-custom-stop-message &lt;java.lang.String&gt; (property: customStopMessage)
+ * &nbsp;&nbsp;&nbsp;The custom stop message to use in case a user cancelation stops the flow 
+ * &nbsp;&nbsp;&nbsp;(default is the full name of the actor)
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-initial-charset &lt;adams.core.base.BaseCharset&gt; (property: initialCharset)
+ * &nbsp;&nbsp;&nbsp;The initial character set to use for the dialog.
+ * &nbsp;&nbsp;&nbsp;default: Default
+ * </pre>
+ * 
+ * <pre>-non-interactive &lt;boolean&gt; (property: nonInteractive)
+ * &nbsp;&nbsp;&nbsp;If enabled, the initial directory is forwarded without user interaction.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class SelectCharset
+  extends AbstractInteractiveSource 
+  implements AutomatableInteractiveActor {
+
+  /** for serialization. */
+  private static final long serialVersionUID = -3223325917850709883L;
+
+  /** the initial directory. */
+  protected BaseCharset m_InitialCharset;
+
+  /** whether to automate the actor. */
+  protected boolean m_NonInteractive;
+
+  /** for the chosen directory. */
+  protected Token m_OutputToken;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Prompts the user to select a character set.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+      "initial-charset", "initialCharset",
+      new BaseCharset());
+
+    m_OptionManager.add(
+      "non-interactive", "nonInteractive",
+      false);
+  }
+
+  /**
+   * Resets the actor.
+   */
+  @Override
+  protected void reset() {
+    super.reset();
+
+    m_OutputToken = null;
+  }
+
+  /**
+   * Returns a quick info about the actor, which will be displayed in the GUI.
+   *
+   * @return		null if no info available, otherwise short string
+   */
+  @Override
+  public String getQuickInfo() {
+    String		result;
+    List<String>	options;
+
+    result = QuickInfoHelper.toString(this, "initialCharset", m_InitialCharset, "charset: ");
+    options = new ArrayList<String>();
+    QuickInfoHelper.add(options, QuickInfoHelper.toString(this, "stopFlowIfCanceled", m_StopFlowIfCanceled, "stops flow if canceled"));
+    QuickInfoHelper.add(options, QuickInfoHelper.toString(this, "nonInteractive", m_NonInteractive, "non-interactive"));
+    result += QuickInfoHelper.flatten(options);
+    
+    return result;
+  }
+
+  /**
+   * Sets the initial character set.
+   *
+   * @param value	the initial character set
+   */
+  public void setInitialCharset(BaseCharset value) {
+    m_InitialCharset = value;
+    reset();
+  }
+
+  /**
+   * Returns the initial character set.
+   *
+   * @return 		the initial character set
+   */
+  public BaseCharset getInitialCharset() {
+    return m_InitialCharset;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return		tip text for this property suitable for
+   *             	displaying in the GUI or for listing the options.
+   */
+  public String initialCharsetTipText() {
+    return "The initial character set to use for the dialog.";
+  }
+
+  /**
+   * Sets whether to enable/disable interactiveness.
+   *
+   * @param value	if true actor is not interactive, but automated
+   */
+  public void setNonInteractive(boolean value) {
+    m_NonInteractive = value;
+    reset();
+  }
+
+  /**
+   * Returns whether interactiveness is enabled/disabled.
+   *
+   * @return 		true if actor is not interactive i.e., automated
+   */
+  public boolean isNonInteractive() {
+    return m_NonInteractive;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return		tip text for this property suitable for
+   *             	displaying in the GUI or for listing the options.
+   */
+  public String nonInteractiveTipText() {
+    return "If enabled, the initial directory is forwarded without user interaction.";
+  }
+
+  /**
+   * Returns the base class of the items.
+   *
+   * @return		the class
+   */
+  protected Class getItemClass() {
+    return String.class;
+  }
+
+  /**
+   * Performs the interaction with the user.
+   *
+   * @return		true if successfully interacted
+   */
+  @Override
+  public boolean doInteract() {
+    boolean	result;
+    int		retVal;
+    String	charset;
+
+    if (m_NonInteractive) {
+      m_OutputToken = new Token(m_InitialCharset.getValue());
+      return true;
+    }
+    
+    result  = false;
+    charset = GUIHelper.showInputDialog(
+      getParentComponent(),
+      "Please select character set",
+      m_InitialCharset.getValue(),
+      CharsetHelper.getIDs(),
+      getName());
+    if (charset != null) {
+      result        = true;
+      m_OutputToken = new Token(charset);
+    }
+
+    return result;
+  }
+
+  /**
+   * Returns the class of objects that it generates.
+   *
+   * @return		the Class of the generated tokens
+   */
+  public Class[] generates() {
+    return new Class[]{String.class};
+  }
+
+  /**
+   * Returns the generated token.
+   *
+   * @return		the generated token
+   */
+  public Token output() {
+    Token	result;
+
+    result        = m_OutputToken;
+    m_OutputToken = null;
+
+    return result;
+  }
+
+  /**
+   * Checks whether there is pending output to be collected after
+   * executing the flow item.
+   *
+   * @return		true if there is pending output
+   */
+  public boolean hasPendingOutput() {
+    return (m_OutputToken != null);
+  }
+}
