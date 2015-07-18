8ef0486d04dd396ad432cd427d661095f4818f98
fracpete <fracpete@gmail.com>
2015-07-18 02:40:37 +0000

    Quick edit tab is now disabled by default

--- adams-core/src/main/java/adams/gui/flow/tab/FlowTabManager.java
+++ adams-core/src/main/java/adams/gui/flow/tab/FlowTabManager.java
@@ -126,7 +126,7 @@ public class FlowTabManager
 	m_TabList.add(tab);
 	key = createPropertyKey(tab.getClass());
 	if (!props.hasKey(key)) {
-	  props.setBoolean(key, !(tab instanceof RuntimeTab));
+	  props.setBoolean(key, !(tab instanceof RuntimeTab) && !(tab instanceof ActorQuickEditTab));
 	  update = true;
 	}
 	else if (tab instanceof RuntimeTab) {
