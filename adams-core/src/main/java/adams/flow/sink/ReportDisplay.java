f7771fc1c1a8e84ac9d15d0b3b8fb28b66939e88
fracpete <fracpete@gmail.com>
2015-07-22 02:03:45 +0000

    can make use of variable now for naming the entries

--- adams-core/src/main/java/adams/flow/sink/ReportDisplay.java
+++ adams-core/src/main/java/adams/flow/sink/ReportDisplay.java
@@ -15,18 +15,12 @@
 
 /*
  * ReportDisplay.java
- * Copyright (C) 2010-2013 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2010-2015 University of Waikato, Hamilton, New Zealand
  */
 
 package adams.flow.sink;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.util.ArrayList;
-
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-
+import adams.core.VariableNameNoUpdate;
 import adams.data.report.Report;
 import adams.data.report.ReportHandler;
 import adams.flow.core.Token;
@@ -39,76 +33,114 @@ import adams.gui.event.SearchListener;
 import adams.gui.visualization.report.ReportContainer;
 import adams.gui.visualization.report.ReportFactory;
 
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.util.ArrayList;
+
 /**
  <!-- globalinfo-start -->
- * Displays quantitation reports.
+ * Displays reports.
  * <br><br>
  <!-- globalinfo-end -->
  *
  <!-- flow-summary-start -->
- * Input/output:<br>
+ * Input&#47;output:<br>
  * - accepts:<br>
- * &nbsp;&nbsp;&nbsp;gcms.data.chromatogram.Chromatogram<br>
- * &nbsp;&nbsp;&nbsp;gcms.data.quantitation.QuantitationReport<br>
+ * &nbsp;&nbsp;&nbsp;adams.data.report.ReportHandler<br>
+ * &nbsp;&nbsp;&nbsp;adams.data.report.Report<br>
  * <br><br>
  <!-- flow-summary-end -->
  *
  <!-- options-start -->
- * Valid options are: <br><br>
- *
- * <pre>-D &lt;int&gt; (property: debugLevel)
- * &nbsp;&nbsp;&nbsp;The greater the number the more additional info the scheme may output to
- * &nbsp;&nbsp;&nbsp;the console (0 = off).
- * &nbsp;&nbsp;&nbsp;default: 0
- * &nbsp;&nbsp;&nbsp;minimum: 0
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
  * </pre>
- *
+ * 
  * <pre>-name &lt;java.lang.String&gt; (property: name)
  * &nbsp;&nbsp;&nbsp;The name of the actor.
  * &nbsp;&nbsp;&nbsp;default: ReportDisplay
  * </pre>
- *
- * <pre>-annotation &lt;adams.core.base.BaseText&gt; (property: annotations)
+ * 
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
  * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
- * &nbsp;&nbsp;&nbsp;default:
+ * &nbsp;&nbsp;&nbsp;default: 
  * </pre>
- *
- * <pre>-skip (property: skip)
- * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded
+ * 
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded 
  * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- *
+ * 
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow gets stopped in case this actor encounters an error;
+ * &nbsp;&nbsp;&nbsp; useful for critical actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-short-title &lt;boolean&gt; (property: shortTitle)
+ * &nbsp;&nbsp;&nbsp;If enabled uses just the name for the title instead of the actor's full 
+ * &nbsp;&nbsp;&nbsp;name.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-display-in-editor &lt;boolean&gt; (property: displayInEditor)
+ * &nbsp;&nbsp;&nbsp;If enabled displays the panel in a tab in the flow editor rather than in 
+ * &nbsp;&nbsp;&nbsp;a separate frame.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
  * <pre>-width &lt;int&gt; (property: width)
  * &nbsp;&nbsp;&nbsp;The width of the dialog.
- * &nbsp;&nbsp;&nbsp;default: 450
- * &nbsp;&nbsp;&nbsp;minimum: 1
+ * &nbsp;&nbsp;&nbsp;default: 600
+ * &nbsp;&nbsp;&nbsp;minimum: -1
  * </pre>
- *
+ * 
  * <pre>-height &lt;int&gt; (property: height)
  * &nbsp;&nbsp;&nbsp;The height of the dialog.
  * &nbsp;&nbsp;&nbsp;default: 600
- * &nbsp;&nbsp;&nbsp;minimum: 1
+ * &nbsp;&nbsp;&nbsp;minimum: -1
  * </pre>
- *
+ * 
  * <pre>-x &lt;int&gt; (property: x)
  * &nbsp;&nbsp;&nbsp;The X position of the dialog (&gt;=0: absolute, -1: left, -2: center, -3: right
  * &nbsp;&nbsp;&nbsp;).
  * &nbsp;&nbsp;&nbsp;default: -1
  * &nbsp;&nbsp;&nbsp;minimum: -3
  * </pre>
- *
+ * 
  * <pre>-y &lt;int&gt; (property: y)
  * &nbsp;&nbsp;&nbsp;The Y position of the dialog (&gt;=0: absolute, -1: top, -2: center, -3: bottom
  * &nbsp;&nbsp;&nbsp;).
  * &nbsp;&nbsp;&nbsp;default: -1
  * &nbsp;&nbsp;&nbsp;minimum: -3
  * </pre>
- *
+ * 
  * <pre>-font &lt;java.awt.Font&gt; (property: font)
  * &nbsp;&nbsp;&nbsp;The font of the dialog.
  * &nbsp;&nbsp;&nbsp;default: Monospaced-PLAIN-12
  * </pre>
- *
+ * 
+ * <pre>-always-clear &lt;boolean&gt; (property: alwaysClear)
+ * &nbsp;&nbsp;&nbsp;If enabled, the display is always cleared before processing a token.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-entry-name-variable &lt;adams.core.VariableNameNoUpdate&gt; (property: entryNameVariable)
+ * &nbsp;&nbsp;&nbsp;The variable to use for naming the entries; gets ignored if variable not 
+ * &nbsp;&nbsp;&nbsp;available; an existing history entry gets replaced if a new one with the 
+ * &nbsp;&nbsp;&nbsp;same name gets added.
+ * &nbsp;&nbsp;&nbsp;default: entryNameVariable
+ * </pre>
+ * 
  <!-- options-end -->
  *
  * @author  fracpete (fracpete at waikato dot ac dot nz)
@@ -126,6 +158,9 @@ public class ReportDisplay
   /** the search panel. */
   protected SearchPanel m_SearchPanel;
 
+  /** the variable to use for naming the entries. */
+  protected VariableNameNoUpdate m_EntryNameVariable;
+
   /** whether a report has been selected. */
   protected boolean m_ReportSelected;
 
@@ -140,13 +175,25 @@ public class ReportDisplay
   }
 
   /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+      "entry-name-variable", "entryNameVariable",
+      new VariableNameNoUpdate("entryNameVariable"));
+  }
+
+  /**
    * Returns the default width for the dialog.
    *
    * @return		the default width
    */
   @Override
   protected int getDefaultWidth() {
-    return 450;
+    return 600;
   }
 
   /**
@@ -157,6 +204,37 @@ public class ReportDisplay
   @Override
   protected int getDefaultHeight() {
     return 600;
+  }
+
+  /**
+   * Sets the variable name which value gets used to name the entries. Gets
+   * ignored if variable does not exist.
+   *
+   * @param value	the variable name
+   */
+  public void setEntryNameVariable(VariableNameNoUpdate value) {
+    m_EntryNameVariable = value;
+    reset();
+  }
+
+  /**
+   * Returns the variable name which value gets used to name the entries.
+   * Gets ignored if variable does not exist.
+   *
+   * @return		the variable name
+   */
+  public VariableNameNoUpdate getEntryNameVariable() {
+    return m_EntryNameVariable;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String entryNameVariableTipText() {
+    return "The variable to use for naming the entries; gets ignored if variable not available; an existing history entry gets replaced if a new one with the same name gets added.";
   }
 
   /**
@@ -206,7 +284,7 @@ public class ReportDisplay
   /**
    * Returns the class that the consumer accepts.
    *
-   * @return		<!-- flow-accepts-start -->gcms.data.chromatogram.Chromatogram.class, gcms.data.quantitation.QuantitationReport.class<!-- flow-accepts-end -->
+   * @return		<!-- flow-accepts-start -->adams.data.report.ReportHandler.class, adams.data.report.Report.class<!-- flow-accepts-end -->
    */
   @Override
   public Class[] accepts() {
@@ -221,15 +299,25 @@ public class ReportDisplay
    */
   @Override
   protected void display(Token token) {
-    Report	report;
+    Report		report;
+    ReportContainer	cont;
+    String		var;
+    String		id;
 
     if (token.getPayload() instanceof ReportHandler)
       report = ((ReportHandler) token.getPayload()).getReport();
     else
       report = (Report) token.getPayload();
 
-    if (report != null)
-      m_Panel.getContainerManager().add(m_Panel.getContainerManager().newContainer(report));
+    if (report != null) {
+      cont = m_Panel.getContainerManager().newContainer(report);
+      var = getEntryNameVariable().getValue();
+      if (getVariables().has(var)) {
+	id = getVariables().get(var);
+	cont.setID(id);
+      }
+      m_Panel.getContainerManager().add(cont);
+    }
   }
 
   /**
