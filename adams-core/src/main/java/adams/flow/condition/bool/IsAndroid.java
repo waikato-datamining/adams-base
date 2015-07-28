75d14cebd605b7c88ac3cfe4a26bf401d490dd60
fracpete <fracpete@gmail.com>
2015-07-28 22:41:00 +0000

    added boolean condition for android OS detection

---
+++ adams-core/src/main/java/adams/flow/condition/bool/IsAndroid.java
@@ -0,0 +1,91 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * IsAndroid.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ */
+package adams.flow.condition.bool;
+
+import adams.core.management.OS;
+import adams.flow.core.Actor;
+import adams.flow.core.Token;
+import adams.flow.core.Unknown;
+
+/**
+ <!-- globalinfo-start -->
+ * Evaluates to 'true' if the operating system is Android.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision: 7533 $
+ */
+public class IsAndroid
+  extends AbstractBooleanCondition {
+
+  /** for serialization. */
+  private static final long serialVersionUID = 3278345095591806425L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Evaluates to 'true' if the operating system is Android.";
+  }
+
+  /**
+   * Returns the quick info string to be displayed in the flow editor.
+   *
+   * @return		always null
+   */
+  @Override
+  public String getQuickInfo() {
+    return "Android?";
+  }
+
+  /**
+   * Returns the class that the consumer accepts.
+   *
+   * @return		Unknown
+   */
+  @Override
+  public Class[] accepts() {
+    return new Class[]{Unknown.class};
+  }
+
+  /**
+   * Performs the actual evaluation.
+   *
+   * @param owner	the owning actor
+   * @param token	the current token passing through
+   * @return		the result of the evaluation
+   */
+  @Override
+  protected boolean doEvaluate(Actor owner, Token token) {
+    return OS.isAndroid();
+  }
+}
