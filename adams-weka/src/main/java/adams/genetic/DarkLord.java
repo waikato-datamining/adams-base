e7207a69458f529722e4f334536959f542e52115
fracpete <fracpete@gmail.com>
2015-08-14 04:38:16 +0000

    centralized Measure enum

--- adams-weka/src/main/java/adams/genetic/DarkLord.java
+++ adams-weka/src/main/java/adams/genetic/DarkLord.java
@@ -35,7 +35,6 @@ import weka.classifiers.Evaluation;
 import weka.classifiers.rules.ZeroR;
 import weka.core.Instance;
 import weka.core.Instances;
-import weka.core.UnassignedClassException;
 import weka.filters.unsupervised.attribute.Remove;
 
 import java.io.BufferedWriter;
@@ -386,83 +385,6 @@ public class DarkLord
       }
 
       return result;
-    }
-  }
-
-  /**
-   * The measure to use for evaluating.
-   *
-   * @author FracPete (fracpete at waikato dot ac dot nz)
-   * @version $Revision: 4322 $
-   */
-  public enum Measure {
-
-    /** evaluation via: Correlation coefficient. */
-    CC(false, false, true),
-    /** evaluation via: Root mean squared error. */
-    RMSE(true, true, true),
-    /** evaluation via: Root relative squared error. */
-    RRSE(true, true, true),
-    /** evaluation via: Mean absolute error. */
-    MAE(true, true, true),
-    /** evaluation via: Relative absolute error. */
-    RAE(true, true, true),
-    /** evaluation via: Accuracy. */
-    ACC(false, true, false);
-
-
-    /** whether the measure is multiplied by -1 or not. Only used in sorting. */
-    private boolean m_Negative;
-
-    /** whether a nominal class is allowed. */
-    private boolean m_Nominal;
-
-    /** whether a numeric class is allowed. */
-    private boolean m_Numeric;
-
-    /**
-     * initializes the measure with the given flags.
-     *
-     * @param negative	whether measures gets multiplied with -1
-     * @param nominal	whether used for nominal classes
-     * @param numeric	whether used for numeric classes
-     */
-    private Measure(boolean negative, boolean nominal, boolean numeric) {
-      m_Negative = negative;
-      m_Nominal  = nominal;
-      m_Numeric  = numeric;
-    }
-
-    /**
-     * Adjusts the measure value for sorting: either multiplies it with -1 or 1.
-     *
-     * @param measure	the raw measure
-     * @return		the adjusted measure
-     */
-    public double adjust(double measure) {
-      if (m_Negative)
-        return -measure;
-      else
-        return measure;
-    }
-
-    /**
-     * Checks whether the data can be used with this measure.
-     *
-     * @param data	the data to check
-     * @return		true if the measure can be obtain for this kind of data
-     */
-    public boolean isValid(Instances data) {
-      if (data.classIndex() == -1)
-        throw new UnassignedClassException("No class attribute set!");
-
-      if (data.classAttribute().isNominal())
-        return m_Nominal;
-      else if (data.classAttribute().isNumeric())
-        return m_Numeric;
-      else
-        throw new IllegalStateException(
-          "Class attribute '" + data.classAttribute().type() + "' not handled!");
     }
   }
 
