bae1410c4fb16270e5c83044da83e97b90aa8cc9
fracpete <fracpete@gmail.com>
2015-08-27 00:38:13 +0000

    fixed copyright

--- adams-core/src/test/java/adams/data/compare/JavaComparableTest.java
+++ adams-core/src/test/java/adams/data/compare/JavaComparableTest.java
@@ -14,8 +14,8 @@
  */
 
 /**
- * TextTest.java
- * Copyright (C) 2014 University of Waikato, Hamilton, New Zealand
+ * JavaComparableTest.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
  */
 package adams.data.compare;
 
