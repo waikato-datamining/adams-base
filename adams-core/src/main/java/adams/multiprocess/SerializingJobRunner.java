b23490dbd24f05e49d9f9b5e6eea591ee029631e
fracpete <fracpete@gmail.com>
2015-09-19 20:46:43 +0000

    implementing MultiAttemptWithWaitSupporter now

--- adams-core/src/main/java/adams/multiprocess/SerializingJobRunner.java
+++ adams-core/src/main/java/adams/multiprocess/SerializingJobRunner.java
@@ -20,8 +20,10 @@
 
 package adams.multiprocess;
 
+import adams.core.MultiAttemptWithWaitSupporter;
 import adams.core.SerializationHelper;
 import adams.core.Utils;
+import adams.core.io.FileUtils;
 import adams.core.io.PlaceholderFile;
 
 /**
@@ -51,13 +53,26 @@ import adams.core.io.PlaceholderFile;
  * &nbsp;&nbsp;&nbsp;default: ${CWD}
  * </pre>
  * 
+ * <pre>-max-attempts &lt;int&gt; (property: numAttempts)
+ * &nbsp;&nbsp;&nbsp;The maximum number of intervals to wait.
+ * &nbsp;&nbsp;&nbsp;default: 10
+ * &nbsp;&nbsp;&nbsp;minimum: 1
+ * </pre>
+ * 
+ * <pre>-attempt-nterval &lt;int&gt; (property: attemptInterval)
+ * &nbsp;&nbsp;&nbsp;The interval in milli-seconds to wait before continuing with the execution.
+ * &nbsp;&nbsp;&nbsp;default: 100
+ * &nbsp;&nbsp;&nbsp;minimum: 1
+ * </pre>
+ * 
  <!-- options-end -->
  *
  * @author FracPete (fracpete at waikato dot ac dot nz)
  * @version $Revision$
  */
 public class SerializingJobRunner
-  extends AbstractMetaJobRunner {
+  extends AbstractMetaJobRunner
+  implements MultiAttemptWithWaitSupporter {
 
   private static final long serialVersionUID = 6656064128031953130L;
 
@@ -66,6 +81,12 @@ public class SerializingJobRunner
 
   /** the file to deserialize the finished jobs from. */
   protected PlaceholderFile m_Import;
+
+  /** the maximum number of interval to wait. */
+  protected int m_NumAttempts;
+
+  /** the interval in milli-seconds to wait. */
+  protected int m_AttemptInterval;
 
   /**
    * Returns a string describing the object.
@@ -94,6 +115,14 @@ public class SerializingJobRunner
     m_OptionManager.add(
       "import", "import",
       getDefaultImport());
+
+    m_OptionManager.add(
+      "max-attempts", "numAttempts",
+      10, 1, null);
+
+    m_OptionManager.add(
+      "attempt-nterval", "attemptInterval",
+      100, 1, null);
   }
 
   /**
@@ -173,6 +202,68 @@ public class SerializingJobRunner
   }
 
   /**
+   * Sets the maximum number of intervals to wait.
+   *
+   * @param value	the maximum
+   */
+  public void setNumAttempts(int value) {
+    if (getOptionManager().isValid("numAttempts", value)) {
+      m_NumAttempts = value;
+      reset();
+    }
+  }
+
+  /**
+   * Returns the maximum number of intervals to wait.
+   *
+   * @return		the maximum
+   */
+  public int getNumAttempts() {
+    return m_NumAttempts;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String numAttemptsTipText() {
+    return "The maximum number of intervals to wait.";
+  }
+
+  /**
+   * Sets the interval in milli-seconds to wait.
+   *
+   * @param value	the interval
+   */
+  public void setAttemptInterval(int value) {
+    if (getOptionManager().isValid("attemptInterval", value)) {
+      m_AttemptInterval = value;
+      reset();
+    }
+  }
+
+  /**
+   * Returns the interval to wait in milli-seconds.
+   *
+   * @return		the interval
+   */
+  public int getAttemptInterval() {
+    return m_AttemptInterval;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String attemptIntervalTipText() {
+    return "The interval in milli-seconds to wait before continuing with the execution.";
+  }
+
+  /**
    * Serializes the jobs to the specified export file.
    *
    * @return		null if successful, otherwise error message
@@ -196,6 +287,10 @@ public class SerializingJobRunner
    */
   @Override
   protected String doStop() {
+    int		count;
+    boolean	inUse;
+
+    // wait for file to appear
     while (isRunning() || isPaused()) {
       if (!isPaused()) {
 	if (m_Import.exists())
@@ -203,7 +298,29 @@ public class SerializingJobRunner
       }
       Utils.wait(this, 100, 100);
     }
+
+    // file still in use?
+    count = 0;
     if (isRunning()) {
+      while ((count < m_NumAttempts) && isRunning()) {
+	if (!FileUtils.isOpen(m_Import))
+	  break;
+	count++;
+	Utils.wait(this, m_AttemptInterval, Math.min(100, m_AttemptInterval));
+      }
+    }
+
+    // read jobs
+    if (isRunning()) {
+      inUse = FileUtils.isOpen(m_Import);
+      if (isLoggingEnabled())
+	getLogger().info("count=" + count + ", inUse=" + inUse + ", file=" + m_Import);
+
+      // still open?
+      if ((count == m_NumAttempts) && inUse) {
+	return "File '" + m_Import + "' is still in use after " + m_NumAttempts + " * " + m_AttemptInterval + "msec!";
+      }
+
       try {
 	m_ActualJobRunner = (JobRunner) SerializationHelper.read(m_Import.getAbsolutePath());
       }
--- adams-net/src/main/java/adams/multiprocess/ScpJobRunner.java
+++ adams-net/src/main/java/adams/multiprocess/ScpJobRunner.java
@@ -20,8 +20,10 @@
 
 package adams.multiprocess;
 
+import adams.core.MultiAttemptWithWaitSupporter;
 import adams.core.SerializationHelper;
 import adams.core.Utils;
+import adams.core.io.FileUtils;
 import adams.core.io.PlaceholderFile;
 import adams.core.io.TempUtils;
 import adams.core.net.InternetHelper;
@@ -75,13 +77,26 @@ import java.io.File;
  * &nbsp;&nbsp;&nbsp;default: ${CWD}
  * </pre>
  * 
+ * <pre>-max-attempts &lt;int&gt; (property: numAttempts)
+ * &nbsp;&nbsp;&nbsp;The maximum number of intervals to wait.
+ * &nbsp;&nbsp;&nbsp;default: 10
+ * &nbsp;&nbsp;&nbsp;minimum: 1
+ * </pre>
+ * 
+ * <pre>-attempt-nterval &lt;int&gt; (property: attemptInterval)
+ * &nbsp;&nbsp;&nbsp;The interval in milli-seconds to wait before continuing with the execution.
+ * &nbsp;&nbsp;&nbsp;default: 100
+ * &nbsp;&nbsp;&nbsp;minimum: 1
+ * </pre>
+ * 
  <!-- options-end -->
  *
  * @author FracPete (fracpete at waikato dot ac dot nz)
  * @version $Revision$
  */
 public class ScpJobRunner
-  extends AbstractMetaJobRunner {
+  extends AbstractMetaJobRunner
+  implements MultiAttemptWithWaitSupporter  {
 
   private static final long serialVersionUID = 6656064128031953130L;
 
@@ -349,6 +364,12 @@ public class ScpJobRunner
   /** the local file to deserialize the finished jobs from. */
   protected PlaceholderFile m_LocalFile;
 
+  /** the maximum number of interval to wait. */
+  protected int m_NumAttempts;
+
+  /** the interval in milli-seconds to wait. */
+  protected int m_AttemptInterval;
+
   /** the ssh connection to use. */
   protected transient SSHConnection m_Connection;
 
@@ -388,6 +409,14 @@ public class ScpJobRunner
     m_OptionManager.add(
       "local-file", "localFile",
       getDefaultLocalFile());
+
+    m_OptionManager.add(
+      "max-attempts", "numAttempts",
+      10, 1, null);
+
+    m_OptionManager.add(
+      "attempt-nterval", "attemptInterval",
+      100, 1, null);
   }
 
   /**
@@ -541,6 +570,68 @@ public class ScpJobRunner
   }
 
   /**
+   * Sets the maximum number of intervals to wait.
+   *
+   * @param value	the maximum
+   */
+  public void setNumAttempts(int value) {
+    if (getOptionManager().isValid("numAttempts", value)) {
+      m_NumAttempts = value;
+      reset();
+    }
+  }
+
+  /**
+   * Returns the maximum number of intervals to wait.
+   *
+   * @return		the maximum
+   */
+  public int getNumAttempts() {
+    return m_NumAttempts;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String numAttemptsTipText() {
+    return "The maximum number of intervals to wait.";
+  }
+
+  /**
+   * Sets the interval in milli-seconds to wait.
+   *
+   * @param value	the interval
+   */
+  public void setAttemptInterval(int value) {
+    if (getOptionManager().isValid("attemptInterval", value)) {
+      m_AttemptInterval = value;
+      reset();
+    }
+  }
+
+  /**
+   * Returns the interval to wait in milli-seconds.
+   *
+   * @return		the interval
+   */
+  public int getAttemptInterval() {
+    return m_AttemptInterval;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String attemptIntervalTipText() {
+    return "The interval in milli-seconds to wait before continuing with the execution.";
+  }
+
+  /**
    * Returns whether to transfer the listeners to the actual job runner.
    *
    * @return		true if to transfer
@@ -649,6 +740,10 @@ public class ScpJobRunner
    */
   @Override
   protected String doStop() {
+    int		count;
+    boolean	inUse;
+
+    // wait for file to appear
     while (isRunning() || isPaused()) {
       if (!isPaused()) {
 	if (m_LocalFile.exists())
@@ -656,7 +751,29 @@ public class ScpJobRunner
       }
       Utils.wait(this, 100, 100);
     }
+
+    // file still in use?
+    count = 0;
     if (isRunning()) {
+      while ((count < m_NumAttempts) && isRunning()) {
+	if (!FileUtils.isOpen(m_LocalFile))
+	  break;
+	count++;
+	Utils.wait(this, m_AttemptInterval, Math.min(100, m_AttemptInterval));
+      }
+    }
+
+    // read jobs
+    if (isRunning()) {
+      inUse = FileUtils.isOpen(m_LocalFile);
+      if (isLoggingEnabled())
+	getLogger().info("count=" + count + ", inUse=" + inUse + ", file=" + m_LocalFile);
+
+      // still open?
+      if ((count == m_NumAttempts) && inUse) {
+	return "File '" + m_LocalFile + "' is still in use after " + m_NumAttempts + " * " + m_AttemptInterval + "msec!";
+      }
+
       try {
         if (isLoggingEnabled())
           getLogger().info("Reading jobs from " + m_LocalFile);
