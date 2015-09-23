7a083b7bf9d6337352d1e83d20910878192be050
fracpete <fracpete@gmail.com>
2015-09-23 21:14:51 +0000

    simple processor for listing all unique paths in the flow

---
+++ adams-core/src/main/java/adams/flow/processor/ListPaths.java
@@ -0,0 +1,112 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * ListPaths.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.processor;
+
+import adams.core.option.OptionHandler;
+import adams.core.option.OptionTraversalPath;
+
+import java.io.File;
+
+/**
+ <!-- globalinfo-start -->
+ * Lists all the paths (files and directories) that are used in the flow.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class ListPaths
+  extends AbstractListingProcessor {
+
+  private static final long serialVersionUID = -6340700367008421185L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 		a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Lists all the paths (files and directories) that are used in the flow.";
+  }
+
+  /**
+   * Returns the title for the dialog.
+   *
+   * @return		the title
+   */
+  public String getTitle() {
+    return "Paths";
+  }
+
+  /**
+   * Checks whether the object is valid and should be added to the list.
+   *
+   * @param handler	the option handler this object belongs to
+   * @param obj		the object to check
+   * @param path	the traversal path of properties
+   * @return		true if valid
+   */
+  @Override
+  protected boolean isValid(OptionHandler handler, Object obj, OptionTraversalPath path) {
+    return (obj instanceof File);
+  }
+
+  /**
+   * Returns whether the list should be sorted.
+   *
+   * @return		true if the list should get sorted
+   */
+  @Override
+  protected boolean isSortedList() {
+    return true;
+  }
+
+  /**
+   * Returns whether the list should not contain any duplicates.
+   *
+   * @return		true if the list contains no duplicates
+   */
+  @Override
+  protected boolean isUniqueList() {
+    return true;
+  }
+
+  /**
+   * Returns the header to use in the dialog, i.e., the one-liner that
+   * explains the output.
+   *
+   * @return		the header, null if no header available
+   */
+  @Override
+  protected String getHeader() {
+    return "Paths in use:";
+  }
+}
