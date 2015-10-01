422783cfd1c37b5531fa89b1a0094adf8b511930
fracpete <fracpete@gmail.com>
2015-10-01 00:03:17 +0000

    added support for attribute label indices

---
+++ adams-weka/src/main/java/adams/data/weka/WekaLabelIndex.java
@@ -0,0 +1,110 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * WekaLabelIndex.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ */
+package adams.data.weka;
+
+import adams.core.AbstractDataBackedIndex;
+import adams.core.Index;
+import weka.core.Attribute;
+
+/**
+ * Extended {@link Index} class that can use a label name to determine an
+ * index of a label as well.
+ * 
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class WekaLabelIndex
+  extends AbstractDataBackedIndex<Attribute> {
+
+  /** for serialization. */
+  private static final long serialVersionUID = -4358263779315198808L;
+
+  /**
+   * Initializes with no index.
+   */
+  public WekaLabelIndex() {
+    super();
+  }
+
+  /**
+   * Initializes with the given index, but no maximum.
+   *
+   * @param index	the index to use
+   */
+  public WekaLabelIndex(String index) {
+    super(index);
+  }
+
+  /**
+   * Initializes with the given index and maximum.
+   *
+   * @param index	the index to use
+   * @param max		the maximum of the 1-based index (e.g., use "10" to
+   * 			allow "1-10" or -1 for uninitialized)
+   */
+  public WekaLabelIndex(String index, int max) {
+    super(index, max);
+  }
+
+  /**
+   * Returns a clone of the object.
+   *
+   * @return		the clone
+   */
+  @Override
+  public WekaLabelIndex getClone() {
+    return (WekaLabelIndex) super.getClone();
+  }
+
+  /**
+   * Returns the number of labels that the attribute has.
+   * 
+   * @param data	the attribute to retrieve the number of labels
+   */
+  @Override
+  protected int getNumColumns(Attribute data) {
+    return data.numValues();
+  }
+  
+  /**
+   * Returns the label name at the specified index.
+   * 
+   * @param data	the dataset to use
+   * @param colIndex	the label index
+   * @return		the label name
+   */
+  @Override
+  protected String getColumnName(Attribute data, int colIndex) {
+    return data.value(colIndex);
+  }
+
+  /**
+   * Returns the example.
+   *
+   * @return		the example
+   */
+  @Override
+  public String getExample() {
+    return
+        "An index is a number starting with 1; apart from label names "
+      + "(case-sensitive), the following placeholders can be used as well: "
+      + FIRST + ", " + SECOND + ", " + THIRD + ", " + LAST_2 + ", " + LAST_1 + ", " + LAST;
+  }
+}
---
+++ adams-weka/src/test/java/adams/data/weka/WekaLabelIndexTest.java
@@ -0,0 +1,175 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * WekaLabelIndexTest.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ */
+package adams.data.weka;
+
+import adams.core.Index;
+import adams.core.IndexTest;
+import adams.env.Environment;
+import adams.test.AbstractTestHelper;
+import adams.test.TestHelper;
+import adams.test.TmpFile;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+import java.io.FileNotFoundException;
+
+/**
+ * Tests the WekaLabelIndex class. Run from commandline with: <br><br>
+ * java adams.data.weka.WekaLabelIndexTest
+ * 
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class WekaLabelIndexTest
+  extends IndexTest {
+
+  /** the dataset to use for testing. */
+  protected Instances m_Data;
+
+  /**
+   * Initializes the test.
+   *
+   * @param name	the name of the test
+   */
+  public WekaLabelIndexTest(String name) {
+    super(name);
+  }
+
+  /**
+   * Returns the test helper class to use.
+   *
+   * @return		the helper class instance
+   */
+  @Override
+  protected AbstractTestHelper newTestHelper() {
+    return new TestHelper(this, "adams/data/weka/data");
+  }
+  
+  /**
+   * Called by JUnit before each test method.
+   *
+   * @throws Exception if an error occurs.
+   */
+  @Override
+  protected void setUp() throws Exception {
+    String	filename;
+    
+    super.setUp();
+    
+    filename = "labor.arff";
+    m_TestHelper.copyResourceToTmp(filename);
+    m_Data = loadDataset(filename);
+
+    m_TestHelper.copyResourceToTmp("simple.arff");
+  }
+  
+  /**
+   * Called by JUnit after each test method.
+   *
+   * @throws Exception	if tear-down fails
+   */
+  @Override
+  protected void tearDown() throws Exception {
+    m_TestHelper.deleteFileFromTmp("labor.arff");
+    m_TestHelper.deleteFileFromTmp("simple.arff");
+
+    super.tearDown();
+  }
+
+  /**
+   * Loads the specified dataset from the tmp directory.
+   *
+   * @param filename	the dataset to load (no path)
+   * @return		the dataset
+   * @throws Exception	if loading of dataset failed
+   */
+  protected Instances loadDataset(String filename) throws Exception {
+    Instances 	result;
+
+    result = DataSource.read(new TmpFile(filename).getAbsolutePath());
+    if (result == null)
+      throw new FileNotFoundException("Test file '" + filename + "' not found?");
+
+    return result;
+  }
+
+  /**
+   * Returns a new Index instance.
+   * 
+   * @param index	the index to use, null for default constructor
+   * @param max		the maximum to use
+   * @return		the new instance
+   */
+  @Override
+  protected Index newIndex(String index, int max) {
+    if (index == null)
+      return new WekaLabelIndex();
+    else
+      return new WekaLabelIndex(index, max);
+  }
+  
+  /**
+   * Tests the index using column names.
+   */
+  public void testColumnNames() {
+    WekaLabelIndex index = new WekaLabelIndex();
+    
+    index.setData(null);
+    index.setIndex("blah");
+    assertEquals("should be invalid", -1, index.getIntIndex());
+    assertEquals("should be same", "blah", index.getIndex());
+    
+    index.setData(m_Data.attribute("pension"));
+    index.setIndex("none");
+    assertEquals("should be valid", 0, index.getIntIndex());
+    assertEquals("should be same", "none", index.getIndex());
+    
+    index.setData(m_Data.attribute("pension"));
+    index.setIndex("Empl_contr");
+    assertEquals("should be invalid", -1, index.getIntIndex());
+    assertEquals("should be same", "Empl_contr", index.getIndex());
+
+    index.setData(m_Data.attribute("pension"));
+    index.setIndex("empl_contr");
+    assertEquals("should be valid", 2, index.getIntIndex());
+    assertEquals("should be same", "empl_contr", index.getIndex());
+  }
+
+  /**
+   * Returns a test suite.
+   *
+   * @return		the test suite
+   */
+  public static Test suite() {
+    return new TestSuite(WekaLabelIndexTest.class);
+  }
+
+  /**
+   * Runs the test from commandline.
+   *
+   * @param args	ignored
+   */
+  public static void main(String[] args) {
+    Environment.setEnvironmentClass(Environment.class);
+    runTest(suite());
+  }
+}
