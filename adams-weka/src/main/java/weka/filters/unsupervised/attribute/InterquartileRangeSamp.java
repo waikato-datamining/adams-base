63d04e2366cfc4ce6f793fee9420dbc9e554ec96
fracpete <fracpete@gmail.com>
2015-10-12 21:56:13 +0000

    added regexp option for ignoring attributes based on their name; exposed min-samples and sample-size properties as options

--- adams-weka/src/main/java/weka/filters/unsupervised/attribute/InterquartileRangeSamp.java
+++ adams-weka/src/main/java/weka/filters/unsupervised/attribute/InterquartileRangeSamp.java
@@ -20,73 +20,94 @@
 
 package weka.filters.unsupervised.attribute;
 
+import adams.core.base.BaseRegExp;
 import gnu.trove.list.array.TDoubleArrayList;
 import weka.core.Instances;
+import weka.core.WekaOptionUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.Vector;
 
 /**
  <!-- globalinfo-start -->
- * A filter for detecting outliers and extreme values based on interquartile ranges. The filter skips the class attribute.<br>
- * <br>
- * Outliers:<br>
- *   Q3 + OF*IQR &lt; x &lt;= Q3 + EVF*IQR<br>
- *   or<br>
- *   Q1 - EVF*IQR &lt;= x &lt; Q1 - OF*IQR<br>
- * <br>
- * Extreme values:<br>
- *   x &gt; Q3 + EVF*IQR<br>
- *   or<br>
- *   x &lt; Q1 - EVF*IQR<br>
- * <br>
- * Key:<br>
- *   Q1  = 25% quartile<br>
- *   Q3  = 75% quartile<br>
- *   IQR = Interquartile Range, difference between Q1 and Q3<br>
- *   OF  = Outlier Factor<br>
+ * A sampling filter for detecting outliers and extreme values based on interquartile ranges. The filter skips the class attribute.<br/>
+ * <br/>
+ * Outliers:<br/>
+ *   Q3 + OF*IQR &lt; x &lt;= Q3 + EVF*IQR<br/>
+ *   or<br/>
+ *   Q1 - EVF*IQR &lt;= x &lt; Q1 - OF*IQR<br/>
+ * <br/>
+ * Extreme values:<br/>
+ *   x &gt; Q3 + EVF*IQR<br/>
+ *   or<br/>
+ *   x &lt; Q1 - EVF*IQR<br/>
+ * <br/>
+ * Key:<br/>
+ *   Q1  = 25% quartile<br/>
+ *   Q3  = 75% quartile<br/>
+ *   IQR = Interquartile Range, difference between Q1 and Q3<br/>
+ *   OF  = Outlier Factor<br/>
  *   EVF = Extreme Value Factor
- * <br><br>
+ * <p/>
  <!-- globalinfo-end -->
  *
  <!-- options-start -->
- * Valid options are: <br><br>
- *
- * <pre> -D
- *  Turns on output of debugging information.</pre>
- *
+ * Valid options are: <p/>
+ * 
+ * <pre> -sample-size &lt;value&gt;
+ *  The sample size to use.
+ *  (default: 150)</pre>
+ * 
+ * <pre> -min-samples &lt;value&gt;
+ *  The minimum number of samples that are required for calculating IQR stats.
+ *  (default: 5)</pre>
+ * 
+ * <pre> -ignored-attributes &lt;value&gt;
+ *  The regular expression for attributes to ignore/skip.
+ *  (default: ^.*_id$)</pre>
+ * 
  * <pre> -R &lt;col1,col2-col4,...&gt;
  *  Specifies list of columns to base outlier/extreme value detection
  *  on. If an instance is considered in at least one of those
  *  attributes an outlier/extreme value, it is tagged accordingly.
  *  'first' and 'last' are valid indexes.
  *  (default none)</pre>
- *
+ * 
  * <pre> -O &lt;num&gt;
  *  The factor for outlier detection.
  *  (default: 3)</pre>
- *
+ * 
  * <pre> -E &lt;num&gt;
  *  The factor for extreme values detection.
  *  (default: 2*Outlier Factor)</pre>
- *
+ * 
  * <pre> -E-as-O
  *  Tags extreme values also as outliers.
  *  (default: off)</pre>
- *
+ * 
  * <pre> -P
  *  Generates Outlier/ExtremeValue pair for each numeric attribute in
  *  the range, not just a single indicator pair for all the attributes.
  *  (default: off)</pre>
- *
+ * 
  * <pre> -M
  *  Generates an additional attribute 'Offset' per Outlier/ExtremeValue
  *  pair that contains the multiplier that the value is off the median.
  *     value = median + 'multiplier' * IQR
  * Note: implicitely sets '-P'. (default: off)</pre>
- *
+ * 
+ * <pre> -output-debug-info
+ *  If set, filter is run in debug mode and
+ *  may output additional info to the console</pre>
+ * 
+ * <pre> -do-not-check-capabilities
+ *  If set, filter capabilities are not checked before filter is built
+ *  (use with caution).</pre>
+ * 
  <!-- options-end -->
  *
  * Thanks to Dale for a few brainstorming sessions.
@@ -101,13 +122,18 @@ public class InterquartileRangeSamp
   /** for serialization */
   protected static final long serialVersionUID = 3811630774543798261L;
 
-  protected Hashtable<Integer,TDoubleArrayList> ht = new Hashtable<Integer,TDoubleArrayList>();
+  protected Hashtable<Integer,TDoubleArrayList> m_AttValues = new Hashtable<Integer,TDoubleArrayList>();
 
-  protected Hashtable<Integer,List<IQRs>> m_iqrs = new Hashtable<Integer,List<IQRs>>();
+  protected Hashtable<Integer,List<IQRs>> m_IQRs = new Hashtable<Integer,List<IQRs>>();
 
-  protected int m_sample_size = 150;
+  /** the sample size to use. */
+  protected int m_SampleSize = getDefaultSampleSize();
 
-  protected int min_samples = 5;
+  /** the minimum number of samples. */
+  protected int m_MinSamples = getDefaultMinSamples();
+
+  /** the regular expression for attributes to skip. */
+  protected BaseRegExp m_IgnoredAttributes = getDefaultIgnoredAttributes();
 
   /**
    * Container class for the IQR values.
@@ -133,7 +159,7 @@ public class InterquartileRangeSamp
    */
   public String globalInfo() {
     return
-      "A sampling filter made to behave like S2 for detecting outliers and extreme values based on "
+      "A sampling filter for detecting outliers and extreme values based on "
 	+ "interquartile ranges. The filter skips the class attribute.\n\n"
 	+ "Outliers:\n"
 	+ "  Q3 + OF*IQR < x <= Q3 + EVF*IQR\n"
@@ -154,13 +180,176 @@ public class InterquartileRangeSamp
   }
 
   /**
+   * Returns the default sample size.
+   *
+   * @return 		the default
+   */
+  protected int getDefaultSampleSize() {
+    return 150;
+  }
+
+  /**
+   * Sets the sample size to use.
+   *
+   * @param value 	the size
+   */
+  public void setSampleSize(int value) {
+    if (value > 0)
+      m_SampleSize = value;
+    else
+      System.err.println("SampleSize must meet >0, provided: " + value);
+  }
+
+  /**
+   * Returns the sample size to use.
+   *
+   * @return 		the samples
+   */
+  public int getSampleSize() {
+    return m_SampleSize;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the gui
+   */
+  public String sampleSizeTipText() {
+    return "The sample size to use.";
+  }
+
+  /**
+   * Returns the default minimum number of samples.
+   *
+   * @return 		the default
+   */
+  protected int getDefaultMinSamples() {
+    return 5;
+  }
+
+  /**
+   * Sets the minimum number of samples that are required for calculating IQR stats.
+   *
+   * @param value 	the samples
+   */
+  public void setMinSamples(int value) {
+    if (value > 0)
+      m_MinSamples = value;
+    else
+      System.err.println("MinSamples must meet >0, provided: " + value);
+  }
+
+  /**
+   * Returns the minimum number of samples that are required for calculating IQR stats.
+   *
+   * @return 		the samples
+   */
+  public int getMinSamples() {
+    return m_MinSamples;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the gui
+   */
+  public String minSamplesTipText() {
+    return "The minimum number of samples that are required for calculating IQR stats.";
+  }
+
+  /**
+   * Returns the default regular expression for ignored/skipped attributes.
+   *
+   * @return 		the default
+   */
+  protected BaseRegExp getDefaultIgnoredAttributes() {
+    return new BaseRegExp("^.*_id$");
+  }
+
+  /**
+   * Sets the regular expression for ignored/skipped attributes.
+   *
+   * @param value 	the regexp
+   */
+  public void setIgnoredAttributes(BaseRegExp value) {
+    m_IgnoredAttributes = value;
+  }
+
+  /**
+   * Returns the regular expression for ignored/skipped attributes.
+   *
+   * @return 		the regexp
+   */
+  public BaseRegExp getIgnoredAttributes() {
+    return m_IgnoredAttributes;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the gui
+   */
+  public String ignoredAttributesTipText() {
+    return "The regular expression for attributes to ignore/skip.";
+  }
+
+  /**
+   * Returns an enumeration describing the available options.
+   *
+   * @return an enumeration of all the available options.
+   */
+  @Override
+  public Enumeration listOptions() {
+    Vector result = new Vector();
+    WekaOptionUtils.addOption(result, sampleSizeTipText(), "" + getDefaultSampleSize(), "sample-size");
+    WekaOptionUtils.addOption(result, minSamplesTipText(), "" + getDefaultMinSamples(), "min-samples");
+    WekaOptionUtils.addOption(result, ignoredAttributesTipText(), "" + getDefaultIgnoredAttributes(), "ignored-attributes");
+    WekaOptionUtils.add(result, super.listOptions());
+    return WekaOptionUtils.toEnumeration(result);
+  }
+
+  /**
+   * Sets the OptionHandler's options using the given list. All options
+   * will be set (or reset) during this call (i.e. incremental setting
+   * of options is not possible).
+   *
+   * @param options the list of options as an array of strings
+   * @throws Exception if an option is not supported
+   */
+  @Override
+  public void setOptions(String[] options) throws Exception {
+    setSampleSize(WekaOptionUtils.parse(options, "sample-size", getDefaultSampleSize()));
+    setMinSamples(WekaOptionUtils.parse(options, "min-samples", getDefaultMinSamples()));
+    setIgnoredAttributes(new BaseRegExp(WekaOptionUtils.parse(options, "ignored-attributes", getDefaultIgnoredAttributes().getValue())));
+    super.setOptions(options);
+  }
+
+  /**
+   * Gets the current option settings for the OptionHandler.
+   *
+   * @return the list of current option settings as an array of strings
+   */
+  @Override
+  public String[] getOptions() {
+    List<String> result = new ArrayList<>();
+    WekaOptionUtils.add(result, "sample-size", getSampleSize());
+    WekaOptionUtils.add(result, "min-samples", getMinSamples());
+    WekaOptionUtils.add(result, "ignored-attributes", getIgnoredAttributes().getValue());
+    WekaOptionUtils.add(result, super.getOptions());
+    return WekaOptionUtils.toArray(result);
+  }
+
+  /**
    * Calculates and adds the IQR stats for this key.
    *
    * @param key		the key for the stats
    * @param v		the values
    */
   protected void addIQR(Integer key, TDoubleArrayList v) {
-    if (v.size() >= min_samples) {
+    if (v.size() >= m_MinSamples) {
       double[] arr = v.toArray();
       Arrays.sort(arr);
       double q3val = valueAtPct(arr, 0.75);
@@ -168,10 +357,10 @@ public class InterquartileRangeSamp
       double med   = valueAtPct(arr, 0.5);
       double d     = arr[arr.length-1];
       IQRs is = new IQRs(q1val, q3val, d, med);
-      List<IQRs> viqr = m_iqrs.get(key);
+      List<IQRs> viqr = m_IQRs.get(key);
       if (viqr == null) {
 	viqr = new ArrayList<IQRs>();
-	m_iqrs.put(key,viqr);
+	m_IQRs.put(key,viqr);
       }
       viqr.add(is);
     }
@@ -203,10 +392,10 @@ public class InterquartileRangeSamp
   }
 
   protected void clearRemainder() {
-    for (Integer key: ht.keySet()) {
-      List<IQRs> viqr = m_iqrs.get(key);
+    for (Integer key: m_AttValues.keySet()) {
+      List<IQRs> viqr = m_IQRs.get(key);
       if (viqr == null) { //nothing there, so lets add this remainder
-	TDoubleArrayList v = ht.get(key);
+	TDoubleArrayList v = m_AttValues.get(key);
 	addIQR(key, v);
       }
     }
@@ -222,6 +411,7 @@ public class InterquartileRangeSamp
     double	q1;
     double	q2;
     double	q3;
+    String	name;
 
     m_UpperExtremeValue = new double[m_AttributeIndices.length];
     m_UpperOutlier      = new double[m_AttributeIndices.length];
@@ -231,18 +421,28 @@ public class InterquartileRangeSamp
     m_IQR               = new double[m_AttributeIndices.length];
 
     for (int i = 0; i < m_AttributeIndices.length; i++) {
+      name = instances.attribute(i).name();
       // non-numeric attribute?
-      if (m_AttributeIndices[i] == NON_NUMERIC)
+      if (m_AttributeIndices[i] == NON_NUMERIC) {
+	if (getDebug())
+	  System.out.println("Skipping non-numeric attribute: " + name);
 	continue;
+      }
+      // ignored attribute?
+      if (m_IgnoredAttributes.isMatch(name)) {
+	if (getDebug())
+	  System.out.println("Ignored attribute: " + name);
+	continue;
+      }
 
       // sort attribute data
       values = instances.attributeToDoubleArray(m_AttributeIndices[i]);
       TDoubleArrayList v = new TDoubleArrayList();
-      ht.put(i,v);
+      m_AttValues.put(i, v);
 
       for (int j = 0; j < values.length; j++) {
 	v.add(values[j]);
-	if (v.size() == m_sample_size) {
+	if (v.size() == m_SampleSize) {
 	  addIQR(i, v);
 	  v.clear();
 	}
@@ -250,9 +450,9 @@ public class InterquartileRangeSamp
       clearRemainder();
     }
 
-    for (Integer key: m_iqrs.keySet()) {
+    for (Integer key: m_IQRs.keySet()) {
       double dmax = Double.NEGATIVE_INFINITY;
-      List<IQRs> v = m_iqrs.get(key);
+      List<IQRs> v = m_IQRs.get(key);
       if (v.size() == 0)
 	continue;
       double[] q1s  = new double[v.size()];
@@ -291,8 +491,8 @@ public class InterquartileRangeSamp
       m_LowerExtremeValue[key] = q1 - getExtremeValuesFactor() * m_IQR[key];
     }
 
-    ht     = new Hashtable<>(); // clear current store
-    m_iqrs = new Hashtable<>(); // clear IQRs of samples
+    m_AttValues = new Hashtable<>(); // clear current store
+    m_IQRs = new Hashtable<>(); // clear IQRs of samples
   }
 
   /**
