fabaac770fa43087c2bf4ce2300f5351098ad68a
fracpete <fracpete@gmail.com>
2015-12-14 23:09:28 +0000

    simple overlay for text

---
+++ adams-core/src/main/java/adams/gui/visualization/sequence/TextOverlayPaintlet.java
@@ -0,0 +1,76 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * TextOverlayPaintlet.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, NZ
+ */
+
+package adams.gui.visualization.sequence;
+
+import adams.gui.event.PaintEvent.PaintMoment;
+
+/**
+ * Outputs the supplied text at the specified location.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class TextOverlayPaintlet
+  extends adams.gui.visualization.core.TextOverlayPaintlet
+  implements XYSequencePaintlet {
+
+  private static final long serialVersionUID = -5974722755106517158L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Outputs the supplied text at the specified location.";
+  }
+
+  /**
+   * Returns the XY sequence panel currently in use.
+   *
+   * @return		the panel in use
+   */
+  @Override
+  public XYSequencePanel getSequencePanel() {
+    return (XYSequencePanel) getPanel();
+  }
+
+  /**
+   * Returns when this paintlet is to be executed.
+   *
+   * @return		when this paintlet is to be executed
+   */
+  @Override
+  public PaintMoment getPaintMoment() {
+    return PaintMoment.GRID;
+  }
+
+  /**
+   * Returns a new instance of the hit detector to use.
+   *
+   * @return		always null
+   */
+  @Override
+  public AbstractXYSequencePointHitDetector newHitDetector() {
+    return null;
+  }
+}
