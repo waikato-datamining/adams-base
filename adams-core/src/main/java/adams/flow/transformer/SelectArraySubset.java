e89d2730376a232fde9b2cee9ecb963ee4342bdf
fracpete <fracpete@gmail.com>
2016-01-20 02:04:47 +0000

    SelectArraySubset transformer allows user to select subset of array elements to be forwarded

---
+++ adams-core/src/main/java/adams/flow/transformer/SelectArraySubset.java
@@ -0,0 +1,369 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * SelectArraySubset.java
+ * Copyright (C) 2016 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.transformer;
+
+import adams.core.QuickInfoHelper;
+import adams.flow.core.Token;
+import adams.flow.core.Unknown;
+import adams.gui.core.BaseList;
+import adams.gui.core.BasePanel;
+import com.googlecode.jfilechooserbookmarks.gui.BaseScrollPane;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.lang.reflect.Array;
+
+/**
+ <!-- globalinfo-start -->
+ * Allows the user to select a subset of the incoming array to be forwarded in the flow.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- flow-summary-start -->
+ * Input&#47;output:<br>
+ * - accepts:<br>
+ * &nbsp;&nbsp;&nbsp;java.lang.Object[]<br>
+ * - generates:<br>
+ * &nbsp;&nbsp;&nbsp;adams.flow.core.Unknown<br>
+ * <br><br>
+ <!-- flow-summary-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: SelectArraySubset
+ * </pre>
+ * 
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded 
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow gets stopped in case this actor encounters an error;
+ * &nbsp;&nbsp;&nbsp; useful for critical actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-short-title &lt;boolean&gt; (property: shortTitle)
+ * &nbsp;&nbsp;&nbsp;If enabled uses just the name for the title instead of the actor's full 
+ * &nbsp;&nbsp;&nbsp;name.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-width &lt;int&gt; (property: width)
+ * &nbsp;&nbsp;&nbsp;The width of the dialog.
+ * &nbsp;&nbsp;&nbsp;default: 600
+ * &nbsp;&nbsp;&nbsp;minimum: 1
+ * </pre>
+ * 
+ * <pre>-height &lt;int&gt; (property: height)
+ * &nbsp;&nbsp;&nbsp;The height of the dialog.
+ * &nbsp;&nbsp;&nbsp;default: 400
+ * &nbsp;&nbsp;&nbsp;minimum: 1
+ * </pre>
+ * 
+ * <pre>-x &lt;int&gt; (property: x)
+ * &nbsp;&nbsp;&nbsp;The X position of the dialog (&gt;=0: absolute, -1: left, -2: center, -3: right
+ * &nbsp;&nbsp;&nbsp;).
+ * &nbsp;&nbsp;&nbsp;default: -2
+ * &nbsp;&nbsp;&nbsp;minimum: -3
+ * </pre>
+ * 
+ * <pre>-y &lt;int&gt; (property: y)
+ * &nbsp;&nbsp;&nbsp;The Y position of the dialog (&gt;=0: absolute, -1: top, -2: center, -3: bottom
+ * &nbsp;&nbsp;&nbsp;).
+ * &nbsp;&nbsp;&nbsp;default: -2
+ * &nbsp;&nbsp;&nbsp;minimum: -3
+ * </pre>
+ * 
+ * <pre>-stop-if-canceled &lt;boolean&gt; (property: stopFlowIfCanceled)
+ * &nbsp;&nbsp;&nbsp;If enabled, the flow gets stopped in case the user cancels the dialog.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-custom-stop-message &lt;java.lang.String&gt; (property: customStopMessage)
+ * &nbsp;&nbsp;&nbsp;The custom stop message to use in case a user cancelation stops the flow 
+ * &nbsp;&nbsp;&nbsp;(default is the full name of the actor)
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-message &lt;java.lang.String&gt; (property: message)
+ * &nbsp;&nbsp;&nbsp;The message to prompt the user with; variables get expanded prior to prompting 
+ * &nbsp;&nbsp;&nbsp;user.
+ * &nbsp;&nbsp;&nbsp;default: Please make your selection
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class SelectArraySubset
+  extends AbstractInteractiveTransformerDialog {
+
+  private static final long serialVersionUID = -7861621358380784108L;
+
+  /** the message for the user. */
+  protected String m_Message;
+
+  /** the list model to use. */
+  protected DefaultListModel<Object> m_ListModel;
+
+  /** the list in use. */
+  protected BaseList m_List;
+
+  /** whether the data was accepted. */
+  protected boolean m_Accepted;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return
+      "Allows the user to select a subset of the incoming array to be "
+	+ "forwarded in the flow.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+	    "message", "message",
+	    "Please make your selection");
+  }
+
+  /**
+   * Returns a quick info about the actor, which will be displayed in the GUI.
+   *
+   * @return		null if no info available, otherwise short string
+   */
+  @Override
+  public String getQuickInfo() {
+    String	result;
+
+    result  = super.getQuickInfo();
+    result += QuickInfoHelper.toString(this, "message", m_Message, ", message: ");
+
+    return result;
+  }
+
+  /**
+   * Returns the default width for the dialog.
+   *
+   * @return		the default width
+   */
+  @Override
+  protected int getDefaultWidth() {
+    return 600;
+  }
+
+  /**
+   * Returns the default height for the dialog.
+   *
+   * @return		the default height
+   */
+  @Override
+  protected int getDefaultHeight() {
+    return 400;
+  }
+
+  /**
+   * Returns the default X position for the dialog.
+   *
+   * @return		the default X position
+   */
+  @Override
+  protected int getDefaultX() {
+    return -2;
+  }
+
+  /**
+   * Returns the default Y position for the dialog.
+   *
+   * @return		the default Y position
+   */
+  @Override
+  protected int getDefaultY() {
+    return -2;
+  }
+
+  /**
+   * Sets the message to prompt the user with.
+   *
+   * @param value	the message
+   */
+  public void setMessage(String value) {
+    m_Message = value;
+    reset();
+  }
+
+  /**
+   * Returns the message the user is prompted with.
+   *
+   * @return 		the message
+   */
+  public String getMessage() {
+    return m_Message;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return		tip text for this property suitable for
+   *             	displaying in the GUI or for listing the options.
+   */
+  public String messageTipText() {
+    return "The message to prompt the user with; variables get expanded prior to prompting user.";
+  }
+
+  /**
+   * Returns the class that the consumer accepts.
+   *
+   * @return		the Class of objects that can be processed
+   */
+  @Override
+  public Class[] accepts() {
+    return new Class[]{Object[].class};
+  }
+
+  /**
+   * Returns the class of objects that it generates.
+   *
+   * @return		the Class of the generated tokens
+   */
+  @Override
+  public Class[] generates() {
+    return new Class[]{Unknown.class};
+  }
+
+  /**
+   * Clears the content of the panel.
+   */
+  @Override
+  public void clearPanel() {
+    m_ListModel.clear();
+  }
+
+  /**
+   * Creates the panel to display in the dialog.
+   *
+   * @return		the panel
+   */
+  @Override
+  protected BasePanel newPanel() {
+    BasePanel		result;
+    JLabel		label;
+    JPanel		panel;
+    final JButton 	buttonOK;
+    final JButton	buttonCancel;
+
+    result = new BasePanel(new BorderLayout());
+
+    m_ListModel = new DefaultListModel<>();
+    m_List      = new BaseList(m_ListModel);
+    result.add(new BaseScrollPane(m_List), BorderLayout.CENTER);
+
+    label = new JLabel(getVariables().expand(m_Message));
+    panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    panel.add(label);
+    result.add(panel, BorderLayout.NORTH);
+
+    panel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+    result.add(panel, BorderLayout.SOUTH);
+
+    buttonOK = new JButton("OK");
+    buttonOK.setMnemonic('O');
+    buttonOK.addActionListener((ActionEvent e) -> {
+      m_Accepted = true;
+      m_Dialog.setVisible(false);
+    });
+    panel.add(buttonOK);
+
+    buttonCancel = new JButton("Cancel");
+    buttonCancel.setMnemonic('C');
+    buttonCancel.addActionListener((ActionEvent e) -> {
+      m_Accepted = false;
+      m_Dialog.setVisible(false);
+    });
+    panel.add(buttonCancel);
+
+    return result;
+  }
+
+  /**
+   * Performs the interaction with the user.
+   *
+   * @return		true if successfully interacted
+   */
+  @Override
+  public boolean doInteract() {
+    Object	array;
+    int[]	indices;
+    int		i;
+
+    m_ListModel.clear();
+    array = m_InputToken.getPayload();
+    for (i = 0; i < Array.getLength(array); i++)
+      m_ListModel.addElement(Array.get(array, i));
+
+    m_Accepted = false;
+    m_Dialog.setVisible(true);
+
+    if (m_Accepted) {
+      indices = m_List.getSelectedIndices();
+      array   = Array.newInstance(m_InputToken.getPayload().getClass().getComponentType(), indices.length);
+      for (i = 0; i < indices.length; i++)
+	Array.set(array, i, m_ListModel.get(indices[i]));
+      m_OutputToken = new Token(array);
+    }
+
+    return m_Accepted;
+  }
+}
