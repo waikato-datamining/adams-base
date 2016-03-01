b76fcd76f6cfa6edb97b24bc9b8365282315960e
fracpete <fracpete@gmail.com>
2016-03-01 02:43:09 +0000

    simple "am alive" command

---
+++ adams-core/src/main/java/adams/scripting/command/basic/Ping.java
@@ -0,0 +1,133 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Ping.java
+ * Copyright (C) 2016 University of Waikato, Hamilton, NZ
+ */
+
+package adams.scripting.command.basic;
+
+import adams.core.Properties;
+import adams.core.net.InternetHelper;
+import adams.scripting.command.AbstractCommandWithResponse;
+
+import java.io.StringReader;
+
+/**
+ * Requests an 'am alive' signal from the remote host.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class Ping
+  extends AbstractCommandWithResponse {
+
+  private static final long serialVersionUID = -3350680106789169314L;
+
+  /** the host. */
+  protected String m_Host;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Requests an 'am alive' signal from the remote host.";
+  }
+
+  @Override
+  protected void initialize() {
+    super.initialize();
+
+    m_Host = "";
+  }
+
+  /**
+   * Ignored.
+   *
+   * @param value	the payload
+   */
+  @Override
+  public void setRequestPayload(byte[] value) {
+  }
+
+  /**
+   * Always zero-length array.
+   *
+   * @return		the payload
+   */
+  @Override
+  public byte[] getRequestPayload() {
+    return new byte[0];
+  }
+
+  /**
+   * Sets the payload for the response.
+   *
+   * @param value	the payload
+   */
+  @Override
+  public void setResponsePayload(byte[] value) {
+    Properties		props;
+    StringReader	reader;
+    byte[]		decomp;
+
+    if (value.length == 0) {
+      m_Host = "";
+      return;
+    }
+
+    m_Host = new String(value);
+  }
+
+  /**
+   * Returns the payload of the response, if any.
+   *
+   * @return		the payload
+   */
+  @Override
+  public byte[] getResponsePayload() {
+    return m_Host.getBytes();
+  }
+
+  /**
+   * Hook method for preparing the response payload,
+   */
+  @Override
+  protected void prepareResponsePayload() {
+    m_Host = InternetHelper.getLocalHostIP() + "/" + InternetHelper.getLocalHostName();
+  }
+
+  /**
+   * Returns the host.
+   *
+   * @return		the host
+   */
+  public String getHost() {
+    return m_Host;
+  }
+
+  /**
+   * Returns a short description of the command.
+   *
+   * @return		the description
+   */
+  public String toString() {
+    return m_Host;
+  }
+}
