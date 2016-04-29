d69f95e460a634118df1fa0ecab83977e626084a
fracpete <fracpete@gmail.com>
2016-04-29 23:59:25 +0000

    DeleteReportValue transformer allows removal of fields from a report/reporthandler

---
+++ adams-core/src/main/java/adams/flow/transformer/AbstractDeleteReportValue.java
@@ -0,0 +1,250 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * AbstractDeleteReportValue.java
+ * Copyright (C) 2016 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.flow.transformer;
+
+import adams.core.QuickInfoHelper;
+import adams.core.base.BaseRegExp;
+import adams.data.report.AbstractField;
+import adams.data.report.Report;
+import adams.data.report.ReportHandler;
+import adams.flow.core.Token;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Ancestor for transformers that delete values from a report.
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public abstract class AbstractDeleteReportValue
+  extends AbstractTransformer {
+
+  /** for serialization. */
+  private static final long serialVersionUID = -6237324482439105653L;
+
+  /**
+   * How to select fields.
+   *
+   * @author  fracpete (fracpete at waikato dot ac dot nz)
+   * @version $Revision$
+   */
+  public enum MatchType {
+    REGEXP,
+    FIELDS
+  }
+
+  /** how to match fields. */
+  protected MatchType m_Type;
+
+  /** the regular expression for matching field names. */
+  protected BaseRegExp m_RegExp;
+
+  /** the field(s) to delete from the report. */
+  protected AbstractField[] m_Fields;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public abstract String globalInfo();
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+	    "type", "type",
+	    MatchType.FIELDS);
+
+    m_OptionManager.add(
+	    "field", "fields",
+	    getDefaultFields());
+
+    m_OptionManager.add(
+	    "regexp", "regExp",
+	    new BaseRegExp());
+  }
+
+  /**
+   * Returns a quick info about the actor, which will be displayed in the GUI.
+   *
+   * @return		null if no info available, otherwise short string
+   */
+  @Override
+  public String getQuickInfo() {
+    String	result;
+
+    result  = QuickInfoHelper.toString(this, "type", m_Type, "type: ");
+    result += QuickInfoHelper.toString(this, "fields", m_Fields, ", fields: ");
+    result += QuickInfoHelper.toString(this, "regExp", m_RegExp, ", regexp: ");
+
+    return result;
+  }
+
+  /**
+   * Sets the match type.
+   *
+   * @param value	the type
+   */
+  public void setType(MatchType value) {
+    m_Type = value;
+    reset();
+  }
+
+  /**
+   * Returns the match type.
+   *
+   * @return		the type
+   */
+  public MatchType getType() {
+    return m_Type;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String typeTipText() {
+    return "How to match the fields.";
+  }
+
+  /**
+   * Returns the default fields for the option.
+   *
+   * @return		the default fields
+   */
+  protected abstract AbstractField[] getDefaultFields();
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public abstract String fieldsTipText();
+
+  /**
+   * Sets the regular expression to use for matching the field names.
+   *
+   * @param value	the expression
+   */
+  public void setRegExp(BaseRegExp value) {
+    m_RegExp = value;
+    reset();
+  }
+
+  /**
+   * Returns the regular expression to use for matching the field names.
+   *
+   * @return		the expression
+   */
+  public BaseRegExp getRegExp() {
+    return m_RegExp;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String regExpTipText() {
+    return "The regular expression for matching the field names.";
+  }
+
+  /**
+   * Returns the class that the consumer accepts.
+   *
+   * @return		the accepted classes
+   */
+  public abstract Class[] accepts();
+
+  /**
+   * Returns the class of objects that it generates.
+   *
+   * @return		the generated classes
+   */
+  public abstract Class[] generates();
+
+  /**
+   * Executes the flow item.
+   *
+   * @return		null if everything is fine, otherwise error message
+   */
+  @Override
+  protected String doExecute() {
+    String			result;
+    Report			report;
+    List<AbstractField>		fields;
+
+    result = null;
+
+    if (m_InputToken.getPayload() instanceof ReportHandler)
+      report = ((ReportHandler) m_InputToken.getPayload()).getReport();
+    else
+      report = (Report) m_InputToken.getPayload();
+
+    try {
+      if (report != null) {
+	switch (m_Type) {
+	  case REGEXP:
+	    fields = report.getFields();
+	    break;
+	  case FIELDS:
+	    fields = new ArrayList<>(Arrays.asList(m_Fields));
+	    break;
+	  default:
+	    throw new IllegalStateException("Unhandled match type: " + m_Type);
+	}
+
+	// remove fields
+	for (AbstractField field: fields) {
+	  if (m_RegExp.isMatch(field.getName())) {
+	    if (isLoggingEnabled())
+	      getLogger().info("Deleting field: " + field);
+	    report.removeValue(field);
+	  }
+	}
+      }
+      else {
+	if (isLoggingEnabled())
+	  getLogger().info("No report available: " + m_InputToken);
+      }
+    }
+    catch (Exception e) {
+      result = handleException("Failed to delete report values!", e);
+    }
+
+    m_OutputToken = new Token(m_InputToken.getPayload());
+
+    return result;
+  }
+}
---
+++ adams-core/src/main/java/adams/flow/transformer/DeleteReportValue.java
@@ -0,0 +1,165 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * DeleteReportValue.java
+ * Copyright (C) 2016 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.transformer;
+
+import adams.data.report.AbstractField;
+import adams.data.report.Field;
+import adams.data.report.Report;
+import adams.data.report.ReportHandler;
+import adams.flow.core.Unknown;
+
+/**
+ <!-- globalinfo-start -->
+ * Deletes values from a report. Can either use a regular expression to match against the names or specific fields.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- flow-summary-start -->
+ * Input&#47;output:<br>
+ * - accepts:<br>
+ * &nbsp;&nbsp;&nbsp;adams.data.report.Report<br>
+ * &nbsp;&nbsp;&nbsp;adams.data.report.ReportHandler<br>
+ * - generates:<br>
+ * &nbsp;&nbsp;&nbsp;adams.flow.core.Unknown<br>
+ * <br><br>
+ <!-- flow-summary-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ *
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: DeleteReportValue
+ * </pre>
+ *
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default:
+ * </pre>
+ *
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow gets stopped in case this actor encounters an error;
+ * &nbsp;&nbsp;&nbsp; useful for critical actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-field &lt;adams.data.report.Field&gt; [-field ...] (property: fields)
+ * &nbsp;&nbsp;&nbsp;The fields to remove from the report.
+ * &nbsp;&nbsp;&nbsp;default:
+ * </pre>
+ *
+ <!-- options-end -->
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class DeleteReportValue
+  extends AbstractDeleteReportValue {
+
+  private static final long serialVersionUID = -2551776226766445092L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return
+      "Deletes values from a report. Can either use a regular expression "
+	+ "to match against the names or specific fields.";
+  }
+
+  /**
+   * Returns the default fields for the option.
+   *
+   * @return		the default fields
+   */
+  @Override
+  protected AbstractField[] getDefaultFields() {
+    return new Field[0];
+  }
+
+  /**
+   * Sets the fields to delete from the report.
+   *
+   * @param value	the fields
+   */
+  public void setFields(Field[] value) {
+    m_Fields = value;
+    reset();
+  }
+
+  /**
+   * Returns the fields to delete from the report.
+   *
+   * @return		the fields
+   */
+  public Field[] getFields() {
+    return (Field[]) m_Fields;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  @Override
+  public String fieldsTipText() {
+    return "The fields to remove from the report.";
+  }
+
+  /**
+   * Returns the class that the consumer accepts.
+   *
+   * @return		the accepted classes
+   */
+  @Override
+  public Class[] accepts() {
+    return new Class[]{Report.class, ReportHandler.class};
+  }
+
+  /**
+   * Returns the class of objects that it generates.
+   *
+   * @return		the generated classes
+   */
+  @Override
+  public Class[] generates() {
+    return new Class[]{Unknown.class};
+  }
+}
