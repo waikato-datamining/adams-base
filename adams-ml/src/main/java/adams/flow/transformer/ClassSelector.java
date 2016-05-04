af4dc3139a84590bca1dcdee9386b07f93e1c778
fracpete <fracpete@gmail.com>
2016-05-04 23:46:02 +0000

    fixed setting of class attributes

--- adams-ml/src/main/java/adams/flow/transformer/ClassSelector.java
+++ adams-ml/src/main/java/adams/flow/transformer/ClassSelector.java
@@ -15,7 +15,7 @@
 
 /*
  * ClassSelector.java
- * Copyright (C) 2013-2014 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2013-2016 University of Waikato, Hamilton, New Zealand
  */
 
 package adams.flow.transformer;
@@ -304,19 +304,16 @@ public class ClassSelector
     else
       data = new DefaultDataset(sheet);
     
-    indices = data.getClassAttributeIndices();
     if (m_Unset) {
       data.removeClassAttributes();
     }
     else {
       if (m_Override)
 	data.removeClassAttributes();
-      if ((indices.length == 0) || ((indices.length == 0) && m_Override)) {
-	m_ClassIndex.setData(data);
-	indices = m_ClassIndex.getIntIndices();
-	for (int index: indices)
-	  data.setClassAttribute(index, true);
-      }
+      m_ClassIndex.setData(data);
+      indices = m_ClassIndex.getIntIndices();
+      for (int index: indices)
+        data.setClassAttribute(index, true);
     }
 
     m_OutputToken = new Token(data);
