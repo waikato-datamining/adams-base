02a44e822c85340f6b1938736047c854b0b69148
fracpete <fracpete@gmail.com>
2016-06-16 04:48:00 +0000

    updated unit test for GetMapValue transformer

--- adams-core/src/test/java/adams/flow/transformer/GetMapValueTest.java
+++ adams-core/src/test/java/adams/flow/transformer/GetMapValueTest.java
@@ -20,19 +20,34 @@
 
 package adams.flow.transformer;
 
+import adams.core.base.BaseString;
+import adams.core.io.PlaceholderFile;
+import adams.core.option.AbstractArgumentOption;
+import adams.data.conversion.StringToDouble;
 import adams.env.Environment;
 import adams.flow.AbstractFlowTest;
 import adams.flow.control.Flow;
+import adams.flow.core.AbstractActor;
 import adams.flow.core.Actor;
+import adams.flow.core.CallableActorReference;
+import adams.flow.execution.NullListener;
+import adams.flow.sink.DumpFile;
+import adams.flow.source.NewMap;
+import adams.flow.source.SequenceSource;
+import adams.flow.source.StringConstants;
+import adams.flow.standalone.CallableActors;
+import adams.test.TmpFile;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Tests the GetMapValue actor.
- * <br>
- * NB: Dummy test.
+ * Test for GetMapValue actor.
  *
- * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @author fracpete
+ * @author adams.core.option.FlowJUnitTestProducer (code generator)
  * @version $Revision$
  */
 public class GetMapValueTest
@@ -48,15 +63,39 @@ public class GetMapValueTest
   }
 
   /**
-   * Used to create an instance of a specific actor.
+   * Called by JUnit before each test method.
    *
-   * @return a suitably configured <code>Actor</code> value
+   * @throws Exception 	if an error occurs.
    */
-  public Actor getActor() {
-    return new Flow();
+  protected void setUp() throws Exception {
+    super.setUp();
+    
+    m_TestHelper.deleteFileFromTmp("dumpfile.txt");
   }
 
   /**
+   * Called by JUnit after each test method.
+   *
+   * @throws Exception	if tear-down fails
+   */
+  protected void tearDown() throws Exception {
+    m_TestHelper.deleteFileFromTmp("dumpfile.txt");
+    
+    super.tearDown();
+  }
+
+  /**
+   * Performs a regression test, comparing against previously generated output.
+   */
+  public void testRegression() {
+    performRegressionTest(
+        new TmpFile[]{
+          new TmpFile("dumpfile.txt")
+        });
+  }
+
+  /**
+   * 
    * Returns a test suite.
    *
    * @return		the test suite
@@ -66,12 +105,101 @@ public class GetMapValueTest
   }
 
   /**
+   * Used to create an instance of a specific actor.
+   *
+   * @return a suitably configured <code>AbstractActor</code> value
+   */
+  public AbstractActor getActor() {
+    AbstractArgumentOption    argOption;
+    
+    Flow flow = new Flow();
+    
+    try {
+      List<Actor> actors = new ArrayList<>();
+
+      // Flow.CallableActors
+      CallableActors callableactors = new CallableActors();
+      List<Actor> actors2 = new ArrayList<>();
+
+      // Flow.CallableActors.SequenceSource
+      SequenceSource sequencesource = new SequenceSource();
+      List<Actor> actors3 = new ArrayList<>();
+
+      // Flow.CallableActors.SequenceSource.StringConstants
+      StringConstants stringconstants = new StringConstants();
+      argOption = (AbstractArgumentOption) stringconstants.getOptionManager().findByProperty("strings");
+      List<BaseString> strings = new ArrayList<>();
+      strings.add((BaseString) argOption.valueOf("11.1"));
+      stringconstants.setStrings(strings.toArray(new BaseString[0]));
+      actors3.add(stringconstants);
+
+      // Flow.CallableActors.SequenceSource.Convert
+      Convert convert = new Convert();
+      StringToDouble stringtodouble = new StringToDouble();
+      convert.setConversion(stringtodouble);
+
+      actors3.add(convert);
+      sequencesource.setActors(actors3.toArray(new Actor[0]));
+
+      actors2.add(sequencesource);
+      callableactors.setActors(actors2.toArray(new Actor[0]));
+
+      actors.add(callableactors);
+
+      // Flow.NewMap
+      NewMap newmap = new NewMap();
+      actors.add(newmap);
+
+      // Flow.SetMapValue
+      SetMapValue setmapvalue = new SetMapValue();
+      argOption = (AbstractArgumentOption) setmapvalue.getOptionManager().findByProperty("key");
+      setmapvalue.setKey((String) argOption.valueOf("hello"));
+      argOption = (AbstractArgumentOption) setmapvalue.getOptionManager().findByProperty("value");
+      setmapvalue.setValue((String) argOption.valueOf("world"));
+      actors.add(setmapvalue);
+
+      // Flow.SetMapValue-1
+      SetMapValue setmapvalue2 = new SetMapValue();
+      argOption = (AbstractArgumentOption) setmapvalue2.getOptionManager().findByProperty("name");
+      setmapvalue2.setName((String) argOption.valueOf("SetMapValue-1"));
+      argOption = (AbstractArgumentOption) setmapvalue2.getOptionManager().findByProperty("key");
+      setmapvalue2.setKey((String) argOption.valueOf("number"));
+      argOption = (AbstractArgumentOption) setmapvalue2.getOptionManager().findByProperty("source");
+      setmapvalue2.setSource((CallableActorReference) argOption.valueOf("SequenceSource"));
+      actors.add(setmapvalue2);
+
+      // Flow.GetMapValue
+      GetMapValue getmapvalue = new GetMapValue();
+      argOption = (AbstractArgumentOption) getmapvalue.getOptionManager().findByProperty("key");
+      getmapvalue.setKey((String) argOption.valueOf("number"));
+      actors.add(getmapvalue);
+
+      // Flow.DumpFile
+      DumpFile dumpfile = new DumpFile();
+      argOption = (AbstractArgumentOption) dumpfile.getOptionManager().findByProperty("outputFile");
+      dumpfile.setOutputFile((PlaceholderFile) argOption.valueOf("${TMP}/dumpfile.txt"));
+      actors.add(dumpfile);
+      flow.setActors(actors.toArray(new Actor[0]));
+
+      NullListener nulllistener = new NullListener();
+      flow.setFlowExecutionListener(nulllistener);
+
+    }
+    catch (Exception e) {
+      fail("Failed to set up actor: " + e);
+    }
+    
+    return flow;
+  }
+
+  /**
    * Runs the test from commandline.
    *
    * @param args	ignored
    */
   public static void main(String[] args) {
-    Environment.setEnvironmentClass(Environment.class);
+    Environment.setEnvironmentClass(adams.env.Environment.class);
     runTest(suite());
   }
 }
+
