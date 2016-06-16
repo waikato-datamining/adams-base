ce6ad10e227cd7835451754fbf94e0181c6e5723
fracpete <fracpete@gmail.com>
2016-06-16 04:06:11 +0000

    added NewMap source for creating now java.util.Map implementation classes

---
+++ adams-core/src/main/java/adams/flow/source/NewMap.java
@@ -0,0 +1,194 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * NewMap.java
+ * Copyright (C) 2016 University of Waikato, Hamilton, New Zealand
+ */
+package adams.flow.source;
+
+import adams.core.ClassLocator;
+import adams.core.QuickInfoHelper;
+import adams.core.base.BaseClassname;
+import adams.flow.core.Token;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ <!-- globalinfo-start -->
+ * Generates a new java.util.Map object using the specified implementation class.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- flow-summary-start -->
+ * Input&#47;output:<br>
+ * - generates:<br>
+ * &nbsp;&nbsp;&nbsp;java.util.HashMap<br>
+ * <br><br>
+ <!-- flow-summary-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: NewMap
+ * </pre>
+ * 
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded 
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow gets stopped in case this actor encounters an error;
+ * &nbsp;&nbsp;&nbsp; useful for critical actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing 
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-map-class &lt;adams.core.base.BaseClassname&gt; (property: mapClass)
+ * &nbsp;&nbsp;&nbsp;The class to use for the map (must implemented java.util.Map interface).
+ * &nbsp;&nbsp;&nbsp;default: java.util.HashMap
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class NewMap
+  extends AbstractSimpleSource {
+
+  /** for serialization. */
+  private static final long serialVersionUID = 7272049518765623563L;
+
+  /** the class for the map. */
+  protected BaseClassname m_MapClass;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return 
+	"Generates a new " + Map.class.getName() + " object using the specified implementation class.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+	    "map-class", "mapClass",
+	    new BaseClassname(HashMap.class));
+  }
+  
+  /**
+   * Returns a quick info about the actor, which will be displayed in the GUI.
+   *
+   * @return		null if no info available, otherwise short string
+   */
+  @Override
+  public String getQuickInfo() {
+    return QuickInfoHelper.toString(this, "mapClass", m_MapClass, "Class: ");
+  }
+
+  /**
+   * Sets the map class.
+   *
+   * @param value	the map class
+   */
+  public void setMapClass(BaseClassname value) {
+    if (ClassLocator.hasInterface(Map.class, value.classValue())) {
+      m_MapClass = value;
+      reset();
+    }
+    else {
+      getLogger().warning("Map class must implemented " + Map.class.getName() + " interface!");
+    }
+  }
+
+  /**
+   * Returns the class for the array.
+   *
+   * @return		the classname, empty string if class of first element
+   * 			is used
+   */
+  public BaseClassname getMapClass() {
+    return m_MapClass;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String mapClassTipText() {
+    return "The class to use for the map (must implemented " + Map.class.getName() + " interface).";
+  }
+
+  /**
+   * Returns the class of objects that it generates.
+   *
+   * @return		the Class of the generated tokens
+   */
+  @Override
+  public Class[] generates() {
+    return new Class[]{m_MapClass.classValue()};
+  }
+
+  /**
+   * Executes the flow item.
+   *
+   * @return		null if everything is fine, otherwise error message
+   */
+  @Override
+  protected String doExecute() {
+    String	result;
+
+    result = null;
+
+    try {
+      m_OutputToken = new Token(m_MapClass.classValue().newInstance());
+    }
+    catch (Exception e) {
+      result = handleException("Failed to create map instance!", e);
+    }
+
+    return result;
+  }
+}
---
+++ adams-core/src/test/java/adams/flow/source/NewMapTest.java
@@ -0,0 +1,142 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * NewMapTest.java
+ * Copyright (C) 2016 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.flow.source;
+
+import adams.core.option.AbstractArgumentOption;
+import adams.env.Environment;
+import adams.flow.AbstractFlowTest;
+import adams.flow.control.Flow;
+import adams.flow.core.Actor;
+import adams.test.TmpFile;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Test for NewMap actor.
+ *
+ * @author fracpete
+ * @author adams.core.option.FlowJUnitTestProducer (code generator)
+ * @version $Revision$
+ */
+public class NewMapTest
+  extends AbstractFlowTest {
+
+  /**
+   * Initializes the test.
+   *
+   * @param name	the name of the test
+   */
+  public NewMapTest(String name) {
+    super(name);
+  }
+
+  /**
+   * Called by JUnit before each test method.
+   *
+   * @throws Exception 	if an error occurs.
+   */
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    
+    m_TestHelper.deleteFileFromTmp("dumpfile.txt");
+  }
+
+  /**
+   * Called by JUnit after each test method.
+   *
+   * @throws Exception	if tear-down fails
+   */
+  @Override
+  protected void tearDown() throws Exception {
+    m_TestHelper.deleteFileFromTmp("dumpfile.txt");
+    
+    super.tearDown();
+  }
+
+  /**
+   * Performs a regression test, comparing against previously generated output.
+   */
+  public void testRegression() {
+    performRegressionTest(
+        new TmpFile[]{
+          new TmpFile("dumpfile.txt")
+        });
+  }
+
+  /**
+   * 
+   * Returns a test suite.
+   *
+   * @return		the test suite
+   */
+  public static Test suite() {
+    return new TestSuite(NewMapTest.class);
+  }
+
+  /**
+   * Used to create an instance of a specific actor.
+   *
+   * @return a suitably configured <code>Actor</code> value
+   */
+  @Override
+  public Actor getActor() {
+    AbstractArgumentOption    argOption;
+    
+    Flow flow = new Flow();
+    
+    try {
+      argOption = (AbstractArgumentOption) flow.getOptionManager().findByProperty("actors");
+      Actor[] tmp1 = new Actor[3];
+      NewMap tmp2 = new NewMap();
+      tmp1[0] = tmp2;
+
+      adams.flow.transformer.Convert tmp14 = new adams.flow.transformer.Convert();
+      argOption = (AbstractArgumentOption) tmp14.getOptionManager().findByProperty("conversion");
+      adams.data.conversion.AnyToString tmp16 = new adams.data.conversion.AnyToString();
+      tmp14.setConversion(tmp16);
+      tmp1[1] = tmp14;
+
+      adams.flow.sink.DumpFile tmp17 = new adams.flow.sink.DumpFile();
+      argOption = (AbstractArgumentOption) tmp17.getOptionManager().findByProperty("outputFile");
+      tmp17.setOutputFile((adams.core.io.PlaceholderFile) argOption.valueOf("${TMP}/dumpfile.txt"));
+      tmp1[2] = tmp17;
+
+      flow.setActors(tmp1);
+
+    }
+    catch (Exception e) {
+      fail("Failed to set up actor: " + e);
+    }
+    
+    return flow;
+  }
+
+  /**
+   * Runs the test from commandline.
+   *
+   * @param args	ignored
+   */
+  public static void main(String[] args) {
+    Environment.setEnvironmentClass(Environment.class);
+    runTest(suite());
+  }
+}
