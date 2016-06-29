42f3cade7939b233836d2fd35923a9b5e3b6d084
fracpete <fracpete@gmail.com>
2016-06-29 08:31:30 +0000

    updated use of DebugOptionProducer interface

--- adams-core/src/main/java/adams/core/option/DebugNestedProducer.java
+++ adams-core/src/main/java/adams/core/option/DebugNestedProducer.java
@@ -15,7 +15,7 @@
 
 /**
  * DebugNestedProducer.java
- * Copyright (C) 2011-2014 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2011-2016 University of Waikato, Hamilton, New Zealand
  */
 package adams.core.option;
 
@@ -43,7 +43,7 @@ import java.util.List;
  */
 public class DebugNestedProducer
   extends NestedProducer
-  implements DebugOptionProducer {
+  implements DebugOptionProducer<List,List> {
 
   /** for serialization. */
   private static final long serialVersionUID = 931016182843089428L;
