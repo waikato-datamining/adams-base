8996f9219633660ce2098a30114c72db6e63687b
fracpete <fracpete@gmail.com>
2016-07-18 04:24:32 +0000

    added rename action (changing relation name)

---
+++ adams-weka/src/main/java/adams/gui/tools/wekainvestigator/tab/datatabactions/Rename.java
@@ -0,0 +1,76 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Rename.java
+ * Copyright (C) 2016 University of Waikato, Hamilton, NZ
+ */
+
+package adams.gui.tools.wekainvestigator.tab.datatabactions;
+
+import adams.gui.core.GUIHelper;
+import adams.gui.tools.wekainvestigator.data.DataContainer;
+
+import java.awt.event.ActionEvent;
+
+/**
+ * Renames the selected dataset.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class Rename
+  extends AbstractDataTabAction {
+
+  private static final long serialVersionUID = -8374323161691034031L;
+
+  /**
+   * Instantiates the action.
+   */
+  public Rename() {
+    super();
+    setName("Rename");
+    setIcon("question.png");
+  }
+
+  /**
+   * Invoked when an action occurs.
+   *
+   * @param e		the event
+   */
+  @Override
+  protected void doActionPerformed(ActionEvent e) {
+    DataContainer 	cont;
+    String  		newName;
+
+    cont = getSelectedData()[0];
+    logMessage("Renaming dataset: " + cont.getData().relationName() + " [" + cont.getSourceFull() + "]");
+    newName = GUIHelper.showInputDialog(getOwner(), "Please enter new relation name: ", cont.getData().relationName());
+    if (newName == null) {
+      logMessage("Renaming cancelled!");
+      return;
+    }
+    cont.getData().setRelationName(newName);
+    fireDataChange();
+  }
+
+  /**
+   * Updates the action.
+   */
+  @Override
+  public void update() {
+    setEnabled(getTable().getSelectedRowCount() == 1);
+  }
+}
