bf2586a7b138e5558c2da57cd5cafdff55ed63cd
fracpete <fracpete@gmail.com>
2016-07-22 05:03:50 +0000

    added support for loading data from a JDBC-accessible database

---
+++ adams-weka/src/main/java/adams/gui/tools/wekainvestigator/data/DatabaseContainer.java
@@ -0,0 +1,149 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * DatabaseContainer.java
+ * Copyright (C) 2016 University of Waikato, Hamilton, NZ
+ */
+
+package adams.gui.tools.wekainvestigator.data;
+
+import weka.core.Instances;
+import weka.experiment.InstanceQuery;
+
+import java.util.logging.Level;
+
+/**
+ * Dataset loaded from database.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class DatabaseContainer
+  extends AbstractDataContainer {
+
+  private static final long serialVersionUID = 6267905940957451551L;
+
+  /** the database URL. */
+  protected String m_URL;
+
+  /** the query used to load the data. */
+  protected String m_Query;
+
+  /** the user. */
+  protected String m_User;
+
+  /** the password. */
+  protected String m_Password;
+
+  /**
+   * Loads the data using the specified url/query.
+   *
+   * @param url		the JDBC URL
+   * @param user	the database user
+   * @param pw		the password
+   * @param query	the query used
+   */
+  public DatabaseContainer(String url, String user, String pw, String query) {
+    super();
+    try {
+      InstanceQuery instq = new InstanceQuery();
+      instq.setDatabaseURL(url);
+      instq.setUsername(user);
+      instq.setPassword(pw);
+      instq.setQuery(query);
+      m_Data     = instq.retrieveInstances();
+      m_URL      = url;
+      m_Query    = query;
+      m_User     = user;
+      m_Password = pw;
+    }
+    catch (Exception e) {
+      throw new IllegalArgumentException("Failed to load data from DB: " + url, e);
+    }
+  }
+
+  /**
+   * Returns a short version of the source of the data item.
+   *
+   * @return		the source
+   */
+  public String getSourceShort() {
+    if (m_URL == null)
+      return "<unknown>";
+    else
+      return m_Query;
+  }
+
+  /**
+   * Returns the source of the data item.
+   *
+   * @return		the source
+   */
+  @Override
+  public String getSourceFull() {
+    if (m_URL == null)
+      return "<unknown>";
+    else
+      return m_User + ":" + m_Password.replaceAll(".", "*") + "@" + m_URL + " using " + m_Query;
+  }
+
+  /**
+   * Whether it is possible to reload this item.
+   *
+   * @return		true if reloadable
+   */
+  @Override
+  public boolean canReload() {
+    return (m_URL != null);
+  }
+
+  /**
+   * Reloads the data.
+   *
+   * @return		true if successfully reloaded
+   */
+  @Override
+  protected boolean doReload() {
+    InstanceQuery 	instq;
+
+    try {
+      instq = new InstanceQuery();
+      instq.setDatabaseURL(m_URL);
+      instq.setUsername(m_User);
+      instq.setPassword(m_Password);
+      instq.setQuery(m_Query);
+      m_Data = instq.retrieveInstances();
+      return true;
+    }
+    catch (Exception e) {
+      getLogger().log(Level.SEVERE, "Failed to reload from database: " + m_URL, e);
+      return false;
+    }
+  }
+
+  /**
+   * Sets the data.
+   *
+   * @param value	the data to use
+   */
+  public void setData(Instances value) {
+    super.setData(value);
+    m_URL      = null;
+    m_User     = null;
+    m_Password = null;
+    m_Query    = null;
+  }
+}
---
+++ adams-weka/src/main/java/adams/gui/tools/wekainvestigator/source/Database.java
@@ -0,0 +1,72 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Database.java
+ * Copyright (C) 2016 University of Waikato, Hamilton, NZ
+ */
+
+package adams.gui.tools.wekainvestigator.source;
+
+import adams.gui.chooser.SpreadSheetFileChooser;
+import adams.gui.tools.wekainvestigator.data.DatabaseContainer;
+import weka.gui.sql.SqlViewerDialog;
+
+import javax.swing.JOptionPane;
+import java.awt.event.ActionEvent;
+
+/**
+ * For loading data from a database.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class Database
+  extends AbstractSource {
+
+  private static final long serialVersionUID = 5646388990155938153L;
+
+  /** the filechooser. */
+  protected SpreadSheetFileChooser m_FileChooser;
+
+  /**
+   * Instantiates the action.
+   */
+  public Database() {
+    super();
+    setName("Database...");
+    setIcon("database.gif");
+  }
+
+  /**
+   * Invoked when an action occurs.
+   *
+   * @param e		the event
+   */
+  @Override
+  protected void doActionPerformed(ActionEvent e) {
+    SqlViewerDialog	dialog;
+
+    dialog = new SqlViewerDialog(null);
+    dialog.setDefaultCloseOperation(SqlViewerDialog.DISPOSE_ON_CLOSE);
+    dialog.pack();
+    dialog.setLocationRelativeTo(getOwner());
+    dialog.setVisible(true);
+    if (dialog.getReturnValue() != JOptionPane.OK_OPTION)
+      return;
+
+    addData(new DatabaseContainer(dialog.getURL(), dialog.getUser(), dialog.getPassword(), dialog.getQuery()));
+  }
+}
