d6a0d4c007c206ad3a03e832cb27b52c0d5eca47
fracpete <fracpete@gmail.com>
2016-07-25 04:00:21 +0000

    more legacy outputs

---
+++ adams-weka/src/main/java/adams/gui/tools/wekainvestigator/tab/classifytab/output/LegacyTreeVisualizer.java
@@ -0,0 +1,89 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * LegacyTreeVisualizer.java
+ * Copyright (C) 2016 University of Waikato, Hamilton, NZ
+ */
+
+package adams.gui.tools.wekainvestigator.tab.classifytab.output;
+
+import adams.flow.core.Token;
+import adams.flow.sink.WekaTreeVisualizer;
+import adams.gui.tools.wekainvestigator.output.ComponentContentPanel;
+import adams.gui.tools.wekainvestigator.tab.classifytab.ResultItem;
+import weka.core.Drawable;
+
+import javax.swing.JPanel;
+
+/**
+ * Displays the tree that the model generated (legacy Weka output).
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class LegacyTreeVisualizer
+  extends AbstractOutputGenerator {
+
+  private static final long serialVersionUID = -6829245659118360739L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Generates a tree visualization of the model (legacy Weka output).";
+  }
+
+  /**
+   * The title to use for the tab.
+   *
+   * @return		the title
+   */
+  public String getTitle() {
+    return "Tree visualizer (legacy)";
+  }
+
+  /**
+   * Checks whether output can be generated from this item.
+   *
+   * @param item	the item to check
+   * @return		true if output can be generated
+   */
+  public boolean canGenerateOutput(ResultItem item) {
+    return item.hasClassifier() && (item.getClassifier() instanceof Drawable);
+  }
+
+  /**
+   * Generates output and adds it to the {@link ResultItem}.
+   *
+   * @param item	the item to add the output to
+   * @return		null if output could be generated, otherwise error message
+   */
+  @Override
+  public String generateOutput(ResultItem item) {
+    WekaTreeVisualizer	sink;
+    JPanel 		panel;
+
+    sink  = new WekaTreeVisualizer();
+    panel = sink.createDisplayPanel(new Token(item.getClassifier()));
+
+    addTab(item, new ComponentContentPanel(panel, sink.displayPanelRequiresScrollPane()));
+
+    return null;
+  }
+}
