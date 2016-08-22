7e7b1ac4c0b778adfe93014cb85a8f276117a534
fracpete <fracpete@gmail.com>
2016-08-22 07:19:26 +0000

    storing individual weights now, per property

--- adams-weka/src/main/java/adams/genetic/Hermione.java
+++ adams-weka/src/main/java/adams/genetic/Hermione.java
@@ -15,11 +15,12 @@
 
 /*
  * Hermione.java
- * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2015-2016 University of Waikato, Hamilton, New Zealand
  */
 
 package adams.genetic;
 
+import adams.core.Properties;
 import adams.core.discovery.AbstractGeneticDiscoveryHandler;
 import adams.core.discovery.DefaultPropertyDiscovery;
 import adams.core.discovery.PropertyPath;
@@ -43,132 +44,132 @@ import java.util.logging.Level;
  * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
  * &nbsp;&nbsp;&nbsp;default: WARNING
  * </pre>
- * 
+ *
  * <pre>-num-threads &lt;int&gt; (property: numThreads)
  * &nbsp;&nbsp;&nbsp;The number of threads to use for executing the jobs; use -1 for all available 
  * &nbsp;&nbsp;&nbsp;cores.
  * &nbsp;&nbsp;&nbsp;default: -1
  * &nbsp;&nbsp;&nbsp;minimum: -1
  * </pre>
- * 
+ *
  * <pre>-num-chrom &lt;int&gt; (property: numChrom)
  * &nbsp;&nbsp;&nbsp;The number of chromosomes, ie, the population size.
  * &nbsp;&nbsp;&nbsp;default: 50
  * </pre>
- * 
+ *
  * <pre>-stopping-criterion &lt;adams.genetic.stopping.AbstractStoppingCriterion&gt; (property: stoppingCriterion)
  * &nbsp;&nbsp;&nbsp;The stopping criterion to use.
  * &nbsp;&nbsp;&nbsp;default: adams.genetic.stopping.MaxIterations
  * </pre>
- * 
+ *
  * <pre>-seed &lt;long&gt; (property: seed)
  * &nbsp;&nbsp;&nbsp;The seed value for the random number generator.
  * &nbsp;&nbsp;&nbsp;default: 1
  * </pre>
- * 
+ *
  * <pre>-initial-setups-provider &lt;adams.genetic.initialsetups.AbstractInitialSetupsProvider&gt; (property: initialSetupsProvider)
  * &nbsp;&nbsp;&nbsp;The initial setups provider to use.
  * &nbsp;&nbsp;&nbsp;default: adams.genetic.initialsetups.EmptyInitialSetupsProvider
  * </pre>
- * 
+ *
  * <pre>-initial-weights &lt;java.lang.String&gt; (property: initialWeights)
  * &nbsp;&nbsp;&nbsp;The initial weights to use, rather than random ones (string of 0s and 1s
  * &nbsp;&nbsp;&nbsp;).
  * &nbsp;&nbsp;&nbsp;default: 
  * </pre>
- * 
+ *
  * <pre>-favor-zeroes &lt;boolean&gt; (property: favorZeroes)
  * &nbsp;&nbsp;&nbsp;Whether to favor 0s instead of 1s.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- * 
+ *
  * <pre>-best &lt;java.lang.String&gt; (property: bestRange)
  * &nbsp;&nbsp;&nbsp;The range of the best attributes.
  * &nbsp;&nbsp;&nbsp;default: -none-
  * </pre>
- * 
+ *
  * <pre>-notify &lt;int&gt; (property: notificationInterval)
  * &nbsp;&nbsp;&nbsp;The time interval in seconds after which notification events about changes 
  * &nbsp;&nbsp;&nbsp;in the fitness can be sent (-1 = never send notifications; 0 = whenever 
  * &nbsp;&nbsp;&nbsp;a change occurs).
  * &nbsp;&nbsp;&nbsp;default: -1
  * </pre>
- * 
+ *
  * <pre>-bits-per-gene &lt;int&gt; (property: bitsPerGene)
  * &nbsp;&nbsp;&nbsp;The number of bits per gene to use.
  * &nbsp;&nbsp;&nbsp;default: 1
  * &nbsp;&nbsp;&nbsp;minimum: 1
  * </pre>
- * 
+ *
  * <pre>-class &lt;adams.data.weka.WekaAttributeIndex&gt; (property: classIndex)
  * &nbsp;&nbsp;&nbsp;The class index of the dataset, in case no class attribute is set.
  * &nbsp;&nbsp;&nbsp;default: last
  * &nbsp;&nbsp;&nbsp;example: An index is a number starting with 1; apart from attribute names (case-sensitive), the following placeholders can be used as well: first, second, third, last_2, last_1, last; numeric indices can be enforced by preceding them with '#' (eg '#12'); attribute names can be surrounded by double quotes.
  * </pre>
- * 
+ *
  * <pre>-folds &lt;int&gt; (property: folds)
  * &nbsp;&nbsp;&nbsp;The number of folds to use in cross-validation.
  * &nbsp;&nbsp;&nbsp;default: 10
  * &nbsp;&nbsp;&nbsp;minimum: 2
  * </pre>
- * 
+ *
  * <pre>-cv-seed &lt;int&gt; (property: crossValidationSeed)
  * &nbsp;&nbsp;&nbsp;The seed value for cross-validation.
  * &nbsp;&nbsp;&nbsp;default: 55
  * </pre>
- * 
+ *
  * <pre>-classifier &lt;weka.classifiers.Classifier&gt; (property: classifier)
  * &nbsp;&nbsp;&nbsp;The classifier to use.
  * &nbsp;&nbsp;&nbsp;default: weka.classifiers.rules.ZeroR
  * </pre>
- * 
+ *
  * <pre>-measure &lt;CC|RMSE|RRSE|MAE|RAE|ACC&gt; (property: measure)
  * &nbsp;&nbsp;&nbsp;The measure used for evaluating the fitness.
  * &nbsp;&nbsp;&nbsp;default: RMSE
  * </pre>
- * 
+ *
  * <pre>-output-dir &lt;adams.core.io.PlaceholderDirectory&gt; (property: outputDirectory)
  * &nbsp;&nbsp;&nbsp;The directory for storing the generated ARFF files.
  * &nbsp;&nbsp;&nbsp;default: ${CWD}
  * </pre>
- * 
+ *
  * <pre>-output-type &lt;NONE|SETUP|DATA|ALL&gt; (property: outputType)
  * &nbsp;&nbsp;&nbsp;The type of output to generate.
  * &nbsp;&nbsp;&nbsp;default: SETUP
  * </pre>
- * 
+ *
  * <pre>-output-prefix-type &lt;NONE|RELATION|SUPPLIED&gt; (property: outputPrefixType)
  * &nbsp;&nbsp;&nbsp;The type of prefix to use for the output.
  * &nbsp;&nbsp;&nbsp;default: NONE
  * </pre>
- * 
+ *
  * <pre>-supplied-prefix &lt;java.lang.String&gt; (property: suppliedPrefix)
  * &nbsp;&nbsp;&nbsp;The prefix to use in case of SUPPLIED.
  * &nbsp;&nbsp;&nbsp;default: 
  * </pre>
- * 
+ *
  * <pre>-use-second-evaluation &lt;boolean&gt; (property: useSecondEvaluation)
  * &nbsp;&nbsp;&nbsp;If enabled, a second evaluation is performed using the separate folds and 
  * &nbsp;&nbsp;&nbsp;seed.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- * 
+ *
  * <pre>-second-folds &lt;int&gt; (property: secondFolds)
  * &nbsp;&nbsp;&nbsp;The number of folds to use in cross-validation (second evaluation).
  * &nbsp;&nbsp;&nbsp;default: 10
  * &nbsp;&nbsp;&nbsp;minimum: 2
  * </pre>
- * 
+ *
  * <pre>-second-cv-seed &lt;int&gt; (property: secondCrossValidationSeed)
  * &nbsp;&nbsp;&nbsp;The seed value for cross-validation (second evaluation).
  * &nbsp;&nbsp;&nbsp;default: 42
  * </pre>
- * 
+ *
  * <pre>-handler &lt;adams.core.discovery.AbstractGeneticDiscoveryHandler&gt; [-handler ...] (property: handlers)
  * &nbsp;&nbsp;&nbsp;The discovery handlers to use.
  * &nbsp;&nbsp;&nbsp;default: 
  * </pre>
- * 
+ *
  <!-- options-end -->
  *
  * @author Dale (dale at cs dot waikato dot ac dot nz)
@@ -215,6 +216,47 @@ public class Hermione
      */
     public HermioneJob(Hermione g, int num, int[] w, Instances data) {
       super(g, num, w, data);
+    }
+
+    /**
+     * Assembles the data for the textual setup output.
+     *
+     * @param fitness	the current fitness
+     * @param cls	the current classifier
+     * @return		the data
+     */
+    protected Properties assembleSetup(double fitness, Classifier cls, int[] weights) {
+      Properties 				result;
+      int 					pos;
+      List<Integer> 				numbits;
+      List<Integer> 				start;
+      AbstractGeneticDiscoveryHandler[] 	handlers;
+      DefaultPropertyDiscovery 			discovery;
+      List<PropertyPath.PropertyContainer> 	lpc;
+      String 					sa;
+
+      result = super.assembleSetup(fitness, cls, weights);
+
+      // store individual weights
+      pos      = 0;
+      numbits  = getOwner().getNumBitsForAll();
+      start    = getOwner().getStartPoints();
+      handlers = new AbstractGeneticDiscoveryHandler[getOwner().getHandlers().length];
+      for (int i = 0; i < getOwner().getHandlers().length; i++)
+	handlers[i] = (AbstractGeneticDiscoveryHandler) getOwner().getHandlers()[i].shallowCopy();
+      discovery = new DefaultPropertyDiscovery();
+      discovery.setLoggingLevel(getLoggingLevel());
+      discovery.discover(handlers, cls);
+      for (AbstractGeneticDiscoveryHandler ag: handlers) {
+	lpc = ag.getContainers();
+	for (PropertyPath.PropertyContainer pc : lpc) {
+	  sa = getOwner().intArrayToString(getOwner().getBitsForPosition(weights, start, numbits, pos));
+	  result.setProperty("Weights." + pos, sa);
+	  pos++;
+	}
+      }
+
+      return result;
     }
 
     /**
