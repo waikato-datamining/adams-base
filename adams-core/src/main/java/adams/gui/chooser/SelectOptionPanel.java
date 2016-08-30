fa6791df8460bdcdba70a79953b8b967c3666af5
fracpete <fracpete@gmail.com>
2016-08-30 02:15:12 +0000

    chooser for selecting one or more options from a predefined list of options

---
+++ adams-core/src/main/java/adams/gui/chooser/SelectOptionPanel.java
@@ -0,0 +1,211 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * SelectOptionPanel.java
+ * Copyright (C) 2016 University of Waikato, Hamilton, NZ
+ */
+
+package adams.gui.chooser;
+
+import adams.core.option.OptionUtils;
+import adams.gui.core.BaseList;
+import adams.gui.core.ConsolePanel;
+import adams.gui.dialog.ApprovalDialog;
+import com.googlecode.jfilechooserbookmarks.gui.BaseScrollPane;
+import gnu.trove.list.TIntList;
+import gnu.trove.list.array.TIntArrayList;
+
+import javax.swing.DefaultListModel;
+import javax.swing.ListSelectionModel;
+import java.awt.Dialog.ModalityType;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+
+/**
+ * Allows the user to select either a single or multiple options from a list
+ * of available options.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class SelectOptionPanel
+  extends AbstractChooserPanel<String[]> {
+
+  private static final long serialVersionUID = -4122866764098538057L;
+
+  /** the title for the dialog. */
+  protected String m_DialogTitle;
+
+  /** whether to use multi-select. */
+  protected boolean m_MultiSelect;
+
+  /** the available options. */
+  protected String[] m_Options;
+
+  /**
+   * Initializes the members.
+   */
+  @Override
+  protected void initialize() {
+    super.initialize();
+
+    m_Options     = new String[0];
+    m_MultiSelect = false;
+    m_DialogTitle = "Select option";
+  }
+
+  /**
+   * Sets the options to choose from.
+   *
+   * @param value	the options
+   */
+  public void setOptions(String[] value) {
+    m_Options = value;
+  }
+
+  /**
+   * Returns the options to choose from.
+   *
+   * @return		the options
+   */
+  public String[] getOptions() {
+    return m_Options;
+  }
+
+  /**
+   * Sets the title for the dialog.
+   *
+   * @param value	the title
+   */
+  public void setDialogTitle(String value) {
+    m_DialogTitle = value;
+  }
+
+  /**
+   * Returns the title to use for the dialog.
+   *
+   * @return		the title
+   */
+  public String getDialogTitle() {
+    return m_DialogTitle;
+  }
+
+  /**
+   * Sets whether to allow selection of multiple options.
+   *
+   * @param value	true if multi-select enabled
+   */
+  public void setMultiSelect(boolean value) {
+    m_MultiSelect = value;
+  }
+
+  /**
+   * Returns whether multiple options can be selected.
+   *
+   * @return		true if multi-select enabled
+   */
+  public boolean isMultiSelect() {
+    return m_MultiSelect;
+  }
+
+  /**
+   * Converts the value into its string representation.
+   *
+   * @param value	the value to convert
+   * @return		the generated string
+   */
+  @Override
+  protected String toString(String[] value) {
+    return OptionUtils.joinOptions(value);
+  }
+
+  /**
+   * Converts the string representation into its object representation.
+   *
+   * @param value	the string value to convert
+   * @return		the generated object
+   */
+  @Override
+  protected String[] fromString(String value) {
+    String[]	result;
+
+    try {
+      result = OptionUtils.splitOptions(value);
+    }
+    catch (Exception e) {
+      result = new String[0];
+      ConsolePanel.getSingleton().append(Level.SEVERE, "Failed to parse options for selection: " + value, e);
+    }
+
+    return result;
+  }
+
+  /**
+   * Performs the actual choosing of an object.
+   *
+   * @return		the chosen object or null if none chosen
+   */
+  @Override
+  protected String[] doChoose() {
+    List<String> 		result;
+    ApprovalDialog		dialog;
+    DefaultListModel<String>	model;
+    BaseList 			list;
+    int				i;
+    TIntList			selected;
+    Set<String> 		current;
+
+    // populate list
+    model    = new DefaultListModel<>();
+    selected = new TIntArrayList();
+    current  = new HashSet<>(Arrays.asList(getCurrent()));
+    for (i = 0; i < m_Options.length; i++) {
+      model.addElement(m_Options[i]);
+      if (current.contains(m_Options[i])) {
+        selected.add(i);
+        if (!isMultiSelect())
+          break;
+      }
+    }
+    list = new BaseList(model);
+    list.setSelectionMode(isMultiSelect() ? ListSelectionModel.MULTIPLE_INTERVAL_SELECTION : ListSelectionModel.SINGLE_SELECTION);
+    list.setSelectedIndices(selected.toArray());
+
+    // create dialog
+    if (getParentDialog() != null)
+      dialog = new ApprovalDialog(getParentDialog(), ModalityType.DOCUMENT_MODAL);
+    else
+      dialog = new ApprovalDialog(getParentFrame(), true);
+    dialog.setTitle(m_DialogTitle);
+    dialog.getContentPane().add(new BaseScrollPane(list));
+    dialog.pack();
+    dialog.setLocationRelativeTo(null);
+    dialog.setVisible(true);
+
+    if (dialog.getOption() != ApprovalDialog.APPROVE_OPTION)
+      return null;
+
+    result = new ArrayList<>();
+    for (int index: list.getSelectedIndices())
+      result.add(model.get(index));
+
+    return result.toArray(new String[result.size()]);
+  }
+}
