27f858e53d7e96cd17173ff401f15b78cd0ecdd4
fracpete <fracpete@gmail.com>
2016-08-31 02:56:58 +0000

    ancestor for ADAMS setup panels

---
+++ adams-weka/src/main/java/adams/gui/tools/wekamultiexperimenter/setup/AbstractAdamsSetupPanel.java
@@ -0,0 +1,35 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * AbstractWekaSetupPanel.java
+ * Copyright (C) 2016 University of Waikato, Hamilton, NZ
+ */
+
+package adams.gui.tools.wekamultiexperimenter.setup;
+
+import adams.gui.tools.wekamultiexperimenter.experiment.AbstractExperiment;
+
+/**
+ * Ancestor for setup panels for ADAMS experiments.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public abstract class AbstractAdamsSetupPanel
+  extends AbstractSetupPanel<AbstractExperiment> {
+
+  private static final long serialVersionUID = 8023889227903901226L;
+}
