770f4b6d84fa8ab57ec1b7894780479ef32c339e
fracpete <fracpete@gmail.com>
2016-09-14 02:35:48 +0000

    RegExpTextField is now derived from BaseObjectTextField

--- adams-core/src/main/java/adams/gui/core/RegExpTextField.java
+++ adams-core/src/main/java/adams/gui/core/RegExpTextField.java
@@ -15,14 +15,12 @@
 
 /**
  * RegExpTextField.java
- * Copyright (C) 2015 University of Waikato, Hamilton, NZ
+ * Copyright (C) 2015-2016 University of Waikato, Hamilton, NZ
  */
 
 package adams.gui.core;
 
 import adams.core.base.BaseRegExp;
-
-import java.util.regex.Pattern;
 
 /**
  * Text field designed for entering a regular expression.
@@ -31,60 +29,24 @@ import java.util.regex.Pattern;
  * @version $Revision$
  */
 public class RegExpTextField
-  extends CheckedTextField {
+  extends BaseObjectTextField<BaseRegExp> {
 
   private static final long serialVersionUID = -6624338080908941975L;
 
   /**
-   * A model for checking regexp values.
-   *
-   * @author  fracpete (fracpete at waikato dot ac dot nz)
-   * @version $Revision: 4584 $
-   */
-  public static class RegExpCheckModel
-    extends AbstractCheckModel {
-
-    /** for serialization. */
-    private static final long serialVersionUID = -2579549735806129821L;
-
-    /**
-     * Checks whether the content is valid.
-     *
-     * @param text	the string to check
-     * @return		true if valid
-     */
-    public boolean isValid(String text) {
-      boolean 		result;
-
-      try {
-	Pattern.compile(text);
-	result = true;
-      }
-      catch (Exception e) {
-	result = false;
-      }
-
-      return result;
-    }
-  }
-
-  /**
-   * Constructs a new <code>TextField</code>. A default model is created,
-   * the initial string is <code>.*</code>,
-   * and the number of columns is set to 0.
+   * Constructs a new <code>TextField</code>.
    */
   public RegExpTextField() {
     this(BaseRegExp.MATCH_ALL);
   }
 
   /**
-   * Constructs a new <code>TextField</code>. A default model is created
-   * and the number of columns is set to 0.
+   * Constructs a new <code>TextField</code>.
    *
-   * @param index	the initial string
+   * @param initial	the initial string
    */
-  public RegExpTextField(String index) {
-    super(index, new RegExpCheckModel());
+  public RegExpTextField(String initial) {
+    super(new BaseRegExp(initial), initial);
   }
 
   /**
@@ -93,7 +55,7 @@ public class RegExpTextField
    * @param value	the regular expression
    */
   public void setRegExp(BaseRegExp value) {
-    setText(value.getValue());
+    setObject(value);
   }
 
   /**
@@ -102,6 +64,6 @@ public class RegExpTextField
    * @return		the regular expression
    */
   public BaseRegExp getRegExp() {
-    return new BaseRegExp(getText());
+    return getObject();
   }
 }
