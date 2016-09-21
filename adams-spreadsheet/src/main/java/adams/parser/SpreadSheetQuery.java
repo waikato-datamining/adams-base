b4a6e56a029bacea5b77e5f4bbb07da43ba5ce58
fracpete <fracpete@gmail.com>
2016-09-21 01:01:05 +0000

    updated javadoc

--- adams-spreadsheet/src/main/java/adams/parser/SpreadSheetQuery.java
+++ adams-spreadsheet/src/main/java/adams/parser/SpreadSheetQuery.java
@@ -15,7 +15,7 @@
 
 /*
  * SpreadSheetQuery.java
- * Copyright (C) 2013-2015 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2013-2016 University of Waikato, Hamilton, New Zealand
  */
 
 package adams.parser;
@@ -116,7 +116,7 @@ import java.util.logging.Level;
  *               | agg<br>
  *               ;<br>
  * <br>
- * agg       ::=   COUNT [(*)] [AS COLUMN]<br>
+ * agg       ::=   COUNT [(*)|(COLUMN)] [AS COLUMN]<br>
  *               | MIN ( COLUMN ) [AS COLUMN]<br>
  *               | MAX ( COLUMN ) [AS COLUMN]<br>
  *               | RANGE ( COLUMN ) [AS COLUMN] (= MIN - MAX)<br>
