300c4c71de68a2117d230a2acf7798b2a64673e0
fracpete <fracpete@gmail.com>
2016-09-22 02:32:20 +0000

    removed popup menu again, as it stops dragging of tabs to work

--- adams-weka/src/main/java/adams/gui/tools/wekainvestigator/tab/InvestigatorTabbedPane.java
+++ adams-weka/src/main/java/adams/gui/tools/wekainvestigator/tab/InvestigatorTabbedPane.java
@@ -24,15 +24,9 @@ import adams.core.CleanUpHandler;
 import adams.gui.core.ButtonTabComponent;
 import adams.gui.core.DragAndDropTabbedPane;
 import adams.gui.core.GUIHelper;
-import adams.gui.core.MouseUtils;
-import adams.gui.core.PopupMenuProvider;
 import adams.gui.tools.wekainvestigator.InvestigatorPanel;
 
-import javax.swing.Icon;
-import javax.swing.JPopupMenu;
 import java.awt.Component;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 /**
  * Tabbed pane for managing the tabs of the Investigator.
@@ -77,8 +71,8 @@ public class InvestigatorTabbedPane
    *
    * @param tab		the tab to add
    */
-  public void addTab(AbstractInvestigatorTab tab) {
-    ButtonTabComponent	button;
+  public void addTab(final AbstractInvestigatorTab tab) {
+    final ButtonTabComponent	button;
 
     tab.setOwner(getOwner());
     tab.setFrameTitle(tab.getTitle());
@@ -87,48 +81,6 @@ public class InvestigatorTabbedPane
     // icon
     button = (ButtonTabComponent) getTabComponentAt(getTabCount() - 1);
     button.setIcon((tab.getTabIcon() == null) ? null : GUIHelper.getIcon(tab.getTabIcon()));
-  }
-
-  /**
-   * Inserts a new tab for the given component, at the given index,
-   * represented by the given title and/or icon, either of which may
-   * be {@code null}.
-   *
-   * @param title the title to be displayed on the tab
-   * @param icon the icon to be displayed on the tab
-   * @param component the component to be displayed when this tab is clicked.
-   * @param tip the tooltip to be displayed for this tab
-   * @param index the position to insert this new tab
-   *       ({@code > 0 and <= getTabCount()})
-   *
-   * @throws IndexOutOfBoundsException if the index is out of range
-   *         ({@code < 0 or > getTabCount()})
-   */
-  public void insertTab(String title, Icon icon, final Component component, String tip, int index) {
-    ButtonTabComponent 		tabComp;
-
-    super.insertTab(title, icon, component, tip, index);
-
-    if (component instanceof PopupMenuProvider) {
-      tabComp = (ButtonTabComponent) getTabComponentAt(index);
-      tabComp.addMouseListener(new MouseAdapter() {
-	@Override
-	public void mouseClicked(MouseEvent e) {
-	  if (MouseUtils.isRightClick(e)) {
-	    JPopupMenu menu = ((PopupMenuProvider) component).getPopupMenu();
-	    menu.show(tabComp, e.getX(), e.getY());
-	  }
-	  // for some reason, adding a mouse listener stops left/middle
-	  // mouse button clicks from working??
-	  else if (MouseUtils.isLeftClick(e)) {
-	    setSelectedComponent(component);
-	  }
-	  else if (MouseUtils.isMiddleClick(e)) {
-	    tabClicked(e);
-	  }
-	}
-      });
-    }
   }
 
   /**
