85aa196403af9ff5e57c7d1d0a972121c13a3500
fracpete <fracpete@gmail.com>
2016-09-28 23:22:42 +0000

    introduced InstanceViewCreator interface for classes that support view creation of Instances objects

--- adams-weka/src/main/java/adams/flow/transformer/WekaCrossValidationSplit.java
+++ adams-weka/src/main/java/adams/flow/transformer/WekaCrossValidationSplit.java
@@ -15,13 +15,14 @@
 
 /*
  * WekaCrossValidationSplit.java
- * Copyright (C) 2010-2013 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2010-2016 University of Waikato, Hamilton, New Zealand
  */
 
 package adams.flow.transformer;
 
 import adams.core.QuickInfoHelper;
 import adams.core.Randomizable;
+import adams.data.weka.InstancesViewCreator;
 import adams.flow.container.WekaTrainTestSetContainer;
 import adams.flow.core.Token;
 import adams.flow.provenance.ActorType;
@@ -36,63 +37,81 @@ import java.util.Hashtable;
 
 /**
  <!-- globalinfo-start -->
- * Generates train/test pairs like during a cross-validation run. It is possible to generate pairs for leave-one-out cross-validation (LOOCV) as well.<br>
+ * Generates train&#47;test pairs like during a cross-validation run. It is possible to generate pairs for leave-one-out cross-validation (LOOCV) as well.<br>
  * It is essential that a class attribute is set. In case of a nominal class attribute, the data gets stratified automatically.<br>
  * Each of the pairs gets forwarded as a container. The training set can be accessed in the container with 'Train' and the test set with 'Test'.
  * <br><br>
  <!-- globalinfo-end -->
  *
  <!-- flow-summary-start -->
- * Input/output:<br>
+ * Input&#47;output:<br>
  * - accepts:<br>
  * &nbsp;&nbsp;&nbsp;weka.core.Instances<br>
  * - generates:<br>
- * &nbsp;&nbsp;&nbsp;adams.flow.container.TrainTestSetContainer<br>
+ * &nbsp;&nbsp;&nbsp;adams.flow.container.WekaTrainTestSetContainer<br>
+ * <br><br>
+ * Container information:<br>
+ * - adams.flow.container.WekaTrainTestSetContainer: Train, Test, Seed, FoldNumber, FoldCount, Train original indices, Test original indices
  * <br><br>
  <!-- flow-summary-end -->
  *
  <!-- options-start -->
- * Valid options are: <br><br>
- *
- * <pre>-D &lt;int&gt; (property: debugLevel)
- * &nbsp;&nbsp;&nbsp;The greater the number the more additional info the scheme may output to
- * &nbsp;&nbsp;&nbsp;the console (0 = off).
- * &nbsp;&nbsp;&nbsp;default: 0
- * &nbsp;&nbsp;&nbsp;minimum: 0
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
  * </pre>
- *
+ * 
  * <pre>-name &lt;java.lang.String&gt; (property: name)
  * &nbsp;&nbsp;&nbsp;The name of the actor.
- * &nbsp;&nbsp;&nbsp;default: CrossValidationSplit
+ * &nbsp;&nbsp;&nbsp;default: WekaCrossValidationSplit
  * </pre>
- *
- * <pre>-annotation &lt;adams.core.base.BaseText&gt; (property: annotations)
+ * 
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
  * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
- * &nbsp;&nbsp;&nbsp;default:
+ * &nbsp;&nbsp;&nbsp;default: 
  * </pre>
- *
- * <pre>-skip (property: skip)
- * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded
+ * 
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded 
  * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- *
+ * 
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this 
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical 
+ * &nbsp;&nbsp;&nbsp;actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing 
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
  * <pre>-seed &lt;long&gt; (property: seed)
  * &nbsp;&nbsp;&nbsp;The seed value for the randomization.
  * &nbsp;&nbsp;&nbsp;default: 1
  * </pre>
- *
+ * 
  * <pre>-folds &lt;int&gt; (property: folds)
- * &nbsp;&nbsp;&nbsp;The folds to use; using '-1' will generate folds for leave-one-out cross-validation
+ * &nbsp;&nbsp;&nbsp;The folds to use; using '-1' will generate folds for leave-one-out cross-validation 
  * &nbsp;&nbsp;&nbsp;(LOOCV).
  * &nbsp;&nbsp;&nbsp;default: 10
  * </pre>
- *
+ * 
  * <pre>-relation &lt;java.lang.String&gt; (property: relationName)
  * &nbsp;&nbsp;&nbsp;The placeholders for creating the relation name: &#64; = original relation name,
- * &nbsp;&nbsp;&nbsp; $T = type (train/test), $N = current fold number.
+ * &nbsp;&nbsp;&nbsp; $T = type (train&#47;test), $N = current fold number.
  * &nbsp;&nbsp;&nbsp;default: &#64;
  * </pre>
- *
+ * 
+ * <pre>-create-view &lt;boolean&gt; (property: createView)
+ * &nbsp;&nbsp;&nbsp;If enabled, views of the dataset are created instead of actual copies.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
  <!-- options-end -->
  *
  * @author  fracpete (fracpete at waikato dot ac dot nz)
@@ -100,7 +119,7 @@ import java.util.Hashtable;
  */
 public class WekaCrossValidationSplit
   extends AbstractTransformer
-  implements Randomizable, ProvenanceSupporter {
+  implements Randomizable, ProvenanceSupporter, InstancesViewCreator {
 
   /** for serialization. */
   private static final long serialVersionUID = 4026105903223741240L;
@@ -117,9 +136,12 @@ public class WekaCrossValidationSplit
   /** the format of the relation names of the generated datasets. */
   protected String m_RelationName;
 
+  /** whether to create a view only. */
+  protected boolean m_CreateView;
+
   /** the fold generator. */
   protected CrossValidationFoldGenerator m_Generator;
-  
+
   /**
    * Returns a string describing the object.
    *
@@ -128,14 +150,14 @@ public class WekaCrossValidationSplit
   @Override
   public String globalInfo() {
     return
-        "Generates train/test pairs like during a cross-validation run. It is "
-      + "possible to generate pairs for leave-one-out cross-validation (LOOCV) "
-      + "as well.\n"
-      + "It is essential that a class attribute is set. In case of a nominal "
-      + "class attribute, the data gets stratified automatically.\n"
-      + "Each of the pairs gets forwarded as a container. The training set can "
-      + "be accessed in the container with '" + WekaTrainTestSetContainer.VALUE_TRAIN + "' "
-      + "and the test set with '" + WekaTrainTestSetContainer.VALUE_TEST + "'.";
+      "Generates train/test pairs like during a cross-validation run. It is "
+	+ "possible to generate pairs for leave-one-out cross-validation (LOOCV) "
+	+ "as well.\n"
+	+ "It is essential that a class attribute is set. In case of a nominal "
+	+ "class attribute, the data gets stratified automatically.\n"
+	+ "Each of the pairs gets forwarded as a container. The training set can "
+	+ "be accessed in the container with '" + WekaTrainTestSetContainer.VALUE_TRAIN + "' "
+	+ "and the test set with '" + WekaTrainTestSetContainer.VALUE_TEST + "'.";
   }
 
   /**
@@ -146,16 +168,20 @@ public class WekaCrossValidationSplit
     super.defineOptions();
 
     m_OptionManager.add(
-	    "seed", "seed",
-	    1L);
+      "seed", "seed",
+      1L);
 
     m_OptionManager.add(
-	    "folds", "folds",
-	    10);
+      "folds", "folds",
+      10);
 
     m_OptionManager.add(
-	    "relation", "relationName",
-	    "@");
+      "relation", "relationName",
+      "@");
+
+    m_OptionManager.add(
+      "create-view", "createView",
+      false);
   }
 
   /**
@@ -166,10 +192,14 @@ public class WekaCrossValidationSplit
   @Override
   public String getQuickInfo() {
     String	result;
+    String	value;
 
     result  = QuickInfoHelper.toString(this, "folds", m_Folds, "folds: ");
     result += QuickInfoHelper.toString(this, "seed", m_Seed, ", seed: ");
     result += QuickInfoHelper.toString(this, "relationName", m_RelationName, ", relation: ");
+    value  = QuickInfoHelper.toString(this, "createView", m_CreateView, ", view only");
+    if (value != null)
+      result += value;
 
     return result;
   }
@@ -186,7 +216,7 @@ public class WekaCrossValidationSplit
   /**
    * Returns the class of objects that it generates.
    *
-   * @return		<!-- flow-generates-start -->adams.flow.container.TrainTestSetContainer.class<!-- flow-generates-end -->
+   * @return		<!-- flow-generates-start -->adams.flow.container.WekaTrainTestSetContainer.class<!-- flow-generates-end -->
    */
   public Class[] generates() {
     return new Class[]{WekaTrainTestSetContainer.class};
@@ -233,7 +263,7 @@ public class WekaCrossValidationSplit
     }
     else {
       getLogger().severe(
-	  "Folds must be >=2 or -1 for LOOCV (provided: " + value + ")!");
+	"Folds must be >=2 or -1 for LOOCV (provided: " + value + ")!");
     }
   }
 
@@ -282,6 +312,35 @@ public class WekaCrossValidationSplit
    */
   public String relationNameTipText() {
     return "The placeholders for creating the relation name: @ = original relation name, $T = type (train/test), $N = current fold number.";
+  }
+
+  /**
+   * Sets whether to create a view only.
+   *
+   * @param value	true if to create a view only
+   */
+  public void setCreateView(boolean value) {
+    m_CreateView = value;
+    reset();
+  }
+
+  /**
+   * Returns whether to create only a view.
+   *
+   * @return		true if to create view only
+   */
+  public boolean getCreateView() {
+    return m_CreateView;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String createViewTipText() {
+    return "If enabled, views of the dataset are created instead of actual copies.";
   }
 
   /**
@@ -348,6 +407,7 @@ public class WekaCrossValidationSplit
     result = null;
     try {
       m_Generator = new CrossValidationFoldGenerator((Instances) m_InputToken.getPayload(), m_Folds, m_Seed, true, true, m_RelationName);
+      m_Generator.setUseViews(m_CreateView);
     }
     catch (Exception e) {
       result = handleException("Failed to initalize fold generator!", e);
@@ -377,7 +437,7 @@ public class WekaCrossValidationSplit
     Token	result;
 
     result = new Token(m_Generator.next());
-    
+
     updateProvenance(result);
 
     return result;
--- adams-weka/src/main/java/adams/flow/transformer/WekaRandomSplit.java
+++ adams-weka/src/main/java/adams/flow/transformer/WekaRandomSplit.java
@@ -15,11 +15,12 @@
 
 /*
  * WekaRandomSplit.java
- * Copyright (C) 2009-2013 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2009-2016 University of Waikato, Hamilton, New Zealand
  */
 
 package adams.flow.transformer;
 
+import adams.data.weka.InstancesViewCreator;
 import weka.classifiers.RandomSplitGenerator;
 import weka.core.Instances;
 import adams.core.QuickInfoHelper;
@@ -40,55 +41,74 @@ import adams.flow.provenance.ProvenanceSupporter;
  <!-- globalinfo-end -->
  *
  <!-- flow-summary-start -->
- * Input/output:<br>
+ * Input&#47;output:<br>
  * - accepts:<br>
  * &nbsp;&nbsp;&nbsp;weka.core.Instances<br>
  * - generates:<br>
- * &nbsp;&nbsp;&nbsp;adams.flow.container.TrainTestSetContainer<br>
+ * &nbsp;&nbsp;&nbsp;adams.flow.container.WekaTrainTestSetContainer<br>
+ * <br><br>
+ * Container information:<br>
+ * - adams.flow.container.WekaTrainTestSetContainer: Train, Test, Seed, FoldNumber, FoldCount, Train original indices, Test original indices
  * <br><br>
  <!-- flow-summary-end -->
  *
  <!-- options-start -->
- * Valid options are: <br><br>
- *
- * <pre>-D &lt;int&gt; (property: debugLevel)
- * &nbsp;&nbsp;&nbsp;The greater the number the more additional info the scheme may output to
- * &nbsp;&nbsp;&nbsp;the console (0 = off).
- * &nbsp;&nbsp;&nbsp;default: 0
- * &nbsp;&nbsp;&nbsp;minimum: 0
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
  * </pre>
- *
+ * 
  * <pre>-name &lt;java.lang.String&gt; (property: name)
  * &nbsp;&nbsp;&nbsp;The name of the actor.
- * &nbsp;&nbsp;&nbsp;default: RandomSplit
+ * &nbsp;&nbsp;&nbsp;default: WekaRandomSplit
  * </pre>
- *
- * <pre>-annotation &lt;adams.core.base.BaseText&gt; (property: annotations)
+ * 
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
  * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
- * &nbsp;&nbsp;&nbsp;default:
+ * &nbsp;&nbsp;&nbsp;default: 
  * </pre>
- *
- * <pre>-skip (property: skip)
- * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded
+ * 
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded 
  * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- *
- * <pre>-preserve-order (property: preserveOrder)
+ * 
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this 
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical 
+ * &nbsp;&nbsp;&nbsp;actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing 
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-preserve-order &lt;boolean&gt; (property: preserveOrder)
  * &nbsp;&nbsp;&nbsp;If set to true, then the order is preserved by suppressing randomization.
+ * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- *
+ * 
  * <pre>-seed &lt;long&gt; (property: seed)
  * &nbsp;&nbsp;&nbsp;The seed value for the randomization.
  * &nbsp;&nbsp;&nbsp;default: 1
  * </pre>
- *
+ * 
  * <pre>-percentage &lt;double&gt; (property: percentage)
  * &nbsp;&nbsp;&nbsp;The percentage for the split (between 0 and 1).
  * &nbsp;&nbsp;&nbsp;default: 0.66
  * &nbsp;&nbsp;&nbsp;minimum: 1.0E-4
  * &nbsp;&nbsp;&nbsp;maximum: 0.9999
  * </pre>
- *
+ * 
+ * <pre>-create-view &lt;boolean&gt; (property: createView)
+ * &nbsp;&nbsp;&nbsp;If enabled, views of the dataset are created instead of actual copies.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
  <!-- options-end -->
  *
  * @author  fracpete (fracpete at waikato dot ac dot nz)
@@ -96,7 +116,7 @@ import adams.flow.provenance.ProvenanceSupporter;
  */
 public class WekaRandomSplit
   extends AbstractTransformer
-  implements Randomizable, ProvenanceSupporter {
+  implements Randomizable, ProvenanceSupporter, InstancesViewCreator {
 
   /** for serialization. */
   private static final long serialVersionUID = -6447945986570354931L;
@@ -110,6 +130,9 @@ public class WekaRandomSplit
   /** the percentage for the split (0-1). */
   protected double m_Percentage;
 
+  /** whether to create a view only. */
+  protected boolean m_CreateView;
+
   /**
    * Returns a string describing the object.
    *
@@ -118,11 +141,11 @@ public class WekaRandomSplit
   @Override
   public String globalInfo() {
     return
-        "Splits a dataset into a training and test set according to a "
-      + "specified split percentage. Randomization can be suppressed using "
-      + "with 'preserve order'.\n"
-      + "The training set can be accessed in the container with '" + WekaTrainTestSetContainer.VALUE_TRAIN + "' "
-      + "and the test set with '" + WekaTrainTestSetContainer.VALUE_TEST + "'.";
+      "Splits a dataset into a training and test set according to a "
+        + "specified split percentage. Randomization can be suppressed using "
+        + "with 'preserve order'.\n"
+        + "The training set can be accessed in the container with '" + WekaTrainTestSetContainer.VALUE_TRAIN + "' "
+        + "and the test set with '" + WekaTrainTestSetContainer.VALUE_TEST + "'.";
   }
 
   /**
@@ -133,16 +156,20 @@ public class WekaRandomSplit
     super.defineOptions();
 
     m_OptionManager.add(
-	    "preserve-order", "preserveOrder",
-	    false);
+      "preserve-order", "preserveOrder",
+      false);
 
     m_OptionManager.add(
-	    "seed", "seed",
-	    1L);
+      "seed", "seed",
+      1L);
 
     m_OptionManager.add(
-	    "percentage", "percentage",
-	    0.66, 0.0001, 0.9999);
+      "percentage", "percentage",
+      0.66, 0.0001, 0.9999);
+
+    m_OptionManager.add(
+      "create-view", "createView",
+      false);
   }
 
   /**
@@ -153,10 +180,14 @@ public class WekaRandomSplit
   @Override
   public String getQuickInfo() {
     String	result;
+    String	value;
 
     result  = QuickInfoHelper.toString(this, "percentage", m_Percentage);
     result += QuickInfoHelper.toString(this, "seed", m_Seed, ", seed: ");
     result += QuickInfoHelper.toString(this, "preserveOrder", m_PreserveOrder, "order preserved", ", ");
+    value  = QuickInfoHelper.toString(this, "createView", m_CreateView, ", view only");
+    if (value != null)
+      result += value;
 
     return result;
   }
@@ -173,7 +204,7 @@ public class WekaRandomSplit
   /**
    * Returns the class of objects that it generates.
    *
-   * @return		<!-- flow-generates-start -->adams.flow.container.TrainTestSetContainer.class<!-- flow-generates-end -->
+   * @return		<!-- flow-generates-start -->adams.flow.container.WekaTrainTestSetContainer.class<!-- flow-generates-end -->
    */
   public Class[] generates() {
     return new Class[]{WekaTrainTestSetContainer.class};
@@ -249,7 +280,7 @@ public class WekaRandomSplit
     }
     else {
       getLogger().severe(
-	  "Percentage must be between 0 and 1 (provided: " + value + ")!");
+        "Percentage must be between 0 and 1 (provided: " + value + ")!");
     }
   }
 
@@ -273,6 +304,35 @@ public class WekaRandomSplit
   }
 
   /**
+   * Sets whether to create a view only.
+   *
+   * @param value	true if to create a view only
+   */
+  public void setCreateView(boolean value) {
+    m_CreateView = value;
+    reset();
+  }
+
+  /**
+   * Returns whether to create only a view.
+   *
+   * @return		true if to create view only
+   */
+  public boolean getCreateView() {
+    return m_CreateView;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String createViewTipText() {
+    return "If enabled, views of the dataset are created instead of actual copies.";
+  }
+
+  /**
    * Executes the flow item.
    *
    * @return		null if everything is fine, otherwise error message
@@ -288,9 +348,10 @@ public class WekaRandomSplit
 
     try {
       if (m_PreserveOrder)
-	generator = new RandomSplitGenerator(inst, m_Percentage);
+        generator = new RandomSplitGenerator(inst, m_Percentage);
       else
-	generator = new RandomSplitGenerator(inst, m_Seed, m_Percentage);
+        generator = new RandomSplitGenerator(inst, m_Seed, m_Percentage);
+      generator.setUseViews(m_CreateView);
     }
     catch (Exception e) {
       generator = null;
@@ -313,7 +374,7 @@ public class WekaRandomSplit
   public void updateProvenance(ProvenanceContainer cont) {
     if (Provenance.getSingleton().isEnabled()) {
       if (m_InputToken.hasProvenance())
-	cont.setProvenance(m_InputToken.getProvenance().getClone());
+        cont.setProvenance(m_InputToken.getProvenance().getClone());
       cont.addProvenance(new ProvenanceInformation(ActorType.DATAGENERATOR, m_InputToken.getPayload().getClass(), this, m_OutputToken.getPayload().getClass()));
     }
   }
