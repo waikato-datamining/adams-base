95bdf5f202cc375c10c1015f642e38313e1df719
fracpete <fracpete@gmail.com>
2016-10-03 02:47:24 +0000

    added LookUpInit standalone, which initializes an empty lookup table in storage

---
+++ adams-spreadsheet/src/main/java/adams/flow/standalone/LookUpInit.java
@@ -0,0 +1,185 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * LookUpInit.java
+ * Copyright (C) 2016 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.standalone;
+
+import adams.core.ClassCrossReference;
+import adams.core.QuickInfoHelper;
+import adams.data.spreadsheet.LookUpHelper;
+import adams.flow.control.StorageName;
+import adams.flow.control.StorageUpdater;
+import adams.flow.transformer.LookUpAdd;
+
+/**
+ <!-- globalinfo-start -->
+ * Initializes an empty look up table in storage.<br>
+ * <br>
+ * See also:<br>
+ * adams.flow.transformer.LookUpAdd
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- flow-summary-start -->
+ <!-- flow-summary-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: LookUpInit
+ * </pre>
+ * 
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded 
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this 
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical 
+ * &nbsp;&nbsp;&nbsp;actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing 
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-storage-name &lt;adams.flow.control.StorageName&gt; (property: storageName)
+ * &nbsp;&nbsp;&nbsp;The name for the lookup table in the internal storage.
+ * &nbsp;&nbsp;&nbsp;default: lookup
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class LookUpInit
+  extends AbstractStandalone
+  implements StorageUpdater, ClassCrossReference {
+
+  private static final long serialVersionUID = 8858771610997384634L;
+
+  /** the name of the lookup table in the internal storage. */
+  protected StorageName m_StorageName;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Initializes an empty look up table in storage.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+	    "storage-name", "storageName",
+	    new StorageName("lookup"));
+  }
+
+  /**
+   * Returns the cross-referenced classes.
+   *
+   * @return		the classes
+   */
+  public Class[] getClassCrossReferences() {
+    return new Class[]{LookUpAdd.class};
+  }
+
+  /**
+   * Returns whether storage items are being updated.
+   *
+   * @return		true if storage items are updated
+   */
+  public boolean isUpdatingStorage() {
+    return !getSkip();
+  }
+
+  /**
+   * Returns a quick info about the actor, which will be displayed in the GUI.
+   *
+   * @return		null if no info available, otherwise short string
+   */
+  @Override
+  public String getQuickInfo() {
+    return QuickInfoHelper.toString(this, "storageName", m_StorageName, "storage: ");
+  }
+
+  /**
+   * Sets the name for the lookup table in the internal storage.
+   *
+   * @param value	the name
+   */
+  public void setStorageName(StorageName value) {
+    m_StorageName = value;
+    reset();
+  }
+
+  /**
+   * Returns the name for the lookup table in the internal storage.
+   *
+   * @return		the name
+   */
+  public StorageName getStorageName() {
+    return m_StorageName;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String storageNameTipText() {
+    return "The name for the lookup table in the internal storage.";
+  }
+
+  /**
+   * Executes the flow item.
+   *
+   * @return		null if everything is fine, otherwise error message
+   */
+  @Override
+  protected String doExecute() {
+    getStorageHandler().getStorage().put(m_StorageName, LookUpHelper.newTable());
+    return null;
+  }
+}
