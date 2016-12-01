83f04573a50824fac49f14478e06f4cc0274bbd2
fracpete <fracpete@gmail.com>
2016-12-01 01:41:42 +0000

    can make use of storage items now as well

--- adams-core/src/main/java/adams/flow/source/MakeContainer.java
+++ adams-core/src/main/java/adams/flow/source/MakeContainer.java
@@ -26,6 +26,8 @@ import adams.core.Utils;
 import adams.core.base.BaseString;
 import adams.flow.container.AbstractContainer;
 import adams.flow.container.SequencePlotterContainer;
+import adams.flow.control.StorageName;
+import adams.flow.control.StorageUser;
 import adams.flow.core.CallableActorHelper;
 import adams.flow.core.CallableActorReference;
 import adams.flow.core.Token;
@@ -39,7 +41,7 @@ import java.util.Iterator;
 
 /**
  <!-- globalinfo-start -->
- * Assembles a container with data obtained from callable actors.
+ * Assembles a container with data obtained from either callable actors or storage items.
  * <br><br>
  <!-- globalinfo-end -->
  *
@@ -49,7 +51,7 @@ import java.util.Iterator;
  * &nbsp;&nbsp;&nbsp;adams.flow.container.SequencePlotterContainer<br>
  * <br><br>
  * Container information:<br>
- * - adams.flow.container.SequencePlotterContainer: PlotName, X, Y, Content type, Error X, Error Y
+ * - adams.flow.container.SequencePlotterContainer: PlotName, X, Y, Content type, Error X, Error Y, MetaData
  * <br><br>
  <!-- flow-summary-end -->
  *
@@ -64,7 +66,7 @@ import java.util.Iterator;
  * &nbsp;&nbsp;&nbsp;default: MakeContainer
  * </pre>
  * 
- * <pre>-annotation &lt;adams.core.base.BaseText&gt; (property: annotations)
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
  * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
  * &nbsp;&nbsp;&nbsp;default: 
  * </pre>
@@ -76,19 +78,31 @@ import java.util.Iterator;
  * </pre>
  * 
  * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
- * &nbsp;&nbsp;&nbsp;If set to true, the flow gets stopped in case this actor encounters an error;
- * &nbsp;&nbsp;&nbsp; useful for critical actors.
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this 
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical 
+ * &nbsp;&nbsp;&nbsp;actors.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
  * 
- * <pre>-global-actor &lt;adams.flow.core.CallableActorReference&gt; [-global-actor ...] (property: callableActors)
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing 
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-callable-actor &lt;adams.flow.core.CallableActorReference&gt; [-callable-actor ...] (property: callableActors)
  * &nbsp;&nbsp;&nbsp;The callable actors to obtain the data from.
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-storage-name &lt;adams.flow.control.StorageName&gt; [-storage-name ...] (property: storageNames)
+ * &nbsp;&nbsp;&nbsp;The storage items to use.
  * &nbsp;&nbsp;&nbsp;default: 
  * </pre>
  * 
  * <pre>-value-name &lt;adams.core.base.BaseString&gt; [-value-name ...] (property: valueNames)
  * &nbsp;&nbsp;&nbsp;The names to use for storing the values in the container:PlotName, X, Y, 
- * &nbsp;&nbsp;&nbsp;Content type, Error X, Error Y.
+ * &nbsp;&nbsp;&nbsp;Content type, Error X, Error Y, MetaData.
  * &nbsp;&nbsp;&nbsp;default: 
  * </pre>
  * 
@@ -103,8 +117,8 @@ import java.util.Iterator;
  * @version $Revision$
  */
 public class MakeContainer
-  extends AbstractSimpleSource 
-  implements ProvenanceSupporter {
+  extends AbstractSimpleSource
+  implements ProvenanceSupporter, StorageUser {
 
   /** for serialization. */
   private static final long serialVersionUID = -132045002653940359L;
@@ -112,8 +126,14 @@ public class MakeContainer
   /** the callable actors to retrieve the data from. */
   protected CallableActorReference[] m_CallableActors;
 
+  /** the storage names to retrieve the data from. */
+  protected StorageName[] m_StorageNames;
+
   /** the names under which to store the data. */
   protected BaseString[] m_ValueNames;
+
+  /** whether to use callable actors or storage. */
+  protected boolean m_UseStorage;
 
   /** the container class to create. */
   protected AbstractContainer m_ContainerClass;
@@ -129,7 +149,8 @@ public class MakeContainer
   @Override
   public String globalInfo() {
     return
-        "Assembles a container with data obtained from callable actors.";
+      "Assembles a container with data obtained from either callable actors " 
+        + "or storage items.";
   }
 
   /**
@@ -140,16 +161,20 @@ public class MakeContainer
     super.defineOptions();
 
     m_OptionManager.add(
-	    "callable-actor", "callableActors",
-	    new CallableActorReference[0]);
+      "callable-actor", "callableActors",
+      new CallableActorReference[0]);
 
     m_OptionManager.add(
-	    "value-name", "valueNames",
-	    new BaseString[0]);
+      "storage-name", "storageNames",
+      new StorageName[0]);
 
     m_OptionManager.add(
-	    "container-class", "containerClass",
-	    new SequencePlotterContainer());
+      "value-name", "valueNames",
+      new BaseString[0]);
+
+    m_OptionManager.add(
+      "container-class", "containerClass",
+      new SequencePlotterContainer());
   }
 
   /**
@@ -161,6 +186,15 @@ public class MakeContainer
 
     m_OutputToken     = null;
     m_ContainerValues = null;
+  }
+
+  /**
+   * Returns whether storage items are being used.
+   *
+   * @return		true if storage items are used
+   */
+  public boolean isUsingStorage() {
+    return !getSkip() && m_UseStorage;
   }
 
   /**
@@ -204,13 +238,73 @@ public class MakeContainer
   }
 
   /**
+   * Sets the names of the storage items to use.
+   *
+   * @param value	the names
+   */
+  public void setStorageNames(StorageName[] value) {
+    m_StorageNames = value;
+    m_ValueNames   = (BaseString[]) Utils.adjustArray(m_ValueNames, m_StorageNames.length, new BaseString("unknown"));
+    reset();
+  }
+
+  /**
+   * Returns the names of the storage items to use.
+   *
+   * @return 		the names
+   */
+  public StorageName[] getStorageNames() {
+    return m_StorageNames;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return		tip text for this property suitable for
+   *             	displaying in the GUI or for listing the options.
+   */
+  public String storageNamesTipText() {
+    return "The storage items to use.";
+  }
+
+  /**
+   * Sets whether to use storage items or data from callable actors.
+   *
+   * @param value	true if to use storage
+   */
+  public void setUseStorage(boolean value) {
+    m_UseStorage = value;
+    reset();
+  }
+
+  /**
+   * Returns whether to use storage items or data from callable actors.
+   *
+   * @return 		true if to use storage
+   */
+  public boolean getUseStorage() {
+    return m_UseStorage;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return		tip text for this property suitable for
+   *             	displaying in the GUI or for listing the options.
+   */
+  public String useStorageTipText() {
+    return "Whether to use storage items or data from callable actors.";
+  }
+
+  /**
    * Sets the names of the values in the container.
    *
    * @param value	the names
    */
   public void setValueNames(BaseString[] value) {
-    m_ValueNames   = value;
+    m_ValueNames     = value;
     m_CallableActors = (CallableActorReference[]) Utils.adjustArray(m_CallableActors, m_ValueNames.length, new CallableActorReference("unknown"));
+    m_StorageNames   = (StorageName[]) Utils.adjustArray(m_StorageNames, m_ValueNames.length, new StorageName());
     reset();
   }
 
@@ -237,8 +331,8 @@ public class MakeContainer
       values = values.replace(",", ", ");
 
     return
-        "The names to use for storing the values in the container"
-      + ((values == null) ? "" : ":" + values) + ".";
+      "The names to use for storing the values in the container"
+        + ((values == null) ? "" : ":" + values) + ".";
   }
 
   /**
@@ -298,9 +392,9 @@ public class MakeContainer
       m_ContainerValues = "";
       names             = m_ContainerClass.names();
       while (names.hasNext()) {
-	if (m_ContainerValues.length() > 0)
-	  m_ContainerValues += ",";
-	m_ContainerValues += names.next();
+        if (m_ContainerValues.length() > 0)
+          m_ContainerValues += ",";
+        m_ContainerValues += names.next();
       }
     }
 
@@ -322,8 +416,24 @@ public class MakeContainer
     result = CallableActorHelper.getSetupFromSource(null, name, this, errors);
     if (result == null) {
       if (!errors.isEmpty())
-	getLogger().severe(errors.toString());
+        getLogger().severe(errors.toString());
     }
+
+    return result;
+  }
+
+  /**
+   * Returns the value obtained from the callable actor.
+   *
+   * @param name	the name of the callable actor to get the value from
+   * @return		the obtained value
+   */
+  protected Object getValue(StorageName name) {
+    Object		result;
+
+    result = getStorageHandler().getStorage().get(name);
+    if (result == null)
+      getLogger().severe("Failed to retrieve storage item for name: " + name);
 
     return result;
   }
@@ -343,9 +453,17 @@ public class MakeContainer
     result = null;
 
     cont = getContainerClass().getClone();
-    for (i = 0; i < m_CallableActors.length; i++) {
-      value = getValue(m_CallableActors[i]);
-      cont.setValue(m_ValueNames[i].getValue(), value);
+    if (m_UseStorage) {
+      for (i = 0; i < m_StorageNames.length; i++) {
+        value = getValue(m_StorageNames[i]);
+        cont.setValue(m_ValueNames[i].getValue(), value);
+      }
+    }
+    else {
+      for (i = 0; i < m_CallableActors.length; i++) {
+        value = getValue(m_CallableActors[i]);
+        cont.setValue(m_ValueNames[i].getValue(), value);
+      }
     }
     if (cont.isValid())
       m_OutputToken = new Token(cont);
