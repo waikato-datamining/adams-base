71e31def9746386062bdc8150f3cb5c4300fec27
fracpete <fracpete@gmail.com>
2016-12-05 03:17:00 +0000

    using shortened titles for tabs now

--- adams-imaging/src/main/java/adams/gui/tools/ImageProcessorTabbedPane.java
+++ adams-imaging/src/main/java/adams/gui/tools/ImageProcessorTabbedPane.java
@@ -15,7 +15,7 @@
 
 /**
  * ImageProcessorTabbedPane.java
- * Copyright (C) 2014-2015 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2014-2016 University of Waikato, Hamilton, New Zealand
  */
 package adams.gui.tools;
 
@@ -154,6 +154,7 @@ public class ImageProcessorTabbedPane
     }
     else {
       addTab(file.getName(), panel);
+      setShortenedTitleAt(getTabCount() - 1, file.getName());
       setSelectedComponent(panel);
       return true;
     }
