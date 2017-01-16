185652d4a8a475c93ae497e9d5e24954719c9a0f
fracpete <fracpete@gmail.com>
2017-01-16 02:28:47 +0000

    added ConditionalSequence control actor

---
+++ adams-core/src/main/java/adams/flow/control/ConditionalSequence.java
@@ -0,0 +1,215 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * ConditionalSequence.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.flow.control;
+
+import adams.flow.condition.bool.BooleanCondition;
+import adams.flow.condition.bool.BooleanConditionSupporter;
+import adams.flow.condition.bool.Expression;
+
+/**
+ <!-- globalinfo-start -->
+ * The sequence gets only executed if the condition holds true.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: ConditionalSequence
+ * </pre>
+ * 
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded 
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this 
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical 
+ * &nbsp;&nbsp;&nbsp;actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing 
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-finish-before-stopping &lt;boolean&gt; (property: finishBeforeStopping)
+ * &nbsp;&nbsp;&nbsp;If enabled, actor first finishes processing all data before stopping.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-actor &lt;adams.flow.core.Actor&gt; [-actor ...] (property: actors)
+ * &nbsp;&nbsp;&nbsp;All the actors that define this sequence.
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-condition &lt;adams.flow.condition.bool.BooleanCondition&gt; (property: condition)
+ * &nbsp;&nbsp;&nbsp;The condition to evaluate - only as long as it evaluates to 'true' the loop 
+ * &nbsp;&nbsp;&nbsp;actors get executed.
+ * &nbsp;&nbsp;&nbsp;default: adams.flow.condition.bool.Expression
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class ConditionalSequence
+  extends Sequence
+  implements BooleanConditionSupporter {
+
+  /** for serialization. */
+  private static final long serialVersionUID = 619693652039880564L;
+  
+  /** the condition that determines the execution of the subprocess items. */
+  protected BooleanCondition m_Condition;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "The sequence gets only executed if the condition holds true.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+	    "condition", "condition",
+	    new Expression());
+  }
+
+  /**
+   * Sets the condition to evaluate.
+   *
+   * @param value	the condition
+   */
+  public void setCondition(BooleanCondition value) {
+    m_Condition = value;
+    reset();
+  }
+
+  /**
+   * Returns the condtion to evaluate.
+   *
+   * @return		the condition
+   */
+  public BooleanCondition getCondition() {
+    return m_Condition;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String conditionTipText() {
+    return
+        "The condition to evaluate - only as long as it evaluates to 'true' "
+      + "the loop actors get executed.";
+  }
+
+  /**
+   * Returns a quick info about the actor, which will be displayed in the GUI.
+   *
+   * @return		null if no info available, otherwise short string
+   */
+  @Override
+  public String getQuickInfo() {
+    String	result;
+    
+    result = m_Condition.getQuickInfo();
+    
+    if (super.getQuickInfo() != null)
+      result += ", " + super.getQuickInfo();
+    
+    return result;
+  }
+
+  /**
+   * Initializes the sub-actors for flow execution.
+   *
+   * @return		null if everything is fine, otherwise error message
+   */
+  @Override
+  public String setUp() {
+    String	result;
+
+    result = super.setUp();
+
+    if (result == null) {
+      if (m_Condition == null)
+	result = "No condition provided!";
+    }
+
+    if (result == null)
+      result = m_Condition.setUp(this);
+    
+    return result;
+  }
+
+  /**
+   * Executes the actor.
+   *
+   * @return		null if everything is fine, otherwise error message
+   */
+  @Override
+  protected String doExecute() {
+    String	result;
+
+    result = null;
+
+    if (m_Condition.evaluate(this, m_CurrentToken))
+      result = super.doExecute();
+
+    return result;
+  }
+
+  /**
+   * Stops the execution.
+   */
+  public void stopExecution() {
+    m_Condition.stopExecution();
+    super.stopExecution();
+  }
+}
