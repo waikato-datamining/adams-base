56a620d111bb8223209f91539d189b3f467525d6
fracpete <fracpete@gmail.com>
2017-02-11 09:47:29 +0000

    ConfusionMatrix transformer generates confusion matrix in spreadsheet format fromm spreadsheet with actual/predicted labels

---
+++ adams-ml/src/main/java/adams/flow/transformer/ConfusionMatrix.java
@@ -0,0 +1,388 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * ConfusionMatrix.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.transformer;
+
+import adams.core.QuickInfoHelper;
+import adams.data.spreadsheet.DefaultSpreadSheet;
+import adams.data.spreadsheet.Row;
+import adams.data.spreadsheet.SpreadSheet;
+import adams.data.spreadsheet.SpreadSheetColumnIndex;
+import adams.flow.core.Token;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ <!-- globalinfo-start -->
+ * Generates a confusion matrix from the specified actual and predicted columns containing class labels.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- flow-summary-start -->
+ * Input&#47;output:<br>
+ * - accepts:<br>
+ * &nbsp;&nbsp;&nbsp;adams.data.spreadsheet.SpreadSheet<br>
+ * - generates:<br>
+ * &nbsp;&nbsp;&nbsp;adams.data.spreadsheet.SpreadSheet<br>
+ * <br><br>
+ <!-- flow-summary-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: ConfusionMatrix
+ * </pre>
+ * 
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded 
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this 
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical 
+ * &nbsp;&nbsp;&nbsp;actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing 
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-actual-column &lt;adams.data.spreadsheet.SpreadSheetColumnIndex&gt; (property: actualColumn)
+ * &nbsp;&nbsp;&nbsp;The column with the actual labels.
+ * &nbsp;&nbsp;&nbsp;default: 1
+ * &nbsp;&nbsp;&nbsp;example: An index is a number starting with 1; column names (case-sensitive) as well as the following placeholders can be used: first, second, third, last_2, last_1, last; numeric indices can be enforced by preceding them with '#' (eg '#12'); column names can be surrounded by double quotes.
+ * </pre>
+ * 
+ * <pre>-actual-prefix &lt;java.lang.String&gt; (property: actualPrefix)
+ * &nbsp;&nbsp;&nbsp;The prefix for the actual labels.
+ * &nbsp;&nbsp;&nbsp;default: a: 
+ * </pre>
+ * 
+ * <pre>-predicted-column &lt;adams.data.spreadsheet.SpreadSheetColumnIndex&gt; (property: predictedColumn)
+ * &nbsp;&nbsp;&nbsp;The column with the predicted labels.
+ * &nbsp;&nbsp;&nbsp;default: 2
+ * &nbsp;&nbsp;&nbsp;example: An index is a number starting with 1; column names (case-sensitive) as well as the following placeholders can be used: first, second, third, last_2, last_1, last; numeric indices can be enforced by preceding them with '#' (eg '#12'); column names can be surrounded by double quotes.
+ * </pre>
+ * 
+ * <pre>-predicted-prefix &lt;java.lang.String&gt; (property: predictedPrefix)
+ * &nbsp;&nbsp;&nbsp;The prefix for the predicted labels.
+ * &nbsp;&nbsp;&nbsp;default: p: 
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class ConfusionMatrix
+  extends AbstractTransformer {
+
+  private static final long serialVersionUID = 6499246835313298302L;
+
+  /** the column with the actual labels. */
+  protected SpreadSheetColumnIndex m_ActualColumn;
+  
+  /** the optional prefix for the actual labels. */
+  protected String m_ActualPrefix;
+
+  /** the column with the predicted labels. */
+  protected SpreadSheetColumnIndex m_PredictedColumn;
+  
+  /** the optional prefix for the predicted labels. */
+  protected String m_PredictedPrefix;
+  
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return
+      "Generates a confusion matrix from the specified actual and predicted "
+	+ "columns containing class labels.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+      "actual-column", "actualColumn",
+      new SpreadSheetColumnIndex("1"));
+
+    m_OptionManager.add(
+      "actual-prefix", "actualPrefix",
+      "a: ");
+
+    m_OptionManager.add(
+      "predicted-column", "predictedColumn",
+      new SpreadSheetColumnIndex("2"));
+
+    m_OptionManager.add(
+      "predicted-prefix", "predictedPrefix",
+      "p: ");
+  }
+
+  /**
+   * Returns a quick info about the actor, which will be displayed in the GUI.
+   *
+   * @return		null if no info available, otherwise short string
+   */
+  @Override
+  public String getQuickInfo() {
+    String	result;
+
+    result  = QuickInfoHelper.toString(this, "actualColumn", m_ActualColumn, "actual: ");
+    result += QuickInfoHelper.toString(this, "predictedColumn", m_PredictedColumn, ", predicted: ");
+
+    return result;
+  }
+
+  /**
+   * Sets the column of the actual labels.
+   *
+   * @param value	the index
+   */
+  public void setActualColumn(SpreadSheetColumnIndex value) {
+    m_ActualColumn = value;
+    reset();
+  }
+
+  /**
+   * Returns the column of the actual labels.
+   *
+   * @return		the index
+   */
+  public SpreadSheetColumnIndex getActualColumn() {
+    return m_ActualColumn;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String actualColumnTipText() {
+    return "The column with the actual labels.";
+  }
+
+  /**
+   * Sets the prefix of the actual labels.
+   *
+   * @param value	the prefix
+   */
+  public void setActualPrefix(String value) {
+    m_ActualPrefix = value;
+    reset();
+  }
+
+  /**
+   * Returns the prefix of the actual labels.
+   *
+   * @return		the prefix
+   */
+  public String getActualPrefix() {
+    return m_ActualPrefix;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String actualPrefixTipText() {
+    return "The prefix for the actual labels.";
+  }
+
+  /**
+   * Sets the column of the predicted labels.
+   *
+   * @param value	the index
+   */
+  public void setPredictedColumn(SpreadSheetColumnIndex value) {
+    m_PredictedColumn = value;
+    reset();
+  }
+
+  /**
+   * Returns the column of the predicted labels.
+   *
+   * @return		the index
+   */
+  public SpreadSheetColumnIndex getPredictedColumn() {
+    return m_PredictedColumn;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String predictedColumnTipText() {
+    return "The column with the predicted labels.";
+  }
+
+  /**
+   * Sets the prefix of the predicted labels.
+   *
+   * @param value	the prefix
+   */
+  public void setPredictedPrefix(String value) {
+    m_PredictedPrefix = value;
+    reset();
+  }
+
+  /**
+   * Returns the prefix of the predicted labels.
+   *
+   * @return		the prefix
+   */
+  public String getPredictedPrefix() {
+    return m_PredictedPrefix;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String predictedPrefixTipText() {
+    return "The prefix for the predicted labels.";
+  }
+
+  /**
+   * Returns the class that the consumer accepts.
+   *
+   * @return		the Class of objects that can be processed
+   */
+  @Override
+  public Class[] accepts() {
+    return new Class[]{SpreadSheet.class};
+  }
+
+  /**
+   * Returns the class of objects that it generates.
+   *
+   * @return		the Class of the generated tokens
+   */
+  @Override
+  public Class[] generates() {
+    return new Class[]{SpreadSheet.class};
+  }
+
+  /**
+   * Executes the flow item.
+   *
+   * @return		null if everything is fine, otherwise error message
+   */
+  @Override
+  protected String doExecute() {
+    String		result;
+    SpreadSheet		sheet;
+    int			actCol;
+    int			predCol;
+    SpreadSheet		matrix;
+    Row			row;
+    List<String> 	actLabels;
+    Map<String,Integer> actIndices;
+    Map<String,Integer> predIndices;
+    String 		actLabel;
+    String 		predLabel;
+    int			actIndex;
+    int			predIndex;
+    int			i;
+    int			n;
+
+    result = null;
+    sheet  = (SpreadSheet) m_InputToken.getPayload();
+    m_ActualColumn.setData(sheet);
+    m_PredictedColumn.setData(sheet);
+    actCol  = m_ActualColumn.getIntIndex();
+    predCol = m_PredictedColumn.getIntIndex();
+    if (actCol == -1)
+      result = "Actual column not found: " + m_ActualColumn;
+    else if (predCol == -1)
+      result = "Predicted column not found: " + m_PredictedColumn;
+
+    if (result == null) {
+      // set up matrix
+      actIndices = new HashMap<>();
+      predIndices = new HashMap<>();
+      actLabels = sheet.getCellValues(actCol);
+      matrix = new DefaultSpreadSheet();
+      row = matrix.getHeaderRow();
+      row.addCell("0").setContentAsString("x");
+      for (i = 0; i < actLabels.size(); i++) {
+	row.addCell("" + (i + 1)).setContentAsString(m_PredictedPrefix + actLabels.get(i));
+	predIndices.put(actLabels.get(i), i+1);
+      }
+      for (i = 0; i < actLabels.size(); i++) {
+	row = matrix.addRow();
+	for (n = 0; n < matrix.getColumnCount(); n++)
+	  row.getCell(n).setContent(0);
+	row.addCell(0).setContentAsString(m_ActualPrefix + actLabels.get(i));
+	actIndices.put(actLabels.get(i), i);
+      }
+
+      // fill in matrix
+      for (i = 0; i < sheet.getRowCount(); i++) {
+	row = sheet.getRow(i);
+	if (!row.hasCell(actCol) || row.getCell(actCol).isMissing())
+	  continue;
+	if (!row.hasCell(predCol) || row.getCell(predCol).isMissing())
+	  continue;
+	actLabel  = row.getCell(actCol).getContent();
+	predLabel = row.getCell(predCol).getContent();
+	actIndex  = actIndices.get(actLabel);
+	predIndex = predIndices.get(predLabel);
+	matrix.getCell(actIndex, predIndex).setContent(matrix.getCell(actIndex, predIndex).toLong() + 1);
+      }
+
+      m_OutputToken = new Token(matrix);
+    }
+
+    return result;
+  }
+}
---
+++ adams-ml/src/test/java/adams/flow/transformer/ConfusionMatrixTest.java
@@ -0,0 +1,180 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * ConfusionMatrixTest.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.flow.transformer;
+
+import adams.core.Range;
+import adams.core.io.PlaceholderFile;
+import adams.core.option.AbstractArgumentOption;
+import adams.data.io.input.CsvSpreadSheetReader;
+import adams.data.spreadsheet.DefaultSpreadSheet;
+import adams.data.spreadsheet.DenseDataRow;
+import adams.data.spreadsheet.SpreadSheetColumnIndex;
+import adams.env.Environment;
+import adams.flow.AbstractFlowTest;
+import adams.flow.control.Flow;
+import adams.flow.core.AbstractActor;
+import adams.flow.core.Actor;
+import adams.flow.execution.NullListener;
+import adams.flow.sink.DumpFile;
+import adams.flow.source.FileSupplier;
+import adams.test.TmpFile;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Test for ConfusionMatrix actor.
+ *
+ * @author fracpete
+ * @author adams.core.option.FlowJUnitTestProducer (code generator)
+ * @version $Revision$
+ */
+public class ConfusionMatrixTest
+  extends AbstractFlowTest {
+
+  /**
+   * Initializes the test.
+   *
+   * @param name	the name of the test
+   */
+  public ConfusionMatrixTest(String name) {
+    super(name);
+  }
+
+  /**
+   * Called by JUnit before each test method.
+   *
+   * @throws Exception 	if an error occurs.
+   */
+  protected void setUp() throws Exception {
+    super.setUp();
+    
+    m_TestHelper.copyResourceToTmp("confusion.csv");
+    m_TestHelper.deleteFileFromTmp("dumpfile.csv");
+  }
+
+  /**
+   * Called by JUnit after each test method.
+   *
+   * @throws Exception	if tear-down fails
+   */
+  protected void tearDown() throws Exception {
+    m_TestHelper.deleteFileFromTmp("confusion.csv");
+    m_TestHelper.deleteFileFromTmp("dumpfile.csv");
+    
+    super.tearDown();
+  }
+
+  /**
+   * Performs a regression test, comparing against previously generated output.
+   */
+  public void testRegression() {
+    performRegressionTest(
+        new TmpFile[]{
+          new TmpFile("dumpfile.csv")
+        });
+  }
+
+  /**
+   * 
+   * Returns a test suite.
+   *
+   * @return		the test suite
+   */
+  public static Test suite() {
+    return new TestSuite(ConfusionMatrixTest.class);
+  }
+
+  /**
+   * Used to create an instance of a specific actor.
+   *
+   * @return a suitably configured <code>AbstractActor</code> value
+   */
+  public AbstractActor getActor() {
+    AbstractArgumentOption    argOption;
+    
+    Flow flow = new Flow();
+    
+    try {
+      List<Actor> actors = new ArrayList<>();
+
+      // Flow.FileSupplier
+      FileSupplier filesupplier = new FileSupplier();
+      argOption = (AbstractArgumentOption) filesupplier.getOptionManager().findByProperty("files");
+      List<PlaceholderFile> files = new ArrayList<>();
+      files.add((PlaceholderFile) argOption.valueOf("${TMP}/confusion.csv"));
+      filesupplier.setFiles(files.toArray(new PlaceholderFile[0]));
+      actors.add(filesupplier);
+
+      // Flow.SpreadSheetFileReader
+      SpreadSheetFileReader spreadsheetfilereader = new SpreadSheetFileReader();
+      CsvSpreadSheetReader csvspreadsheetreader = new CsvSpreadSheetReader();
+      DenseDataRow densedatarow = new DenseDataRow();
+      csvspreadsheetreader.setDataRowType(densedatarow);
+
+      DefaultSpreadSheet defaultspreadsheet = new DefaultSpreadSheet();
+      csvspreadsheetreader.setSpreadSheetType(defaultspreadsheet);
+
+      argOption = (AbstractArgumentOption) csvspreadsheetreader.getOptionManager().findByProperty("textColumns");
+      csvspreadsheetreader.setTextColumns((Range) argOption.valueOf("first-last"));
+      spreadsheetfilereader.setReader(csvspreadsheetreader);
+
+      actors.add(spreadsheetfilereader);
+
+      // Flow.ConfusionMatrix
+      ConfusionMatrix confusionmatrix = new ConfusionMatrix();
+      argOption = (AbstractArgumentOption) confusionmatrix.getOptionManager().findByProperty("actualColumn");
+      confusionmatrix.setActualColumn((SpreadSheetColumnIndex) argOption.valueOf("last"));
+      argOption = (AbstractArgumentOption) confusionmatrix.getOptionManager().findByProperty("predictedColumn");
+      confusionmatrix.setPredictedColumn((SpreadSheetColumnIndex) argOption.valueOf("last_1"));
+      actors.add(confusionmatrix);
+
+      // Flow.DumpFile
+      DumpFile dumpfile = new DumpFile();
+      argOption = (AbstractArgumentOption) dumpfile.getOptionManager().findByProperty("outputFile");
+      dumpfile.setOutputFile((PlaceholderFile) argOption.valueOf("${TMP}/dumpfile.csv"));
+      actors.add(dumpfile);
+      flow.setActors(actors.toArray(new Actor[0]));
+
+      NullListener nulllistener = new NullListener();
+      flow.setFlowExecutionListener(nulllistener);
+
+    }
+    catch (Exception e) {
+      fail("Failed to set up actor: " + e);
+    }
+    
+    return flow;
+  }
+
+  /**
+   * Runs the test from commandline.
+   *
+   * @param args	ignored
+   */
+  public static void main(String[] args) {
+    Environment.setEnvironmentClass(adams.env.Environment.class);
+    runTest(suite());
+  }
+}
+
