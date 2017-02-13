8445d8dad917b19841ff33a031fe4ce2cfd1c4a6
fracpete <fracpete@gmail.com>
2017-02-13 23:22:57 +0000

    HashSetInit now allows initializing with string values

--- adams-core/src/main/java/adams/flow/standalone/HashSetInit.java
+++ adams-core/src/main/java/adams/flow/standalone/HashSetInit.java
@@ -15,19 +15,21 @@
 
 /**
  * HashSetInit.java
- * Copyright (C) 2013 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2013-2017 University of Waikato, Hamilton, New Zealand
  */
 package adams.flow.standalone;
 
-import java.util.HashSet;
-
 import adams.core.QuickInfoHelper;
+import adams.core.base.BaseString;
 import adams.flow.control.StorageName;
 import adams.flow.control.StorageUpdater;
 
+import java.util.HashSet;
+
 /**
  <!-- globalinfo-start -->
- * Creates an empty hashset in internal storage under the specified name.
+ * Creates an empty hashset in internal storage under the specified name.<br>
+ * Initial (string) values can be supplied as well.
  * <br><br>
  <!-- globalinfo-end -->
  *
@@ -45,7 +47,7 @@ import adams.flow.control.StorageUpdater;
  * &nbsp;&nbsp;&nbsp;default: HashSetInit
  * </pre>
  * 
- * <pre>-annotation &lt;adams.core.base.BaseText&gt; (property: annotations)
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
  * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
  * &nbsp;&nbsp;&nbsp;default: 
  * </pre>
@@ -57,14 +59,26 @@ import adams.flow.control.StorageUpdater;
  * </pre>
  * 
  * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
- * &nbsp;&nbsp;&nbsp;If set to true, the flow gets stopped in case this actor encounters an error;
- * &nbsp;&nbsp;&nbsp; useful for critical actors.
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this 
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical 
+ * &nbsp;&nbsp;&nbsp;actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing 
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
  * 
  * <pre>-storage-name &lt;adams.flow.control.StorageName&gt; (property: storageName)
  * &nbsp;&nbsp;&nbsp;The name of the hashset in the internal storage.
  * &nbsp;&nbsp;&nbsp;default: hashset
+ * </pre>
+ * 
+ * <pre>-initial &lt;adams.core.base.BaseString&gt; [-initial ...] (property: initial)
+ * &nbsp;&nbsp;&nbsp;The (optional) initial strings to populate the hashset with.
+ * &nbsp;&nbsp;&nbsp;default: 
  * </pre>
  * 
  <!-- options-end -->
@@ -82,6 +96,9 @@ public class HashSetInit
   /** the name of the hashset in the internal storage. */
   protected StorageName m_StorageName;
 
+  /** the initial strings to populate the hashset with. */
+  protected BaseString[] m_Initial;
+
   /**
    * Returns a string describing the object.
    *
@@ -89,8 +106,9 @@ public class HashSetInit
    */
   @Override
   public String globalInfo() {
-    return 
-	"Creates an empty hashset in internal storage under the specified name.";
+    return
+      "Creates an empty hashset in internal storage under the specified name.\n"
+	+ "Initial (string) values can be supplied as well.";
   }
   
   /**
@@ -103,6 +121,10 @@ public class HashSetInit
     m_OptionManager.add(
 	    "storage-name", "storageName",
 	    new StorageName("hashset"));
+
+    m_OptionManager.add(
+	    "initial", "initial",
+	    new BaseString[0]);
   }
 
   /**
@@ -124,6 +146,7 @@ public class HashSetInit
     String	result;
 
     result  = QuickInfoHelper.toString(this, "storageName", m_StorageName, "storage: ");
+    result += QuickInfoHelper.toString(this, "initial", (m_Initial.length == 0 ? "-no initial values-" : m_Initial), ", initial: ");
 
     return result;
   }
@@ -156,7 +179,36 @@ public class HashSetInit
   public String storageNameTipText() {
     return "The name of the hashset in the internal storage.";
   }
-  
+
+  /**
+   * Sets the (optional) initial values to populate the hashset with.
+   *
+   * @param value	the values
+   */
+  public void setInitial(BaseString[] value) {
+    m_Initial = value;
+    reset();
+  }
+
+  /**
+   * Returns the (optional) initial values to populate the hashset with.
+   *
+   * @return		the values
+   */
+  public BaseString[] getInitial() {
+    return m_Initial;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String initialTipText() {
+    return "The (optional) initial strings to populate the hashset with.";
+  }
+
   /**
    * Executes the flow item.
    *
@@ -164,7 +216,12 @@ public class HashSetInit
    */
   @Override
   protected String doExecute() {
-    getStorageHandler().getStorage().put(m_StorageName, new HashSet());
+    HashSet	hashset;
+
+    hashset = new HashSet();
+    for (BaseString value: m_Initial)
+      hashset.add(value.getValue());
+    getStorageHandler().getStorage().put(m_StorageName, hashset);
     return null;
   }
 }
