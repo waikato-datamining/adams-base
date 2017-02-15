64acc33b5215b8fff0970a94cc3b2e0c21a935bc
fracpete <fracpete@gmail.com>
2017-02-15 00:34:00 +0000

    added auto-detect-data-type flag to SetReportValue transformers

--- adams-core/src/main/java/adams/flow/transformer/AbstractSetReportValue.java
+++ adams-core/src/main/java/adams/flow/transformer/AbstractSetReportValue.java
@@ -15,7 +15,7 @@
 
 /*
  * AbstractSetReportValue.java
- * Copyright (C) 2010-2016 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2010-2017 University of Waikato, Hamilton, New Zealand
  */
 
 package adams.flow.transformer;
@@ -33,7 +33,6 @@ import adams.flow.core.Token;
  *
  * @author  fracpete (fracpete at waikato dot ac dot nz)
  * @version $Revision$
- * @param <T> the type of field to handle
  */
 public abstract class AbstractSetReportValue
   extends AbstractTransformer {
@@ -46,6 +45,9 @@ public abstract class AbstractSetReportValue
 
   /** the value to set. */
   protected String m_Value;
+
+  /** whether to auto-detect the type of the string value. */
+  protected boolean m_AutoDetectDataType;
 
   /**
    * Returns a string describing the object.
@@ -63,12 +65,16 @@ public abstract class AbstractSetReportValue
     super.defineOptions();
 
     m_OptionManager.add(
-	    "field", "field",
-	    getDefaultField());
+      "field", "field",
+      getDefaultField());
 
     m_OptionManager.add(
-	    "value", "value",
-	    "");
+      "value", "value",
+      "");
+
+    m_OptionManager.add(
+      "auto-detect-data-type", "autoDetectDataType",
+      false);
   }
 
   /**
@@ -104,6 +110,35 @@ public abstract class AbstractSetReportValue
    * 			displaying in the GUI or for listing the options.
    */
   public abstract String valueTipText();
+
+  /**
+   * Sets whether to use auto-detection for the data type.
+   *
+   * @param value	true if to use auto-detect
+   */
+  public void setAutoDetectDataType(boolean value) {
+    m_AutoDetectDataType = value;
+    reset();
+  }
+
+  /**
+   * Returns whether to use auto-detection for the data type.
+   *
+   * @return		true if to auto-detect
+   */
+  public boolean getAutoDetectDataType() {
+    return m_AutoDetectDataType;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String autoDetectDataTypeTipText() {
+    return "If enabled, tries to determine the data type of the value string automatically.";
+  }
 
   /**
    * Returns a quick info about the actor, which will be displayed in the GUI.
@@ -172,7 +207,10 @@ public abstract class AbstractSetReportValue
       if (report != null) {
 	field = new Field(m_Field);
 	report.addField(field);
-	report.addParameter(m_Field.getName(), value);
+	if (m_AutoDetectDataType)
+	  report.addParameter(m_Field.getName(), value);
+	else
+	  report.setValue(m_Field, value);
       }
       else {
 	if (isLoggingEnabled())
