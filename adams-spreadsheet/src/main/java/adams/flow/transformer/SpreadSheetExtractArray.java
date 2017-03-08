c170b4d604213d2d58f0909d35f95a291a9a4ad2
fracpete <fracpete@gmail.com>
2017-03-08 04:02:51 +0000

    removed implementation of generates() method, as output can be array or element-by-element

--- adams-spreadsheet/src/main/java/adams/flow/transformer/SpreadSheetExtractArray.java
+++ adams-spreadsheet/src/main/java/adams/flow/transformer/SpreadSheetExtractArray.java
@@ -15,7 +15,7 @@
 
 /**
  * SpreadSheetExtractArray.java
- * Copyright (C) 2016 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2016-2017 University of Waikato, Hamilton, New Zealand
  */
 package adams.flow.transformer;
 
@@ -247,15 +247,6 @@ public class SpreadSheetExtractArray
    */
   public Class[] accepts() {
     return new Class[]{SpreadSheet.class};
-  }
-
-  /**
-   * Returns the class of objects that it generates.
-   *
-   * @return		<!-- flow-generates-start -->java.lang.Object[].class<!-- flow-generates-end -->
-   */
-  public Class[] generates() {
-    return new Class[]{Object[].class};
   }
 
   /**
