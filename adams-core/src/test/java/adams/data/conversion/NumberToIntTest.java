1da90abe24591cdbba7586f809ee484805f4c41a
fracpete <fracpete@gmail.com>
2017-03-16 23:32:36 +0000

    added unit tests

---
+++ adams-core/src/test/java/adams/data/conversion/NumberToIntTest.java
@@ -0,0 +1,102 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * NumberToIntTest.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.data.conversion;
+
+import adams.env.Environment;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Tests the NumberToInt conversion.
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision: 10234 $
+ */
+public class NumberToIntTest
+  extends AbstractConversionTestCase {
+
+  /**
+   * Constructs the test case. Called by subclasses.
+   *
+   * @param name 	the name of the test
+   */
+  public NumberToIntTest(String name) {
+    super(name);
+  }
+
+  /**
+   * Returns the input data to use in the regression test.
+   *
+   * @return		the objects
+   */
+  @Override
+  protected Object[] getRegressionInput() {
+    return new Number[]{
+	1,
+	-1L,
+	3.4,
+	-3.4,
+	31.415f,
+	-3.1415f
+    };
+  }
+
+  /**
+   * Returns the setups to use in the regression test.
+   *
+   * @return		the setups
+   */
+  @Override
+  protected Conversion[] getRegressionSetups() {
+    return new Conversion[]{
+	new NumberToInt()
+    };
+  }
+
+  /**
+   * Returns the ignored line indices to use in the regression test.
+   *
+   * @return		the setups
+   */
+  @Override
+  protected int[] getRegressionIgnoredLineIndices() {
+    return new int[0];
+  }
+
+  /**
+   * Returns the test suite.
+   *
+   * @return		the suite
+   */
+  public static Test suite() {
+    return new TestSuite(NumberToIntTest.class);
+  }
+
+  /**
+   * Runs the test from commandline.
+   *
+   * @param args	ignored
+   */
+  public static void main(String[] args) {
+    Environment.setEnvironmentClass(Environment.class);
+    runTest(suite());
+  }
+}
