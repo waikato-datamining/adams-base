9f37a53a03c5018e03ddc4efb3a4b2dbddf0f7a9
fracpete <fracpete@gmail.com>
2017-03-16 23:25:13 +0000

    additional conversions

---
+++ adams-core/src/main/java/adams/data/conversion/NumberToInt.java
@@ -0,0 +1,89 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * NumberToInt.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, New Zealand
+ */
+package adams.data.conversion;
+
+/**
+ <!-- globalinfo-start -->
+ * Turns an Number into a Int.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * Valid options are: <br><br>
+ * 
+ * <pre>-D &lt;int&gt; (property: debugLevel)
+ * &nbsp;&nbsp;&nbsp;The greater the number the more additional info the scheme may output to 
+ * &nbsp;&nbsp;&nbsp;the console (0 = off).
+ * &nbsp;&nbsp;&nbsp;default: 0
+ * &nbsp;&nbsp;&nbsp;minimum: 0
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision: 10824 $
+ */
+public class NumberToInt
+  extends AbstractConversion {
+
+  /** for serialization. */
+  private static final long serialVersionUID = 8575654879898181338L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Turns a Number into an Integer.";
+  }
+
+  /**
+   * Returns the class that is accepted as input.
+   *
+   * @return		the class
+   */
+  @Override
+  public Class accepts() {
+    return Number.class;
+  }
+
+  /**
+   * Returns the class that is generated as output.
+   *
+   * @return		the class
+   */
+  @Override
+  public Class generates() {
+    return Integer.class;
+  }
+
+  /**
+   * Performs the actual conversion.
+   *
+   * @return		the converted data
+   * @throws Exception	if something goes wrong with the conversion
+   */
+  @Override
+  protected Object doConvert() throws Exception {
+    return ((Number) m_Input).intValue();
+  }
+}
