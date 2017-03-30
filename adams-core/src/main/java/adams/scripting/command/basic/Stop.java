ee8cb092e395e3c08dbd9638e19aa733956deb25
fracpete <fracpete@gmail.com>
2017-03-30 02:49:27 +0000

    command for gracefully stopping an ADAMS instance

---
+++ adams-core/src/main/java/adams/scripting/command/basic/Stop.java
@@ -0,0 +1,107 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Stop.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, NZ
+ */
+
+package adams.scripting.command.basic;
+
+import adams.core.ClassCrossReference;
+import adams.scripting.engine.RemoteScriptingEngine;
+
+/**
+ * Attempts to stop the remote ADAMS instance.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class Stop
+  extends AbstractCommandWithFlowStopping
+  implements ClassCrossReference {
+
+  private static final long serialVersionUID = -1657908444959620122L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Stops the ADAMS instance.";
+  }
+
+  /**
+   * Returns the cross-referenced classes.
+   *
+   * @return		the classes
+   */
+  public Class[] getClassCrossReferences() {
+    return new Class[]{Kill.class, Restart.class};
+  }
+
+  /**
+   * Ignored.
+   *
+   * @param value	the payload
+   */
+  @Override
+  public void setRequestPayload(byte[] value) {
+  }
+
+  /**
+   * Always zero-length array.
+   *
+   * @return		the payload
+   */
+  @Override
+  public byte[] getRequestPayload() {
+    return new byte[0];
+  }
+
+  /**
+   * Returns the objects that represent the request payload.
+   *
+   * @return		the objects
+   */
+  public Object[] getRequestPayloadObjects() {
+    return new Object[0];
+  }
+
+  /**
+   * Handles the request.
+   *
+   * @param engine	the remote engine handling the request
+   * @return		null if successful, otherwise error message
+   */
+  protected String doHandleRequest(RemoteScriptingEngine engine) {
+    if (m_StopFlows)
+      stopFlows();
+
+    if (getApplicationContext() != null) {
+      if (getApplicationContext().getRemoteScriptingEngine() != null) {
+        getLogger().info("Stopping scripting engine");
+        getApplicationContext().getRemoteScriptingEngine().stopExecution();
+      }
+    }
+
+    getLogger().info("Stopping");
+    System.exit(0);
+
+    return null;
+  }
+}
