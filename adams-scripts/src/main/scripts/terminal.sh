a21eb6e6a6162701a2db5165622f483ca0dfd62c
fracpete <fracpete@gmail.com>
2017-04-18 21:36:16 +0000

    moved out determination of Java command etc into env.sh for *nix-based systems

--- adams-scripts/src/main/scripts/terminal.sh
+++ adams-scripts/src/main/scripts/terminal.sh
@@ -23,60 +23,9 @@
 BASEDIR=`dirname $0`/..
 BASEDIR=`(cd "$BASEDIR"; pwd)`
 
-# OS specific support.  $var _must_ be set to either true or false.
-cygwin=false;
-darwin=false;
-case "`uname`" in
-  CYGWIN*) cygwin=true ;;
-  Darwin*) darwin=true
-           if [ -z "$JAVA_VERSION" ] ; then
-             JAVA_VERSION="CurrentJDK"
-           else
-             echo "Using Java version: $JAVA_VERSION"
-           fi
-           if [ -z "$JAVA_HOME" ] ; then
-             JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/${JAVA_VERSION}/Home
-           fi
-           ;;
-esac
+. "$BASEDIR"/bin/env.sh
 
-if [ -z "$JAVA_HOME" ] ; then
-  if [ -r /etc/gentoo-release ] ; then
-    JAVA_HOME=`java-config --jre-home`
-  fi
-fi
-
-# For Cygwin, ensure paths are in UNIX format before anything is touched
-if $cygwin ; then
-  [ -n "$JAVA_HOME" ] && JAVA_HOME=`cygpath --unix "$JAVA_HOME"`
-  [ -n "$CLASSPATH" ] && CLASSPATH=`cygpath --path --unix "$CLASSPATH"`
-fi
-
-# If a specific java binary isn't specified search for the standard 'java' binary
-if [ -z "$JAVACMD" ] ; then
-  if [ -n "$JAVA_HOME"  ] ; then
-    if [ -x "$JAVA_HOME/jre/sh/java" ] ; then
-      # IBM's JDK on AIX uses strange locations for the executables
-      JCMD="$JAVA_HOME/jre/sh/java"
-    else
-      JCMD="$JAVA_HOME/bin/java"
-    fi
-  else
-    JCMD=`which java`
-  fi
-else
-  JCMD=$JAVACMD
-fi
-
-if [ ! -x "$JCMD" ] ; then
-  echo "Error: JAVA_HOME is not defined correctly."
-  echo "  We cannot execute $JCMD"
-  exit 1
-fi
-
-REPO="$BASEDIR"/lib
-RESOURCES="$BASEDIR"/resources
-CLASSPATH="$RESOURCES":"$REPO/java-cup-11b-2015.03.26.jar":"$REPO/*"
+CLASSPATH="$RESOURCES":"$PRIORITY":"$REPO/*"
 
 # For Cygwin, switch paths to Windows format before running java
 if $cygwin; then
