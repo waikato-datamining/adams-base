f2818a4dce05d04440d770020a49340e74b8b1a9
fracpete <fracpete@gmail.com>
2017-05-18 23:56:10 +0000

    shortcut check for whether a boolean variable is set to true (faster than using Expression condition)

---
+++ adams-core/src/main/java/adams/flow/condition/bool/VariableFlagSet.java
@@ -0,0 +1,150 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * VariableFlagSet.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, New Zealand
+ */
+package adams.flow.condition.bool;
+
+import adams.core.QuickInfoHelper;
+import adams.core.Utils;
+import adams.core.VariableName;
+import adams.core.Variables;
+import adams.flow.core.Actor;
+import adams.flow.core.Token;
+
+/**
+ <!-- globalinfo-start -->
+ * Evaluates to true if the specified variable is present, a boolean and true.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ * <pre>-variable-name &lt;adams.core.VariableName&gt; (property: variableName)
+ * &nbsp;&nbsp;&nbsp;The name of the flag variable to evaluate.
+ * &nbsp;&nbsp;&nbsp;default: variable
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision: 8036 $
+ */
+public class VariableFlagSet
+  extends AbstractBooleanCondition {
+  
+  /** for serialization. */
+  private static final long serialVersionUID = -1349114354556041598L;
+  
+  /** the name of the flag variable. */
+  protected VariableName m_VariableName;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return 
+	"Evaluates to true if the specified variable is present, a boolean and true.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+	    "variable-name", "variableName",
+	    new VariableName("variable"));
+    
+  }
+  /**
+   * Returns the quick info string to be displayed in the flow editor.
+   *
+   * @return		the info or null if no info to be displayed
+   */
+  @Override
+  public String getQuickInfo() {
+    return QuickInfoHelper.toString(this, "variableName", m_VariableName, "variable: ");
+  }
+
+  /**
+   * Sets the name of the variable flag to evaluate.
+   *
+   * @param value	the name
+   */
+  public void setVariableName(VariableName value) {
+    m_VariableName = value;
+    reset();
+  }
+
+  /**
+   * Returns the name of the variable flag to evaluate.
+   *
+   * @return		the name
+   */
+  public VariableName getVariableName() {
+    return m_VariableName;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String variableNameTipText() {
+    return "The name of the flag variable to evaluate.";
+  }
+
+  /**
+   * Returns the class that the consumer accepts.
+   *
+   * @return		adams.flow.core.Unknown.class
+   */
+  @Override
+  public Class[] accepts() {
+    return new Class[]{Object.class};
+  }
+
+  /**
+   * Performs the actual evaluation.
+   *
+   * @param owner	the owning actor
+   * @param token	the current token passing through
+   * @return		the result of the evaluation
+   */
+  @Override
+  protected boolean doEvaluate(Actor owner, Token token) {
+    Variables 	variables;
+
+    variables = owner.getVariables();
+
+    return
+      variables.has(m_VariableName.getValue())
+      && Utils.isBoolean(variables.get(m_VariableName.getValue()))
+      && Boolean.parseBoolean(variables.get(m_VariableName.getValue()).toLowerCase());
+  }
+}
