06112306c6caaba1e4e73245837820b569f655d6
fracpete <fracpete@gmail.com>
2017-08-28 21:39:36 +0000

    added optional use of distinct colors per type of object and optional display of type label

--- adams-imaging/src/main/java/adams/gui/visualization/image/ObjectLocationsOverlayFromReport.java
+++ adams-imaging/src/main/java/adams/gui/visualization/image/ObjectLocationsOverlayFromReport.java
@@ -19,15 +19,22 @@
  */
 package adams.gui.visualization.image;
 
+import adams.data.report.AbstractField;
 import adams.data.report.Report;
 import adams.flow.transformer.locateobjects.LocatedObject;
 import adams.flow.transformer.locateobjects.LocatedObjects;
+import adams.gui.core.Fonts;
+import adams.gui.visualization.core.ColorProvider;
+import adams.gui.visualization.core.DefaultColorProvider;
 import adams.gui.visualization.image.ImagePanel.PaintPanel;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -38,6 +45,7 @@ import java.util.List;
  * .y<br>
  * .width<br>
  * .height<br>
+ * Optionally, if type information is available per object, the locations can be displayed in distinct colors per type. The type itself can be displayed as well.
  * <br><br>
  <!-- globalinfo-end -->
  *
@@ -46,21 +54,48 @@ import java.util.List;
  * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
  * &nbsp;&nbsp;&nbsp;default: WARNING
  * </pre>
- * 
+ *
  * <pre>-enabled &lt;boolean&gt; (property: enabled)
  * &nbsp;&nbsp;&nbsp;If enabled, this overlay is painted over the image.
  * &nbsp;&nbsp;&nbsp;default: true
  * </pre>
- * 
+ *
  * <pre>-prefix &lt;java.lang.String&gt; (property: prefix)
  * &nbsp;&nbsp;&nbsp;The prefix of fields in the report to identify as object location, eg 'Object.
  * &nbsp;&nbsp;&nbsp;'.
  * &nbsp;&nbsp;&nbsp;default: Object.
  * </pre>
- * 
+ *
  * <pre>-color &lt;java.awt.Color&gt; (property: color)
  * &nbsp;&nbsp;&nbsp;The color to use for the objects.
  * &nbsp;&nbsp;&nbsp;default: #ff0000
+ * </pre>
+ *
+ * <pre>-use-colors-per-type &lt;boolean&gt; (property: useColorsPerType)
+ * &nbsp;&nbsp;&nbsp;If enabled, individual colors per type are used.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-type-color-provider &lt;adams.gui.visualization.core.ColorProvider&gt; (property: typeColorProvider)
+ * &nbsp;&nbsp;&nbsp;The color provider to use for the various types.
+ * &nbsp;&nbsp;&nbsp;default: adams.gui.visualization.core.DefaultColorProvider
+ * </pre>
+ *
+ * <pre>-type-suffix &lt;java.lang.String&gt; (property: typeSuffix)
+ * &nbsp;&nbsp;&nbsp;The suffix of fields in the report to identify the type.
+ * &nbsp;&nbsp;&nbsp;default: .type
+ * </pre>
+ *
+ * <pre>-label-format &lt;java.lang.String&gt; (property: labelFormat)
+ * &nbsp;&nbsp;&nbsp;The label format string to use for the rectangles; '#' for index, '&#64;' for
+ * &nbsp;&nbsp;&nbsp;type and '$' for short type (type suffix must be defined for '&#64;' and '$'
+ * &nbsp;&nbsp;&nbsp;); for instance: '# &#64;'.
+ * &nbsp;&nbsp;&nbsp;default: #
+ * </pre>
+ *
+ * <pre>-label-font &lt;java.awt.Font&gt; (property: labelFont)
+ * &nbsp;&nbsp;&nbsp;The font to use for the labels.
+ * &nbsp;&nbsp;&nbsp;default: Display-PLAIN-14
  * </pre>
  * 
  <!-- options-end -->
@@ -82,9 +117,33 @@ public class ObjectLocationsOverlayFromReport
 
   /** the color for the objects. */
   protected Color m_Color;
-  
+
+  /** whether to use colors per type. */
+  protected boolean m_UseColorsPerType;
+
+  /** the color provider to use. */
+  protected ColorProvider m_TypeColorProvider;
+
+  /** the suffix for the type. */
+  protected String m_TypeSuffix;
+
+  /** the label for the rectangles. */
+  protected String m_LabelFormat;
+
+  /** the label font. */
+  protected Font m_LabelFont;
+
   /** the cached locations. */
   protected List<Rectangle> m_Locations;
+
+  /** the type/color mapping. */
+  protected HashMap<String,Color> m_TypeColors;
+
+  /** the cached colors. */
+  protected HashMap<Rectangle,Color> m_Colors;
+
+  /** the labels. */
+  protected HashMap<Rectangle,String> m_Labels;
 
   /**
    * Returns a string describing the object.
@@ -101,7 +160,9 @@ public class ObjectLocationsOverlayFromReport
 	+ LocatedObjects.KEY_Y + "\n"
 	+ LocatedObjects.KEY_WIDTH + "\n"
 	+ LocatedObjects.KEY_HEIGHT + "\n"
-      ;
+	+ "Optionally, if type information is available per object, the locations "
+	+ "can be displayed in distinct colors per type. The type itself can be "
+	+ "displayed as well.";
   }
 
   /**
@@ -118,6 +179,26 @@ public class ObjectLocationsOverlayFromReport
     m_OptionManager.add(
 	"color", "color",
 	Color.RED);
+
+    m_OptionManager.add(
+	"use-colors-per-type", "useColorsPerType",
+	false);
+
+    m_OptionManager.add(
+	"type-color-provider", "typeColorProvider",
+	new DefaultColorProvider());
+
+    m_OptionManager.add(
+	"type-suffix", "typeSuffix",
+	".type");
+
+    m_OptionManager.add(
+	"label-format", "labelFormat",
+	"#");
+
+    m_OptionManager.add(
+	"label-font", "labelFont",
+	Fonts.getSansFont(14));
   }
 
   /**
@@ -179,6 +260,151 @@ public class ObjectLocationsOverlayFromReport
   }
 
   /**
+   * Sets whether to use colors per type.
+   *
+   * @param value 	true if to use colors per type
+   */
+  public void setUseColorsPerType(boolean value) {
+    m_UseColorsPerType = value;
+    reset();
+  }
+
+  /**
+   * Returns whether to use colors per type.
+   *
+   * @return 		true if to use colors per type
+   */
+  public boolean getUseColorsPerType() {
+    return m_UseColorsPerType;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String useColorsPerTypeTipText() {
+    return "If enabled, individual colors per type are used.";
+  }
+
+  /**
+   * Sets the color provider to use for the types.
+   *
+   * @param value 	the provider
+   */
+  public void setTypeColorProvider(ColorProvider value) {
+    m_TypeColorProvider = value;
+    reset();
+  }
+
+  /**
+   * Returns the color provider to use for the types.
+   *
+   * @return 		the provider
+   */
+  public ColorProvider getTypeColorProvider() {
+    return m_TypeColorProvider;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String typeColorProviderTipText() {
+    return "The color provider to use for the various types.";
+  }
+
+  /**
+   * Sets the suffix to use for the types.
+   *
+   * @param value 	the suffix
+   */
+  public void setTypeSuffix(String value) {
+    m_TypeSuffix = value;
+    reset();
+  }
+
+  /**
+   * Returns the suffix to use for the types.
+   *
+   * @return 		the suffix
+   */
+  public String getTypeSuffix() {
+    return m_TypeSuffix;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String typeSuffixTipText() {
+    return "The suffix of fields in the report to identify the type.";
+  }
+
+  /**
+   * Sets the label format.
+   *
+   * @param value 	the label format
+   */
+  public void setLabelFormat(String value) {
+    m_LabelFormat = value;
+    reset();
+  }
+
+  /**
+   * Returns the label format.
+   *
+   * @return 		the label format
+   */
+  public String getLabelFormat() {
+    return m_LabelFormat;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String labelFormatTipText() {
+    return "The label format string to use for the rectangles; '#' for index, '@' for type and '$' for short type (type suffix must be defined for '@' and '$'); for instance: '# @'.";
+  }
+
+  /**
+   * Sets the label font.
+   *
+   * @param value 	the label font
+   */
+  public void setLabelFont(Font value) {
+    m_LabelFont = value;
+    reset();
+  }
+
+  /**
+   * Returns the label font.
+   *
+   * @return 		the label font
+   */
+  public Font getLabelFont() {
+    return m_LabelFont;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String labelFontTipText() {
+    return "The font to use for the labels.";
+  }
+
+  /**
    * Notifies the overlay that the image has changed.
    *
    * @param panel	the panel this overlay belongs to
@@ -189,20 +415,66 @@ public class ObjectLocationsOverlayFromReport
   }
   
   /**
-   * Determines the locations of the bugs.
+   * Determines the locations of the objects.
    * 
    * @param report	the report to inspect
    */
   protected void determineLocations(Report report) {
     LocatedObjects	located;
+    HashSet<String>	types;
+    Rectangle		rect;
+    String		suffix;
+    String		type;
+    Color		color;
+    String		label;
 
     if (m_Locations != null)
       return;
 
+    // initialize colors
+    if (m_UseColorsPerType) {
+      m_TypeColors = new HashMap<>();
+      m_TypeColorProvider.resetColors();
+      types = new HashSet<>();
+      for (AbstractField field: report.getFields()) {
+	if (field.getName().endsWith(m_TypeSuffix))
+	  types.add("" + report.getValue(field));
+      }
+      for (String t: types)
+	m_TypeColors.put(t, m_TypeColorProvider.next());
+    }
+
     m_Locations = new ArrayList<>();
+    m_Colors    = new HashMap<>();
+    m_Labels    = new HashMap<>();
+    suffix      = m_TypeSuffix.isEmpty() ? "" : m_TypeSuffix.substring(1);
     located     = LocatedObjects.fromReport(report, m_Prefix);
-    for (LocatedObject object: located)
+    for (LocatedObject object: located) {
       m_Locations.add(object.getRectangle());
+
+      color = m_Color;
+      rect  = object.getRectangle();
+
+      if (!suffix.isEmpty() && (object.getMetaData() != null) && (object.getMetaData().containsKey(suffix))) {
+	type  = "" + object.getMetaData().get(suffix);
+	// color per type?
+	if (m_UseColorsPerType) {
+	  if (m_TypeColors.containsKey(type))
+	    color = m_TypeColors.get(type);
+	}
+
+	// label?
+	if (!m_LabelFormat.isEmpty()) {
+	  label = m_LabelFormat
+	    .replace("#", "" + object.getMetaData().get(LocatedObjects.KEY_INDEX))
+	    .replace("@", type)
+	    .replace("$", type.replaceAll(".*\\.", ""));
+	  m_Labels.put(rect, label);
+	}
+      }
+
+      m_Colors.put(rect, color);
+    }
   }
 
   /**
@@ -213,12 +485,23 @@ public class ObjectLocationsOverlayFromReport
    */
   @Override
   protected synchronized void doPaintOverlay(PaintPanel panel, Graphics g) {
+    String	label;
+
     determineLocations(panel.getOwner().getAdditionalProperties());
     
     if (m_Locations.size() > 0) {
       g.setColor(m_Color);
       for (Rectangle rect: m_Locations) {
+        if (m_UseColorsPerType) {
+	  if (m_Colors.containsKey(rect))
+	    g.setColor(m_Colors.get(rect));
+	}
 	g.drawRect((int) rect.getX(), (int) rect.getY(), (int) rect.getWidth(), (int) rect.getHeight());
+	if (!m_LabelFormat.isEmpty()) {
+	  label = m_Labels.get(rect);
+	  if (label != null)
+	    g.drawString(label, (int) (rect.getX() + rect.getWidth()), (int) rect.getY());
+	}
       }
     }
   }
