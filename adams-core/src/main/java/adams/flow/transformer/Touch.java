b7ea85fe1097048fc6977b99ca0624e7f968005a
Peter Reutemann <fracpete@gmail.com>
2017-09-08 11:45:10 +1200

    added transformer for updating the 'last modified' timestamp of files: Touch

---
+++ adams-core/src/main/java/adams/flow/transformer/Touch.java
@@ -0,0 +1,221 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Touch.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.transformer;
+
+import adams.core.QuickInfoHelper;
+import adams.core.Utils;
+import adams.core.io.PlaceholderFile;
+
+import java.io.File;
+
+/**
+ <!-- globalinfo-start -->
+ * Like the Unix 'touch' command, updates the 'last modified' timestamp of the file coming through.<br>
+ * By default, it creates the file if it doesn't exist.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- flow-summary-start -->
+ * Input&#47;output:<br>
+ * - accepts:<br>
+ * &nbsp;&nbsp;&nbsp;java.lang.String<br>
+ * &nbsp;&nbsp;&nbsp;java.io.File<br>
+ * - generates:<br>
+ * &nbsp;&nbsp;&nbsp;java.lang.String<br>
+ * &nbsp;&nbsp;&nbsp;java.io.File<br>
+ * <br><br>
+ <!-- flow-summary-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ *
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: Touch
+ * </pre>
+ *
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default:
+ * </pre>
+ *
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical
+ * &nbsp;&nbsp;&nbsp;actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-create-file &lt;boolean&gt; (property: createFile)
+ * &nbsp;&nbsp;&nbsp;If enabled, the file gets created if it doesn't exist.
+ * &nbsp;&nbsp;&nbsp;default: true
+ * </pre>
+ *
+ <!-- options-end -->
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class Touch
+  extends AbstractTransformer {
+
+  private static final long serialVersionUID = 5841846014350138596L;
+
+  /** whether to create the file if not existing. */
+  protected boolean m_CreateFile;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return
+      "Like the Unix 'touch' command, updates the 'last modified' timestamp "
+	+ "of the file coming through.\n"
+	+ "By default, it creates the file if it doesn't exist.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+      "create-file", "createFile",
+      true);
+  }
+
+  /**
+   * Returns a quick info about the actor, which will be displayed in the GUI.
+   *
+   * @return		null if no info available, otherwise short string
+   */
+  @Override
+  public String getQuickInfo() {
+    return QuickInfoHelper.toString(this, "createFile", (m_CreateFile ? "create" : "don't create"));
+  }
+
+  /**
+   * Sets whether to create the file if it doesn't exist.
+   *
+   * @param value	true if to create
+   */
+  public void setCreateFile(boolean value) {
+    m_CreateFile = value;
+    reset();
+  }
+
+  /**
+   * Returns whether to create the file it doesn't exist.
+   *
+   * @return 		true if to create
+   */
+  public boolean getCreateFile() {
+    return m_CreateFile;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return         tip text for this property suitable for
+   *             displaying in the GUI or for listing the options.
+   */
+  public String createFileTipText() {
+    return "If enabled, the file gets created if it doesn't exist.";
+  }
+
+  /**
+   * Returns the class that the consumer accepts.
+   *
+   * @return		the Class of objects that can be processed
+   */
+  @Override
+  public Class[] accepts() {
+    return new Class[]{String.class, File.class};
+  }
+
+  /**
+   * Returns the class of objects that it generates.
+   *
+   * @return		the Class of the generated tokens
+   */
+  @Override
+  public Class[] generates() {
+    return new Class[]{String.class, File.class};
+  }
+
+  /**
+   * Executes the flow item.
+   *
+   * @return		null if everything is fine, otherwise error message
+   */
+  @Override
+  protected String doExecute() {
+    String	result;
+    File	file;
+
+    result = null;
+
+    file = null;
+    if (m_InputToken.getPayload() instanceof String)
+      file = new PlaceholderFile((String) m_InputToken.getPayload());
+    else if (m_InputToken.getPayload() instanceof File)
+      file = new PlaceholderFile((File) m_InputToken.getPayload());
+    else
+      result = "Unhandled input type: " + Utils.classToString(m_InputToken.getPayload().getClass());
+
+    if (result == null) {
+      if (file.exists()) {
+	if (!file.setLastModified(System.currentTimeMillis()))
+	  result = "Failed to update 'last modified' timestamp of " + file;
+      }
+      else if (m_CreateFile) {
+	try {
+	  if (!file.createNewFile())
+	    result = "Failed to create: " + file;
+	}
+	catch (Exception e) {
+	  result = handleException("Failed to create: " + file, e);
+	}
+      }
+    }
+
+    return result;
+  }
+}
---
+++ adams-core/src/test/java/adams/flow/transformer/TouchTest.java
@@ -0,0 +1,155 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * TouchTest.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.flow.transformer;
+
+import adams.core.base.BaseString;
+import adams.core.option.AbstractArgumentOption;
+import adams.data.conversion.StringToString;
+import adams.env.Environment;
+import adams.flow.AbstractFlowTest;
+import adams.flow.control.Flow;
+import adams.flow.core.AbstractActor;
+import adams.flow.core.Actor;
+import adams.flow.execution.NullListener;
+import adams.flow.source.StringConstants;
+import adams.test.TmpFile;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Test for Touch actor.
+ *
+ * @author fracpete
+ * @author adams.core.option.FlowJUnitTestProducer (code generator)
+ * @version $Revision$
+ */
+public class TouchTest
+  extends AbstractFlowTest {
+
+  /**
+   * Initializes the test.
+   *
+   * @param name	the name of the test
+   */
+  public TouchTest(String name) {
+    super(name);
+  }
+
+  /**
+   * Called by JUnit before each test method.
+   *
+   * @throws Exception 	if an error occurs.
+   */
+  protected void setUp() throws Exception {
+    super.setUp();
+    
+    m_TestHelper.deleteFileFromTmp("dumpfile.txt");
+  }
+
+  /**
+   * Called by JUnit after each test method.
+   *
+   * @throws Exception	if tear-down fails
+   */
+  protected void tearDown() throws Exception {
+    m_TestHelper.deleteFileFromTmp("dumpfile.txt");
+    
+    super.tearDown();
+  }
+
+  /**
+   * Performs a regression test, comparing against previously generated output.
+   */
+  public void testRegression() {
+    performRegressionTest(
+        new TmpFile[]{
+          new TmpFile("dumpfile.txt")
+        });
+  }
+
+  /**
+   * 
+   * Returns a test suite.
+   *
+   * @return		the test suite
+   */
+  public static Test suite() {
+    return new TestSuite(TouchTest.class);
+  }
+
+  /**
+   * Used to create an instance of a specific actor.
+   *
+   * @return a suitably configured <code>AbstractActor</code> value
+   */
+  public AbstractActor getActor() {
+    AbstractArgumentOption    argOption;
+    
+    Flow flow = new Flow();
+    
+    try {
+      List<Actor> actors = new ArrayList<>();
+
+      // Flow.StringConstants
+      StringConstants stringconstants = new StringConstants();
+      argOption = (AbstractArgumentOption) stringconstants.getOptionManager().findByProperty("strings");
+      List<BaseString> strings = new ArrayList<>();
+      strings.add((BaseString) argOption.valueOf("${TMP}/dumpfile.txt"));
+      stringconstants.setStrings(strings.toArray(new BaseString[0]));
+      StringToString stringtostring = new StringToString();
+      stringconstants.setConversion(stringtostring);
+
+      actors.add(stringconstants);
+
+      // Flow.DeleteFile
+      DeleteFile deletefile = new DeleteFile();
+      actors.add(deletefile);
+
+      // Flow.Touch
+      Touch touch = new Touch();
+      actors.add(touch);
+      flow.setActors(actors.toArray(new Actor[0]));
+
+      NullListener nulllistener = new NullListener();
+      flow.setFlowExecutionListener(nulllistener);
+
+    }
+    catch (Exception e) {
+      fail("Failed to set up actor: " + e);
+    }
+    
+    return flow;
+  }
+
+  /**
+   * Runs the test from commandline.
+   *
+   * @param args	ignored
+   */
+  public static void main(String[] args) {
+    Environment.setEnvironmentClass(adams.env.Environment.class);
+    runTest(suite());
+  }
+}
+
