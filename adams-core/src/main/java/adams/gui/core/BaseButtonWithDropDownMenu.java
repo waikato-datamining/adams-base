7a770795e1b75a7703004d69c2129a992cd678cd
Peter Reutemann <fracpete@gmail.com>
2017-11-12 14:10:10 +1300

    added ability to supply whole menu instead of just menu items/actions

--- adams-core/src/main/java/adams/gui/core/BaseButtonWithDropDownMenu.java
+++ adams-core/src/main/java/adams/gui/core/BaseButtonWithDropDownMenu.java
@@ -24,12 +24,15 @@ import javax.swing.Action;
 import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Button that opens a dropdown menu when clicked.
+ * You can either supply a whole menu or just add menu items/actions that
+ * should get displayed.
  *
  * @author FracPete (fracpete at waikato dot ac dot nz)
  */
@@ -40,6 +43,9 @@ public class BaseButtonWithDropDownMenu
 
   /** the menu items. */
   protected List<JMenuItem> m_MenuItems;
+
+  /** the menu to display (if not to generate automatically). */
+  protected JPopupMenu m_Menu;
 
   /**
    * Initializes the button.
@@ -85,17 +91,8 @@ public class BaseButtonWithDropDownMenu
    */
   protected void initialize() {
     m_MenuItems = new ArrayList<>();
+    m_Menu      = null;
     addActionListener((ActionEvent ae) -> showMenu());
-  }
-
-  /**
-   * Shows the menu.
-   */
-  protected void showMenu() {
-    BasePopupMenu menu = new BasePopupMenu();
-    for (JMenuItem menuitem: m_MenuItems)
-      menu.add(menuitem);
-    menu.show(this, 0, this.getHeight());
   }
 
   /**
@@ -114,5 +111,40 @@ public class BaseButtonWithDropDownMenu
    */
   public void addToMenu(Action action) {
     m_MenuItems.add(new JMenuItem(action));
+  }
+
+  /**
+   * Sets the menu to display.
+   *
+   * @param menu 	the menu to use
+   */
+  public void setDropDownMenu(JPopupMenu menu) {
+    m_Menu = menu;
+  }
+
+  /**
+   * Returns the menu to display.
+   *
+   * @return		the menu to use, null if none set
+   */
+  public JPopupMenu getDropDownMenu() {
+    return m_Menu;
+  }
+
+  /**
+   * Shows the menu.
+   */
+  protected void showMenu() {
+    JPopupMenu	menu;
+
+    if (m_Menu != null) {
+      menu = m_Menu;
+    }
+    else {
+      menu = new BasePopupMenu();
+      for (JMenuItem menuitem : m_MenuItems)
+	menu.add(menuitem);
+    }
+    menu.show(this, 0, this.getHeight());
   }
 }
