4b394e512b74f4643bb5322bccc872613955261c
Peter Reutemann <fracpete@gmail.com>
2017-11-16 10:33:33 +1300

    now uses an object finder

--- adams-imaging/src/main/java/adams/flow/transformer/GetImageObjectIndices.java
+++ adams-imaging/src/main/java/adams/flow/transformer/GetImageObjectIndices.java
@@ -23,73 +23,23 @@ package adams.flow.transformer;
 import adams.core.QuickInfoHelper;
 import adams.core.Utils;
 import adams.data.image.AbstractImageContainer;
+import adams.data.objectfinder.AllFinder;
+import adams.data.objectfinder.ObjectFinder;
 import adams.data.report.Report;
 import adams.flow.transformer.locateobjects.LocatedObject;
 import adams.flow.transformer.locateobjects.LocatedObjects;
+import gnu.trove.set.hash.TIntHashSet;
 
 import java.util.Collections;
 
 /**
  <!-- globalinfo-start -->
- * Retrieves all the indices of objects from the report passing through that match the prefix.
- * <br><br>
  <!-- globalinfo-end -->
  *
  <!-- flow-summary-start -->
- * Input&#47;output:<br>
- * - accepts:<br>
- * &nbsp;&nbsp;&nbsp;adams.data.image.AbstractImageContainer<br>
- * &nbsp;&nbsp;&nbsp;adams.data.report.Report<br>
- * - generates:<br>
- * &nbsp;&nbsp;&nbsp;java.lang.String<br>
- * <br><br>
  <!-- flow-summary-end -->
  *
  <!-- options-start -->
- * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
- * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
- * &nbsp;&nbsp;&nbsp;default: WARNING
- * </pre>
- *
- * <pre>-name &lt;java.lang.String&gt; (property: name)
- * &nbsp;&nbsp;&nbsp;The name of the actor.
- * &nbsp;&nbsp;&nbsp;default: GetImageObjectIndices
- * </pre>
- *
- * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
- * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
- * &nbsp;&nbsp;&nbsp;default:
- * </pre>
- *
- * <pre>-skip &lt;boolean&gt; (property: skip)
- * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded
- * &nbsp;&nbsp;&nbsp;as it is.
- * &nbsp;&nbsp;&nbsp;default: false
- * </pre>
- *
- * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
- * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this
- * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical
- * &nbsp;&nbsp;&nbsp;actors.
- * &nbsp;&nbsp;&nbsp;default: false
- * </pre>
- *
- * <pre>-silent &lt;boolean&gt; (property: silent)
- * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing
- * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
- * &nbsp;&nbsp;&nbsp;default: false
- * </pre>
- *
- * <pre>-output-array &lt;boolean&gt; (property: outputArray)
- * &nbsp;&nbsp;&nbsp;If enabled, the indices get output as array instead of one-by-one.
- * &nbsp;&nbsp;&nbsp;default: false
- * </pre>
- *
- * <pre>-prefix &lt;java.lang.String&gt; (property: prefix)
- * &nbsp;&nbsp;&nbsp;The report field prefix used in the report.
- * &nbsp;&nbsp;&nbsp;default: Object.
- * </pre>
- *
  <!-- options-end -->
  *
  * @author FracPete (fracpete at waikato dot ac dot nz)
@@ -99,8 +49,8 @@ public class GetImageObjectIndices
 
   private static final long serialVersionUID = -5644432725273726622L;
 
-  /** the prefix to use when generating a report. */
-  protected String m_Prefix;
+  /** the object finder to use. */
+  protected ObjectFinder m_Finder;
 
   /** whether to sort lists. */
   protected boolean m_Sort;
@@ -112,9 +62,7 @@ public class GetImageObjectIndices
    */
   @Override
   public String globalInfo() {
-    return
-      "Retrieves all the indices of objects from the report passing through "
-	+ "that match the prefix.";
+    return "Uses the specified object finder to locate objects and forwards the indices.";
   }
 
   /**
@@ -125,8 +73,8 @@ public class GetImageObjectIndices
     super.defineOptions();
 
     m_OptionManager.add(
-      "prefix", "prefix",
-      "Object.");
+      "finder", "finder",
+      new AllFinder());
 
     m_OptionManager.add(
       "sort", "sort",
@@ -155,22 +103,22 @@ public class GetImageObjectIndices
   }
 
   /**
-   * Sets the field prefix used in the report.
+   * Sets the object finder to use.
    *
-   * @param value 	the field prefix
+   * @param value 	the finder
    */
-  public void setPrefix(String value) {
-    m_Prefix = value;
+  public void setFinder(ObjectFinder value) {
+    m_Finder = value;
     reset();
   }
 
   /**
-   * Returns the field prefix used in the report.
+   * Returns the object finder in use.
    *
-   * @return 		the field prefix
+   * @return 		the finder
    */
-  public String getPrefix() {
-    return m_Prefix;
+  public ObjectFinder getFinder() {
+    return m_Finder;
   }
 
   /**
@@ -179,8 +127,8 @@ public class GetImageObjectIndices
    * @return 		tip text for this property suitable for
    * 			displaying in the GUI or for listing the options.
    */
-  public String prefixTipText() {
-    return "The report field prefix used in the report.";
+  public String finderTipText() {
+    return "The object finder to use for locating objects in the report.";
   }
 
   /**
@@ -231,7 +179,7 @@ public class GetImageObjectIndices
   public String getQuickInfo() {
     String	result;
 
-    result  = QuickInfoHelper.toString(this, "prefix", m_Prefix, "prefix: ");
+    result  = QuickInfoHelper.toString(this, "finder", m_Finder, "finder: ");
     result += QuickInfoHelper.toString(this, "sort", m_Sort, "sorted", ", ");
 
     return result;
@@ -246,26 +194,26 @@ public class GetImageObjectIndices
   protected String doExecute() {
     String		result;
     Report		report;
-    LocatedObjects	objs;
-    String		index;
+    LocatedObjects	objects;
+    TIntHashSet		set;
 
     result = null;
 
     report = null;
-    if (m_InputToken.getPayload() instanceof AbstractImageContainer)
-      report = ((AbstractImageContainer) m_InputToken.getPayload()).getReport();
-    else if (m_InputToken.getPayload() instanceof Report)
-      report = (Report) m_InputToken.getPayload();
+    if (m_InputToken.hasPayload(AbstractImageContainer.class))
+      report = m_InputToken.getPayload(AbstractImageContainer.class).getReport();
+    else if (m_InputToken.hasPayload(Report.class))
+      report = m_InputToken.getPayload(Report.class);
     else
       result = "Unhandled input: " + Utils.classToString(m_InputToken.getPayload());
 
     m_Queue.clear();
     if (result == null) {
-      objs = LocatedObjects.fromReport(report, m_Prefix);
-      for (LocatedObject obj: objs) {
-	index = obj.getIndexString();
-	if (index != null)
-	  m_Queue.add(index);
+      objects = LocatedObjects.fromReport(report, m_Finder.getPrefix());
+      set     = new TIntHashSet(m_Finder.find(report));
+      for (LocatedObject obj: objects) {
+        if (set.contains(obj.getIndex()))
+	  m_Queue.add(obj.getIndexString());
       }
     }
     if (m_Sort)
