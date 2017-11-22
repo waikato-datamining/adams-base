f0c0cf7cd9b18b55b4f4875ac7ec92926f4ad29d
Peter Reutemann <fracpete@gmail.com>
2017-11-22 16:04:41 +1300

    ImageObjectIndexOffset changes the offset of the objects in the report by the specified amount

---
+++ adams-imaging/src/main/java/adams/flow/transformer/ImageObjectIndexOffset.java
@@ -0,0 +1,283 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * ImageObjectIndexOffset.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.transformer;
+
+import adams.core.QuickInfoHelper;
+import adams.data.report.AbstractField;
+import adams.data.report.MutableReportHandler;
+import adams.data.report.Report;
+import adams.flow.core.Token;
+import adams.flow.transformer.locateobjects.LocatedObjects;
+
+/**
+ <!-- globalinfo-start -->
+ * Offsets the object index. Useful when merging multiple reports.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- flow-summary-start -->
+ * Input&#47;output:<br>
+ * - accepts:<br>
+ * &nbsp;&nbsp;&nbsp;adams.data.report.Report<br>
+ * &nbsp;&nbsp;&nbsp;adams.data.report.MutableReportHandler<br>
+ * - generates:<br>
+ * &nbsp;&nbsp;&nbsp;adams.data.report.Report<br>
+ * &nbsp;&nbsp;&nbsp;adams.data.report.MutableReportHandler<br>
+ * <br><br>
+ <!-- flow-summary-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ *
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: ImageObjectIndexOffset
+ * </pre>
+ *
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default:
+ * </pre>
+ *
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical
+ * &nbsp;&nbsp;&nbsp;actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-prefix &lt;java.lang.String&gt; (property: prefix)
+ * &nbsp;&nbsp;&nbsp;The report field prefix used in the report.
+ * &nbsp;&nbsp;&nbsp;default: Object.
+ * </pre>
+ *
+ * <pre>-offset &lt;int&gt; (property: offset)
+ * &nbsp;&nbsp;&nbsp;The offset for the index.
+ * &nbsp;&nbsp;&nbsp;default: 0
+ * </pre>
+ *
+ <!-- options-end -->
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class ImageObjectIndexOffset
+  extends AbstractTransformer {
+
+  private static final long serialVersionUID = -1086186805796683098L;
+
+  /** the prefix of the objects in the report. */
+  protected String m_Prefix;
+
+  /** the offset for the index. */
+  protected int m_Offset;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Offsets the object index. Useful when merging multiple reports.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+      "prefix", "prefix",
+      "Object.");
+
+    m_OptionManager.add(
+      "offset", "offset",
+      0);
+  }
+
+  /**
+   * Sets the field prefix used in the report.
+   *
+   * @param value 	the field prefix
+   */
+  public void setPrefix(String value) {
+    m_Prefix = value;
+    reset();
+  }
+
+  /**
+   * Returns the field prefix used in the report.
+   *
+   * @return 		the field prefix
+   */
+  public String getPrefix() {
+    return m_Prefix;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String prefixTipText() {
+    return "The report field prefix used in the report.";
+  }
+
+  /**
+   * Sets the offset for the index.
+   *
+   * @param value	the offset
+   */
+  public void setOffset(int value) {
+    if (getOptionManager().isValid("offset", value)) {
+      m_Offset = value;
+      reset();
+    }
+  }
+
+  /**
+   * Returns the offset for the index.
+   *
+   * @return		the offset
+   */
+  public int getOffset() {
+    return m_Offset;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String offsetTipText() {
+    return "The offset for the index.";
+  }
+
+  /**
+   * Returns a quick info about the actor, which will be displayed in the GUI.
+   *
+   * @return		null if no info available, otherwise short string
+   */
+  @Override
+  public String getQuickInfo() {
+    String	result;
+
+    result = QuickInfoHelper.toString(this, "prefix", m_Prefix, "prefix: ");;
+    result += QuickInfoHelper.toString(this, "offset", m_Offset, ", offset: ");
+
+    return result;
+  }
+
+  /**
+   * Returns the class that the consumer accepts.
+   *
+   * @return		the Class of objects that can be processed
+   */
+  @Override
+  public Class[] accepts() {
+    return new Class[]{Report.class, MutableReportHandler.class};
+  }
+
+  /**
+   * Returns the class of objects that it generates.
+   *
+   * @return		the Class of the generated tokens
+   */
+  @Override
+  public Class[] generates() {
+    return new Class[]{Report.class, MutableReportHandler.class};
+  }
+
+  /**
+   * Executes the flow item.
+   *
+   * @return		null if everything is fine, otherwise error message
+   */
+  @Override
+  protected String doExecute() {
+    String			result;
+    MutableReportHandler	handler;
+    Report			report;
+    Report			newReport;
+    LocatedObjects 		objs;
+
+    result  = null;
+    report  = null;
+    handler = null;
+    if (m_InputToken.hasPayload(MutableReportHandler.class)) {
+      handler = m_InputToken.getPayload(MutableReportHandler.class);
+      report  = handler.getReport();
+    }
+    else if (m_InputToken.hasPayload(Report.class)) {
+      report = m_InputToken.getPayload(Report.class);
+    }
+    else {
+      result = m_InputToken.unhandledData();
+    }
+
+    if (result == null) {
+      try {
+	objs      = LocatedObjects.fromReport(report, m_Prefix);
+	newReport = objs.toReport(m_Prefix, m_Offset);
+	for (AbstractField field : report.getFields()) {
+	  if (field.getName().startsWith(m_Prefix))
+	    report.removeValue(field);
+	}
+	for (AbstractField field : newReport.getFields()) {
+	  report.addField(field);
+	  report.setValue(field, newReport.getValue(field));
+	}
+
+	if (handler != null) {
+	  handler.setReport(report);
+	  m_OutputToken = new Token(handler);
+	}
+	else {
+	  m_OutputToken = new Token(report);
+	}
+      }
+      catch (Exception e) {
+	result = handleException("Failed to filter objects!", e);
+      }
+    }
+
+    return result;
+  }
+}
