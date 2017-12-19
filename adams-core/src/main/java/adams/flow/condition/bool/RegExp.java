d93305d57b406b4ac78e306fffcf260384420318
Peter Reutemann <fracpete@gmail.com>
2017-12-19 20:36:40 +1300

    boolean condition for matching strings with a regular expression

---
+++ adams-core/src/main/java/adams/flow/condition/bool/RegExp.java
@@ -0,0 +1,139 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * RegExp.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.condition.bool;
+
+import adams.core.QuickInfoHelper;
+import adams.core.base.BaseRegExp;
+import adams.flow.core.Actor;
+import adams.flow.core.Token;
+
+/**
+ <!-- globalinfo-start -->
+ * Returns the result of the regular expression matching the incoming string.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ *
+ * <pre>-regexp &lt;adams.core.base.BaseRegExp&gt; (property: regExp)
+ * &nbsp;&nbsp;&nbsp;The regular expression used for matching the strings.
+ * &nbsp;&nbsp;&nbsp;default: .*
+ * </pre>
+ *
+ <!-- options-end -->
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class RegExp
+  extends AbstractBooleanCondition {
+
+  private static final long serialVersionUID = -8098607684693546619L;
+
+  /** the regular expression to match. */
+  protected BaseRegExp m_RegExp;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Returns the result of the regular expression matching the incoming string.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+      "regexp", "regExp",
+      new BaseRegExp(BaseRegExp.MATCH_ALL));
+  }
+
+  /**
+   * Returns a quick info about the actor, which will be displayed in the GUI.
+   *
+   * @return		null if no info available, otherwise short string
+   */
+  @Override
+  public String getQuickInfo() {
+    return QuickInfoHelper.toString(this, "regExp", m_RegExp, "regexp: ");
+  }
+
+  /**
+   * Sets the regular expression to match the strings against.
+   *
+   * @param value	the regular expression
+   */
+  public void setRegExp(BaseRegExp value) {
+    m_RegExp = value;
+    reset();
+  }
+
+  /**
+   * Returns the regular expression to match the strings against.
+   *
+   * @return		the regular expression
+   */
+  public BaseRegExp getRegExp() {
+    return m_RegExp;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String regExpTipText() {
+    return "The regular expression used for matching the strings.";
+  }
+
+  /**
+   * Returns the class that the consumer accepts.
+   *
+   * @return		adams.flow.core.Unknown.class
+   */
+  @Override
+  public Class[] accepts() {
+    return new Class[]{String.class};
+  }
+
+  /**
+   * Performs the actual evaluation.
+   *
+   * @param owner	the owning actor
+   * @param token	the current token passing through
+   * @return		the result of the evaluation
+   */
+  @Override
+  protected boolean doEvaluate(Actor owner, Token token) {
+    return m_RegExp.isMatch(token.getPayload(String.class));
+  }
+}
