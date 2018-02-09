46a5796655f19dc2b05aff2410a5a31147c84e68
Peter Reutemann <fracpete@gmail.com>
2018-02-09 15:26:05 +1300

    added unit tests for RectangleToString and StringToRectangle conversions

---
+++ adams-imaging/src/test/java/adams/data/conversion/StringToRectangleTest.java
@@ -0,0 +1,101 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * StringToRectangleTest.java
+ * Copyright (C) 2018 University of Waikato, Hamilton, New Zealand
+ */
+package adams.data.conversion;
+
+import adams.env.Environment;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Tests the StringToRectangle conversion.
+ * 
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ */
+public class StringToRectangleTest
+  extends AbstractConversionTestCase {
+
+  /**
+   * Constructs the test case. Called by subclasses.
+   *
+   * @param name 	the name of the test
+   */
+  public StringToRectangleTest(String name) {
+    super(name);
+  }
+
+  /**
+   * Returns the input data to use in the regression test.
+   *
+   * @return		the objects
+   */
+  @Override
+  protected Object[] getRegressionInput() {
+    return new String[]{
+      "1 3 10 20",
+      "1.1 3.2 10.3 20.4",
+    };
+  }
+
+  /**
+   * Returns the setups to use in the regression test.
+   *
+   * @return		the setups
+   */
+  @Override
+  protected Conversion[] getRegressionSetups() {
+    StringToRectangle[]	result;
+
+    result = new StringToRectangle[2];
+    result[0] = new StringToRectangle();
+    result[1] = new StringToRectangle();
+    result[1].setUseXY(true);
+
+    return result;
+  }
+
+  /**
+   * Returns the ignored line indices to use in the regression test.
+   *
+   * @return		the setups
+   */
+  @Override
+  protected int[] getRegressionIgnoredLineIndices() {
+    return new int[0];
+  }
+
+  /**
+   * Returns the test suite.
+   *
+   * @return		the suite
+   */
+  public static Test suite() {
+    return new TestSuite(StringToRectangleTest.class);
+  }
+
+  /**
+   * Runs the test from commandline.
+   *
+   * @param args	ignored
+   */
+  public static void main(String[] args) {
+    Environment.setEnvironmentClass(Environment.class);
+    runTest(suite());
+  }
+}
