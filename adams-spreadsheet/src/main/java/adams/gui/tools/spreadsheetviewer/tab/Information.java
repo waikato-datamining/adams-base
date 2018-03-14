993fc6f37b633f0037c90dfd9d6014f86446ab54
Peter Reutemann <fracpete@gmail.com>
2018-03-15 10:55:50 +1300

    safe guarding against NPE now

--- adams-spreadsheet/src/main/java/adams/gui/tools/spreadsheetviewer/tab/Information.java
+++ adams-spreadsheet/src/main/java/adams/gui/tools/spreadsheetviewer/tab/Information.java
@@ -13,9 +13,9 @@
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/**
+/*
  * Information.java
- * Copyright (C) 2013-2016 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2013-2018 University of Waikato, Hamilton, New Zealand
  */
 package adams.gui.tools.spreadsheetviewer.tab;
 
@@ -33,7 +33,6 @@ import java.awt.BorderLayout;
  * Simple information tab.
  * 
  * @author  fracpete (fracpete at waikato dot ac dot nz)
- * @version $Revision$
  */
 public class Information
   extends AbstractViewerTab
@@ -81,7 +80,10 @@ public class Information
     SpreadSheet			info;
     Row				row;
     SpreadSheetTableModel	model;
-    
+
+    if (panel == null)
+      return;
+
     sheet = panel.getSheet();
 
     info  = new DefaultSpreadSheet();
