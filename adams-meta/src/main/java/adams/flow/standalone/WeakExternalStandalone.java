89b6e007b7ca8affc38aac2faccc5d4b6018834b
Peter Reutemann <fracpete@gmail.com>
2019-02-14 16:46:03 +1300

    introduced "weak" external actors, which get disabled at runtime

---
+++ adams-meta/src/main/java/adams/flow/standalone/WeakExternalStandalone.java
@@ -0,0 +1,70 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * WeakExternalStandalone.java
+ * Copyright (C) 2019 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.standalone;
+
+import adams.flow.core.WeakExternalActor;
+
+/**
+ <!-- globalinfo-start -->
+ <!-- globalinfo-end -->
+ *
+ <!-- flow-summary-start -->
+ <!-- flow-summary-end -->
+ *
+ <!-- options-start -->
+ <!-- options-end -->
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class WeakExternalStandalone
+  extends ExternalStandalone
+  implements WeakExternalActor {
+
+  private static final long serialVersionUID = 7596901440985301567L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return super.globalInfo() + "\n"
+      + "Only used at design time, gets deactivated at runtime.";
+  }
+
+  /**
+   * Initializes the item for flow execution.
+   *
+   * @return		null if everything is fine, otherwise error message
+   */
+  @Override
+  public String setUp() {
+    String	result;
+
+    result = super.setUp();
+
+    if (result == null)
+      setSkip(true);
+
+    return result;
+  }
+}
