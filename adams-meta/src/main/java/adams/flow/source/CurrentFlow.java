53080ee537ca49e65fda79def85968d83d82c99d
Peter Reutemann <fracpete@gmail.com>
2019-06-21 10:20:37 +1200

    CurrentFlow source outputs itself (optionally expanding variables), eg for storing current state of flow

---
+++ adams-meta/src/main/java/adams/flow/source/CurrentFlow.java
@@ -0,0 +1,189 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * CurrentFlow.java
+ * Copyright (C) 2019 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.source;
+
+import adams.core.QuickInfoHelper;
+import adams.flow.core.Actor;
+import adams.flow.core.Token;
+
+/**
+ <!-- globalinfo-start -->
+ * Outputs the current flow, i.e., itself.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- flow-summary-start -->
+ * Input&#47;output:<br>
+ * - generates:<br>
+ * &nbsp;&nbsp;&nbsp;adams.flow.core.Actor<br>
+ * <br><br>
+ <!-- flow-summary-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ *
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: CurrentFlow
+ * </pre>
+ *
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default:
+ * </pre>
+ *
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical
+ * &nbsp;&nbsp;&nbsp;actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-expand-variables &lt;boolean&gt; (property: expandVariables)
+ * &nbsp;&nbsp;&nbsp;If enabled, variables get expanded to their current values.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ <!-- options-end -->
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class CurrentFlow
+  extends AbstractSimpleSource {
+
+  private static final long serialVersionUID = 3823225606368312035L;
+
+  /** whether to expand variables. */
+  protected boolean m_ExpandVariables;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Outputs the current flow, i.e., itself.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+      "expand-variables", "expandVariables",
+      false);
+  }
+
+  /**
+   * Sets the whether to expand variables using their current values.
+   *
+   * @param value 	true if to expand
+   */
+  public void setExpandVariables(boolean value) {
+    m_ExpandVariables = value;
+    reset();
+  }
+
+  /**
+   * Returns whether to expand variables using their current values.
+   *
+   * @return 		true if to expand
+   */
+  public boolean getExpandVariables() {
+    return m_ExpandVariables;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String expandVariablesTipText() {
+    return "If enabled, variables get expanded to their current values.";
+  }
+
+  /**
+   * Returns a quick info about the actor, which will be displayed in the GUI.
+   *
+   * @return		null if no info available, otherwise short string
+   */
+  @Override
+  public String getQuickInfo() {
+    return QuickInfoHelper.toString(this, "expandVariables", (m_ExpandVariables ? "expand vars" : ""));
+  }
+
+  /**
+   * Returns the class of objects that it generates.
+   *
+   * @return		the Class of the generated tokens
+   */
+  @Override
+  public Class[] generates() {
+    return new Class[]{Actor.class};
+  }
+
+  /**
+   * Executes the flow item.
+   *
+   * @return		null if everything is fine, otherwise error message
+   */
+  @Override
+  protected String doExecute() {
+    String	result;
+    Actor	actor;
+
+    result = null;
+
+    actor = getRoot();
+    if (actor == null) {
+      result = "Failed to obtain root actor!";
+    }
+    else {
+      if (m_ExpandVariables)
+        actor = actor.shallowCopy(true);
+    }
+
+    if (actor != null)
+      m_OutputToken = new Token(actor);
+
+    return result;
+  }
+}
---
+++ adams-meta/src/test/java/adams/flow/source/CurrentFlowTest.java
@@ -0,0 +1,177 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * CurrentFlowTest.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.flow.source;
+
+import adams.core.base.BaseRegExp;
+import adams.core.io.PlaceholderFile;
+import adams.core.option.AbstractArgumentOption;
+import adams.core.option.NestedProducer;
+import adams.env.Environment;
+import adams.flow.AbstractFlowTest;
+import adams.flow.control.Flow;
+import adams.flow.core.AbstractActor;
+import adams.flow.core.Actor;
+import adams.flow.execution.NullListener;
+import adams.flow.sink.DumpFile;
+import adams.flow.transformer.OptionProducer;
+import adams.flow.transformer.StringJoin;
+import adams.flow.transformer.StringMatcher;
+import adams.flow.transformer.StringSplit;
+import adams.test.TmpFile;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Test for CurrentFlow actor.
+ *
+ * @author fracpete
+ */
+public class CurrentFlowTest
+  extends AbstractFlowTest {
+
+  /**
+   * Initializes the test.
+   *
+   * @param name	the name of the test
+   */
+  public CurrentFlowTest(String name) {
+    super(name);
+  }
+
+  /**
+   * Called by JUnit before each test method.
+   *
+   * @throws Exception 	if an error occurs.
+   */
+  protected void setUp() throws Exception {
+    super.setUp();
+    
+    m_TestHelper.deleteFileFromTmp("dumpfile.txt");
+  }
+
+  /**
+   * Called by JUnit after each test method.
+   *
+   * @throws Exception	if tear-down fails
+   */
+  protected void tearDown() throws Exception {
+    m_TestHelper.deleteFileFromTmp("dumpfile.txt");
+    
+    super.tearDown();
+  }
+
+  /**
+   * Performs a regression test, comparing against previously generated output.
+   */
+  public void testRegression() {
+    performRegressionTest(
+        new TmpFile[]{
+          new TmpFile("dumpfile.txt")
+        });
+  }
+
+  /**
+   * 
+   * Returns a test suite.
+   *
+   * @return		the test suite
+   */
+  public static Test suite() {
+    return new TestSuite(CurrentFlowTest.class);
+  }
+
+  /**
+   * Used to create an instance of a specific actor.
+   *
+   * @return a suitably configured <code>AbstractActor</code> value
+   */
+  public AbstractActor getActor() {
+    AbstractArgumentOption    argOption;
+    
+    Flow flow = new Flow();
+    
+    try {
+      List<Actor> actors = new ArrayList<>();
+
+      // Flow.NewFlow
+      CurrentFlow newflow = new CurrentFlow();
+
+      actors.add(newflow);
+
+      // Flow.OptionProducer
+      OptionProducer optionproducer = new OptionProducer();
+      NestedProducer nestedproducer = new NestedProducer();
+      optionproducer.setProducer(nestedproducer);
+
+      actors.add(optionproducer);
+
+      // Flow.StringSplit
+      StringSplit stringsplit = new StringSplit();
+      argOption = (AbstractArgumentOption) stringsplit.getOptionManager().findByProperty("expression");
+      stringsplit.setExpression((String) argOption.valueOf("\n"));
+      actors.add(stringsplit);
+
+      // Flow.StringMatcher
+      StringMatcher stringmatcher = new StringMatcher();
+      argOption = (AbstractArgumentOption) stringmatcher.getOptionManager().findByProperty("regExp");
+      stringmatcher.setRegExp((BaseRegExp) argOption.valueOf("#.*"));
+      stringmatcher.setInvert(true);
+
+      actors.add(stringmatcher);
+
+      // Flow.StringJoin
+      StringJoin stringjoin = new StringJoin();
+      argOption = (AbstractArgumentOption) stringjoin.getOptionManager().findByProperty("glue");
+      stringjoin.setGlue((String) argOption.valueOf("\n"));
+      actors.add(stringjoin);
+
+      // Flow.DumpFile
+      DumpFile dumpfile = new DumpFile();
+      argOption = (AbstractArgumentOption) dumpfile.getOptionManager().findByProperty("outputFile");
+      dumpfile.setOutputFile((PlaceholderFile) argOption.valueOf("${TMP}/dumpfile.txt"));
+      actors.add(dumpfile);
+      flow.setActors(actors.toArray(new Actor[0]));
+
+      NullListener nulllistener = new NullListener();
+      flow.setFlowExecutionListener(nulllistener);
+
+    }
+    catch (Exception e) {
+      fail("Failed to set up actor: " + e);
+    }
+    
+    return flow;
+  }
+
+  /**
+   * Runs the test from commandline.
+   *
+   * @param args	ignored
+   */
+  public static void main(String[] args) {
+    Environment.setEnvironmentClass(Environment.class);
+    runTest(suite());
+  }
+}
+
