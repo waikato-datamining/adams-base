a04ad5e445b9de9eb27b5528299a7d54b46b1369
Peter Reutemann <fracpete@gmail.com>
2019-07-15 14:25:44 +1200

    SpreadSheetSupporterToSpreadSheet simply turns an object implementing SpreadSheetSupporter into a spreadsheet representation

---
+++ adams-spreadsheet/src/main/java/adams/data/conversion/SpreadSheetSupporterToSpreadSheet.java
@@ -0,0 +1,81 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * SpreadSheetSupporterToSpreadSheet.java
+ * Copyright (C) 2019 University of Waikato, Hamilton, NZ
+ */
+
+package adams.data.conversion;
+
+import adams.core.Utils;
+import adams.data.spreadsheet.SpreadSheet;
+import adams.data.spreadsheet.SpreadSheetSupporter;
+
+/**
+ <!-- globalinfo-start -->
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ <!-- options-end -->
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class SpreadSheetSupporterToSpreadSheet
+  extends AbstractConversion {
+
+  private static final long serialVersionUID = 3230757871850846362L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Converts any object that implements " + Utils.classToString(SpreadSheetSupporter.class) + " into the corresponding spreadsheet.";
+  }
+
+  /**
+   * Returns the class that is accepted as input.
+   *
+   * @return		the class
+   */
+  @Override
+  public Class accepts() {
+    return SpreadSheetSupporter.class;
+  }
+
+  /**
+   * Returns the class that is generated as output.
+   *
+   * @return		the class
+   */
+  @Override
+  public Class generates() {
+    return SpreadSheet.class;
+  }
+
+  /**
+   * Performs the actual conversion.
+   *
+   * @return		the converted data
+   * @throws Exception	if something goes wrong with the conversion
+   */
+  @Override
+  protected Object doConvert() throws Exception {
+    return ((SpreadSheetSupporter) m_Input).toSpreadSheet();
+  }
+}
