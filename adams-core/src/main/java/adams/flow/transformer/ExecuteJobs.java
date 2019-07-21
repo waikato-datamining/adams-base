e1433694fda3723ed8d3adb79e3b654e1c6b85b2
Peter Reutemann <fracpete@gmail.com>
2019-07-22 10:30:21 +1200

    added ability to override numThreads of incoming JobRunner

--- adams-core/src/main/java/adams/flow/transformer/ExecuteJobs.java
+++ adams-core/src/main/java/adams/flow/transformer/ExecuteJobs.java
@@ -15,12 +15,16 @@
 
 /*
  * ExecuteJobs.java
- * Copyright (C) 2013-2016 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2013-2019 University of Waikato, Hamilton, New Zealand
  */
 
 package adams.flow.transformer;
 
 import adams.core.Pausable;
+import adams.core.Performance;
+import adams.core.QuickInfoHelper;
+import adams.core.ThreadLimiter;
+import adams.core.Utils;
 import adams.event.FlowPauseStateEvent;
 import adams.event.FlowPauseStateEvent.Type;
 import adams.event.FlowPauseStateListener;
@@ -65,30 +69,51 @@ import adams.multiprocess.JobRunner;
  * </pre>
  * 
  * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
- * &nbsp;&nbsp;&nbsp;If set to true, the flow gets stopped in case this actor encounters an error;
- * &nbsp;&nbsp;&nbsp; useful for critical actors.
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical
+ * &nbsp;&nbsp;&nbsp;actors.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- * 
+ *
  * <pre>-silent &lt;boolean&gt; (property: silent)
- * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console.
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
  * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-update-num-threads &lt;boolean&gt; (property: updateNumThreads)
+ * &nbsp;&nbsp;&nbsp;If enabled (and possible), the number of threads of the incoming jobrunner
+ * &nbsp;&nbsp;&nbsp;get updated with the value specified here.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-num-threads &lt;int&gt; (property: numThreads)
+ * &nbsp;&nbsp;&nbsp;The number of threads to use for parallel execution; &gt; 0: specific number
+ * &nbsp;&nbsp;&nbsp;of cores to use (capped by actual number of cores available, 1 = sequential
+ * &nbsp;&nbsp;&nbsp;execution); = 0: number of cores; &lt; 0: number of free cores (eg -2 means
+ * &nbsp;&nbsp;&nbsp;2 free cores; minimum of one core is used)
+ * &nbsp;&nbsp;&nbsp;default: -1
  * </pre>
  * 
  <!-- options-end -->
  *
  * @author  fracpete (fracpete at waikato dot ac dot nz)
- * @version $Revision$
  */
 public class ExecuteJobs
   extends AbstractTransformer
-  implements Pausable, FlowPauseStateListener {
+  implements Pausable, FlowPauseStateListener, ThreadLimiter {
 
   /** for serialization. */
   private static final long serialVersionUID = 7491100983182267771L;
 
   /** the job runner. */
   protected JobRunner m_JobRunner;
+
+  /** whether to update the number of threads to use (if possible). */
+  protected boolean m_UpdateNumThreads;
+
+  /** the number of threads to use for parallel execution. */
+  protected int m_NumThreads;
 
   /**
    * Returns a string describing the object.
@@ -98,6 +123,93 @@ public class ExecuteJobs
   @Override
   public String globalInfo() {
     return "Executes the incoming jobs.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+      "update-num-threads", "updateNumThreads",
+      false);
+
+    m_OptionManager.add(
+      "num-threads", "numThreads",
+      -1);
+  }
+
+  /**
+   * Sets whether to update the number of threads (if possible).
+   *
+   * @param value 	true if to update
+   */
+  public void setUpdateNumThreads(boolean value) {
+    m_UpdateNumThreads = value;
+    reset();
+  }
+
+  /**
+   * Returns whether to update the number of threads (if possible).
+   *
+   * @return 		true if to update
+   */
+  public boolean getUpdateNumThreads() {
+    return m_UpdateNumThreads;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String updateNumThreadsTipText() {
+    return "If enabled (and possible), the number of threads of the incoming jobrunner get updated with the value specified here.";
+  }
+
+  /**
+   * Sets the number of threads to use for parallel training of groups.
+   *
+   * @param value 	the number of threads: -1 = # of CPUs/cores; 0/1 = sequential execution
+   */
+  public void setNumThreads(int value) {
+    m_NumThreads = value;
+    reset();
+  }
+
+  /**
+   * Returns the number of threads to use for parallel training of groups.
+   *
+   * @return 		the number of threads: -1 = # of CPUs/cores; 0/1 = sequential execution
+   */
+  public int getNumThreads() {
+    return m_NumThreads;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String numThreadsTipText() {
+    return Performance.getNumThreadsHelp();
+  }
+
+  /**
+   * Returns a quick info about the actor, which will be displayed in the GUI.
+   *
+   * @return		null if no info available, otherwise short string
+   */
+  @Override
+  public String getQuickInfo() {
+    if (m_UpdateNumThreads)
+      return QuickInfoHelper.toString(this, "numThreads", "" + Performance.getNumThreadsQuickInfo(m_NumThreads));
+    else
+      return null;
   }
 
   /**
@@ -134,6 +246,12 @@ public class ExecuteJobs
     try {
       m_JobRunner = (JobRunner) m_InputToken.getPayload();
       m_JobRunner.setFlowContext(this);
+      if (m_UpdateNumThreads) {
+	if (m_JobRunner instanceof ThreadLimiter)
+	  ((ThreadLimiter) m_JobRunner).setNumThreads(m_NumThreads);
+	else
+	  getLogger().warning("JobRunner does not implement " + Utils.classToString(ThreadLimiter.class) + ", cannot update # of threads!");
+      }
       m_JobRunner.start();
       m_JobRunner.stop();
       m_OutputToken = new Token(m_JobRunner);
