eb61d42dbe639cb284add92ae8c70f2a9e0182af
Peter Reutemann <fracpete@gmail.com>
2019-09-26 16:43:03 +1200

    moved JSON related classes into adams-json module

---
+++ adams-json/src/main/java/adams/flow/sink/JsonFileWriter.java
@@ -0,0 +1,202 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * JsonFileWriter.java
+ * Copyright (C) 2013-2016 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.flow.sink;
+
+import adams.core.io.FileUtils;
+import adams.core.io.PrettyPrintingSupporter;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import net.minidev.json.JSONAware;
+
+/**
+ <!-- globalinfo-start -->
+ * Writes a JSON object&#47;array to a file.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- flow-summary-start -->
+ * Input&#47;output:<br>
+ * - accepts:<br>
+ * &nbsp;&nbsp;&nbsp;net.minidev.json.JSONAware<br>
+ * <br><br>
+ <!-- flow-summary-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: JsonFileWriter
+ * </pre>
+ * 
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded 
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow gets stopped in case this actor encounters an error;
+ * &nbsp;&nbsp;&nbsp; useful for critical actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing 
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-output &lt;adams.core.io.PlaceholderFile&gt; (property: outputFile)
+ * &nbsp;&nbsp;&nbsp;The name of the output file.
+ * &nbsp;&nbsp;&nbsp;default: ${CWD}
+ * </pre>
+ * 
+ * <pre>-pretty-printing &lt;boolean&gt; (property: prettyPrinting)
+ * &nbsp;&nbsp;&nbsp;If enabled, the output is printed in a 'pretty' format.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class JsonFileWriter
+  extends AbstractFileWriter
+  implements PrettyPrintingSupporter {
+
+  /** for serialization. */
+  private static final long serialVersionUID = 3613018887562327088L;
+
+  /** whether to use pretty-printing. */
+  protected boolean m_PrettyPrinting;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Writes a JSON object/array to a file.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+      "pretty-printing", "prettyPrinting",
+      false);
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  @Override
+  public String outputFileTipText() {
+    return "The name of the output file.";
+  }
+
+  /**
+   * Sets whether to use pretty-printing or not.
+   *
+   * @param value	true if to use pretty-printing
+   */
+  public void setPrettyPrinting(boolean value) {
+    m_PrettyPrinting = value;
+    reset();
+  }
+
+  /**
+   * Returns whether pretty-printing is used or not.
+   *
+   * @return		true if to use pretty-printing
+   */
+  public boolean getPrettyPrinting() {
+    return m_PrettyPrinting;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String prettyPrintingTipText() {
+    return "If enabled, the output is printed in a 'pretty' format.";
+  }
+
+  /**
+   * Returns the class that the consumer accepts.
+   *
+   * @return		<!-- flow-accepts-start -->net.minidev.json.JSONAware.class<!-- flow-accepts-end -->
+   */
+  public Class[] accepts() {
+    return new Class[]{JSONAware.class};
+  }
+
+  /**
+   * Executes the flow item.
+   *
+   * @return		null if everything is fine, otherwise error message
+   */
+  @Override
+  protected String doExecute() {
+    String	result;
+    String	content;
+    Gson 	gson;
+    JsonParser 	jp;
+    JsonElement	je;
+
+    result = null;
+
+    content = ((JSONAware) m_InputToken.getPayload()).toJSONString();
+
+    if (m_PrettyPrinting) {
+      gson    = new GsonBuilder().setPrettyPrinting().create();
+      jp      = new JsonParser();
+      je      = jp.parse(content);
+      content = gson.toJson(je);
+    }
+
+    if (!FileUtils.writeToFile(m_OutputFile.getAbsolutePath(), content, false))
+      result = "Failed to write JSON file: " + m_OutputFile;
+
+    return result;
+  }
+}
--- adams-meta/src/test/java/adams/flow/sink/FlowFileWriterTest.java
+++ adams-meta/src/test/java/adams/flow/sink/FlowFileWriterTest.java
@@ -15,35 +15,25 @@
 
 /*
  * FlowFileWriterTest.java
- * Copyright (C) 2017 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2017-2019 University of Waikato, Hamilton, New Zealand
  */
 
 package adams.flow.sink;
 
-import adams.core.io.PlaceholderFile;
-import adams.core.option.AbstractArgumentOption;
-import adams.data.io.output.JsonFlowWriter;
 import adams.env.Environment;
 import adams.flow.AbstractFlowTest;
 import adams.flow.control.Flow;
 import adams.flow.core.AbstractActor;
-import adams.flow.core.Actor;
-import adams.flow.execution.NullListener;
-import adams.flow.source.NewFlow;
-import adams.flow.template.PromptUser;
-import adams.test.TmpFile;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Test for FlowFileWriter actor.
+ * <br>
+ * NB: Dummy test.
  *
  * @author fracpete
  * @author adams.core.option.FlowJUnitTestProducer (code generator)
- * @version $Revision$
  */
 public class FlowFileWriterTest
   extends AbstractFlowTest {
@@ -55,38 +45,6 @@ public class FlowFileWriterTest
    */
   public FlowFileWriterTest(String name) {
     super(name);
-  }
-
-  /**
-   * Called by JUnit before each test method.
-   *
-   * @throws Exception 	if an error occurs.
-   */
-  protected void setUp() throws Exception {
-    super.setUp();
-    
-    m_TestHelper.deleteFileFromTmp("dumpfile.txt");
-  }
-
-  /**
-   * Called by JUnit after each test method.
-   *
-   * @throws Exception	if tear-down fails
-   */
-  protected void tearDown() throws Exception {
-    m_TestHelper.deleteFileFromTmp("dumpfile.txt");
-    
-    super.tearDown();
-  }
-
-  /**
-   * Performs a regression test, comparing against previously generated output.
-   */
-  public void testRegression() {
-    performRegressionTest(
-        new TmpFile[]{
-          new TmpFile("dumpfile.txt")
-        });
   }
 
   /**
@@ -105,41 +63,7 @@ public class FlowFileWriterTest
    * @return a suitably configured <code>AbstractActor</code> value
    */
   public AbstractActor getActor() {
-    AbstractArgumentOption    argOption;
-    
-    Flow flow = new Flow();
-    
-    try {
-      List<Actor> actors = new ArrayList<>();
-
-      // Flow.NewFlow
-      NewFlow newflow = new NewFlow();
-      PromptUser promptuser = new PromptUser();
-      newflow.setTemplate(promptuser);
-
-      actors.add(newflow);
-
-      // Flow.FlowFileWriter
-      FlowFileWriter flowfilewriter = new FlowFileWriter();
-      argOption = (AbstractArgumentOption) flowfilewriter.getOptionManager().findByProperty("outputFile");
-      flowfilewriter.setOutputFile((PlaceholderFile) argOption.valueOf("${TMP}/dumpfile.txt"));
-      flowfilewriter.setUseCustomWriter(true);
-
-      JsonFlowWriter jsonflowwriter = new JsonFlowWriter();
-      flowfilewriter.setCustomWriter(jsonflowwriter);
-
-      actors.add(flowfilewriter);
-      flow.setActors(actors.toArray(new Actor[0]));
-
-      NullListener nulllistener = new NullListener();
-      flow.setFlowExecutionListener(nulllistener);
-
-    }
-    catch (Exception e) {
-      fail("Failed to set up actor: " + e);
-    }
-    
-    return flow;
+    return new Flow();
   }
 
   /**
