78d15908eaf9a5d627e43efc8f8416950bbfadee
Peter Reutemann <fracpete@gmail.com>
2019-10-08 15:39:00 +1300

    added output type option for casting the JSON object

--- adams-json/src/main/java/adams/data/conversion/StringToJson.java
+++ adams-json/src/main/java/adams/data/conversion/StringToJson.java
@@ -13,13 +13,16 @@
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/**
+/*
  * StringToJson.java
- * Copyright (C) 2013 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2013-2019 University of Waikato, Hamilton, New Zealand
  */
 package adams.data.conversion;
 
+import adams.flow.transformer.JsonFileReader.OutputType;
+import net.minidev.json.JSONArray;
 import net.minidev.json.JSONAware;
+import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
 
 /**
@@ -29,25 +32,28 @@ import net.minidev.json.parser.JSONParser;
  <!-- globalinfo-end -->
  *
  <!-- options-start -->
- * Valid options are: <br><br>
- * 
- * <pre>-D &lt;int&gt; (property: debugLevel)
- * &nbsp;&nbsp;&nbsp;The greater the number the more additional info the scheme may output to 
- * &nbsp;&nbsp;&nbsp;the console (0 = off).
- * &nbsp;&nbsp;&nbsp;default: 0
- * &nbsp;&nbsp;&nbsp;minimum: 0
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
  * </pre>
- * 
+ *
+ * <pre>-type &lt;ANY|OBJECT|ARRAY&gt; (property: type)
+ * &nbsp;&nbsp;&nbsp;The type of object to forward.
+ * &nbsp;&nbsp;&nbsp;default: ANY
+ * </pre>
+ *
  <!-- options-end -->
  *
  * @author  fracpete (fracpete at waikato dot ac dot nz)
- * @version $Revision$
  */
 public class StringToJson
   extends AbstractConversionFromString {
 
   /** for serialization. */
   private static final long serialVersionUID = 1484255065339335859L;
+
+  /** the type of object to output. */
+  protected OutputType m_Type;
 
   /**
    * Returns a string describing the object.
@@ -60,13 +66,63 @@ public class StringToJson
   }
 
   /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+      "type", "type",
+      OutputType.ANY);
+  }
+
+  /**
+   * Sets the type of object to forward.
+   *
+   * @param value 	the type
+   */
+  public void setType(OutputType value) {
+    m_Type = value;
+    reset();
+  }
+
+  /**
+   * Returns the type of object to forward.
+   *
+   * @return 		the type
+   */
+  public OutputType getType() {
+    return m_Type;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String typeTipText() {
+    return "The type of object to forward.";
+  }
+
+  /**
    * Returns the class that is generated as output.
    *
    * @return		the class
    */
   @Override
   public Class generates() {
-    return JSONAware.class;
+    switch (m_Type) {
+      case ANY:
+	return JSONAware.class;
+      case ARRAY:
+	return JSONArray.class;
+      case OBJECT:
+	return JSONObject.class;
+      default:
+	throw new IllegalStateException("Unhandled type: " + m_Type);
+    }
   }
 
   /**
@@ -77,8 +133,20 @@ public class StringToJson
    */
   @Override
   protected Object doConvert() throws Exception {
+    Object	result;
+
     try {
-      return new JSONParser(JSONParser.MODE_JSON_SIMPLE).parse((String) m_Input);
+      result = new JSONParser(JSONParser.MODE_JSON_SIMPLE).parse((String) m_Input);
+      switch (m_Type) {
+	case ANY:
+	  return result;
+	case ARRAY:
+	  return (JSONArray) result;
+	case OBJECT:
+	  return (JSONObject) result;
+	default:
+	  throw new IllegalStateException("Unhandled type: " + m_Type);
+      }
     }
     catch (Exception e) {
       throw new Exception("Failed to parse: " + m_Input, e);
