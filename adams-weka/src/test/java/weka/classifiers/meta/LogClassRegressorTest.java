7deae278b2d4d44da5690dbeef10f2441e8cefa0
Peter Reutemann <fracpete@gmail.com>
2019-11-12 14:19:05 +1300

    LogClassRegressor only logs the class and no other numeric attributes, unlike LogTargetRegressor

---
+++ adams-weka/src/test/java/weka/classifiers/meta/LogClassRegressorTest.java
@@ -0,0 +1,82 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Copyright (C) 2019 University of Waikato, Hamilton, New Zealand
+ */
+
+package weka.classifiers.meta;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+import weka.classifiers.AbstractAdamsClassifierTest;
+import weka.classifiers.Classifier;
+
+/**
+ * Tests LogClassRegressor. Run from the command line with:<br><br>
+ * java weka.classifiers.meta.LogClassRegressorTest
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class LogClassRegressorTest
+  extends AbstractAdamsClassifierTest {
+
+  /**
+   * Initializes the test.
+   *
+   * @param name	the name of the test
+   */
+  public LogClassRegressorTest(String name) {
+    super(name);
+  }
+
+  /**
+   * Creates a LogClassRegressor.
+   *
+   * @return		the configured classifier
+   */
+  @Override
+  public Classifier getClassifier() {
+    return new LogClassRegressor();
+  }
+
+  /**
+   * We ignore this test here. The test otherwise fails due to performing
+   * worse than ZeroZ.
+   */
+  @Override
+  public void testBuildInitialization() {
+    System.err.println("testBuildInitialization ignored");
+  }
+
+  /**
+   * Returns the test suite.
+   *
+   * @return		the suite
+   */
+  public static Test suite() {
+    return new TestSuite(LogClassRegressorTest.class);
+  }
+
+  /**
+   * Runs the test from commandline.
+   *
+   * @param args	ignored
+   */
+  public static void main(String[] args) {
+    TestRunner.run(suite());
+  }
+}
