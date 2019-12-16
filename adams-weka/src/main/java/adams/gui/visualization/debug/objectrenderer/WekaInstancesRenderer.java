226348d6ceba9c8bb5c7a7108cbdddccac773255
Peter Reutemann <fracpete@gmail.com>
2019-12-16 20:34:08 +1300

    added support for caching; only showing max 100 rows initially

--- adams-weka/src/main/java/adams/gui/visualization/debug/objectrenderer/WekaInstancesRenderer.java
+++ adams-weka/src/main/java/adams/gui/visualization/debug/objectrenderer/WekaInstancesRenderer.java
@@ -15,7 +15,7 @@
 
 /*
  * WekaInstancesRenderer.java
- * Copyright (C) 2015-2017 University of Waikato, Hamilton, NZ
+ * Copyright (C) 2015-2019 University of Waikato, Hamilton, NZ
  */
 
 package adams.gui.visualization.debug.objectrenderer;
@@ -23,6 +23,7 @@ package adams.gui.visualization.debug.objectrenderer;
 import adams.data.spreadsheet.DefaultSpreadSheet;
 import adams.data.spreadsheet.Row;
 import adams.data.spreadsheet.SpreadSheet;
+import adams.gui.core.BaseButton;
 import adams.gui.core.BaseScrollPane;
 import adams.gui.visualization.instances.InstancesTable;
 import adams.gui.visualization.instances.InstancesTableModel;
@@ -30,20 +31,27 @@ import nz.ac.waikato.cms.locator.ClassLocator;
 import weka.core.Attribute;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.InstancesView;
 
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
 
 /**
  * Renders Weka Instances/Instance objects.
  *
  * @author FracPete (fracpete at waikato dot ac dot nz)
- * @version $Revision$
  */
 public class WekaInstancesRenderer
   extends AbstractObjectRenderer {
 
   private static final long serialVersionUID = -3528006886476495175L;
+
+  public static final int MAX_ROWS = 100;
+
+  /** the last setup. */
+  protected InstancesTable m_LastTable;
 
   /**
    * Checks whether the renderer can handle the specified class.
@@ -58,6 +66,48 @@ public class WekaInstancesRenderer
   }
 
   /**
+   * Checks whether the renderer can use a cached setup to render an object.
+   *
+   * @param obj		the object to render
+   * @param panel	the panel to render into
+   * @return		true if possible
+   */
+  @Override
+  public boolean canRenderCached(Object obj, JPanel panel) {
+    return (m_LastTable != null)
+      && (    ((obj instanceof Instances) && (((Instances) obj).numInstances() > 0))
+           || ((obj instanceof Instance) && ((Instance) obj).dataset() != null) );
+  }
+
+  /**
+   * Performs the actual rendering.
+   *
+   * @param obj		the object to render
+   * @param panel	the panel to render into
+   * @return		null if successful, otherwise error message
+   */
+  @Override
+  protected String doRenderCached(Object obj, JPanel panel) {
+    BaseScrollPane	scrollPane;
+    Instances		data;
+    Instance 		inst;
+
+    if (obj instanceof Instances) {
+      data = (Instances) obj;
+    }
+    else {
+      inst = (Instance) obj;
+      data = new Instances(inst.dataset(), 0);
+      data.add((Instance) inst.copy());
+    }
+    m_LastTable.setModel(new InstancesTableModel(data));
+    scrollPane = new BaseScrollPane(m_LastTable);
+    panel.add(scrollPane, BorderLayout.CENTER);
+
+    return null;
+  }
+
+  /**
    * Performs the actual rendering.
    *
    * @param obj		the object to render
@@ -68,7 +118,8 @@ public class WekaInstancesRenderer
   protected String doRender(Object obj, JPanel panel) {
     Instance 		inst;
     Instances 		data;
-    InstancesTable 	table;
+    InstancesView	view;
+    final InstancesTable 	table;
     InstancesTableModel	model;
     BaseScrollPane	scrollPane;
     PlainTextRenderer	plain;
@@ -76,6 +127,8 @@ public class WekaInstancesRenderer
     Row			row;
     int			i;
     SpreadSheetRenderer	sprenderer;
+    final JPanel	panelButton;
+    BaseButton 		buttonAll;
 
     if (obj instanceof Instances) {
       data = (Instances) obj;
@@ -97,11 +150,31 @@ public class WekaInstancesRenderer
 	sprenderer.render(sheet, panel);
       }
       else {
-        model = new InstancesTableModel(data);
-        model.setShowAttributeIndex(true);
-        table = new InstancesTable(model);
-        scrollPane = new BaseScrollPane(table);
-        panel.add(scrollPane, BorderLayout.CENTER);
+        if (data.numInstances() > MAX_ROWS) {
+	  view  = new InstancesView(data, 0, MAX_ROWS);
+	  model = new InstancesTableModel(view);
+	  model.setShowAttributeIndex(true);
+	  table = new InstancesTable(model);
+	  scrollPane = new BaseScrollPane(table);
+	  panel.add(scrollPane, BorderLayout.CENTER);
+	  panelButton = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	  panel.add(panelButton, BorderLayout.SOUTH);
+	  buttonAll = new BaseButton("Show all " + data.numInstances() + " rows");
+	  buttonAll.addActionListener((ActionEvent e) -> {
+	    table.setInstances(data);
+	    panelButton.setVisible(false);
+	    m_LastTable = table;
+	  });
+	  panelButton.add(buttonAll);
+	}
+	else {
+	  model = new InstancesTableModel(data);
+	  model.setShowAttributeIndex(true);
+	  table = new InstancesTable(model);
+	  scrollPane = new BaseScrollPane(table);
+	  panel.add(scrollPane, BorderLayout.CENTER);
+	  m_LastTable = table;
+	}
       }
     }
     else {
@@ -112,6 +185,7 @@ public class WekaInstancesRenderer
 	table      = new InstancesTable(data);
 	scrollPane = new BaseScrollPane(table);
 	panel.add(scrollPane, BorderLayout.CENTER);
+        m_LastTable = table;
       }
       else {
 	plain = new PlainTextRenderer();
