6e66d234b8f8b1e5139764adfe4336eacefd618e
Peter Reutemann <fracpete@gmail.com>
2020-05-06 11:21:00 +1200

    DirectorySupplier source allows selecting/outputting multiple directories, analog to FileSupplier source

---
+++ adams-core/src/main/java/adams/flow/source/DirectorySupplier.java
@@ -0,0 +1,253 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * DirectorySupplier.java
+ * Copyright (C) 2020 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.flow.source;
+
+import adams.core.QuickInfoHelper;
+import adams.core.io.FileUtils;
+import adams.core.io.ForwardSlashSupporter;
+import adams.core.io.PlaceholderDirectory;
+
+import java.util.ArrayList;
+
+/**
+ <!-- globalinfo-start -->
+ * Supplies directories.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- flow-summary-start -->
+ * Input&#47;output:<br>
+ * - generates:<br>
+ * &nbsp;&nbsp;&nbsp;java.lang.String<br>
+ * <br><br>
+ <!-- flow-summary-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ *
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: DirectorySupplier
+ * </pre>
+ *
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default:
+ * </pre>
+ *
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical
+ * &nbsp;&nbsp;&nbsp;actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-output-array &lt;boolean&gt; (property: outputArray)
+ * &nbsp;&nbsp;&nbsp;Whether to return the directories as array or one by one.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-directory &lt;adams.core.io.PlaceholderDirectory&gt; [-directory ...] (property: directories)
+ * &nbsp;&nbsp;&nbsp;The dirs to supply.
+ * &nbsp;&nbsp;&nbsp;default:
+ * </pre>
+ *
+ * <pre>-use-forward-slashes &lt;boolean&gt; (property: useForwardSlashes)
+ * &nbsp;&nbsp;&nbsp;If enabled, forward slashes are used in the output (but the '\\' prefix
+ * &nbsp;&nbsp;&nbsp;of UNC paths is not converted).
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ */
+public class DirectorySupplier
+  extends AbstractArrayProvider
+  implements ForwardSlashSupporter {
+
+  /** for serialization. */
+  private static final long serialVersionUID = -8288435835502863891L;
+
+  /** the directories to broadcast. */
+  protected PlaceholderDirectory[] m_Directories;
+
+  /** whether to output forward slashes. */
+  protected boolean m_UseForwardSlashes;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Supplies directories.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+	    "directory", "directories",
+	    new PlaceholderDirectory[0]);
+
+    m_OptionManager.add(
+	    "use-forward-slashes", "useForwardSlashes",
+	    false);
+  }
+
+  /**
+   * Returns a quick info about the actor, which will be displayed in the GUI.
+   *
+   * @return		null if no info available, otherwise short string
+   */
+  @Override
+  public String getQuickInfo() {
+    String	result;
+
+    result  = QuickInfoHelper.toString(this, "dirs", (m_Directories.length == 1 ? m_Directories[0] : m_Directories.length + " dirs"));
+    result += QuickInfoHelper.toString(this, "outputArray", (m_OutputArray ? "as array" : "one by one"), ", ");
+    result += QuickInfoHelper.toString(this, "useForwardSlashes", (m_UseForwardSlashes ? "forward slashes" : "platform-specific slashes"), ", ");
+
+    return result;
+  }
+
+  /**
+   * Returns the based class of the items.
+   *
+   * @return		the class
+   */
+  @Override
+  protected Class getItemClass() {
+    return String.class;
+  }
+
+  /**
+   * Sets the dirs to broadcast.
+   *
+   * @param value	the dirs
+   */
+  public void setDirectories(PlaceholderDirectory[] value) {
+    m_Directories = value;
+    reset();
+  }
+
+  /**
+   * Returns the dirs to broadcast.
+   *
+   * @return		the dirs
+   */
+  public PlaceholderDirectory[] getDirectories() {
+    return m_Directories;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String directoriesTipText() {
+    return "The dirs to supply.";
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  @Override
+  public String outputArrayTipText() {
+    return "Whether to return the directories as array or one by one.";
+  }
+
+  /**
+   * Sets whether to use forward slashes in the output.
+   *
+   * @param value	if true then use forward slashes
+   */
+  public void setUseForwardSlashes(boolean value) {
+    m_UseForwardSlashes = value;
+    reset();
+  }
+
+  /**
+   * Returns whether to use forward slashes in the output.
+   *
+   * @return		true if forward slashes are used
+   */
+  public boolean getUseForwardSlashes() {
+    return m_UseForwardSlashes;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String useForwardSlashesTipText() {
+    return
+	"If enabled, forward slashes are used in the output (but "
+	+ "the '\\\\' prefix of UNC paths is not converted).";
+  }
+
+  /**
+   * Executes the flow item.
+   *
+   * @return		always null
+   */
+  @Override
+  protected String doExecute() {
+    String 	dirStr;
+
+    m_Queue = new ArrayList<String>();
+    for (PlaceholderDirectory dir : m_Directories) {
+      dirStr = dir.getAbsolutePath();
+      if (m_UseForwardSlashes)
+	dirStr = FileUtils.useForwardSlashes(dirStr);
+      m_Queue.add(dirStr);
+    }
+
+    return null;
+  }
+}
