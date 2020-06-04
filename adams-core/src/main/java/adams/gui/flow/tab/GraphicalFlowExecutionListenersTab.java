58a2037b825b1884e54af89742026acd141a3b83
Peter Reutemann <fracpete@gmail.com>
2020-06-05 11:10:41 +1200

    fixing NPE when no owner is set

--- adams-core/src/main/java/adams/gui/flow/tab/GraphicalFlowExecutionListenersTab.java
+++ adams-core/src/main/java/adams/gui/flow/tab/GraphicalFlowExecutionListenersTab.java
@@ -94,8 +94,10 @@ public class GraphicalFlowExecutionListenersTab
 	m_TabbedDisplays.setDetachableTabs(true);
 	for (GraphicalFlowExecutionListener listener: registered) {
 	  String title = listener.getListenerTitle();
-	  if (listener.getOwner().getParentComponent() instanceof FlowPanel)
-	    title = ((FlowPanel) listener.getOwner().getParentComponent()).getTitle() + ":" + title;
+	  if (listener.getOwner() != null) {
+	    if (listener.getOwner().getParentComponent() instanceof FlowPanel)
+	      title = ((FlowPanel) listener.getOwner().getParentComponent()).getTitle() + ":" + title;
+	  }
 	  BasePanel listenerPanel = listener.newListenerPanelIfNecessary();
 	  DetachablePanel detachable = new DetachablePanel();
 	  detachable.setFrameTitle(title);
