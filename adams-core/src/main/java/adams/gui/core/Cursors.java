ee332741fb53cd9bff0d6b0e42bdd35d1c04dfba
Peter Reutemann <fracpete@gmail.com>
2020-10-20 12:18:04 +1300

    added scaling support

--- adams-core/src/main/java/adams/gui/core/Cursors.java
+++ adams-core/src/main/java/adams/gui/core/Cursors.java
@@ -48,170 +48,6 @@ public class Cursors {
   }
 
   /**
-   * Returns the cursor cache.
-   *
-   * @return		the cache (name - cursor)
-   */
-  public static Map<String,Cursor> getCache() {
-    return m_Cache;
-  }
-
-  /**
-   * Creates a square cursor with the specified width.
-   *
-   * @param width	the width of the square
-   * @return		the cursor
-   */
-  public static synchronized Cursor square(int width) {
-    return square(width, 1.0);
-  }
-
-  /**
-   * Creates a square cursor with the specified width.
-   *
-   * @param width	the width of the square
-   * @param scale 	for scaling the cursor (1.0 = 100%)
-   * @return		the cursor
-   */
-  public static synchronized Cursor square(int width, double scale) {
-    String		name;
-    BufferedImage	img;
-    Graphics2D 		g2d;
-    Point		p;
-
-    name = "square-" + width;
-    if (!m_Cache.containsKey(name)) {
-      img = new BufferedImage((int) (width*scale), (int) (width*scale), BufferedImage.TYPE_INT_ARGB);
-      g2d = img.createGraphics();
-      p = new Point((int) (width*scale) / 2, (int) (width*scale) / 2);
-
-      // clear
-      g2d.scale(scale, scale);
-      g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.CLEAR));
-      g2d.fillRect(0, 0, width, width);
-
-      // reset composite
-      g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
-
-      // draw
-      width= Math.max(4, width);
-      g2d.setPaint(Color.BLACK);
-      g2d.drawRect(0, 0, width - 1, width - 1);
-      g2d.setColor(Color.WHITE);
-      g2d.drawRect(1, 1, width - 3, width - 3);
-      m_Cache.put(name, Toolkit.getDefaultToolkit().createCustomCursor(img, p, name));
-    }
-    return m_Cache.get(name);
-  }
-
-  /**
-   * Creates a circle cursor with the specified diameter.
-   *
-   * @param diameter	the diameter of the circle
-   * @return		the cursor
-   */
-  public static synchronized Cursor circle(int diameter) {
-    return circle(diameter, 1.0);
-  }
-
-  /**
-   * Creates a circle cursor with the specified diameter.
-   *
-   * @param diameter	the diameter of the circle
-   * @param scale 	for scaling the cursor (1.0 = 100%)
-   * @return		the cursor
-   */
-  public static synchronized Cursor circle(int diameter, double scale) {
-    String		name;
-    BufferedImage	img;
-    Graphics2D 		g2d;
-    Point		p;
-
-    name = "circle-" + diameter;
-    if (!m_Cache.containsKey(name)) {
-      img = new BufferedImage((int) (diameter*scale), (int) (diameter*scale), BufferedImage.TYPE_INT_ARGB);
-      g2d = img.createGraphics();
-      p = new Point((int) (diameter*scale) / 2, (int) (diameter*scale) / 2);
-
-      // clear
-      g2d.scale(scale, scale);
-      g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.CLEAR));
-      g2d.fillRect(0, 0, diameter, diameter);
-
-      // reset composite
-      g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
-
-      // draw
-      diameter = Math.max(4, diameter);
-      g2d.setPaint(Color.BLACK);
-      g2d.drawOval(0, 0, diameter - 1, diameter - 1);
-      g2d.setColor(Color.WHITE);
-      g2d.drawOval(1, 1, diameter - 3, diameter - 3);
-      m_Cache.put(name, Toolkit.getDefaultToolkit().createCustomCursor(img, p, name));
-    }
-    return m_Cache.get(name);
-  }
-
-  /**
-   * Creates a cross-hair cursor with the specified diameter.
-   *
-   * @param diameter	the diameter of the cross-hair
-   * @return		the cursor
-   */
-  public static synchronized Cursor crosshair(int diameter) {
-    return crosshair(diameter, 1.0);
-  }
-
-  /**
-   * Creates a cross-hair cursor with the specified diameter.
-   *
-   * @param diameter	the diameter of the cross-hair
-   * @param scale 	for scaling the cursor (1.0 = 100%)
-   * @return		the cursor
-   */
-  public static synchronized Cursor crosshair(int diameter, double scale) {
-    String		name;
-    BufferedImage	img;
-    Graphics2D 		g2d;
-    Point		p;
-    int 		l;
-
-    name = "crosshair-" + diameter + "-" + scale;
-    if (!m_Cache.containsKey(name)) {
-      img = new BufferedImage((int) (diameter*scale), (int) (diameter*scale), BufferedImage.TYPE_INT_ARGB);
-      g2d = img.createGraphics();
-      if (diameter % 2 == 0)
-        diameter++;
-      p = new Point((int) (diameter*scale) / 2, (int) (diameter*scale) / 2);
-
-      // white background
-      g2d.scale(scale, scale);
-      g2d.fillRect(0, 0, diameter, diameter);
-
-      // clear quadrants
-      g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.CLEAR));
-      l = diameter / 2 - 1;
-      g2d.fillRect(0,                0,                l, l);
-      g2d.fillRect(diameter / 2 + 2, 0,                l, l);
-      g2d.fillRect(0,                diameter / 2 + 2, l, l);
-      g2d.fillRect(diameter / 2 + 2, diameter / 2 + 2, l, l);
-      g2d.fillRect(diameter / 2 - 1, diameter / 2 - 1, 3, 3);
-
-      // reset composite
-      g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
-
-      // draw cross-hair
-      g2d.setPaint(Color.BLACK);
-      g2d.fillRect(1,                diameter / 2,     diameter / 2 - 2, 1);
-      g2d.fillRect(diameter / 2 + 2, diameter / 2,     diameter / 2 - 2,     1);
-      g2d.fillRect(diameter / 2, 1,                1, diameter / 2 - 2);
-      g2d.fillRect(diameter / 2, diameter / 2 + 2, 1, diameter / 2 - 2);
-      m_Cache.put(name, Toolkit.getDefaultToolkit().createCustomCursor(img, p, name));
-    }
-    return m_Cache.get(name);
-  }
-
-  /**
    * Scales the image according to the scale factor.
    *
    * @param img		the image to scale
@@ -265,6 +101,228 @@ public class Cursors {
   }
 
   /**
+   * Scales the hotspot point.
+   *
+   * @param p		the hotspot to scale
+   * @param scale	the scale factor (1 = 100%)
+   * @return		the scaled hotspot
+   */
+  protected static Point scale(Point p, double scale) {
+    return new Point((int) (p.getX() * scale), (int) (p.getY() * scale));
+  }
+
+  /**
+   * Creates and caches the cursor.
+   *
+   * @param name	the name of the cursor
+   * @param img		the image for the cursor
+   * @param p		the hotspot
+   * @param scale	the scale factor
+   * @return		the cursor, default cursor if failed to create
+   */
+  protected static synchronized Cursor cache(String name, ImageIcon img, Point p, double scale) {
+    Cursor 	cursor;
+    try {
+      cursor = Toolkit.getDefaultToolkit().createCustomCursor(scale(img, scale), scale(p, scale), name);
+      m_Cache.put(name, cursor);
+    }
+    catch (Exception e) {
+      System.err.println("Failed to create cursor: name=" + name + ", p=" + p + ", scale=" + scale + ", img=" + img);
+      m_Cache.put(name, Cursor.getDefaultCursor());
+    }
+    return m_Cache.get(name);
+  }
+
+  /**
+   * Creates and caches the cursor.
+   *
+   * @param name	the name of the cursor
+   * @param img		the image for the cursor
+   * @param p		the hotspot
+   * @param scale	the scale factor
+   * @return		the cursor, default cursor if failed to create
+   */
+  protected static synchronized Cursor cache(String name, BufferedImage img, Point p, double scale) {
+    Cursor 	cursor;
+    try {
+      cursor = Toolkit.getDefaultToolkit().createCustomCursor(scale(img, scale), scale(p, scale), name);
+      m_Cache.put(name, cursor);
+    }
+    catch (Exception e) {
+      System.err.println("Failed to create cursor: name=" + name + ", p=" + p + ", scale=" + scale + ", img=" + img);
+      m_Cache.put(name, Cursor.getDefaultCursor());
+    }
+    return m_Cache.get(name);
+  }
+
+  /**
+   * Returns the cursor cache.
+   *
+   * @return		the cache (name - cursor)
+   */
+  public static Map<String,Cursor> getCache() {
+    return m_Cache;
+  }
+
+  /**
+   * Creates a square cursor with the specified width.
+   *
+   * @param width	the width of the square
+   * @return		the cursor
+   */
+  public static synchronized Cursor square(int width) {
+    return square(width, 1.0);
+  }
+
+  /**
+   * Creates a square cursor with the specified width.
+   *
+   * @param width	the width of the square
+   * @param scale 	for scaling the cursor (1.0 = 100%)
+   * @return		the cursor
+   */
+  public static synchronized Cursor square(int width, double scale) {
+    String		name;
+    BufferedImage	img;
+    Graphics2D 		g2d;
+    Point		p;
+
+    name = "square-" + width + "-" + scale;
+    if (!m_Cache.containsKey(name)) {
+      img = new BufferedImage((int) (width*scale), (int) (width*scale), BufferedImage.TYPE_INT_ARGB);
+      g2d = img.createGraphics();
+      p = new Point((int) (width*scale) / 2, (int) (width*scale) / 2);
+
+      // clear
+      g2d.scale(scale, scale);
+      g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.CLEAR));
+      g2d.fillRect(0, 0, width, width);
+
+      // reset composite
+      g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
+
+      // draw
+      width = Math.max(4, width);
+      g2d.setPaint(Color.BLACK);
+      g2d.drawRect(0, 0, width - 1, width - 1);
+      g2d.setColor(Color.WHITE);
+      g2d.drawRect(1, 1, width - 3, width - 3);
+
+      cache(name, img, p, scale);
+    }
+    return m_Cache.get(name);
+  }
+
+  /**
+   * Creates a circle cursor with the specified diameter.
+   *
+   * @param diameter	the diameter of the circle
+   * @return		the cursor
+   */
+  public static synchronized Cursor circle(int diameter) {
+    return circle(diameter, 1.0);
+  }
+
+  /**
+   * Creates a circle cursor with the specified diameter.
+   *
+   * @param diameter	the diameter of the circle
+   * @param scale 	for scaling the cursor (1.0 = 100%)
+   * @return		the cursor
+   */
+  public static synchronized Cursor circle(int diameter, double scale) {
+    String		name;
+    BufferedImage	img;
+    Graphics2D 		g2d;
+    Point		p;
+
+    name = "circle-" + diameter + "-" + scale;
+    if (!m_Cache.containsKey(name)) {
+      img = new BufferedImage((int) (diameter*scale), (int) (diameter*scale), BufferedImage.TYPE_INT_ARGB);
+      g2d = img.createGraphics();
+      p = new Point(diameter / 2, diameter / 2);
+
+      // clear
+      g2d.scale(scale, scale);
+      g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.CLEAR));
+      g2d.fillRect(0, 0, diameter, diameter);
+
+      // reset composite
+      g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
+
+      // draw
+      diameter = Math.max(4, diameter);
+      g2d.setPaint(Color.BLACK);
+      g2d.drawOval(0, 0, diameter - 1, diameter - 1);
+      g2d.setColor(Color.WHITE);
+      g2d.drawOval(1, 1, diameter - 3, diameter - 3);
+
+      cache(name, img, p, scale);
+    }
+    return m_Cache.get(name);
+  }
+
+  /**
+   * Creates a cross-hair cursor with the specified diameter.
+   *
+   * @param diameter	the diameter of the cross-hair
+   * @return		the cursor
+   */
+  public static synchronized Cursor crosshair(int diameter) {
+    return crosshair(diameter, 1.0);
+  }
+
+  /**
+   * Creates a cross-hair cursor with the specified diameter.
+   *
+   * @param diameter	the diameter of the cross-hair
+   * @param scale 	for scaling the cursor (1.0 = 100%)
+   * @return		the cursor
+   */
+  public static synchronized Cursor crosshair(int diameter, double scale) {
+    String		name;
+    BufferedImage	img;
+    Graphics2D 		g2d;
+    Point		p;
+    int 		l;
+
+    name = "crosshair-" + diameter + "-" + scale;
+    if (!m_Cache.containsKey(name)) {
+      img = new BufferedImage((int) (diameter*scale), (int) (diameter*scale), BufferedImage.TYPE_INT_ARGB);
+      g2d = img.createGraphics();
+      if (diameter % 2 == 0)
+        diameter++;
+      p = new Point(diameter / 2, diameter / 2);
+
+      // white background
+      g2d.scale(scale, scale);
+      g2d.fillRect(0, 0, diameter, diameter);
+
+      // clear quadrants
+      g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.CLEAR));
+      l = diameter / 2 - 1;
+      g2d.fillRect(0,                0,                l, l);
+      g2d.fillRect(diameter / 2 + 2, 0,                l, l);
+      g2d.fillRect(0,                diameter / 2 + 2, l, l);
+      g2d.fillRect(diameter / 2 + 2, diameter / 2 + 2, l, l);
+      g2d.fillRect(diameter / 2 - 1, diameter / 2 - 1, 3, 3);
+
+      // reset composite
+      g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
+
+      // draw cross-hair
+      g2d.setPaint(Color.BLACK);
+      g2d.fillRect(1,                diameter / 2,     diameter / 2 - 2, 1);
+      g2d.fillRect(diameter / 2 + 2, diameter / 2,     diameter / 2 - 2,     1);
+      g2d.fillRect(diameter / 2, 1,                1, diameter / 2 - 2);
+      g2d.fillRect(diameter / 2, diameter / 2 + 2, 1, diameter / 2 - 2);
+
+      cache(name, img, p, scale);
+    }
+    return m_Cache.get(name);
+  }
+
+  /**
    * Uses the ADAMS image (located adams.gui.images) as icon.
    *
    * @param iconName	the name of the image (no path)
@@ -296,7 +354,8 @@ public class Cursors {
       icon = GUIHelper.getIcon(iconName);
       if (icon == null)
         throw new IllegalStateException("Failed to load icon: " + iconName);
-      m_Cache.put(name, Toolkit.getDefaultToolkit().createCustomCursor(scale(icon, scale), new Point(x, y), name));
+
+      cache(name, icon, new Point(x, y), scale);
     }
     return m_Cache.get(name);
   }
@@ -360,7 +419,7 @@ public class Cursors {
       img = BufferedImageHelper.read(file).toBufferedImage();
       if (img == null)
         throw new IllegalStateException("Failed to load image: " + file);
-      m_Cache.put(name, Toolkit.getDefaultToolkit().createCustomCursor(scale(img, scale), new Point(x, y), name));
+      cache(name, img, new Point(x, y), scale);
     }
     return m_Cache.get(name);
   }
