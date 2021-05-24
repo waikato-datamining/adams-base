daf0436009ce0c567ae31e4726e603e93ebb5582
Peter Reutemann <fracpete@gmail.com>
2021-05-24 15:55:42 +1200

    the value can now be interpreted as comma- or blank-separated values

--- adams-spreadsheet/src/main/java/adams/flow/transformer/SpreadSheetInsertRow.java
+++ adams-spreadsheet/src/main/java/adams/flow/transformer/SpreadSheetInsertRow.java
@@ -15,7 +15,7 @@
 
 /*
  * SpreadSheetInsertRow.java
- * Copyright (C) 2012-2019 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2012-2021 University of Waikato, Hamilton, New Zealand
  */
 
 package adams.flow.transformer;
@@ -26,6 +26,7 @@ import adams.core.QuickInfoHelper;
 import adams.core.Utils;
 import adams.data.spreadsheet.Row;
 import adams.data.spreadsheet.SpreadSheet;
+import adams.data.spreadsheet.SpreadSheetUtils;
 import adams.flow.core.Token;
 
 import java.util.ArrayList;
@@ -48,13 +49,9 @@ import java.util.List;
  <!-- flow-summary-end -->
  *
  <!-- options-start -->
- * Valid options are: <br><br>
- *
- * <pre>-D &lt;int&gt; (property: debugLevel)
- * &nbsp;&nbsp;&nbsp;The greater the number the more additional info the scheme may output to
- * &nbsp;&nbsp;&nbsp;the console (0 = off).
- * &nbsp;&nbsp;&nbsp;default: 0
- * &nbsp;&nbsp;&nbsp;minimum: 0
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
  * </pre>
  *
  * <pre>-name &lt;java.lang.String&gt; (property: name)
@@ -62,46 +59,72 @@ import java.util.List;
  * &nbsp;&nbsp;&nbsp;default: SpreadSheetInsertRow
  * </pre>
  *
- * <pre>-annotation &lt;adams.core.base.BaseText&gt; (property: annotations)
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
  * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
  * &nbsp;&nbsp;&nbsp;default:
  * </pre>
  *
- * <pre>-skip (property: skip)
+ * <pre>-skip &lt;boolean&gt; (property: skip)
  * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded
  * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
  *
- * <pre>-stop-flow-on-error (property: stopFlowOnError)
- * &nbsp;&nbsp;&nbsp;If set to true, the flow gets stopped in case this actor encounters an error;
- * &nbsp;&nbsp;&nbsp; useful for critical actors.
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical
+ * &nbsp;&nbsp;&nbsp;actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-no-copy &lt;boolean&gt; (property: noCopy)
+ * &nbsp;&nbsp;&nbsp;If enabled, no copy of the spreadsheet is created before processing it.
+ * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
  *
  * <pre>-position &lt;adams.core.Index&gt; (property: position)
- * &nbsp;&nbsp;&nbsp;The position where to insert the column; An index is a number starting with
- * &nbsp;&nbsp;&nbsp;1; the following placeholders can be used as well: first, second, third,
- * &nbsp;&nbsp;&nbsp;last_2, last_1, last
+ * &nbsp;&nbsp;&nbsp;The position where to insert the column.
  * &nbsp;&nbsp;&nbsp;default: last
+ * &nbsp;&nbsp;&nbsp;example: An index is a number starting with 1; the following placeholders can be used as well: first, second, third, last_2, last_1, last
  * </pre>
  *
- * <pre>-after (property: after)
+ * <pre>-after &lt;boolean&gt; (property: after)
  * &nbsp;&nbsp;&nbsp;If enabled, the column is inserted after the position instead of at the
  * &nbsp;&nbsp;&nbsp;position.
+ * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
  *
  * <pre>-value &lt;java.lang.String&gt; (property: value)
- * &nbsp;&nbsp;&nbsp;The value to intialize the cells with; you can use '\t' for tab, '\n' for
+ * &nbsp;&nbsp;&nbsp;The value to initialize the cells with; you can use '\t' for tab, '\n' for
  * &nbsp;&nbsp;&nbsp;line-feed and '\r' for carriage-return.
  * &nbsp;&nbsp;&nbsp;default: ?
  * </pre>
  *
- * <pre>-placeholder (property: valueContainsPlaceholder)
- * &nbsp;&nbsp;&nbsp;Set this to true to enable automatic placeholder expansion for the value
- * &nbsp;&nbsp;&nbsp;string.
+ * <pre>-value-type &lt;SINGLE_VALUE|COMMA_SEPARATED_VALUES|BLANK_SEPARATED_VALUES&gt; (property: valueType)
+ * &nbsp;&nbsp;&nbsp;How to interpret the value string.
+ * &nbsp;&nbsp;&nbsp;default: SINGLE_VALUE
  * </pre>
  *
- * <pre>-variable (property: valueContainsVariable)
+ * <pre>-placeholder &lt;boolean&gt; (property: valueContainsPlaceholder)
+ * &nbsp;&nbsp;&nbsp;Set this to true to enable automatic placeholder expansion for the value
+ * &nbsp;&nbsp;&nbsp;string.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-variable &lt;boolean&gt; (property: valueContainsVariable)
  * &nbsp;&nbsp;&nbsp;Set this to true to enable automatic variable expansion for the value string.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-force-string &lt;boolean&gt; (property: forceString)
+ * &nbsp;&nbsp;&nbsp;If enabled, the value is set as string, even if it resembles a number.
+ * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
  *
  <!-- options-end -->
@@ -114,6 +137,13 @@ public class SpreadSheetInsertRow
   /** for serialization. */
   private static final long serialVersionUID = 4198653233287560570L;
 
+  /** how to parse the value. */
+  public enum ValueType {
+    SINGLE_VALUE,
+    COMMA_SEPARATED_VALUES,
+    BLANK_SEPARATED_VALUES,
+  }
+
   /** the position where to insert the row. */
   protected Index m_Position;
 
@@ -123,6 +153,9 @@ public class SpreadSheetInsertRow
   /** the value to initialize the cells with. */
   protected String m_Value;
 
+  /** the value type. */
+  protected ValueType m_ValueType;
+
   /** whether the value string contains a placeholder, which needs to be
    * expanded first. */
   protected boolean m_ValueContainsPlaceholder;
@@ -130,6 +163,9 @@ public class SpreadSheetInsertRow
   /** whether the value string contains a variable, which needs to be
    * expanded first. */
   protected boolean m_ValueContainsVariable;
+
+  /** whether to set value as string. */
+  protected boolean m_ForceString;
 
   /**
    * Returns a string describing the object.
@@ -162,24 +198,32 @@ public class SpreadSheetInsertRow
     super.defineOptions();
 
     m_OptionManager.add(
-	    "position", "position",
-	    new Index(Index.LAST));
+      "position", "position",
+      new Index(Index.LAST));
 
     m_OptionManager.add(
-	    "after", "after",
-	    false);
+      "after", "after",
+      false);
 
     m_OptionManager.add(
-	    "value", "value",
-	    SpreadSheet.MISSING_VALUE);
+      "value", "value",
+      SpreadSheet.MISSING_VALUE);
 
     m_OptionManager.add(
-	    "placeholder", "valueContainsPlaceholder",
-	    false);
+      "value-type", "valueType",
+      ValueType.SINGLE_VALUE);
 
     m_OptionManager.add(
-	    "variable", "valueContainsVariable",
-	    false);
+      "placeholder", "valueContainsPlaceholder",
+      false);
+
+    m_OptionManager.add(
+      "variable", "valueContainsVariable",
+      false);
+
+    m_OptionManager.add(
+      "force-string", "forceString",
+      false);
   }
 
   /**
@@ -201,11 +245,13 @@ public class SpreadSheetInsertRow
     result += QuickInfoHelper.toString(this, "position", m_Position);
 
     result += QuickInfoHelper.toString(this, "value", "'" + getValue() + "'", ", insert: ");
+    result += QuickInfoHelper.toString(this, "valueType", m_ValueType, ", type: ");
 
     options = new ArrayList<>();
     QuickInfoHelper.add(options, QuickInfoHelper.toString(this, "valueContainsPlaceholder", m_ValueContainsPlaceholder, "PH"));
     QuickInfoHelper.add(options, QuickInfoHelper.toString(this, "valueContainsVariable", m_ValueContainsVariable, "Var"));
     QuickInfoHelper.add(options, QuickInfoHelper.toString(this, "noCopy", m_NoCopy, "no copy"));
+    QuickInfoHelper.add(options, QuickInfoHelper.toString(this, "forceString", m_ForceString, "force string"));
     result += QuickInfoHelper.flatten(options);
 
     return result;
@@ -298,7 +344,36 @@ public class SpreadSheetInsertRow
    * 			displaying in the GUI or for listing the options.
    */
   public String valueTipText() {
-    return "The value to intialize the cells with; you can use '\\t' for tab, '\\n' for line-feed and '\\r' for carriage-return.";
+    return "The value to initialize the cells with; you can use '\\t' for tab, '\\n' for line-feed and '\\r' for carriage-return.";
+  }
+
+  /**
+   * Sets how to interpret the value.
+   *
+   * @param value	the type
+   */
+  public void setValueType(ValueType value) {
+    m_ValueType = value;
+    reset();
+  }
+
+  /**
+   * Returns how to interpret the value.
+   *
+   * @return		the type
+   */
+  public ValueType getValueType() {
+    return m_ValueType;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String valueTypeTipText() {
+    return "How to interpret the value string.";
   }
 
   /**
@@ -364,6 +439,37 @@ public class SpreadSheetInsertRow
   }
 
   /**
+   * Sets whether to force setting the value as string even if it resembles
+   * a number.
+   *
+   * @param value	true if to force string
+   */
+  public void setForceString(boolean value) {
+    m_ForceString = value;
+    reset();
+  }
+
+  /**
+   * Returns whether to force setting the value as string even if it resembles
+   * a number.
+   *
+   * @return		true if string type is enforced
+   */
+  public boolean getForceString() {
+    return m_ForceString;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String forceStringTipText() {
+    return "If enabled, the value is set as string, even if it resembles a number.";
+  }
+
+  /**
    * Executes the flow item.
    *
    * @return		null if everything is fine, otherwise error message
@@ -377,6 +483,7 @@ public class SpreadSheetInsertRow
     int			i;
     String		value;
     Row			rowNew;
+    String[]		cells;
 
     result = null;
     sheetOld  = (SpreadSheet) m_InputToken.getPayload();
@@ -405,10 +512,43 @@ public class SpreadSheetInsertRow
     rowNew = sheetNew.insertRow(pos);
 
     // data
-    for (i = 0; i < sheetNew.getColumnCount(); i++)
-      rowNew.addCell(sheetNew.getHeaderRow().getCellKey(i)).setContent(value);
+    switch (m_ValueType) {
+      case SINGLE_VALUE:
+	for (i = 0; i < sheetNew.getColumnCount(); i++) {
+	  if (m_ForceString)
+	    rowNew.addCell(sheetNew.getHeaderRow().getCellKey(i)).setContentAsString(value);
+	  else
+	    rowNew.addCell(sheetNew.getHeaderRow().getCellKey(i)).setContent(value);
+	}
+        break;
 
-    m_OutputToken = new Token(sheetNew);
+      case COMMA_SEPARATED_VALUES:
+        cells = SpreadSheetUtils.split(value, ',', true);
+        for (i = 0; i < cells.length && i < sheetNew.getColumnCount(); i++) {
+	  if (m_ForceString)
+	    rowNew.addCell(sheetNew.getHeaderRow().getCellKey(i)).setContentAsString(cells[i]);
+	  else
+	    rowNew.addCell(sheetNew.getHeaderRow().getCellKey(i)).setContent(cells[i]);
+	}
+        break;
+
+      case BLANK_SEPARATED_VALUES:
+        cells = SpreadSheetUtils.split(value, ' ', true);
+        for (i = 0; i < cells.length && i < sheetNew.getColumnCount(); i++) {
+	  if (m_ForceString)
+	    rowNew.addCell(sheetNew.getHeaderRow().getCellKey(i)).setContentAsString(cells[i]);
+	  else
+	    rowNew.addCell(sheetNew.getHeaderRow().getCellKey(i)).setContent(cells[i]);
+	}
+        break;
+
+      default:
+        result = "Unhandled value type: " + m_ValueType;
+        break;
+    }
+
+    if (result == null)
+      m_OutputToken = new Token(sheetNew);
 
     return result;
   }
