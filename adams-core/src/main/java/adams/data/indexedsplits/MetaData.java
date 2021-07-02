03d644565b9a49294115e3e699845b926017c72e
Peter Reutemann <fracpete@gmail.com>
2021-07-02 15:34:40 +1200

    getXYZ methods now handle strings as well

--- adams-core/src/main/java/adams/data/indexedsplits/MetaData.java
+++ adams-core/src/main/java/adams/data/indexedsplits/MetaData.java
@@ -20,6 +20,8 @@
 
 package adams.data.indexedsplits;
 
+import adams.core.Utils;
+
 import java.util.HashMap;
 
 /**
@@ -31,4 +33,102 @@ public class MetaData
   extends HashMap<String,Object> {
 
   private static final long serialVersionUID = 3534089057338513027L;
+
+  /**
+   * Returns the integer value associated with the key or the default value
+   * if not present or not a number or string representing an integer.
+   *
+   * @param key		the key to retrieve
+   * @param defValue	the default value
+   * @return		the retrieved value or the default value
+   */
+  public int getInteger(String key, int defValue) {
+    int 	result;
+    Object	value;
+
+    result = defValue;
+
+    if (containsKey(key)) {
+      value = get(key);
+      if (value instanceof Number)
+        result = ((Number) value).intValue();
+      else if (Utils.isInteger("" + value))
+        result = Integer.parseInt("" + value);
+    }
+
+    return result;
+  }
+
+  /**
+   * Returns the double value associated with the key or the default value
+   * if not present or not a number or string representing a double.
+   *
+   * @param key		the key to retrieve
+   * @param defValue	the default value
+   * @return		the retrieved value or the default value
+   */
+  public double getDouble(String key, double defValue) {
+    double	result;
+    Object	value;
+
+    result = defValue;
+
+    if (containsKey(key)) {
+      value = get(key);
+      if (value instanceof Number)
+        result = ((Number) value).doubleValue();
+      else if (Utils.isDouble("" + value))
+        result = Double.parseDouble("" + value);
+    }
+
+    return result;
+  }
+
+  /**
+   * Returns the boolean value associated with the key or the default value
+   * if not present or not a boolean or string representing a boolean.
+   *
+   * @param key		the key to retrieve
+   * @param defValue	the default value
+   * @return		the retrieved value or the default value
+   */
+  public boolean getBoolean(String key, boolean defValue) {
+    boolean	result;
+    Object	value;
+
+    result = defValue;
+
+    if (containsKey(key)) {
+      value = get(key);
+      if (value instanceof Boolean)
+        result = (Boolean) value;
+      else if (Utils.isBoolean("" + value))
+        result = Boolean.parseBoolean("" + value);
+    }
+
+    return result;
+  }
+
+  /**
+   * Returns the string value associated with the key or the default value
+   * if not present or not a string.
+   *
+   * @param key		the key to retrieve
+   * @param defValue	the default value
+   * @return		the retrieved value or the default value
+   */
+  public String getString(String key, String defValue) {
+    String	result;
+    Object	value;
+
+    result = defValue;
+
+    if (containsKey(key)) {
+      value = get(key);
+      if (value instanceof String)
+        result = (String) value;
+    }
+
+    return result;
+  }
 }
