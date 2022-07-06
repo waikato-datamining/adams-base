7a2da8b2acd5063a495378b0129f63dc6e199842
Peter Reutemann <fracpete@gmail.com>
2022-07-07 11:56:03 +1200

    added ability to initialize counter names with specific value

--- adams-core/src/main/java/adams/flow/standalone/CounterInit.java
+++ adams-core/src/main/java/adams/flow/standalone/CounterInit.java
@@ -15,12 +15,13 @@
 
 /*
  * CounterInit.java
- * Copyright (C) 2015-2019 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2015-2022 University of Waikato, Hamilton, New Zealand
  */
 package adams.flow.standalone;
 
 import adams.core.NamedCounter;
 import adams.core.QuickInfoHelper;
+import adams.core.base.BaseString;
 import adams.flow.control.StorageName;
 import adams.flow.control.StorageUpdater;
 
@@ -38,52 +39,70 @@ import adams.flow.control.StorageUpdater;
  * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
  * &nbsp;&nbsp;&nbsp;default: WARNING
  * </pre>
- * 
+ *
  * <pre>-name &lt;java.lang.String&gt; (property: name)
  * &nbsp;&nbsp;&nbsp;The name of the actor.
  * &nbsp;&nbsp;&nbsp;default: CounterInit
  * </pre>
- * 
+ *
  * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
  * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
- * &nbsp;&nbsp;&nbsp;default: 
+ * &nbsp;&nbsp;&nbsp;default:
  * </pre>
- * 
+ *
  * <pre>-skip &lt;boolean&gt; (property: skip)
- * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded 
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded
  * &nbsp;&nbsp;&nbsp;as it is.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- * 
+ *
  * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
- * &nbsp;&nbsp;&nbsp;If set to true, the flow gets stopped in case this actor encounters an error;
- * &nbsp;&nbsp;&nbsp; useful for critical actors.
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical
+ * &nbsp;&nbsp;&nbsp;actors.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- * 
+ *
  * <pre>-silent &lt;boolean&gt; (property: silent)
- * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console.
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- * 
+ *
  * <pre>-storage-name &lt;adams.flow.control.StorageName&gt; (property: storageName)
  * &nbsp;&nbsp;&nbsp;The name of the counter in the internal storage.
  * &nbsp;&nbsp;&nbsp;default: counter
  * </pre>
- * 
+ *
+ * <pre>-initial-values &lt;adams.core.base.BaseString&gt; [-initial-values ...] (property: initialValues)
+ * &nbsp;&nbsp;&nbsp;The values to initialize the counter with.
+ * &nbsp;&nbsp;&nbsp;default:
+ * </pre>
+ *
+ * <pre>-initial-count &lt;int&gt; (property: initialCount)
+ * &nbsp;&nbsp;&nbsp;The count to use for the initial values.
+ * &nbsp;&nbsp;&nbsp;default: 0
+ * </pre>
+ *
  <!-- options-end -->
  *
  * @author  fracpete (fracpete at waikato dot ac dot nz)
  */
 public class CounterInit
-  extends AbstractStandalone
-  implements StorageUpdater {
+    extends AbstractStandalone
+    implements StorageUpdater {
 
   /** for serialization. */
   private static final long serialVersionUID = 4182914190162129217L;
 
   /** the name of the counter in the internal storage. */
   protected StorageName m_StorageName;
+
+  /** the initial labels to use. */
+  protected BaseString[] m_InitialValues;
+
+  /** the initial counter value to use. */
+  protected int m_InitialCount;
 
   /**
    * Returns a string describing the object.
@@ -92,10 +111,10 @@ public class CounterInit
    */
   @Override
   public String globalInfo() {
-    return 
+    return
 	"Creates an empty counter in internal storage under the specified name.";
   }
-  
+
   /**
    * Adds options to the internal list of options.
    */
@@ -104,13 +123,21 @@ public class CounterInit
     super.defineOptions();
 
     m_OptionManager.add(
-	    "storage-name", "storageName",
-	    new StorageName("counter"));
+	"storage-name", "storageName",
+	new StorageName("counter"));
+
+    m_OptionManager.add(
+	"initial-values", "initialValues",
+	new BaseString[0]);
+
+    m_OptionManager.add(
+	"initial-count", "initialCount",
+	0);
   }
 
   /**
    * Returns whether storage items are being updated.
-   * 
+   *
    * @return		true if storage items are updated
    */
   public boolean isUpdatingStorage() {
@@ -126,7 +153,9 @@ public class CounterInit
   public String getQuickInfo() {
     String	result;
 
-    result  = QuickInfoHelper.toString(this, "storageName", m_StorageName, "storage: ");
+    result = QuickInfoHelper.toString(this, "storageName", m_StorageName, "storage: ");
+    result += QuickInfoHelper.toString(this, "initialValues", (m_InitialValues.length == 0) ? "-none-" : m_InitialValues, ", initial values: ");
+    result += QuickInfoHelper.toString(this, "initialCount", m_InitialCount, ", initial count: ");
 
     return result;
   }
@@ -159,7 +188,65 @@ public class CounterInit
   public String storageNameTipText() {
     return "The name of the counter in the internal storage.";
   }
-  
+
+  /**
+   * Sets the values to initialize the counter with.
+   *
+   * @param value	the values
+   */
+  public void setInitialValues(BaseString[] value) {
+    m_InitialValues = value;
+    reset();
+  }
+
+  /**
+   * Returns the values to initialize the counter with.
+   *
+   * @return		the values
+   */
+  public BaseString[] getInitialValues() {
+    return m_InitialValues;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String initialValuesTipText() {
+    return "The values to initialize the counter with.";
+  }
+
+  /**
+   * Sets the initial count to use for the initial values.
+   *
+   * @param value	the count
+   */
+  public void setInitialCount(int value) {
+    m_InitialCount = value;
+    reset();
+  }
+
+  /**
+   * Returns the initial count to use for the initial values.
+   *
+   * @return		the count
+   */
+  public int getInitialCount() {
+    return m_InitialCount;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String initialCountTipText() {
+    return "The count to use for the initial values.";
+  }
+
   /**
    * Executes the flow item.
    *
@@ -167,12 +254,17 @@ public class CounterInit
    */
   @Override
   protected String doExecute() {
-    String 	result;
+    String 		result;
+    NamedCounter	counter;
 
     result = getOptionManager().ensureVariableForPropertyExists("storageName");
 
-    if (result == null)
-      getStorageHandler().getStorage().put(m_StorageName, new NamedCounter());
+    if (result == null) {
+      counter = new NamedCounter();
+      for (BaseString initialValue: m_InitialValues)
+	counter.set(initialValue.getValue(), m_InitialCount);
+      getStorageHandler().getStorage().put(m_StorageName, counter);
+    }
 
     return result;
   }
--- adams-core/src/main/java/adams/flow/transformer/CounterInit.java
+++ adams-core/src/main/java/adams/flow/transformer/CounterInit.java
@@ -15,13 +15,14 @@
 
 /*
  * CounterInit.java
- * Copyright (C) 2015-2019 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2015-2022 University of Waikato, Hamilton, New Zealand
  */
 package adams.flow.transformer;
 
 import adams.core.Index;
 import adams.core.NamedCounter;
 import adams.core.QuickInfoHelper;
+import adams.core.base.BaseString;
 import adams.data.spreadsheet.Row;
 import adams.data.spreadsheet.SpreadSheet;
 import adams.flow.control.StorageName;
@@ -51,59 +52,77 @@ import java.lang.reflect.Array;
  * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
  * &nbsp;&nbsp;&nbsp;default: WARNING
  * </pre>
- * 
+ *
  * <pre>-name &lt;java.lang.String&gt; (property: name)
  * &nbsp;&nbsp;&nbsp;The name of the actor.
  * &nbsp;&nbsp;&nbsp;default: CounterInit
  * </pre>
- * 
+ *
  * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
  * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
- * &nbsp;&nbsp;&nbsp;default: 
+ * &nbsp;&nbsp;&nbsp;default:
  * </pre>
- * 
+ *
  * <pre>-skip &lt;boolean&gt; (property: skip)
- * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded 
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded
  * &nbsp;&nbsp;&nbsp;as it is.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- * 
+ *
  * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
- * &nbsp;&nbsp;&nbsp;If set to true, the flow gets stopped in case this actor encounters an error;
- * &nbsp;&nbsp;&nbsp; useful for critical actors.
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical
+ * &nbsp;&nbsp;&nbsp;actors.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- * 
+ *
  * <pre>-silent &lt;boolean&gt; (property: silent)
- * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console.
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- * 
+ *
  * <pre>-storage-name &lt;adams.flow.control.StorageName&gt; (property: storageName)
  * &nbsp;&nbsp;&nbsp;The name for the counter in the internal storage.
  * &nbsp;&nbsp;&nbsp;default: counter
  * </pre>
- * 
+ *
+ * <pre>-initial-values &lt;adams.core.base.BaseString&gt; [-initial-values ...] (property: initialValues)
+ * &nbsp;&nbsp;&nbsp;The values to initialize the counter with.
+ * &nbsp;&nbsp;&nbsp;default:
+ * </pre>
+ *
+ * <pre>-initial-count &lt;int&gt; (property: initialCount)
+ * &nbsp;&nbsp;&nbsp;The count to use for the initial values.
+ * &nbsp;&nbsp;&nbsp;default: 0
+ * </pre>
+ *
  * <pre>-column &lt;adams.core.Index&gt; (property: column)
- * &nbsp;&nbsp;&nbsp;The index of the column in the spreadsheet which values to store in the 
+ * &nbsp;&nbsp;&nbsp;The index of the column in the spreadsheet which values to store in the
  * &nbsp;&nbsp;&nbsp;counter.
  * &nbsp;&nbsp;&nbsp;default: first
  * &nbsp;&nbsp;&nbsp;example: An index is a number starting with 1; the following placeholders can be used as well: first, second, third, last_2, last_1, last
  * </pre>
- * 
+ *
  <!-- options-end -->
  *
  * @author  fracpete (fracpete at waikato dot ac dot nz)
  */
 public class CounterInit
-  extends AbstractTransformer
-  implements StorageUpdater {
+    extends AbstractTransformer
+    implements StorageUpdater {
 
   /** for serialization. */
   private static final long serialVersionUID = 4182914190162129217L;
 
   /** the name of the counter in the internal storage. */
   protected StorageName m_StorageName;
+
+  /** the initial labels to use. */
+  protected BaseString[] m_InitialValues;
+
+  /** the initial counter value to use. */
+  protected int m_InitialCount;
 
   /** the index of the column which values to store in the counter. */
   protected Index m_Column;
@@ -115,11 +134,11 @@ public class CounterInit
    */
   @Override
   public String globalInfo() {
-    return 
+    return
 	"Creates a counter from a column in a spreadsheet or an array. The counter itself "
-	+ "gets stored in the internal storage under the specified name.";
+	    + "gets stored in the internal storage under the specified name.";
   }
-  
+
   /**
    * Adds options to the internal list of options.
    */
@@ -128,17 +147,25 @@ public class CounterInit
     super.defineOptions();
 
     m_OptionManager.add(
-	    "storage-name", "storageName",
-	    new StorageName("counter"));
+	"storage-name", "storageName",
+	new StorageName("counter"));
 
     m_OptionManager.add(
-	    "column", "column",
-	    new Index(Index.FIRST));
+	"initial-values", "initialValues",
+	new BaseString[0]);
+
+    m_OptionManager.add(
+	"initial-count", "initialCount",
+	0);
+
+    m_OptionManager.add(
+	"column", "column",
+	new Index(Index.FIRST));
   }
 
   /**
    * Returns whether storage items are being updated.
-   * 
+   *
    * @return		true if storage items are updated
    */
   public boolean isUpdatingStorage() {
@@ -155,6 +182,8 @@ public class CounterInit
     String	result;
 
     result  = QuickInfoHelper.toString(this, "storageName", m_StorageName, "storage: ");
+    result += QuickInfoHelper.toString(this, "initialValues", (m_InitialValues.length == 0) ? "-none-" : m_InitialValues, ", initial values: ");
+    result += QuickInfoHelper.toString(this, "initialCount", m_InitialCount, ", initial count: ");
     result += QuickInfoHelper.toString(this, "column", m_Column, ", col: ");
 
     return result;
@@ -190,6 +219,64 @@ public class CounterInit
   }
 
   /**
+   * Sets the values to initialize the counter with.
+   *
+   * @param value	the values
+   */
+  public void setInitialValues(BaseString[] value) {
+    m_InitialValues = value;
+    reset();
+  }
+
+  /**
+   * Returns the values to initialize the counter with.
+   *
+   * @return		the values
+   */
+  public BaseString[] getInitialValues() {
+    return m_InitialValues;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String initialValuesTipText() {
+    return "The values to initialize the counter with.";
+  }
+
+  /**
+   * Sets the initial count to use for the initial values.
+   *
+   * @param value	the count
+   */
+  public void setInitialCount(int value) {
+    m_InitialCount = value;
+    reset();
+  }
+
+  /**
+   * Returns the initial count to use for the initial values.
+   *
+   * @return		the count
+   */
+  public int getInitialCount() {
+    return m_InitialCount;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String initialCountTipText() {
+    return "The count to use for the initial values.";
+  }
+
+  /**
    * Sets the index of the column to store in the counter.
    *
    * @param value	the index
@@ -220,7 +307,7 @@ public class CounterInit
 
   /**
    * Returns the class that the consumer accepts.
-   * 
+   *
    * @return		the Class of objects that can be processed
    */
   @Override
@@ -237,7 +324,7 @@ public class CounterInit
   public Class[] generates() {
     return new Class[]{SpreadSheet.class, Object[].class};
   }
-  
+
   /**
    * Executes the flow item.
    *
@@ -252,7 +339,7 @@ public class CounterInit
     String		val;
     Object		array;
     int			i;
-    
+
     result = getOptionManager().ensureVariableForPropertyExists("storageName");
 
     if (result == null) {
@@ -274,6 +361,8 @@ public class CounterInit
 	// create counter
 	if (result == null) {
 	  counter = new NamedCounter();
+	  for (BaseString initialValue: m_InitialValues)
+	    counter.set(initialValue.getValue(), m_InitialCount);
 	  for (Row row : sheet.rows()) {
 	    if (!row.hasCell(valCol))
 	      continue;
@@ -298,7 +387,7 @@ public class CounterInit
 	}
       }
     }
-    
+
     if (result == null)
       m_OutputToken = m_InputToken;
 
