a893f90ab5066063707311bcb3cab5d302622634
Peter Reutemann <fracpete@gmail.com>
2023-03-14 13:23:19 +1300

    added updateCorner method

--- adams-core/src/main/java/adams/gui/visualization/image/RectangleUtils.java
+++ adams-core/src/main/java/adams/gui/visualization/image/RectangleUtils.java
@@ -21,6 +21,7 @@
 package adams.gui.visualization.image;
 
 import java.awt.Point;
+import java.awt.Rectangle;
 
 /**
  * Helper class for selections.
@@ -75,15 +76,79 @@ public class RectangleUtils {
 
     switch (corner) {
       case TOP_LEFT:
-        return new Point(x0, y0);
+	return new Point(x0, y0);
       case TOP_RIGHT:
-        return new Point(x1, y0);
+	return new Point(x1, y0);
       case BOTTOM_LEFT:
-        return new Point(x0, y1);
+	return new Point(x0, y1);
       case BOTTOM_RIGHT:
-        return new Point(x1, y1);
+	return new Point(x1, y1);
       default:
-        throw new IllegalStateException("Unhandled rectangle corner: " + corner);
+	throw new IllegalStateException("Unhandled rectangle corner: " + corner);
     }
+  }
+
+  /**
+   * Updates the corner of a rectangle and returns the new rectangle.
+   *
+   * @param rect	the rectangle to update
+   * @param cornerOld	the old corner
+   * @param cornerNew	the new corner
+   * @return		the updated rectangle
+   */
+  public static Rectangle updateCorner(Rectangle rect, Point cornerOld, Point cornerNew) {
+    RectangleCorner	corner;
+    int			minX;
+    int			maxX;
+    int			minY;
+    int			maxY;
+
+    corner = null;
+
+    minX = rect.x;
+    maxX = rect.x + rect.width - 1;
+    minY = rect.y;
+    maxY = rect.y + rect.height - 1;
+
+    // which corner was updated?
+    if (cornerOld.x == minX) {
+      if (cornerOld.y == minY)
+        corner = RectangleCorner.TOP_LEFT;
+      else if (cornerOld.y == maxY)
+	corner = RectangleCorner.BOTTOM_LEFT;
+    }
+    else if (cornerOld.x == maxX) {
+      if (cornerOld.y == minY)
+	corner = RectangleCorner.TOP_RIGHT;
+      else if (cornerOld.y == maxY)
+	corner = RectangleCorner.BOTTOM_RIGHT;
+    }
+
+    if (corner == null)
+      return rect;
+
+    // update boundaries
+    switch (corner) {
+      case TOP_LEFT:
+        minX = cornerNew.x;
+        minY = cornerNew.y;
+        break;
+      case TOP_RIGHT:
+        maxX = cornerNew.x;
+        minY = cornerNew.y;
+        break;
+      case BOTTOM_LEFT:
+	minX = cornerNew.x;
+	maxY = cornerNew.y;
+        break;
+      case BOTTOM_RIGHT:
+	maxX = cornerNew.x;
+	maxY = cornerNew.y;
+	break;
+      default:
+        throw new IllegalStateException("Unhandled corner: " + corner);
+    }
+
+    return new Rectangle(minX, minY, maxX - minX + 1, maxY - minY + 1);
   }
 }
