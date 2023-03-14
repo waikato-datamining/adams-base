45966b23db17ad185e9e20537284d1948baacfdc
Peter Reutemann <fracpete@gmail.com>
2023-03-14 13:55:41 +1300

    added method to insert vertex into polygon

--- adams-core/src/main/java/adams/gui/visualization/image/PolygonUtils.java
+++ adams-core/src/main/java/adams/gui/visualization/image/PolygonUtils.java
@@ -20,12 +20,16 @@
 
 package adams.gui.visualization.image;
 
+import adams.core.License;
+import adams.core.annotation.MixedCopyright;
+import adams.data.statistics.StatUtils;
 import gnu.trove.list.TIntList;
 import gnu.trove.list.array.TIntArrayList;
 
 import java.awt.Point;
 import java.awt.Polygon;
 import java.awt.Rectangle;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -72,6 +76,24 @@ public class PolygonUtils {
   }
 
   /**
+   * Turns the polygon into a list of points.
+   *
+   * @param poly	the polygon
+   * @return		the list of points
+   */
+  public static List<Point> toPoints(Polygon poly) {
+    List<Point>	result;
+    int		i;
+
+    result = new ArrayList<>();
+
+    for (i = 0; i < poly.npoints; i++)
+      result.add(new Point(poly.xpoints[i], poly.ypoints[i]));
+
+    return result;
+  }
+
+  /**
    * Returns the bounding box for the polygon.
    *
    * @param points	the polygon points to calculate the bounding box for
@@ -96,5 +118,124 @@ public class PolygonUtils {
     }
 
     return new Rectangle(minx, miny, maxx - minx + 1, maxy - miny + 1);
+  }
+
+  /**
+   * Returns the minimum distance between a line segment AB and a point E.
+   *
+   * @param A	the start of the line segment
+   * @param B	the end of the line segment
+   * @param E	the point to calculate the min distance to
+   * @return	the minimum distance
+   */
+  @MixedCopyright(
+    license = License.CC_BY_SA_3,
+    author = "rrlinus",
+    url = "https://www.geeksforgeeks.org/minimum-distance-from-a-point-to-the-line-segment-using-vectors/"
+  )
+  public static double minDistance(Point A, Point B, Point E)
+  {
+    // vector AB
+    Point AB = new Point();
+    AB.x = B.x - A.x;
+    AB.y = B.y - A.y;
+
+    // vector BP
+    Point BE = new Point();
+    BE.x = E.x - B.x;
+    BE.y = E.y - B.y;
+
+    // vector AP
+    Point AE = new Point();
+    AE.x = E.x - A.x;
+    AE.y = E.y - A.y;
+
+    // Variables to store dot product
+    double AB_BE, AB_AE;
+
+    // Calculating the dot product
+    AB_BE = (AB.x * BE.x + AB.y * BE.y);
+    AB_AE = (AB.x * AE.x + AB.y * AE.y);
+
+    // Minimum distance from
+    // point E to the line segment
+    double reqAns = 0;
+
+    // Case 1
+    if (AB_BE > 0)
+    {
+      // Finding the magnitude
+      double y = E.y - B.y;
+      double x = E.x - B.x;
+      reqAns = Math.sqrt(x * x + y * y);
+    }
+    // Case 2
+    else if (AB_AE < 0) {
+      double y = E.y - A.y;
+      double x = E.x - A.x;
+      reqAns = Math.sqrt(x * x + y * y);
+    }
+    // Case 3
+    else {
+      // Finding the perpendicular distance
+      double x1 = AB.x;
+      double y1 = AB.y;
+      double x2 = AE.x;
+      double y2 = AE.y;
+      double mod = Math.sqrt(x1 * x1 + y1 * y1);
+      reqAns = Math.abs(x1 * y2 - y1 * x2) / mod;
+    }
+
+    return reqAns;
+  }
+
+  /**
+   * Adds the vertex to the polygon at the appropriate position.
+   *
+   * @param poly	the polygon to add to
+   * @param vertex 	the vertex to insert
+   * @return		the new polygon
+   */
+  public static Polygon addVertext(Polygon poly, Point vertex) {
+    Polygon		result;
+    List<Point>		points;
+    Point		A;
+    Point		B;
+    int			i;
+    double[]		dist;
+    TIntList		x;
+    TIntList		y;
+
+    // calculate min distance to all line segments
+    points = toPoints(poly);
+    dist   = new double[points.size()];
+    for (i = 0; i < points.size(); i++) {
+      if (i == points.size() - 1) {
+        A = points.get(i);
+        B = points.get(0);
+      }
+      else {
+        A = points.get(i);
+        B = points.get(i + 1);
+      }
+      dist[i] = minDistance(A, B, vertex);
+    }
+
+    // insert between points of line segment with smallest distance
+    i = StatUtils.minIndex(dist);
+    x = new TIntArrayList(poly.xpoints);
+    y = new TIntArrayList(poly.ypoints);
+    if (i == points.size() - 1) {
+      x.add(vertex.x);
+      y.add(vertex.y);
+    }
+    else {
+      x.insert(i + 1, vertex.x);
+      y.insert(i + 1, vertex.y);
+    }
+
+    // create new polygon
+    result = new Polygon(x.toArray(), y.toArray(), x.size());
+    return result;
   }
 }
