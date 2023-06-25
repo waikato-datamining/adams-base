5d4b20bbc37f8f429bc9bfe2005a88fc498d5ff3
Peter Reutemann <fracpete@gmail.com>
2023-06-26 11:56:49 +1200

    dummy panels now show the headers correctly; added border of 5 pixels around grid panel

--- adams-core/src/main/java/adams/flow/standalone/GridView.java
+++ adams-core/src/main/java/adams/flow/standalone/GridView.java
@@ -13,9 +13,9 @@
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/**
+/*
  * GridView.java
- * Copyright (C) 2012-2017 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2012-2023 University of Waikato, Hamilton, New Zealand
  */
 package adams.flow.standalone;
 
@@ -29,6 +29,7 @@ import adams.gui.core.BasePanel;
 import adams.gui.print.JComponentWriter;
 import adams.gui.print.NullWriter;
 
+import javax.swing.BorderFactory;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
@@ -50,119 +51,118 @@ import java.util.List;
  * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
  * &nbsp;&nbsp;&nbsp;default: WARNING
  * </pre>
- * 
+ *
  * <pre>-name &lt;java.lang.String&gt; (property: name)
  * &nbsp;&nbsp;&nbsp;The name of the actor.
  * &nbsp;&nbsp;&nbsp;default: GridView
  * </pre>
- * 
+ *
  * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
  * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
  * &nbsp;&nbsp;&nbsp;default: 
  * </pre>
- * 
+ *
  * <pre>-skip &lt;boolean&gt; (property: skip)
  * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded 
  * &nbsp;&nbsp;&nbsp;as it is.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- * 
+ *
  * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
  * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this 
  * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical 
  * &nbsp;&nbsp;&nbsp;actors.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- * 
+ *
  * <pre>-silent &lt;boolean&gt; (property: silent)
  * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing 
  * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- * 
+ *
  * <pre>-short-title &lt;boolean&gt; (property: shortTitle)
  * &nbsp;&nbsp;&nbsp;If enabled uses just the name for the title instead of the actor's full 
  * &nbsp;&nbsp;&nbsp;name.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- * 
+ *
  * <pre>-display-in-editor &lt;boolean&gt; (property: displayInEditor)
  * &nbsp;&nbsp;&nbsp;If enabled displays the panel in a tab in the flow editor rather than in 
  * &nbsp;&nbsp;&nbsp;a separate frame.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- * 
+ *
  * <pre>-width &lt;int&gt; (property: width)
  * &nbsp;&nbsp;&nbsp;The width of the dialog.
  * &nbsp;&nbsp;&nbsp;default: 800
  * &nbsp;&nbsp;&nbsp;minimum: -1
  * </pre>
- * 
+ *
  * <pre>-height &lt;int&gt; (property: height)
  * &nbsp;&nbsp;&nbsp;The height of the dialog.
  * &nbsp;&nbsp;&nbsp;default: 600
  * &nbsp;&nbsp;&nbsp;minimum: -1
  * </pre>
- * 
+ *
  * <pre>-x &lt;int&gt; (property: x)
  * &nbsp;&nbsp;&nbsp;The X position of the dialog (&gt;=0: absolute, -1: left, -2: center, -3: right
  * &nbsp;&nbsp;&nbsp;).
  * &nbsp;&nbsp;&nbsp;default: -1
  * &nbsp;&nbsp;&nbsp;minimum: -3
  * </pre>
- * 
+ *
  * <pre>-y &lt;int&gt; (property: y)
  * &nbsp;&nbsp;&nbsp;The Y position of the dialog (&gt;=0: absolute, -1: top, -2: center, -3: bottom
  * &nbsp;&nbsp;&nbsp;).
  * &nbsp;&nbsp;&nbsp;default: -1
  * &nbsp;&nbsp;&nbsp;minimum: -3
  * </pre>
- * 
+ *
  * <pre>-actor &lt;adams.flow.core.Actor&gt; [-actor ...] (property: actors)
  * &nbsp;&nbsp;&nbsp;The panel-generating actors to display in the grid.
  * &nbsp;&nbsp;&nbsp;default: 
  * </pre>
- * 
+ *
  * <pre>-num-rows &lt;int&gt; (property: numRows)
  * &nbsp;&nbsp;&nbsp;The number of rows in the grid.
  * &nbsp;&nbsp;&nbsp;default: 1
  * &nbsp;&nbsp;&nbsp;minimum: 1
  * </pre>
- * 
+ *
  * <pre>-num-cols &lt;int&gt; (property: numCols)
  * &nbsp;&nbsp;&nbsp;The number of columns in the grid.
  * &nbsp;&nbsp;&nbsp;default: 1
  * &nbsp;&nbsp;&nbsp;minimum: 1
  * </pre>
- * 
+ *
  * <pre>-add-headers &lt;boolean&gt; (property: addHeaders)
  * &nbsp;&nbsp;&nbsp;If enabled, headers with the names of the actors as labels get added as 
  * &nbsp;&nbsp;&nbsp;well.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- * 
+ *
  * <pre>-show-controls &lt;boolean&gt; (property: showControls)
  * &nbsp;&nbsp;&nbsp;If enabled, the controls for adjusting rows&#47;columns are visible.
  * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
- * 
+ *
  * <pre>-writer &lt;adams.gui.print.JComponentWriter&gt; (property: writer)
  * &nbsp;&nbsp;&nbsp;The writer to use for generating the graphics output.
  * &nbsp;&nbsp;&nbsp;default: adams.gui.print.NullWriter
  * </pre>
- * 
+ *
  <!-- options-end -->
  *
  * @author  fracpete (fracpete at waikato dot ac dot nz)
- * @version $Revision$
  */
 public class GridView
-  extends AbstractMultiView 
+  extends AbstractMultiView
   implements ComponentSupplier, DataPlotUpdaterSupporter {
 
   /** for serialization. */
   private static final long serialVersionUID = -4454052058077687116L;
-  
+
   /** the number of rows to display. */
   protected int m_NumRows;
 
@@ -188,10 +188,10 @@ public class GridView
    */
   @Override
   public String globalInfo() {
-    return 
-	"Displays multiple graphical actors in a grid. The actors get added "
+    return
+      "Displays multiple graphical actors in a grid. The actors get added "
 	+ "row-wise to the grid from top-left to bottom-right. The actors "
-	+ "can be referenced in the flow using " 
+	+ "can be referenced in the flow using "
 	+ CallableSink.class.getName() + " actors.";
   }
 
@@ -203,24 +203,24 @@ public class GridView
     super.defineOptions();
 
     m_OptionManager.add(
-	    "num-rows", "numRows",
-	    1, 1, null);
+      "num-rows", "numRows",
+      1, 1, null);
 
     m_OptionManager.add(
-	    "num-cols", "numCols",
-	    1, 1, null);
+      "num-cols", "numCols",
+      1, 1, null);
 
     m_OptionManager.add(
-	    "add-headers", "addHeaders",
-	    false);
+      "add-headers", "addHeaders",
+      false);
 
     m_OptionManager.add(
-	    "show-controls", "showControls",
-	    false);
+      "show-controls", "showControls",
+      false);
 
     m_OptionManager.add(
-	    "writer", "writer",
-	    new NullWriter());
+      "writer", "writer",
+      new NullWriter());
   }
 
   /**
@@ -229,7 +229,7 @@ public class GridView
   @Override
   protected void initialize() {
     super.initialize();
-    
+
     m_Panels = null;
   }
 
@@ -245,7 +245,7 @@ public class GridView
     result  = super.getQuickInfo();
     result += QuickInfoHelper.toString(this, "numRows", m_NumRows, ", Rows: ");
     result += QuickInfoHelper.toString(this, "numCols", m_NumCols, ", Cols: ");
-    
+
     return result;
   }
 
@@ -421,7 +421,7 @@ public class GridView
 
   /**
    * Ensures that the wrapper is visible.
-   * 
+   *
    * @param wrapper	the wrapper to make visible
    * @return		true if successful
    */
@@ -441,27 +441,39 @@ public class GridView
     AdjustableGridPanel	result;
     JLabel		label;
     BasePanel		panel;
+    BasePanel		outer;
     int			i;
 
     result = new AdjustableGridPanel(m_NumRows, m_NumCols);
+    result.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
     result.setControlsVisible(m_ShowControls);
 
     // add dummy panels
     m_Panels = new ArrayList<>();
     for (i = 0; i < m_Actors.size(); i++) {
-      panel = new BasePanel(new BorderLayout());
-      label = new JLabel(m_Actors.get(i).getName(), JLabel.CENTER);
-      panel.add(label, BorderLayout.CENTER);
-      result.addItem(panel);
+      if (m_AddHeaders) {
+	panel = new BasePanel(new BorderLayout());
+	outer = new BasePanel(new BorderLayout());
+        label = new JLabel(m_Actors.get(i).getName(), JLabel.LEFT);
+        outer.add(label, BorderLayout.NORTH);
+	outer.add(panel, BorderLayout.CENTER);
+	result.addItem(outer);
+      }
+      else {
+	panel = new BasePanel(new BorderLayout());
+	label = new JLabel(m_Actors.get(i).getName(), JLabel.CENTER);
+	panel.add(label, BorderLayout.CENTER);
+	result.addItem(panel);
+      }
       m_Panels.add(panel);
     }
-    
+
     return result;
   }
-  
+
   /**
    * Replaces the current dummy panel with the actual panel.
-   * 
+   *
    * @param actor	the actor this panel is for
    * @param panel	the panel to replace the dummy one
    */
@@ -471,7 +483,7 @@ public class GridView
 
     index = indexOf(actor.getName());
     m_Panels.set(index, panel);
-    
+
     SwingUtilities.invokeLater(() -> {
       AdjustableGridPanel grid = (AdjustableGridPanel) m_Panel;
       grid.clear();
@@ -517,10 +529,10 @@ public class GridView
 	}
       });
     }
-    
+
     super.wrapUp();
   }
-  
+
   /**
    * Cleans up after the execution has finished. Also removes graphical
    * components.
@@ -531,7 +543,7 @@ public class GridView
       m_Panels.clear();
       m_Panels = null;
     }
-    
+
     super.cleanUp();
   }
 }
