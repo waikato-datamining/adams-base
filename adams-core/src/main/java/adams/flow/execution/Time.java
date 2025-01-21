954a20263646cfd07c5c4ae33682b6aa5ea27efb
Peter Reutemann <fracpete@gmail.com>
2025-01-21 14:09:20 +1300

    added support for storing the table as CSV; value column is now typed as Long.class

--- adams-core/src/main/java/adams/flow/execution/Time.java
+++ adams-core/src/main/java/adams/flow/execution/Time.java
@@ -15,10 +15,13 @@
 
 /*
  * Time.java
- * Copyright (C) 2013-2018 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2013-2025 University of Waikato, Hamilton, New Zealand
  */
 package adams.flow.execution;
 
+import adams.core.io.PlaceholderFile;
+import adams.data.io.output.CsvSpreadSheetWriter;
+import adams.data.spreadsheet.SpreadSheet;
 import adams.flow.core.Actor;
 import adams.flow.core.Token;
 import adams.gui.core.MapTableModel;
@@ -34,22 +37,24 @@ import java.util.Map;
  <!-- globalinfo-end -->
  *
  <!-- options-start -->
- * Valid options are: <br><br>
- * 
- * <pre>-D &lt;int&gt; (property: debugLevel)
- * &nbsp;&nbsp;&nbsp;The greater the number the more additional info the scheme may output to 
- * &nbsp;&nbsp;&nbsp;the console (0 = off).
- * &nbsp;&nbsp;&nbsp;default: 0
- * &nbsp;&nbsp;&nbsp;minimum: 0
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * &nbsp;&nbsp;&nbsp;min-user-mode: Expert
  * </pre>
- * 
+ *
  * <pre>-update-interval &lt;int&gt; (property: updateInterval)
- * &nbsp;&nbsp;&nbsp;The update interval (= number of method executions) after which the GUI 
+ * &nbsp;&nbsp;&nbsp;The update interval (= number of method executions) after which the GUI
  * &nbsp;&nbsp;&nbsp;gets refreshed.
  * &nbsp;&nbsp;&nbsp;default: 100
  * &nbsp;&nbsp;&nbsp;minimum: 1
  * </pre>
- * 
+ *
+ * <pre>-log-file &lt;adams.core.io.PlaceholderFile&gt; (property: logFile)
+ * &nbsp;&nbsp;&nbsp;The CSV log file to write to; writing is disabled if pointing to a directory.
+ * &nbsp;&nbsp;&nbsp;default: ${CWD}
+ * </pre>
+ *
  <!-- options-end -->
  *
  * @author  fracpete (fracpete at waikato dot ac dot nz)
@@ -62,20 +67,23 @@ public class Time
   private static final long serialVersionUID = -6155792276833652477L;
 
   /** the suffix for "input". */
-  public final static String SUFFIX_INPUT = ".input";
+  public final static String SUFFIX_INPUT = " [input]";
 
   /** the suffix for "execute". */
-  public final static String SUFFIX_EXECUTE = ".execute";
+  public final static String SUFFIX_EXECUTE = " [execute]";
 
   /** the suffix for "output". */
-  public final static String SUFFIX_OUTPUT = ".output";
-  
+  public final static String SUFFIX_OUTPUT = " [output]";
+
   /** keeps track of the start time of an actor. */
   protected Map<String,Long> m_Start;
-  
+
   /** keeps track of the time that actors used up. */
   protected Map<String,Long> m_Overall;
-  
+
+  /** the file to write to. */
+  protected PlaceholderFile m_LogFile;
+
   /**
    * Returns a string describing the object.
    *
@@ -85,14 +93,26 @@ public class Time
   public String globalInfo() {
     return "Shows how much time actors are taking in their method calls (gets accumulated).";
   }
-  
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+      "log-file", "logFile",
+      new PlaceholderFile("."));
+  }
+
   /**
    * Initializes the members.
    */
   @Override
   protected void initialize() {
     super.initialize();
-    
+
     m_Start   = new HashMap<>();
     m_Overall = new HashMap<>();
   }
@@ -109,44 +129,73 @@ public class Time
   }
 
   /**
+   * Sets the log file.
+   *
+   * @param value	the file
+   */
+  public void setLogFile(PlaceholderFile value) {
+    m_LogFile = value;
+    reset();
+  }
+
+  /**
+   * Returns the log file.
+   *
+   * @return		the condition
+   */
+  public PlaceholderFile getLogFile() {
+    return m_LogFile;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String logFileTipText() {
+    return "The CSV log file to write to; writing is disabled if pointing to a directory.";
+  }
+
+  /**
    * The title of this listener.
-   * 
+   *
    * @return		the title
    */
   public String getListenerTitle() {
     return "Time";
   }
-  
+
   /**
    * Gets called when the flow execution starts.
    */
   @Override
   public void startListening() {
     super.startListening();
-    
+
     m_Overall.clear();
     m_Start.clear();
   }
-  
+
   /**
    * Adds the start time.
-   * 
+   *
    * @param key		the key in the hashtable
    */
   protected void addStart(String key) {
     m_Start.put(key, System.currentTimeMillis());
   }
-  
+
   /**
    * Adds the duration to the overall count.
-   * 
+   *
    * @param key		the key in the hashtable
    */
   protected void addDuration(String key) {
     long	current;
     long	overall;
     long	start;
-    
+
     if (m_Start.containsKey(key)) {
       current = System.currentTimeMillis();
       overall = 0;
@@ -171,17 +220,17 @@ public class Time
 
   /**
    * Creates a new table model with the current data.
-   * 
+   *
    * @return		the model with the current data
    */
   @Override
   protected TableModel createTableModel() {
-    return new MapTableModel(new HashMap<>(m_Overall), new String[]{"Actor", "Time in msec"});
+    return new MapTableModel(new HashMap<>(m_Overall), new String[]{"Actor", "Time in msec"}, Long.class);
   }
 
   /**
    * Gets called before the actor receives the token.
-   * 
+   *
    * @param actor	the actor that will receive the token
    * @param token	the token that the actor will receive
    */
@@ -189,30 +238,30 @@ public class Time
   public void preInput(Actor actor, Token token) {
     addStart(actor.getFullName() + SUFFIX_INPUT);
   }
-  
+
   /**
    * Gets called after the actor received the token.
-   * 
+   *
    * @param actor	the actor that received the token
    */
   @Override
   public void postInput(Actor actor) {
     addDuration(actor.getFullName() + SUFFIX_INPUT);
   }
-  
+
   /**
    * Gets called before the actor gets executed.
-   * 
+   *
    * @param actor	the actor that gets executed
    */
   @Override
   public void preExecute(Actor actor) {
     addStart(actor.getFullName() + SUFFIX_EXECUTE);
   }
-  
+
   /**
    * Gets called after the actor was executed.
-   * 
+   *
    * @param actor	the actor that was executed
    */
   @Override
@@ -222,17 +271,17 @@ public class Time
 
   /**
    * Gets called before a token gets obtained from the actor.
-   * 
+   *
    * @param actor	the actor the token gets obtained from
    */
   @Override
   public void preOutput(Actor actor) {
     addStart(actor.getFullName() + SUFFIX_OUTPUT);
   }
-  
+
   /**
    * Gets called after a token was acquired from the actor.
-   * 
+   *
    * @param actor	the actor that the token was acquired from
    * @param token	the token that was acquired from the actor
    */
@@ -243,17 +292,25 @@ public class Time
 
   /**
    * Gets called when the flow execution ends.
-   * <br><br>
-   * Outputs the counts in debug mode.
    */
   @Override
   public void finishListening() {
+    SpreadSheet sheet;
+    CsvSpreadSheetWriter writer;
+
+    if (!m_LogFile.isDirectory()) {
+      sheet  = m_Table.toSpreadSheet();
+      writer = new CsvSpreadSheetWriter();
+      if (!writer.write(sheet, m_LogFile))
+	getLogger().severe("Failed to write times to: " + m_LogFile);
+    }
+
     super.finishListening();
-    
+
     if (isLoggingEnabled())
-      getLogger().info(m_Overall.toString());
+      getLogger().info(m_Table.toSpreadSheet().toString());
   }
-  
+
   /**
    * Cleans up data structures, frees up memory.
    */
