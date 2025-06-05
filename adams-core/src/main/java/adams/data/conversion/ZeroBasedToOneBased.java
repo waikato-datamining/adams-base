0eb056c048ddab19fbab2468c21dba976d470682
Peter Reutemann <fracpete@gmail.com>
2025-06-06 09:33:43 +1200

    convenience conversions for converting integer indices 0->1 or 1->0

---
+++ adams-core/src/main/java/adams/data/conversion/ZeroBasedToOneBased.java
@@ -0,0 +1,110 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * ZeroBasedToOneBased.java
+ * Copyright (C) 2025 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.data.conversion;
+
+import adams.core.ClassCrossReference;
+
+/**
+ <!-- globalinfo-start -->
+ * Converts 1-based indices into 0-based ones.<br>
+ * <br>
+ * See also:<br>
+ * adams.data.conversion.OneBasedToZeroBased
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * &nbsp;&nbsp;&nbsp;min-user-mode: Expert
+ * </pre>
+ *
+ <!-- options-end -->
+ *
+ * @author fracpete (fracpete at waikato dot ac dot nz)
+ */
+public class ZeroBasedToOneBased
+  extends AbstractConversion
+  implements ClassCrossReference {
+
+  private static final long serialVersionUID = -6331144202417531126L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Converts 1-based indices into 0-based ones.";
+  }
+
+  /**
+   * Returns the cross-referenced classes.
+   *
+   * @return		the classes
+   */
+  @Override
+  public Class[] getClassCrossReferences() {
+    return new Class[]{OneBasedToZeroBased.class};
+  }
+
+  /**
+   * Returns the class that is accepted as input.
+   *
+   * @return the class
+   */
+  @Override
+  public Class accepts() {
+    return Integer[].class;
+  }
+
+  /**
+   * Returns the class that is generated as output.
+   *
+   * @return the class
+   */
+  @Override
+  public Class generates() {
+    return Integer[].class;
+  }
+
+  /**
+   * Performs the actual conversion.
+   *
+   * @throws Exception if something goes wrong with the conversion
+   * @return the converted data
+   */
+  @Override
+  protected Object doConvert() throws Exception {
+    Integer[]	result;
+    Integer[]	input;
+    int		i;
+
+    input = (Integer[]) m_Input;
+    result = new Integer[input.length];
+    for (i = 0; i < input.length; i++)
+      result[i] = input[i] + 1;
+
+    return result;
+  }
+}
